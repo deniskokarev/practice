de66da6b160264f9b2a967c6a1e20cdd448d0cd0
Denis Kokarev <d_kokarev@mail.ru>
2016-12-18 17:39:21 -0500

    adding symmetric tests start using our own boxed int for performance

--- java/dk/struct/test/AttrTreapTest.java
+++ java/dk/struct/test/AttrTreapTest.java
@@ -7,38 +7,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-
 import org.junit.Test;
 import dk.struct.*;
+
 public class AttrTreapTest {
 
 	/**
-	 * our attribute is simply a number of nodes
+	 * mutable integer attribute where we will keep total children count
+	 * @author kokarev
+	 *
+	 */
+	static class IntBox {
+		public int intValue;
+		public IntBox(int n) {
+			intValue = n;
+		}
+		public String toString() {
+			return Integer.toString(intValue);
+		}
+	}
+	
+	/**
+	 * our value is simply a string with attribute containing number of chld nodes
 	 * @author kokarev
 	 */
-	static class Val implements AttrValue<Integer> {
-		static final Integer one = Integer.valueOf(1);
+	static class Val implements AttrValue<IntBox> {
 		String val;
-		Integer num;
+		IntBox num;
 		
 		@Override
-		public Integer getAttr() {
+		public IntBox getAttr() {
 			return num;
 		}
 
+		/**
+		 * this hook will be invoked when tree needs to update chld nodes
+		 * we simply update number of children
+		 */
 		@Override
-		public void updateAttr(AttrValue<Integer> l, AttrValue<Integer> r) {
+		public void updateAttr(AttrValue<IntBox> l, AttrValue<IntBox> r) {
 			int n = 1;
 			if (l != null)
-				n += l.getAttr();
+				n += l.getAttr().intValue;
 			if (r != null)
-				n += r.getAttr();
-			num = new Integer(n);
+				n += r.getAttr().intValue;
+			num.intValue = n;
 		}
 		
 		public Val(String v) {
 			val = v;
-			num = one;
+			num = new IntBox(1);
 		}
 		
 		public String toString() {
@@ -70,7 +88,7 @@ public class AttrTreapTest {
 				root = AttrTreap.insertNode(root, node);
 			}
 			//System.out.println(root.treeToString());
-			assertEquals("root node must have correct count of members", root.value.getAttr(), Integer.valueOf(i));
+			assertEquals("root node must have correct count of members", i, root.value.getAttr().intValue);
 		} catch(IOException ex) {
 			fail(ex.toString());
 		}
@@ -112,7 +130,7 @@ public class AttrTreapTest {
 				assertNotNull("must find before deletion", node);
 				root = AttrTreap.deleteKey(root, Integer.valueOf(i));
 				assertNotNull("tree must stay populated until last node gets deleted", root);
-				//assertEquals("total count must reduce", root.value.getAttr(), Integer.valueOf(i));
+				assertEquals("total count must reduce", i, root.value.getAttr().intValue);
 				node = AttrTreap.findNode(root, Integer.valueOf(i));
 				assertNull("must not find after deletion", node);
 			}
@@ -137,10 +155,47 @@ public class AttrTreapTest {
 		Collections.shuffle(keysToDel);
 		for (Integer k:keysToDel) {
 			assertNotNull("tree must stay populated until last node gets deleted", root);
-			assertEquals("root node must have correct count of members", root.value.getAttr().intValue(), i);
+			assertEquals("root node must have correct count of members", i, root.value.getAttr().intValue);
 			root = AttrTreap.deleteKey(root, k);
+			AttrTreap<Integer, Val> node = AttrTreap.findNode(root, k);
+			assertNull("must not find after deletion", node);
 			i--;
 		}
+		root = AttrTreap.deleteKey(root, Integer.valueOf(i));
+		assertNull("root must be null after we delete everything", root);
+	}
+
+	// building a perfectly symmetrical tree with [from, to-1] values using nodes level for balancing
+	AttrTreap<Integer, Val> addSymmetric(AttrTreap<Integer, Val> root, int from, int to, int level) {
+		if (to>from) {
+			int mid = (from+to)/2;
+			Integer key = Integer.valueOf(mid);
+			AttrTreap<Integer, Val> node = new AttrTreap<>(level, key, new Val(Integer.toString(mid)));
+			root = addSymmetric(root, from, mid, level-1);
+			root = AttrTreap.insertNode(root, node);
+			root = addSymmetric(root, mid+1, to, level-1);
+		}
+		return root;
+	}
+	@Test
+	public void addManySymmetric() {
+		AttrTreap<Integer, Val> root = null;
+		final int size = (1<<20)-1;
+		root = addSymmetric(root, 0, size, 1000);
+		//System.out.println("Original Tree:");
+		//System.out.println(root.treeToString());
+		assertEquals("root node must have correct count of members", root.value.getAttr().intValue, size);
+	}
+	@Test
+	public void addAndDeleteSymmetric() {
+		AttrTreap<Integer, Val> root = null;
+		final int size = (1<<20)-1;
+		root = addSymmetric(root, 0, size, 1000);
+		assertEquals("root node must have correct count of members", root.value.getAttr().intValue, size);
+		int i;
+		for (i=0; i<size-1; i++)
+			root = AttrTreap.deleteKey(root, Integer.valueOf(i));
+		assertEquals("only one node must remain", 1, root.value.getAttr().intValue);
 		root = AttrTreap.deleteKey(root, Integer.valueOf(i));
 		assertNull("root must be null after we delete everything", root);
 	}
