a48f1711a30d5e416af65769297e97883082be97
Denis Kokarev <dk1942@att.com>
2017-01-09 16:39:48 -0500

    CF731C - timeout on test 31

--- java/CF731C.java
+++ java/CF731C.java
@@ -1,7 +1,7 @@
 import java.io.PrintStream;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class CF731C {
@@ -9,112 +9,127 @@ public class CF731C {
 	 * solution result
 	 */
 	public static class Answer {
-		public int colors[];
-		public int cnt;
-		public int ans;
-		public Answer(Input in) {
-			colors = new int[in.ncolors];
-			cnt = 0;
-			ans = 0;
+		int n;
+
+		public Answer(int n) {
+			this.n = n;
 		}
-		void clearColors() {
-			colors = new int[colors.length];
-			cnt = 0;
-		}
+
 		@Override
 		public String toString() {
-			return Integer.toString(ans);
+			return Integer.toString(n);
 		}
 	};
 
 	static class Pair {
-		public int left, right;
-		public Pair(int l, int r) {
-			// order by sock #
-			left = Math.min(l, r);
-			right = Math.max(l, r);
+		int l, r;
+		int cc[];
+		Pair(int l, int r, int cc[]) {
+			this.l = Math.min(l, r);
+			this.r = Math.max(l, r);
+			this.cc = cc;
 		}
 	}
+
 	/**
 	 * input parser
 	 */
 	public static class Input {
-		public int nsocks;	// nsocks
-		public int ndays;
-		public int ncolors;
-		public int color[];
-		public Pair pair[];
-		public Input(Scanner in) {
-			nsocks = in.nextInt();
-			ndays = in.nextInt();
-			ncolors = in.nextInt();
-			color = new int[nsocks];
-			for (int i=0; i<nsocks; i++)
-				color[i] = in.nextInt()-1;
-			pair = new Pair[ndays];
-			for (int i=0; i<ndays; i++)
-				pair[i] = new Pair(in.nextInt()-1, in.nextInt()-1);
+		int ns;
+		int nc;
+		int cc[];
+		Pair ss[];
+
+		public Input(Scanner sc) {
+			ns = sc.nextInt();
+			int np = sc.nextInt();
+			nc = sc.nextInt();
+			cc = new int[ns];
+			for (int i = 0; i < ns; i++)
+				cc[i] = sc.nextInt()-1;
+			ss = new Pair[np];
+			for (int i = 0; i < np; i++)
+				ss[i] = new Pair(sc.nextInt() - 1, sc.nextInt() - 1, cc); // assert<s
 		}
 	};
 
-	static class DAGNode {
-		public int sock;
-		public int color;
-		public Map<Integer,DAGNode> chld;
-		public boolean marked;
-		public void add(DAGNode c) {
-			if (chld.get(c.sock) == null)
-				chld.put(c.sock, c);
+	static void mkg(int nnv, Pair ss[], int g[][]) {
+		int ll[] = new int[nnv];
+		Arrays.sort(ss, (Pair a, Pair b) -> (a.l - b.l));
+		int s;
+		int cnt;
+		s = ss[0].l;
+		cnt = 1;
+		for (int i=1; i < ss.length; i++) {
+			if (s != ss[i].l) {
+				ll[s] += cnt;
+				s = ss[i].l;
+				cnt = 0;
+			}
+			cnt++;
 		}
-		public DAGNode(int sock, int color) {
-			this.sock = sock;
-			this.color = color;
-			chld = new HashMap<>();
+		ll[s] += cnt;
+		Arrays.sort(ss, (Pair a, Pair b) -> (a.r - b.r));
+		s = ss[0].r;
+		cnt = 1;
+		for (int i=1; i < ss.length; i++) {
+			if (s != ss[i].r) {
+				ll[s] += cnt;
+				s = ss[i].r;
+				cnt = 0;
+			}
+			cnt++;
+		}
+		ll[s] += cnt;
+		for (int i=0; i<nnv; i++) 
+			g[i] = new int[ll[i]];
+		for (int i=0; i<ss.length; i++) {
+			int l = ss[i].l;
+			int r = ss[i].r;
+			ll[l]--;
+			g[l][ll[l]] = r;
+			ll[r]--;
+			g[r][ll[r]] = l;
 		}
 	}
-	/**
-	 * walk all nodes from the root (if we haven't visited root yet) and
-	 * count colors 
-	 * @param root
-	 * @param colors
-	 */
-	static void walk(DAGNode root, Answer ans) {
-		if (!root.marked) {
-			root.marked = true;
-			for (DAGNode c:root.chld.values())
-				walk(c, ans);
-			ans.colors[root.color]++;
-			ans.cnt++;
+
+	static int walkPart(int v, int g[][], int cc[], boolean vis[], int cht[]) {
+		Queue<Integer> nodes = new LinkedList<>();
+		nodes.add(v);
+		int rc = 0;
+		while (nodes.size() > 0) {
+			v = nodes.remove();
+			if (!vis[v]) {
+				vis[v] = true;
+				cht[cc[v]]++;
+				rc++;
+				for (int s:g[v])
+					nodes.add(s);
+			}
 		}
+		return rc;
 	}
+	
 	/**
 	 * solver function
 	 */
 	public static Answer solve(Input in) {
-		DAGNode socks[] = new DAGNode[in.nsocks];
-		// build DAG of all socks
-		Arrays.sort(in.pair,(Pair a, Pair b)->Integer.compare(a.left, b.left)*10+Integer.compare(a.right, b.right));
-		for (Pair p:in.pair) {
-			if (socks[p.left] == null)
-				socks[p.left] = new DAGNode(p.left, in.color[p.left]);
-			if (socks[p.right] == null)
-				socks[p.right] = new DAGNode(p.right, in.color[p.right]);
-			socks[p.left].add(socks[p.right]);
+		int g[][] = new int[in.ns][];
+		mkg(in.ns, in.ss, g);
+		boolean vis[] = new boolean[g.length];
+		int rc = 0;
+		for (int i=0; i<in.ns; i++) {
+			int cht[] = new int[in.nc];
+			int cnt = walkPart(i, g, in.cc, vis, cht);
+			if (cnt > 0) {
+				int mxj = 0;
+				for (int j=1; j<cht.length; j++)
+					if (cht[mxj] < cht[j])
+						mxj = j;
+				rc += cnt-cht[mxj];
+			}
 		}
-		// walk the DAG and count colors
-		Answer ans = new Answer(in);
-		for (DAGNode root:socks) {
-			ans.clearColors();
-			walk(root, ans);
-			// find dominant color in the unwalked subtree
-			int maxC = ans.colors[0];
-			for (int i=1; i<ans.colors.length; i++)
-				if (ans.colors[i] > maxC)
-					maxC = ans.colors[i];
-			// each unwalked socks chain has to be painted to the dominant
-			// color
-			ans.ans += ans.cnt - maxC;
-		}
+		Answer ans = new Answer(rc);
 		return ans;
 	}
 
