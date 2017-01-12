3ad4a640241d8368dc521e60a59956d57e0c05bf
Denis Kokarev <dk1942@att.com>
2017-01-12 14:29:45 -0500

    CF757B full solution

--- java/CF757B.java
+++ java/CF757B.java
@@ -1,8 +1,6 @@
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
