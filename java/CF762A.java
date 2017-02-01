961355cd6226a631677b8242a873fb411851eac9
Denis Kokarev <dk1942@att.com>
2017-02-01 10:17:54 -0500

    CF762A - full solution

--- java/CF762A.java
+++ java/CF762A.java
@@ -1,4 +1,7 @@
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class CF762A {
@@ -29,51 +32,76 @@ public class CF762A {
 		}
 	};
 
+	static int uPrimeFactors(long n, long p[], int pc[]) {
+		assert(n > 1);
+		int np = 0;
+		for (long i = 2; i <= n / i; i++) {
+			if (n % i == 0) {
+				pc[np] = 1;
+				p[np] = i;
+				n /= i;
+				while (n % i == 0) {
+					n /= i;
+					pc[np]++;
+				}
+				np++;
+			}
+		}
+		if (n > 1) {
+			pc[np] = 1;
+			p[np++] = n;
+		}
+		return np;
+	}
+
+	static class Val implements Comparable<Val> {
+		long v;
+		int fc[];
+		
+		Val(int fc[], int fcSz, long v) {
+			this.fc = new int[fcSz];
+			System.arraycopy(fc, 0, this.fc, 0, fcSz);
+			this.v = v;
+		}
+		@Override
+		public int compareTo(Val o) {
+			return Long.compare(v,  o.v);
+		}
+		
+	}
 	/**
 	 * solver function
 	 */
 	public static Answer solve(Input in) {
 		int k = in.k;
-		long n = in.n;
-		long i;
-		long sr = (long)Math.sqrt(n);
-		int nd = 1;
-		for (i=2; i<=sr; i++)
-			if (n%i == 0)
-				nd++;
-		int all;
-		if ((i-1)*(i-1)==n)
-			all = 2*nd-1;
-		else
-			all = 2*nd;
-		if (all < k) {
-			return new Answer(-1);
-		} else {
-			long res;
-			if (k>=nd) {
-				res = 1;
-				k = all+1-k;
-				for (i=1; i<=sr; i++) {
-					if (n%i == 0) {
-						res = i;
-						k--;
-						if (k == 0)
-							return new Answer(n/res); 
-					}
-				}
-			} else {
-				res = 1;
-				for (i=1; i<=sr; i++) {
-					if (n%i == 0) {
-						res = i;
-						k--;
-						if (k == 0)
-							return new Answer(res); 
-					}
+		long f[] = new long[64];
+		int fc[] = new int[64];
+		int nf = uPrimeFactors(in.n, f, fc);
+		PriorityQueue<Val> q = new PriorityQueue<>();
+		Map<Long, Boolean> m = new HashMap<>();
+		q.add(new Val(fc, nf, 1L));
+		m.put(1L, true);
+		long prev = 0;
+		Val v = null;
+		while (k>0) {
+			if (q.size() < 1)
+				return new Answer(-1);
+			v = q.remove();
+			m.remove(v.v);
+			for (int i=0; i<nf; i++) {
+				long lv = v.v*f[i];
+				if (v.fc[i] > 0 && m.get(lv) == null) {
+					Val nv = new Val(v.fc, v.fc.length, lv);
+					nv.fc[i]--;
+					q.add(nv);
+					m.put(lv, true);
 				}
 			}
+			if (v.v != prev)
+				k--;
+			prev = v.v;
 		}
-		return new Answer(-1);
+		return new Answer(v.v);
 	}
 
 	/**
