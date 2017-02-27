380eb9afd25726069a259a3d6ff99c073f55cd18
Denis Kokarev <d_kokarev@mail.ru>
2017-02-26 19:33:22 -0500

    adding a simplistic version of lcs() without actual correspondence - it needs less space

--- c/draft/lcs.hh
+++ c/draft/lcs.hh
@@ -1,18 +1,21 @@
 /*
  * LCS - longest common subsequence
+ * Using dynamic programming with O(n*m) complexity and O(n*m) storage size
+ * The simplistic flavor of lcs() without actual correspondence uses O(n) storage
  */
 #include <vector>
 
 namespace lcs {
 	
 	namespace details {
+		/* implementation details */
 		
 		struct entry {
-			int sz;
-			int a, b;
-			int na, nb;
-			entry():sz(0),a(-1),b(-1),na(-1),nb(-1){}
-			entry(int _a, int _b, const entry &e):sz(e.sz+1),a(_a),b(_b),na(e.a),nb(e.b){}
+			size_t sz;
+			size_t a, b;
+			size_t na, nb;
+			entry():sz(0),a(0),b(0),na(0),nb(0){}
+			entry(size_t _b, size_t _a, const entry &e):sz(e.sz+1),a(_a),b(_b),na(e.a),nb(e.b){}
 		};
 
 		template<typename IT> void lcs_r(const IT &ahead, const IT &aend,
@@ -20,18 +23,17 @@ namespace lcs {
 										 std::vector<std::vector<entry> > &cache) {
 			IT aa(ahead);
 			IT bb(bhead);
-			int bi=1;
-			std::vector<std::pair<int,int> > res;
-			while (bb!=bend) {
-				int ai=1;
-				for (IT a(aa); a!=aend; ++a) {
+			size_t bi = 1;
+			while (bb != bend) {
+				size_t ai = 1;
+				for (IT a(aa); a != aend; ++a) {
 					if (*a == *bb) {
-						cache[ai][bi] = entry(ai, bi, cache[ai-1][bi-1]);
+						cache[bi][ai] = entry(bi, ai, cache[bi-1][ai-1]);
 					} else {
-						if (cache[ai-1][bi].sz > cache[ai][bi-1].sz)
-							cache[ai][bi] = cache[ai-1][bi];
+						if (cache[bi][ai-1].sz > cache[bi-1][ai].sz)
+							cache[bi][ai] = cache[bi][ai-1];
 						else
-							cache[ai][bi] = cache[ai][bi-1];
+							cache[bi][ai] = cache[bi-1][ai];
 					}
 					++ai;
 				}
@@ -40,17 +42,17 @@ namespace lcs {
 			}
 		}
 
-		std::vector<std::pair<int,int> > reconstruct(const std::vector<std::vector<entry> > &cache) {
-			std::vector<std::pair<int,int> > res;
-			int na = cache.size()-1;
-			if (na > 0) {
-				int nb = cache[0].size()-1;
-				if (cache[na][nb].a >= 0) {
-					int di = cache[na][nb].sz-1;
+		std::vector<std::pair<size_t,size_t> > reconstruct(const std::vector<std::vector<entry> > &cache) {
+			std::vector<std::pair<size_t,size_t> > res;
+			size_t nb = cache.size()-1;
+			if (nb > 0) {
+				size_t na = cache[0].size()-1;
+				if (cache[nb][na].sz > 0) {
+					size_t di = cache[nb][na].sz-1;
 					res.resize(di+1);
-					while (na >= 0 && nb >= 0) {
-						const entry &e = cache[na][nb];
-						res[di] = std::pair<int,int>(e.a-1, e.b-1);
+					while (na > 0 && nb > 0) {
+						const entry &e = cache[nb][na];
+						res[di] = std::pair<size_t,size_t>(e.a-1, e.b-1);
 						na = e.na;
 						nb = e.nb;
 						di--;
@@ -59,6 +61,7 @@ namespace lcs {
 			}
 			return res;
 		}
+
 		template<typename IT> std::ptrdiff_t getsize(const IT &begin, const IT &end, std::random_access_iterator_tag) {
 			return end-begin;
 		}
@@ -68,18 +71,66 @@ namespace lcs {
 				sz++;
 			return sz;
 		}
+
+		template<typename IT> size_t lcs_sz_only(const IT &ahead, const IT &aend,
+												 const IT &bhead, const IT &bend,
+												 std::vector<std::vector<size_t> > &cache) {
+			IT aa(ahead);
+			IT bb(bhead);
+			size_t bi = 1;
+			while (bb != bend) {
+				size_t ai = 1;
+				for (IT a(aa); a != aend; ++a) {
+					if (*a == *bb) {
+						cache[bi][ai] = cache[bi^1][ai-1]+1;
+					} else {
+						if (cache[bi][ai-1] > cache[bi^1][ai])
+							cache[bi][ai] = cache[bi][ai-1];
+						else
+							cache[bi][ai] = cache[bi^1][ai];
+					}
+					++ai;
+				}
+				++bb;
+				bi ^= 1;
+			}
+			return cache[bi^1][cache[bi^1].size()-1];
+		}
+
 	}
 
 	using namespace details;
-	
-	template<typename IT, typename OIT> int lcs(const IT &ahead, const IT &aend,
-								  const IT &bhead, const IT &bend,
-								  OIT dst) {
-		std::vector<std::vector<entry> > cache(getsize(ahead, aend, typename std::iterator_traits<IT>::iterator_category())+1,
-											   std::vector<entry>(getsize(bhead, bend, typename std::iterator_traits<IT>::iterator_category())+1));
+
+	/* dst must be the back_inserter into the Container of std::pair<size_t, size_t> */
+	template<typename IT, typename OIT>
+	size_t lcs(const IT &ahead, const IT &aend,
+			   const IT &bhead, const IT &bend,
+			   OIT dst)
+	{
+		size_t a_sz = getsize(ahead, aend, typename std::iterator_traits<IT>::iterator_category());
+		size_t b_sz = getsize(bhead, bend, typename std::iterator_traits<IT>::iterator_category());
+		std::vector<std::vector<entry> > cache(b_sz+1, std::vector<entry>(a_sz+1));
 		lcs_r(ahead, aend, bhead, bend, cache);
-		std::vector<std::pair<int,int> > res(reconstruct(cache));
+		std::vector<std::pair<size_t,size_t> > res(reconstruct(cache));
 		std::copy(res.begin(), res.end(), dst);
 		return res.size();
+	}
+
+	/* size-optimized version if you need only the size, but don't need the actual correspondence */
+	template<typename IT>
+	size_t lcs(const IT &ahead, const IT &aend,
+			   const IT &bhead, const IT &bend)
+	{
+		size_t a_sz = getsize(ahead, aend, typename std::iterator_traits<IT>::iterator_category());
+		size_t b_sz = getsize(bhead, bend, typename std::iterator_traits<IT>::iterator_category());
+		if (a_sz <= b_sz) {
+			std::vector<std::vector<size_t> > cache(2, std::vector<size_t>(a_sz+1));
+			std::fill(cache[0].begin(), cache[0].end(), 0);
+			return lcs_sz_only(ahead, aend, bhead, bend, cache);
+		} else {
+			std::vector<std::vector<size_t> > cache(2, std::vector<size_t>(b_sz+1));
+			std::fill(cache[0].begin(), cache[0].end(), 0);
+			return lcs_sz_only(bhead, bend, ahead, aend, cache);
+		}
 	}
 }
--- c/draft/lcs_test.cc
+++ c/draft/lcs_test.cc
@@ -5,71 +5,101 @@
 TEST(LCSTest, T0) {
 	const char *a = "1";
 	const char *b = "1";
-	std::pair<int,int> res[2];
-	int rc = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
-	EXPECT_EQ(1, rc);
+	std::pair<size_t,size_t> res[2];
+	size_t rc1 = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
+	EXPECT_EQ(1, rc1);
+	size_t rc2 = lcs::lcs(a, a+strlen(a), b, b+strlen(b));
+	EXPECT_EQ(1, rc2);
 }
 
 TEST(LCSTest, T1) {
 	const char *a = "1";
 	const char *b = "2";
-	std::pair<int,int> res[2];
-	int rc = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
-	EXPECT_EQ(0, rc);
+	std::pair<size_t,size_t> res[2];
+	size_t rc1 = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
+	EXPECT_EQ(0, rc1);
+	size_t rc2 = lcs::lcs(a, a+strlen(a), b, b+strlen(b));
+	EXPECT_EQ(0, rc2);
 }
 
 TEST(LCSTest, T2) {
 	const char *a = "";
 	const char *b = "2";
-	std::pair<int,int> res[2];
-	int rc = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
-	EXPECT_EQ(0, rc);
+	std::pair<size_t,size_t> res[2];
+	size_t rc1 = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
+	EXPECT_EQ(0, rc1);
+	size_t rc2 = lcs::lcs(a, a+strlen(a), b, b+strlen(b));
+	EXPECT_EQ(0, rc2);
 }
 
 TEST(LCSTest, T3) {
 	const char *a = "1";
 	const char *b = "";
-	std::pair<int,int> res[2];
-	int rc = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
-	EXPECT_EQ(0, rc);
+	std::pair<size_t,size_t> res[2];
+	size_t rc1 = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
+	EXPECT_EQ(0, rc1);
+	size_t rc2 = lcs::lcs(a, a+strlen(a), b, b+strlen(b));
+	EXPECT_EQ(0, rc2);
 }
 
 TEST(LCSTest, T4) {
 	const char *a = "";
 	const char *b = "";
-	std::pair<int,int> res[2];
-	int rc = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
-	EXPECT_EQ(0, rc);
+	std::pair<size_t,size_t> res[2];
+	size_t rc1 = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
+	EXPECT_EQ(0, rc1);
+	size_t rc2 = lcs::lcs(a, a+strlen(a), b, b+strlen(b));
+	EXPECT_EQ(0, rc2);
 }
 
 TEST(LCSTest, T5) {
 	const char *a = "10203040506070";
 	const char *b = "91929394959697912";
-	std::pair<int,int> res[128];
-	int rc = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
-	EXPECT_EQ(7, rc);
+	std::pair<size_t,size_t> res[128];
+	size_t rc1 = lcs::lcs(a, a+strlen(a), b, b+strlen(b), res);
+	EXPECT_EQ(7, rc1);
+	size_t rc2 = lcs::lcs(a, a+strlen(a), b, b+strlen(b));
+	EXPECT_EQ(7, rc2);
+	std::string s;
+	for (int i=0; i<rc1; i++)
+		s += a[res[i].first];
+	EXPECT_EQ(s, "1234567");
 }
 
 TEST(LCSTest, T6) {
 	std::string a("0123456789");
 	std::string b("0123456789");
-	std::vector<std::pair<int,int>> res;
-	int rc = lcs::lcs(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(res));
-	EXPECT_EQ(10, rc);
+	std::vector<std::pair<size_t,size_t>> res;
+	size_t rc1 = lcs::lcs(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(res));
+	EXPECT_EQ(10, rc1);
+	size_t rc2 = lcs::lcs(a.begin(), a.end(), b.begin(), b.end());
+	EXPECT_EQ(10, rc2);
+	std::string s;
+	for (auto p:res)
+		s += a[p.first];
+	EXPECT_EQ(s, "0123456789");
 }
 
 TEST(LCSTest, T7) {
 	std::string a("thisisatest");
 	std::string b("testing123testing");
-	std::vector<std::pair<int,int>> res;
-	int rc = lcs::lcs(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(res));
-	EXPECT_EQ(7, rc);
+	std::vector<std::pair<size_t,size_t>> res;
+	size_t rc1 = lcs::lcs(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(res));
+	EXPECT_EQ(7, rc1);
+	size_t rc2 = lcs::lcs(a.begin(), a.end(), b.begin(), b.end());
+	EXPECT_EQ(7, rc2);
+	std::string s;
+	for (auto p:res)
+		s += a[p.first];
+	EXPECT_EQ(s, "tsitest");
 }
 
 TEST(LCSTest, T8) {
-	std::list<int> a {1, 2, 3, 4, 1};
-	std::list<int> b {3, 4, 1, 2, 1, 3};
-	std::list<std::pair<int,int>> res;
-	int rc = lcs::lcs(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(res));
-	EXPECT_EQ(3, rc);
+	std::list<size_t> a {1, 2, 3, 4, 1};
+	std::list<size_t> b {3, 4, 1, 2, 1, 3};
+	std::list<std::pair<size_t,size_t>> res;
+	size_t rc1 = lcs::lcs(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(res));
+	EXPECT_EQ(3, rc1);
+	size_t rc2 = lcs::lcs(a.begin(), a.end(), b.begin(), b.end());
+	EXPECT_EQ(3, rc2);
 }
