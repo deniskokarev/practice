67c4c5ee7a334e954b0644e9a845d45f433fd068
Denis Kokarev <dk1942@att.com>
2017-02-28 15:19:40 -0500

    cf778a full solution

--- c/cf/cf778a.cc
+++ c/cf/cf778a.cc
@@ -1,33 +1,8 @@
 #include <iostream>
 #include <vector>
 /* CodeForces CF778A problem */
+/* full solution (peeked at the hint) */
 using namespace std;
-
-struct A {
-	int a;
-	int i;
-};
-
-struct bloom {
-	const int sz = 'z'-'a';
-	vector<int> a;
-	vector<int> b;
-	bloom(const string &_a, const string &_b):a(sz, 0),b(sz, 0) {
-		for (char c:_a)
-			a[(c-'a')%sz]++;
-		for (char c:_b)
-			b[(c-'a')%sz]++;
-	}
-	bool can_remove(char c) {
-		int i = (c-'a')%sz;
-		if (a[i] > b[i]) {
-			a[i]--;
-			return true;
-		} else {
-			return false;
-		}
-	}
-};
 
 bool is_subsec(const string &a, const string &b) {
 	auto ai = a.begin();
@@ -40,6 +15,13 @@ bool is_subsec(const string &a, const string &b) {
 	return bi == b.end();
 }
 
+string crossout(const string &a, const vector<int> &r, int sz) {
+	string b(a);
+	for (int i=0; i<sz; i++)
+		b[r[i]] = '_';
+	return b;
+}
+
 int main(int argc, char **argv) {
 	string t;
 	string p;
@@ -50,17 +32,15 @@ int main(int argc, char **argv) {
 		cin >> ai;
 		ai--;
 	}
-	bloom b(t, p);
-	int i=0;
-	for (auto &ai:a) {
-		if (!b.can_remove(t[ai]))
-			break;
-		t[ai] = '_';
-		if (!is_subsec(t, p))
-			break;
-		i++;
-		//cerr << i << ':' << t << endl;
+	int i = 0;
+	int j = t.size();
+	while (i<j) {
+		int m = (i+j)/2;
+		if (!is_subsec(crossout(t, a, m), p))
+			j = m;
+		else
+			i = m+1;
 	}
-	cout << i << endl;
+	cout << i-1 << endl;
 	return 0;
 }
