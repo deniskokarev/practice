2af28b2dfc32e8d6d9f7e30ad5c94da129f1c8b1
Denis Kokarev <d_kokarev@mail.ru>
2017-03-03 21:46:08 -0500

    cf776c customized hash function made it possible to use unordered_map with acceptable performance

--- c/cf/cf776.cc
+++ c/cf/cf776.cc
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 /* CodeForces CF776C problem */
 using namespace std;
 
+/* GNU stdlibc++ is so lousy that you have to customize hash functions for ints */
+struct myhashi64 {
+	size_t operator()(int64_t x) const {
+		x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
+		x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
+		x = x ^ (x >> 31);
+		return x;
+	}
+};
+
 static int64_t cnt(const vector<int64_t> &ss, const vector<int64_t> &vkp) {
 	int64_t sum = 0;
-	map<int64_t, int64_t> r;
+	unordered_map<int64_t, int64_t, myhashi64> r;
 	for (int i=ss.size()-1; i>=0; i--) {
-		for (auto &kp:vkp) {
+		for (auto kp:vkp) {
 			auto v = r.find(kp+ss[i]);
 			if (v != r.end())
 				sum += v->second;
 		}
 		r[ss[i]]++;
 	}
-	for (auto &kp:vkp) {
+	for (auto kp:vkp) {
 		auto v = r.find(kp);
 		if (v != r.end())
 			sum += v->second;
@@ -39,11 +49,11 @@ int main(int argc, char **argv) {
 		vector<int64_t> vkp;
 		for (int64_t kp=1; abs(kp)<=100000000000000; kp *= k)
 			vkp.push_back(kp);
-		sum += cnt(ss, vkp);
+		sum = cnt(ss, vkp);
 	} else if (k == -1) {
-		sum += cnt(ss, vector<int64_t>{-1, 1});
+		sum = cnt(ss, vector<int64_t>{-1, 1});
 	} else {
-		sum += cnt(ss, vector<int64_t>{1});
+		sum = cnt(ss, vector<int64_t>{1});
 	}
 	cout << sum << endl;
 	return 0;
