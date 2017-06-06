4c739b79f694974c90c039fae87da90966d61ed8
Denis Kokarev <dk1942@att.com>
2017-06-06 17:51:55 -0400

    cf757c full solution simplified

--- c/cf/cf757c.cc
+++ c/cf/cf757c.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <algorithm>
 
 /* CodeForces CF757C problem */
 using namespace std;
@@ -13,8 +12,8 @@ struct FactMod {
 	FactMod(uint32_t m) {
 		f.resize(m+1);
 		f[0] = f[1] = 1;
-		for (uint64_t i=2; i<=m; i++)
-			f[i] = (f[i-1] * i)%MOD;
+		for (uint32_t i=2; i<=m; i++)
+			f[i] = (f[i-1] * i) % MOD;
 	}
 	uint64_t operator()(uint32_t n) {
 		return f.at(n);
@@ -25,46 +24,38 @@ struct SC {
 	int sch;
 	int cnt;
 	bool operator<(const SC &b) const {
-		if (sch < b.sch) {
-			return false;
-		} else if (sch == b.sch) {
-			return cnt < b.cnt;
-		} else {
-			return true;
-		}
+		return (sch < b.sch) || ((sch == b.sch) && (cnt < b.cnt));
 	}
 };
 
 int main(int argc, char **argv) {
 	int n, m;
 	cin >> n >> m;
-	FactMod fact(m);
-	vector<map<int, int>> ss(n);	// school type->cnt
-	map<int, vector<SC>> tstat; // type -> (sch,cnt)*
-	vector<bool> tpres(m+1, false); // missing types
-	int nsch = 0;
-	for (auto &s:ss) {
+	vector<vector<SC>> tstat(m+1); // type -> (sch,cnt)*
+	for (int i=0; i<n; i++) {
+		map<int,int> s; // type->cnt
 		int g;
 		for (cin >> g; g>0; g--) {
-			int m;
-			cin >> m;
-			s[m]++;
-			tpres[m] = true;
+			int t;
+			cin >> t;
+			s[t]++;
 		}
 		for (auto &e:s)
-			tstat[e.first].push_back({nsch, e.second});
-		nsch++;
+			tstat[e.first].push_back({i, e.second});
+		s.clear();
 	}
 	map<vector<SC>, vector<int>> itstat; // inverted tstat: (sch,cnt),(sch,cnt),(sch,cnt) -> type*
-	for (auto &e:tstat)
-		itstat[e.second].push_back(e.first);
+	for (int i=1; i<=m; i++)
+		if (tstat[i].size() > 0)
+			itstat[tstat[i]].push_back(i);
+	FactMod fact(m);
 	uint64_t sum = 1;
 	for (auto &e:itstat)
 		sum = sum * fact(e.second.size()) % MOD;
 	// count all missing types separately
 	int tmiss = 0;
-	for (int t=0; t<m; t++)
-		if (!tpres[t+1])
+	for (int i=1; i<=m; i++)
+		if (tstat[i].size() < 1)
 			tmiss++;
 	sum = sum * fact(tmiss) % MOD;
 	cout << sum << endl;
