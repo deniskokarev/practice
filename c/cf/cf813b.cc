87e7e88738d261ef581cd93d59d68e096fed88fa
Denis Kokarev <d_kokarev@mail.ru>
2017-06-07 18:41:51 -0400

    cf813b wrong answer on test 14

--- c/cf/cf813b.cc
+++ c/cf/cf813b.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 /* CodeForces CF813B problem */
 using namespace std;
 
@@ -10,13 +11,13 @@ int main(int argc, char **argv) {
 	ab[0][0] = 1+1;
 	ab[1][0] = x+1;
 	for (int a=1; a<65; a++)
-		if (LLONG_MAX/x > (ab[a-1][0]-1))
+		if (ab[a-1][0] > 0 && LLONG_MAX/x > (ab[a-1][0]-1))
 			ab[a][0] = (ab[a-1][0]-1)*x+1;
 		else
 			ab[a][0] = -1;
 	ab[0][1] = 1+y;
 	for (int b=1; b<65; b++)
-		if (LLONG_MAX/y > (ab[0][b-1]-1))
+		if (ab[0][b-1] > 0 && LLONG_MAX/y > (ab[0][b-1]-1))
 			ab[0][b] = (ab[0][b-1]-1)*y+1;
 		else
 			ab[0][b] = -1;
@@ -29,20 +30,31 @@ int main(int argc, char **argv) {
 	int64_t seq[65*65+2];
 	int nseq = 0;
 	seq[nseq++] = l;
-	for (int a=0; a<65; a++)
-		for (int b=0; b<65; b++)
+	for (int a=0; a<65; a++) {
+		for (int b=0; b<65; b++) {
+			cerr << ab[a][b] << ' ';
 			if (ab[a][b] >= l && ab[a][b] <= r)
 				seq[nseq++] = ab[a][b];
+		}
+		cerr << endl;
+	}
+	//copy(seq, seq+nseq, ostream_iterator<int64_t>(cerr, " "));
+	sort(seq+1, seq+nseq);
+	nseq = unique(seq+1, seq+nseq)-seq;
 	seq[nseq++] = r;
-	sort(seq, seq+nseq);
-	nseq = unique(seq, seq+nseq)-seq;
 #if 1
 	copy(seq, seq+nseq, ostream_iterator<int64_t>(cerr, " "));
 	cerr << endl;
 #endif
 	int64_t mx = 0;
-	for (int i=1; i<nseq; i++)
-		mx = max(mx, seq[i]-seq[i-1]);
+	for (int i=2; i<nseq-1; i++)
+		mx = max(mx, seq[i]-seq[i-1]-1);
+	if (nseq > 2) {
+		mx = max(mx, seq[1]-seq[0]);
+		mx = max(mx, seq[nseq-1]-seq[nseq-2]);
+	} else {
+		mx = r-l+1;
+	}
 	cout << mx << endl;
 	return 0;
 }
