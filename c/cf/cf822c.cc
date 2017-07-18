465962ac02a55980f3b9547ee5a7adfcd485a902
Denis Kokarev <d_kokarev@mail.ru>
2017-07-17 21:07:43 -0400

    cf822c full solution

--- c/cf/cf822c.cc
+++ c/cf/cf822c.cc
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <map>
 #include <climits>
 
 /* CodeForces CF822C problem */
@@ -12,50 +11,50 @@ struct V {
 	int len;
 };
 
-struct P {
-	int p;
-	int64_t mnc;
-	bool operator<(const P &b) const {
-		return p<b.p;
-	}
-};
-
-#define dim(X) ((sizeof(X)/sizeof(X[0])))
+map<int,int64_t> ll[200001];
+map<int,int64_t,greater<int>> rr[200001]; // map in desc order
 
 int main(int argc, char **argv) {
 	int n, x;
 	cin >> n >> x;
 	V vv[n];
-	vector<vector<P>> rr(200001), ll(200001);
 	for (auto &v:vv) {
 		cin >> v.l >> v.r >> v.c;
 		v.len = v.r-v.l+1;
-		rr[v.len].push_back((P){v.r, v.c});
-		ll[v.len].push_back((P){v.l, v.c});
+		if (rr[v.len][v.r] == 0)
+			rr[v.len][v.r] = v.c;
+		else
+			rr[v.len][v.r] = min(rr[v.len][v.r], v.c);
+		if (ll[v.len][v.l] == 0)
+			ll[v.len][v.l] = v.c;
+		else
+			ll[v.len][v.l] = min(ll[v.len][v.l], v.c);
 	}
 	for (auto &r:rr) {
-		sort(r.begin(), r.end());
-		for (int i=1; i<r.size(); i++)
-			r[i].mnc = min(r[i].mnc, r[i-1].mnc);
+		auto pv = r.rbegin();
+		if (pv != r.rend())
+			for (auto nx=next(pv); nx!=r.rend(); pv=nx,++nx)
+				nx->second = min(pv->second, nx->second);
 	}
 	for (auto &l:ll) {
-		sort(l.begin(), l.end());
-		for (int i=l.size()-2; i>=0; i--)
-			l[i].mnc = min(l[i].mnc, l[i+1].mnc);
+		auto pv = l.rbegin();
+		if (pv != l.rend())
+			for (auto nx=next(pv); nx!=l.rend(); pv=nx,++nx)
+				nx->second = min(pv->second, nx->second);
 	}
 	int64_t mn = LLONG_MAX;
 	for (auto &v:vv) {
 		if (v.len >= x)
 			continue;
 		int cl = x-v.len;
-		P r = {v.r};
-		auto it = upper_bound(ll[cl].begin(), ll[cl].end(), r);
-		if (it != ll[cl].end())
-			mn = min(mn, v.c+it->mnc);
-		P l = {v.l-1};
-		it = lower_bound(rr[cl].begin(), rr[cl].end(), l);
-		if (it != rr[cl].end())
-			mn = min(mn, v.c+it->mnc);
+		auto &ml = ll[cl];
+		auto it = ml.upper_bound(v.r);
+		if (it != ml.end())
+			mn = min(mn, v.c + it->second);
+		auto &mr = rr[cl];
+		it = mr.upper_bound(v.l);
+		if (it != mr.end())
+			mn = min(mn, v.c + it->second);
 	}
 	if (mn == LLONG_MAX)
 		cout << -1;
