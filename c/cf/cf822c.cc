f054a0c5b356c71e29fb5f1150c5cf80f4bea581
Denis Kokarev <d_kokarev@mail.ru>
2017-07-17 21:29:56 -0400

    cf822c twice better solution

--- c/cf/cf822c.cc
+++ c/cf/cf822c.cc
@@ -12,7 +12,6 @@ struct V {
 };
 
 map<int,int64_t> ll[200001];
-map<int,int64_t,greater<int>> rr[200001]; // map in desc order
 
 int main(int argc, char **argv) {
 	int n, x;
@@ -21,20 +20,10 @@ int main(int argc, char **argv) {
 	for (auto &v:vv) {
 		cin >> v.l >> v.r >> v.c;
 		v.len = v.r-v.l+1;
-		if (rr[v.len][v.r] == 0)
-			rr[v.len][v.r] = v.c;
-		else
-			rr[v.len][v.r] = min(rr[v.len][v.r], v.c);
 		if (ll[v.len][v.l] == 0)
 			ll[v.len][v.l] = v.c;
 		else
 			ll[v.len][v.l] = min(ll[v.len][v.l], v.c);
-	}
-	for (auto &r:rr) {
-		auto pv = r.rbegin();
-		if (pv != r.rend())
-			for (auto nx=next(pv); nx!=r.rend(); pv=nx,++nx)
-				nx->second = min(pv->second, nx->second);
 	}
 	for (auto &l:ll) {
 		auto pv = l.rbegin();
@@ -50,10 +39,6 @@ int main(int argc, char **argv) {
 		auto &ml = ll[cl];
 		auto it = ml.upper_bound(v.r);
 		if (it != ml.end())
-			mn = min(mn, v.c + it->second);
-		auto &mr = rr[cl];
-		it = mr.upper_bound(v.l);
-		if (it != mr.end())
 			mn = min(mn, v.c + it->second);
 	}
 	if (mn == LLONG_MAX)
