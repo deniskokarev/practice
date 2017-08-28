c9b19bbc6819f92b7a324fd4095fd14d932f7b12
Denis Kokarev <d_kokarev@mail.ru>
2017-08-28 19:34:18 -0400

    acmp112 binary multistage merge solution

--- c/acmp/acmp112.cpp
+++ c/acmp/acmp112.cpp
@@ -1,47 +1,33 @@
-#include <iostream>
+#include <stdio.h>
+#include <math.h>
 #include <algorithm>
-
+ 
 /* ACMP 112 */
-using namespace std;
-
-constexpr int mxp2 = 3; // ceil(log2(10000)) == 14
-constexpr int mxn = 1<<mxp2;
-
+ 
 int main(int argc, char **argv) {
-	int n, k;
-	cin >> n >> k;
-	int ans = 0;
-	for (int ki=0; ki<k; ki++) {
-		uint64_t ss[mxp2+1][mxn];
-		for (int j=0; j<n; j++)
-			cin >> ss[0][j];
-		for (int j=n; j<mxn; j++)
-			ss[0][j] = ULLONG_MAX;
-		for (int i=0; i<mxn; i++)
-			cerr << ss[0][i] << ' ';
-		cerr << endl;
-		for (int l=0, p2=1; l<mxp2; l++,p2*=2) {
-			for (int x=0; x < mxn; x+=2*p2)
-				merge(&ss[l][x], &ss[l][x+p2],
-					  &ss[l][x+p2], &ss[l][x+2*p2],
-					  &ss[l+1][x]);
-			for (int i=0; i<mxn; i++)
-				cerr << ss[l+1][i] << ' ';
-			cerr << endl;
-		}
-		for (int i=1; i<n; i++) {
-			int s = ss[0][i];
-			for (int d=1,p=i,l=0; d<mxn && p>0; d*=2,l++) {
-				if (p%2 == 1) {
-					int x = i/d*d-d;
-					auto it = lower_bound(&ss[l][x], &ss[l][x+d], s);
-					if (it != &ss[l][x+d])
-						ans += &ss[l][x+d]-it;
-				}
-				p /= 2;
-			}
-		}
-	}
-	cout << ans << endl;
-	return 0;
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int l2 = int(ceil(log2(n)));
+    unsigned long long ans = 0;
+    for (int ki=0; ki<k; ki++) {
+        unsigned short ss[l2+1][n]; // for multistage binary search
+        for (int i=0; i<n; i++)
+            scanf("%hu", &ss[0][i]);
+        for (int l=0,p2=1,dp2=p2*2; l<l2 && dp2<n; l++,p2=dp2,dp2*=2)
+            for (int x=0; x+dp2<=n; x+=dp2)
+                std::merge(&ss[l][x], &ss[l][x+p2], &ss[l][x+p2], &ss[l][x+dp2], &ss[l+1][x]);
+        for (int i=1; i<n; i++) {
+            int el = ss[0][i];
+            for (int b=i,l=0; b>0; b>>=1,l++) {
+                if ((b&1)) {
+                    auto bi = &ss[l][((i>>l)-1)<<l];
+                    auto ei = &ss[l][(i>>l)<<l];
+                    auto it = std::upper_bound(bi, ei, el);
+                    ans += ei-it;
+                }
+            }
+        }
+    }
+    printf("%lld\n", ans);
+    return 0;
 }
