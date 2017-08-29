00dec99937383c63dad90bfb3a0040de302d34f9
Denis Kokarev <d_kokarev@mail.ru>
2017-08-28 21:58:41 -0400

    tabifying

--- c/alg/mmerge.hh
+++ c/alg/mmerge.hh
@@ -13,45 +13,45 @@
  
 template<typename T>
 struct mmerge {
-    int sz;
-    int l2;	// ilog2(sz)
-    std::vector<std::vector<T>> mm; // our 2D array [l2+1][sz]
-    using value_type = T;
-    using it = typename std::vector<T>::iterator;
-    using cit = typename std::vector<T>::const_iterator;
+	int sz;
+	int l2;	// ilog2(sz)
+	std::vector<std::vector<T>> mm; // our 2D array [l2+1][sz]
+	using value_type = T;
+	using it = typename std::vector<T>::iterator;
+	using cit = typename std::vector<T>::const_iterator;
 	/**
 	 * @param sz - number of elements we need to deal with
 	 */
-    mmerge(int _sz):sz(_sz),l2(int(ceil(log2(sz)))),mm(l2+1, std::vector<T>(sz)) {
-    }
+	mmerge(int _sz):sz(_sz),l2(int(ceil(log2(sz)))),mm(l2+1, std::vector<T>(sz)) {
+	}
 	/**
 	 * use [] operator to populate the main level
 	 */
-    T& operator[](int i) {
-        return mm[0][i];
-    }
+	T& operator[](int i) {
+		return mm[0][i];
+	}
 	/**
 	 * Run multistage merge using std::merge at each stage
 	 * Each level gets sorted bins. The bin sizes double with each level
 	 */
-    template<class Cmp = std::less<T>> void merge_merge() {
-        Cmp cmp;
-        for (int l=0,p2=1,dp2=p2*2; l<l2 && dp2<sz; l++,p2=dp2,dp2*=2)
-            for (int x=0; x+dp2<=sz; x+=dp2)
-                std::merge(mm[l].begin()+x, mm[l].begin()+x+p2, mm[l].begin()+x+p2, mm[l].begin()+x+dp2, mm[l+1].begin()+x, cmp);
-    }
+	template<class Cmp = std::less<T>> void merge_merge() {
+		Cmp cmp;
+		for (int l=0,p2=1,dp2=p2*2; l<l2 && dp2<sz; l++,p2=dp2,dp2*=2)
+			for (int x=0; x+dp2<=sz; x+=dp2)
+				std::merge(mm[l].begin()+x, mm[l].begin()+x+p2, mm[l].begin()+x+p2, mm[l].begin()+x+dp2, mm[l+1].begin()+x, cmp);
+	}
 	/**
 	 * Run ReduceOp for every sorted binary bin to the left of the element pos
 	 */
-    template<class ReduceOp> void reduce_lt(int pos, ReduceOp &&rop) const {
-        for (int b=pos,l=0; b>0; b>>=1,l++) {
-            if ((b&1)) {
-                auto bi = mm[l].cbegin()+(((pos>>l)-1)<<l);
-                auto ei = mm[l].cbegin()+((pos>>l)<<l);
-                rop(bi, ei);
-            }
-        }
-    }
+	template<class ReduceOp> void reduce_lt(int pos, ReduceOp &&rop) const {
+		for (int b=pos,l=0; b>0; b>>=1,l++) {
+			if ((b&1)) {
+				auto bi = mm[l].cbegin()+(((pos>>l)-1)<<l);
+				auto ei = mm[l].cbegin()+((pos>>l)<<l);
+				rop(bi, ei);
+			}
+		}
+	}
 };
 
 #if 0
