b92dbba1bc447d79c726be106f8b435704da88da
Denis Kokarev <d_kokarev@mail.ru>
2017-08-29 00:05:34 -0400

    a bit slower but simpler solution with Fenwick tree

--- c/acmp/acmp112.cpp
+++ c/acmp/acmp112.cpp
@@ -1,83 +1,57 @@
-#include <stdio.h>
-#include <algorithm>
- 
-/* ACMP 112 */
- 
+/* ACMP 112 problem */
+
 /**
- * Multistage binary merge primitive for binary decomposition
- * Allows O(Nlog(N)) complexity for [0..pos) interval operations
- * Needs O(Nlog(N)) time to initialize
- * Needs O(Nlog(N)) space
+ * organize Fenwick tree over a random iterator
+ * inc(pos, delta) in O(log(n))
+ * sum(0..last) in O(log(n))
  */
-#include <vector>
-#include <cmath>
- 
-template<typename T>
-struct mmerge {
-    int sz;
-    int l2;	// ilog2(sz)
-    std::vector<std::vector<T>> mm; // our 2D array [l2+1][sz]
-    using value_type = T;
-    using it = typename std::vector<T>::iterator;
-    using cit = typename std::vector<T>::const_iterator;
-	/**
-	 * @param sz - number of elements we need to deal with
-	 */
-    mmerge(int _sz):sz(_sz),l2(int(ceil(log2(sz)))),mm(l2+1, std::vector<T>(sz)) {
+#include <iterator>
+#include <algorithm>
+
+template<typename IT> struct FenwickTree {
+	using value_type = typename std::iterator_traits<IT>::value_type;
+	using difference_type = typename std::iterator_traits<IT>::difference_type;
+    IT vv;
+	int sz;
+	// initialize the tree with 0s 
+    FenwickTree(IT begin, IT end):vv(begin),sz(end-begin) {
+		std::fill(begin, end, 0);
+	}
+	// increment the element at pos by delta
+    void inc(difference_type pos, value_type delta) {
+        do vv[pos] += delta; while((pos=pos|(pos+1)) < sz);
     }
-	/**
-	 * use [] operator to populate the main level
-	 */
-    T& operator[](int i) {
-        return mm[0][i];
-    }
-	/**
-	 * Run multistage merge using std::merge at each stage
-	 * Each level gets sorted bins. The bin sizes double with each level
-	 */
-    template<class Cmp = std::less<T>> void merge_merge() {
-        Cmp cmp;
-        for (int l=0,p2=1,dp2=p2*2; l<l2 && dp2<sz; l++,p2=dp2,dp2*=2)
-            for (int x=0; x+dp2<=sz; x+=dp2)
-                std::merge(mm[l].begin()+x, mm[l].begin()+x+p2, mm[l].begin()+x+p2, mm[l].begin()+x+dp2, mm[l+1].begin()+x, cmp);
-    }
-	/**
-	 * Run ReduceOp for every sorted binary bin to the left of the element pos
-	 */
-    template<class ReduceOp> void reduce_lt(int pos, ReduceOp &&rop) const {
-        for (int b=pos,l=0; b>0; b>>=1,l++) {
-            if ((b&1)) {
-                auto bi = mm[l].cbegin()+(((pos>>l)-1)<<l);
-                auto ei = mm[l].cbegin()+((pos>>l)<<l);
-                rop(bi, ei);
-            }
-        }
+    // @return sum[0..last]
+    value_type sum(difference_type last) {
+        value_type sum = 0;
+        do sum += vv[last]; while ((last=(last&(last+1))-1) >= 0);
+        return sum;
     }
 };
- 
+
+// helper function to simplify the use
+template<typename IT> FenwickTree<IT> create_fenwick(IT b, IT e) {
+	return FenwickTree<IT>(b, e);
+}
+
+#include <stdio.h>
+#include <assert.h>
+
 int main(int argc, char **argv) {
-    int n, k;
-    scanf("%d %d", &n, &k);
+	int n, k;
+	scanf("%d %d", &n, &k);
+	assert(n<=10000);
+	unsigned short solders[n+1];
 	unsigned long long ans = 0;
-    using TMM = mmerge<unsigned short>;
-    TMM solders(n);
-    for (int ki=0; ki<k; ki++) {
-        for (int i=0; i<n; i++)
-            scanf("%hu", &solders[i]);
-        solders.merge_merge();
-        for (int i=1; i<n; i++) {
-			struct CountGreater {
-				unsigned long long &ans;
-				unsigned short &el;
-				CountGreater(unsigned long long &_a, unsigned short &_e):ans(_a),el(_e){}
-				void operator()(TMM::cit bi, TMM::cit ei) {
-                    auto it = std::upper_bound(bi, ei, el);
-                    ans += ei-it;
-				}
-			};
-            solders.reduce_lt(i, CountGreater(ans, solders[i]));
-        }
-    }
-    printf("%lld\n", ans);
-    return 0;
+	for (int ki=0; ki<k; ki++) {
+		auto fw = create_fenwick(solders, solders+n+1);
+		for (int i=0; i<n; i++) {
+			unsigned short s;
+			scanf("%hu", &s);
+			ans += i-fw.sum(s);
+			fw.inc(s, 1);
+		}
+	}
+	printf("%lld\n", ans);
+	return 0;
 }
