26e6062c6750822996032056d95f528745e3a407
Denis Kokarev <dk1942@att.com>
2017-09-06 13:32:16 -0400

    acmp 318 solution

--- c/acmp/acmp318.cpp
+++ c/acmp/acmp318.cpp
@@ -1,19 +1,20 @@
 /* ACMP 318 */
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	int32_t n;
+	uint64_t n;
 	cin >> n;
-	int bb[32] = {0};
-	for (int i=0; n>0; i++) {
+	vector<uint64_t> bb(32);
+	for (int i=0; i<32; i++) {
 		bb[31-i] = n&1;
 		n >>= 1;
 	}
-	next_permutation(bb, bb+32);
-	int32_t ans = 0;
+	next_permutation(bb.begin(), bb.end());
+	uint64_t ans = 0;
 	for (int i=0; i<32; i++) {
 		ans <<= 1;
 		ans += bb[i];
