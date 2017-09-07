629e096ba28eec527614f1d010517ef6bfff7ebc
Denis Kokarev <dk1942@att.com>
2017-09-07 15:08:48 -0400

    acmp 659 using stock bitset is still ok from performance prospective

--- c/acmp/acmp659.cpp
+++ c/acmp/acmp659.cpp
@@ -2,78 +2,45 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <set>
- 
+#include <bitset>
+
 using namespace std;
 
-// bitset::bitset performance is not exactly clear
-// using our own O(n)
-struct BITS_CNT {
-	int n[1<<16];
-	BITS_CNT() {
-		fill(n, n+(1<<16), 0);
-		for (int i=0; i<(1<<16); i++)
-			for (int j=0; j<16; j++)
-				n[i] += (i>>j)&1;
-	}
-};
-
-union BITS {
-	static const BITS_CNT bitcnt;
-	uint32_t i;
-	uint16_t bb[2];
-	void set(int n) {
-		i |= 1ULL<<n;
-	};
-	bool isset(int n) {
-		return i & 1ULL<<n;
-	};
-	void clear(int n) {
-		i &= ~(1ULL<<n);
-	};
-	int count() {
-		int cnt = 0;
-			for (auto b:bb)
-				cnt += bitcnt.n[b];
-			return cnt;
-	};
-};
-
-const BITS_CNT BITS::bitcnt;
+using BITS = bitset<32>; // 64 would'be been too slow
 
 struct SEL_CTX {
 	const vector<BITS> &ff;
 	const int n;
-	int mxf;
+	int mx;
+	BITS mx_sel;
 	BITS sel;
-	BITS best;
 };
 
 void choose_team(int k, int f, SEL_CTX &sc) {
 	if (k>0) {
 		for (int i=f; i<sc.n; i++) {
-			sc.sel.set(i);
+			sc.sel[i] = 1;
 			choose_team(k-1, i+1, sc);
-			sc.sel.clear(i);
+			sc.sel[i] = 0;
 		}
 	} else {
-		int tt = 0;
-		int ot = 0;
+		int ft = 0; // friends in this team
+		int fo = 0; // friends in other team
 		for (int i=0; i<sc.n; i++) {
-			if (sc.sel.isset(i)) {
+			if (sc.sel[i]) {
 				BITS b = sc.ff[i];
-				b.i &= sc.sel.i;
-				tt += b.count();
+				b &= sc.sel;
+				ft += b.count();
 			} else {
 				BITS b = sc.ff[i];
-				b.i &= ~sc.sel.i;
-				ot += b.count();
+				b &= ~sc.sel;
+				fo += b.count();
 			}
 		}
-		int fr = tt+ot;
-		if (fr >= sc.mxf) {
-			sc.best = sc.sel;
-			sc.mxf = fr;
+		int f_tot = ft+fo;
+		if (f_tot >= sc.mx) {
+			sc.mx_sel = sc.sel;
+			sc.mx = f_tot;
 		}
 	}
 }
@@ -85,7 +52,7 @@ int main(int argc, char **argv) {
 	for (int i=0; i<m; i++) {
 		int v1, v2;
 		cin >> v1 >> v2;
-		v1--, v2--;
+		v1--; v2--;
 		int mn_v = min(v1, v2);
 		int mx_v = max(v1, v2);
 		ff[mn_v].set(mx_v);
@@ -93,7 +60,7 @@ int main(int argc, char **argv) {
 	SEL_CTX sc {ff, n, 0, {0}, {0}};
 	choose_team(k, 0, sc);
 	for (int p=0; p<n; p++)
-		if (sc.best.isset(p))
+		if (sc.mx_sel[p])
 			cout << p+1 << ' ';
 	cout << endl;
 	return 0;
