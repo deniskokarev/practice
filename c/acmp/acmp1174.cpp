6eb03938e495287381379060f05fcdde37712d31
Denis Kokarev <d_kokarev@mail.ru>
2017-09-09 15:52:53 -0400

    acmp 1174, hmm still timeout 27

--- c/acmp/acmp1174.cpp
+++ c/acmp/acmp1174.cpp
@@ -1,6 +1,7 @@
 /* ACMP 1174 */
 #include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
@@ -9,17 +10,17 @@ int main(int argc, char **argv) {
 	string s;
 	cin >> n >> m >> s;
 	int l = s.length()/m;
-	multimap<string, int> mm;
+	map<string, vector<int>> mm;
 	for (int i=0; i<m; i++) {
 		string ms;
 		cin >> ms;
-		mm.emplace(ms, i);
+		mm[ms].push_back(i);
 	}
 	for (int i=0; i<s.length()/l; i++) {
 		string seg = s.substr(i*l, l);
-		auto mel = mm.find(seg);
-		cout << mel->second+1 << ' ';
-		mm.erase(mel);
+		auto &vv = mm[seg];
+		cout << vv.back()+1 << ' ';
+		vv.pop_back();
 	}
 	cout << endl;
 	return 0;
