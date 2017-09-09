13e143518d37a7b4c898121035bddd41af6ba523
Denis Kokarev <d_kokarev@mail.ru>
2017-09-09 18:56:05 -0400

    acmp 923 still timeout on 26

--- c/acmp/acmp923.cpp
+++ c/acmp/acmp923.cpp
@@ -1,21 +1,42 @@
 /* ACMP 923 */
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-inline uint64_t rem(uint64_t n) {
-	if (n == 3) {
-		return 1;
-	} else if (n < 3) {
-		return 0;
-	} else if ((n&1) == 0) {
-		return 2*rem(n/2);
-	} else {
-		return rem(n/2)+rem(n-n/2);
+struct Rem {
+	vector<uint64_t> rr;
+	Rem():rr(1<<20, ULLONG_MAX) {
+		for (int i=0; i<rr.size(); i++)
+			rr[i] = bld_rem(i);
 	}
-}
+	uint64_t bld_rem(uint64_t n) {
+		if (rr[n] == ULLONG_MAX) {
+			if (n == 3) {
+				rr[n] = 1;
+			} else if (n < 3) {
+				rr[n] = 0;
+			} else if ((n&1) == 0) {
+				rr[n] = 2*bld_rem(n/2);
+			} else {
+				rr[n] = bld_rem(n/2)+bld_rem(n-n/2);
+			}
+		}
+		return rr[n];
+	}
+	uint64_t operator()(uint64_t n) const {
+		if (n < rr.size()) {
+			return rr[n];
+		} else if ((n&1) == 0) {
+			return 2*operator()(n/2);
+		} else {
+			return operator()(n/2)+operator()(n-n/2);
+		}
+	}
+};
 
 int main(int argc, char **argv) {
+	Rem rem;
 	uint64_t n;
 	cin >> n;
 	cout << rem(n) << endl;
