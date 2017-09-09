a31fe131607f6e5d31c0e2c903b0b9863bb08004
Denis Kokarev <d_kokarev@mail.ru>
2017-09-09 19:07:29 -0400

    acmp 923 solution

--- c/acmp/acmp923.cpp
+++ c/acmp/acmp923.cpp
@@ -1,37 +1,26 @@
 /* ACMP 923 */
 #include <iostream>
-#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 struct Rem {
-	vector<uint64_t> rr;
-	Rem():rr(1<<20, ULLONG_MAX) {
-		for (int i=0; i<rr.size(); i++)
-			rr[i] = bld_rem(i);
-	}
-	uint64_t bld_rem(uint64_t n) {
-		if (rr[n] == ULLONG_MAX) {
+	unordered_map<uint64_t, uint64_t> rr;
+	uint64_t operator()(uint64_t n) {
+		auto &r = rr[n];
+		if (!r) {
 			if (n == 3) {
-				rr[n] = 1;
+				r = 1;
 			} else if (n < 3) {
-				rr[n] = 0;
+				r = 0;
 			} else if ((n&1) == 0) {
-				rr[n] = 2*bld_rem(n/2);
+				r = 2*operator()(n/2);
 			} else {
-				rr[n] = bld_rem(n/2)+bld_rem(n-n/2);
+				r = operator()(n/2)+operator()(n-n/2);
 			}
+			r++;
 		}
-		return rr[n];
-	}
-	uint64_t operator()(uint64_t n) const {
-		if (n < rr.size()) {
-			return rr[n];
-		} else if ((n&1) == 0) {
-			return 2*operator()(n/2);
-		} else {
-			return operator()(n/2)+operator()(n-n/2);
-		}
+		return r-1;
 	}
 };
 
