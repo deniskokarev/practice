b2c122fcb4f2e2bee7b6d289feb2ead3f4c518d4
Denis Kokarev <d_kokarev@mail.ru>
2017-09-09 16:44:59 -0400

    acmp 1174 out of memory 23

--- c/acmp/acmp1174.cpp
+++ c/acmp/acmp1174.cpp
@@ -1,26 +1,47 @@
 /* ACMP 1174 */
 #include <iostream>
-#include <map>
+#include <memory>
 #include <vector>
+#include <cassert>
 
 using namespace std;
+
+struct Trie {
+	unique_ptr<Trie> node['z'-'a'];
+	pair<int,int> pos;
+	
+	Trie *operator[](char ch) {
+		assert(ch >= 'a' && ch <= 'z');
+		int ni = ch-'a';
+		if (!node[ni])
+			node[ni] = unique_ptr<Trie>(new Trie());
+		return node[ni].get();
+	}
+};
 
 int main(int argc, char **argv) {
 	int n, m;
 	string s;
 	cin >> n >> m >> s;
 	int l = s.length()/m;
-	map<string, vector<int>> mm;
+	vector<pair<int,int>> pos; // (prev,val)
+	Trie root;
 	for (int i=0; i<m; i++) {
 		string ms;
 		cin >> ms;
-		mm[ms].push_back(i);
+		Trie *np = &root;
+		for (char ch:ms)
+			np = (*np)[ch];
+		pos.push_back(np->pos);
+		np->pos = make_pair(pos.size()-1, i);
 	}
 	for (int i=0; i<s.length()/l; i++) {
-		string seg = s.substr(i*l, l);
-		auto &vv = mm[seg];
-		cout << vv.back()+1 << ' ';
-		vv.pop_back();
+		Trie *np = &root;
+		for (int j=0,p=i*l; j<l; j++,p++)
+			np = (*np)[s[p]];
+		auto &vv = np->pos;
+		cout << vv.second+1 << ' ';
+		vv = pos[vv.first];
 	}
 	cout << endl;
 	return 0;
