c92393a5827d29839cef4f254cca2d901fc5ad56
Denis Kokarev <d_kokarev@mail.ru>
2017-09-10 18:07:06 -0400

    acmp 505 still timeout 7

--- c/acmp/acmp505.cpp
+++ c/acmp/acmp505.cpp
@@ -1,16 +1,45 @@
 /* ACMP 505 */
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
+
+using SV = vector<short>;
+
+
+unsigned int crc32b(const unsigned char *message, int sz) {
+	int i, j;
+	unsigned int byte, crc, mask;
+
+	i = 0;
+	crc = 0xFFFFFFFF;
+	while (sz-- > 0) {
+		byte = message[i];            // Get next byte.
+		crc = crc ^ byte;
+		for (j = 7; j >= 0; j--) {    // Do eight times.
+			mask = -(crc & 1);
+			crc = (crc >> 1) ^ (0xEDB88320 & mask);
+		}
+		i = i + 1;
+	}
+	return ~crc;
+}
+
+namespace std {
+	template<> struct hash<SV> {
+		size_t operator()(const SV &sv) const {
+			return crc32b((const unsigned char*)&sv[0], sv.size()*sizeof(sv[0]));
+		}
+	};
+};
 
 int main(int argc, char **argv) {
 	int l, m, n;
 	cin >> l >> m >> n;
-	map<vector<int>, int> ttmap;
+	unordered_map<SV, int> ttmap;
 	for (int i=0; i<m; i++) {
-		vector<int> tt(l);
+		SV tt(l);
 		int id;
 		cin >> id;
 		for (auto &t:tt)
@@ -19,7 +48,7 @@ int main(int argc, char **argv) {
 	}
 	int ok=0, bad=0;
 	for (int i=0; i<n; i++) {
-		vector<int> ss(l);
+		SV ss(l);
 		for (auto &s:ss)
 			cin >> s;
 		auto fnd = ttmap.find(ss);
