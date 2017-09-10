65f4457c4bcfa71be1619e9fb61300d4d5d2b3e5
Denis Kokarev <d_kokarev@mail.ru>
2017-09-10 19:04:00 -0400

    acmp 505 solution (f..king iostreams again)

--- c/acmp/acmp505.cpp
+++ c/acmp/acmp505.cpp
@@ -1,66 +1,37 @@
 /* ACMP 505 */
-#include <iostream>
-#include <vector>
 #include <unordered_map>
+#include <string>
+#include <stdio.h>
 
 using namespace std;
 
-using SV = vector<short>;
-
-
-unsigned int crc32b(const unsigned char *message, int sz) {
-	int i, j;
-	unsigned int byte, crc, mask;
-
-	i = 0;
-	crc = 0xFFFFFFFF;
-	while (sz-- > 0) {
-		byte = message[i];            // Get next byte.
-		crc = crc ^ byte;
-		for (j = 7; j >= 0; j--) {    // Do eight times.
-			mask = -(crc & 1);
-			crc = (crc >> 1) ^ (0xEDB88320 & mask);
-		}
-		i = i + 1;
-	}
-	return ~crc;
-}
-
-namespace std {
-	template<> struct hash<SV> {
-		size_t operator()(const SV &sv) const {
-			return crc32b((const unsigned char*)&sv[0], sv.size()*sizeof(sv[0]));
-		}
-	};
-};
+char buf[1048576];
 
 int main(int argc, char **argv) {
 	int l, m, n;
-	cin >> l >> m >> n;
-	unordered_map<SV, int> ttmap;
+	scanf("%d%d%d", &l, &m, &n);
+	unordered_map<string, int> ttmap;
 	for (int i=0; i<m; i++) {
-		SV tt(l);
 		int id;
-		cin >> id;
-		for (auto &t:tt)
-			cin >> t;
-		ttmap[tt] = id;
+		char *s = buf;
+		char nl;
+		scanf("%d %1048576[^\n]%c", &id, s, &nl);
+		ttmap[s] = id;
 	}
 	int ok=0, bad=0;
 	for (int i=0; i<n; i++) {
-		SV ss(l);
-		for (auto &s:ss)
-			cin >> s;
-		auto fnd = ttmap.find(ss);
+		char *s = buf;
+		char nl;
+		scanf("%1048576[^\n]%c", s, &nl);
+		auto fnd = ttmap.find(s);
 		if (fnd != ttmap.end()) {
-			cout << fnd->second;
+			printf("%d\n", fnd->second);
 			ok++;
 		} else {
-			cout << '-';
+			printf("-\n");
 			bad++;
 		}
-		cout << endl;
 	}
-	cout << "OK=" << ok << " BAD=" << bad << endl;
+	printf("OK=%d BAD=%d\n", ok, bad);
 	return 0;
 }
