58ebeeba386b08f88b4d76a77f86b3fb27b17e30
Denis Kokarev <d_kokarev@mail.ru>
2017-09-13 21:31:10 -0400

    acmp 1454 solution

--- c/acmp/acmp1454.cpp
+++ c/acmp/acmp1454.cpp
@@ -35,7 +35,7 @@ struct S {
 int main(int argc, char **argv) {
 	int n, m, k;
 	scanf("%d%d%d", &n, &m, &k);
-	vector<P> pp(k+k);
+	vector<P> pp(k+k);		// stop-> <out/in, pass>
 	for (int i=0; i<k; i++) {
 		int f, t;
 		scanf("%d%d", &f, &t);
@@ -43,12 +43,12 @@ int main(int argc, char **argv) {
 		pp[i*2+1] = {f, DIR_IN, i};
 	}
 	sort(pp.begin(), pp.end());
-	priority_queue<int, vector<int>, greater<int>> free[P_SZ];
+	priority_queue<int, vector<int>, greater<int>> free_place[P_SZ]; // sit/stand -> first free place
 	for (int i=0; i<m-1; i++)
-		free[P_SIT].emplace(i);
+		free_place[P_SIT].emplace(i);
 	for (int i=0; i<m; i++)
-		free[P_STAND].emplace(i);
-	vector<pair<int,int>> p_place(k, make_pair(-1, -1)); // pass-><sit/stand, place>
+		free_place[P_STAND].emplace(i);
+	vector<pair<int,int>> p_place(k, make_pair(-1, -1)); // moving bus pass-><sit/stand, place>
 	vector<S> s_s[P_SZ] = {
 		vector<S>(m, {0, -1}), // sit->stat
 		vector<S>(m, {0, -1})	// stand->stat
@@ -57,36 +57,40 @@ int main(int argc, char **argv) {
 		if (p.dir == DIR_OUT) {
 			int s = p_place[p.pass].first;
 			int place = p_place[p.pass].second;
+			if (s == -1 && place == -1)
+				continue; // this guy never sat
 			assert(s != -1 && place != -1);
 			assert(s_s[s][place].on != -1);
 			s_s[s][place].sbusy += p.stop-s_s[s][place].on;
 			s_s[s][place].on = -1;
-			free[s].push(place);
+			free_place[s].push(place);
 			p_place[p.pass] = make_pair(-1, -1);
 		} else {
 			// IN
 			int s;
 			for (s=P_SIT; s<=P_STAND; s++) {
-				if (free[s].size() > 0) {
-					int place = free[s].top();
-					free[s].pop();
+				if (free_place[s].size() > 0) {
+					int place = free_place[s].top();
+					free_place[s].pop();
 					assert(s_s[s][place].on == -1);
 					s_s[s][place].on = p.stop;
 					p_place[p.pass] = make_pair(s, place);
 					break;
 				}
 			}
-			assert(s<P_SZ);
+			// no room - ignore
+			//assert(s<P_SZ);
 		}
 	}
-	// let us be paranoid
-	assert(free[P_SIT].size() == m-1);
-	assert(free[P_STAND].size() == m);
+	// it's good to be paranoid
+	assert(free_place[P_SIT].size() == m-1);
+	assert(free_place[P_STAND].size() == m);
 	for (int i=0; i<k; i++)
 		assert(p_place[i] == make_pair(-1,-1));
-	for (int i=0; i<m; i++)
-		for (int s=P_SIT; s<=P_STAND; s++)
-			assert(s_s[i][s].on == -1);
+	for (int s=P_SIT; s<=P_STAND; s++)
+		for (int i=0; i<m; i++)
+			assert(s_s[s][i].on == -1);
+	// print result
 	int mni = 0;
 	for (int i=0; i<m; i++) {
 		if (s_s[P_STAND][i].sbusy < s_s[P_STAND][mni].sbusy)
