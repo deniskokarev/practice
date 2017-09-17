1694171dd4e0a34b4160a682a0783ae3a91b23f1
Denis Kokarev <d_kokarev@mail.ru>
2017-09-17 08:15:40 -0400

    acmp 121 optimized solution

--- c/acmp/acmp121.cpp
+++ c/acmp/acmp121.cpp
@@ -11,11 +11,11 @@ int main(int argc, char **argv) {
 	for (int i=1; i<=n; i++)
 		cin >> aa[i];
 	sort(aa+1, aa+n+1);
-	long long rr[n+1];
+	unsigned rr[3];
 	rr[0] = 0;
 	rr[1] = INT_MAX;
-	for (int i=2; i<=n; i++)
-		rr[i] = min(rr[i-2]+aa[i]-aa[i-1], rr[i-1]+aa[i]-aa[i-1]);
-	cout << rr[n] << endl;
+	for (int i=2,ri=2; i<=n; i++,ri++)
+		rr[ri%3] = min(rr[(ri+3-2)%3]+aa[i]-aa[i-1], rr[(ri+3-1)%3]+aa[i]-aa[i-1]);
+	cout << rr[n%3] << endl;
 	return 0;
 }
