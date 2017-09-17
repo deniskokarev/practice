9e57d8ce1238ae08231031387cef9364a18a3985
Denis Kokarev <d_kokarev@mail.ru>
2017-09-17 08:21:38 -0400

    acmp 121 slightly more simplified

--- c/acmp/acmp121.cpp
+++ c/acmp/acmp121.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	int aa[n+1];
-	for (int i=1; i<=n; i++)
-		cin >> aa[i];
-	sort(aa+1, aa+n+1);
+	unsigned aa[n];
+	for (auto &a:aa)
+		cin >> a;
+	sort(aa, aa+n);
 	unsigned rr[3];
 	rr[0] = 0;
 	rr[1] = INT_MAX;
-	for (int i=2,ri=2; i<=n; i++,ri++)
+	for (int i=1,ri=2; i<n; i++,ri++)
 		rr[ri%3] = min(rr[(ri+3-2)%3]+aa[i]-aa[i-1], rr[(ri+3-1)%3]+aa[i]-aa[i-1]);
 	cout << rr[n%3] << endl;
 	return 0;
