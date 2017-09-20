d9afcb00900e372fbe4a00d4b101b63088e7217c
Denis Kokarev <d_kokarev@mail.ru>
2017-09-19 22:11:41 -0400

    acmp 122 solution

--- c/acmp/acmp122.cpp
+++ c/acmp/acmp122.cpp
@@ -15,9 +15,12 @@ int main(int argc, char **argv) {
 	fill(cc, cc+n+1, 0);
 	cc[0] = 0;
 	for (int i=1; i<=n; i++) {
-		int j;
-		for (j=i-1; j>0 && aa[i] <= aa[j]; j--);
-		cc[i] = cc[j]+1;
+		int mxj = i;
+		for (int j=i-1; j>0; j--) {
+			if (aa[j]<aa[i] && cc[j]>cc[mxj])
+				mxj = j;
+		}
+		cc[i] = cc[mxj]+1;
 	}
 	int mx = 0;
 	for (auto c:cc)
