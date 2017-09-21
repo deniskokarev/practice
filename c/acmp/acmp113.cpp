80be1551cddc6d917d33efb89de4e12db53aa8a5
Denis Kokarev <d_kokarev@mail.ru>
2017-09-20 22:40:04 -0400

    acmp113 solution

--- c/acmp/acmp113.cpp
+++ c/acmp/acmp113.cpp
@@ -11,27 +11,21 @@ struct P {
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	vector<vector<P>> mm(n, vector<P>(n, {0, 0}));
-	for (int i=0; i<n; i++) {
+	vector<vector<P>> mm(n+1, vector<P>(n+1, {0, 0}));
+	for (int i=1; i<=n; i++) {
 		string s;
 		cin >> s;
-		for (int j=0; j<n; j++)
-			if (s[j] == '1')
+		for (int j=1; j<=n; j++)
+			if (s[j-1] == '1')
 				mm[i][j].x = mm[i][j].y = 1;
 	}
-	for (int i=0; i<n; i++)
-		for (int j=1; j<n; j++)
-			if (mm[i][j].x)
-				mm[i][j].x += mm[i][j-1].x;
-	for (int i=1; i<n; i++)
-		for (int j=0; j<n; j++)
-			if (mm[i][j].y)
-				mm[i][j].y += mm[i-1][j].y;
 	int a = 0;
-	for (int i=1; i<n; i++) {
-		for (int j=1; j<n; j++) {
-			mm[i][j].x = min(mm[i][j-1].x+1, min(mm[i-1][j-1].x+1, mm[i][j].x));
-			mm[i][j].y = min(mm[i-1][j].y+1, min(mm[i-1][j-1].y+1, mm[i][j].y));
+	for (int i=1; i<=n; i++) {
+		for (int j=1; j<=n; j++) {
+			if (mm[i][j].x) {
+				mm[i][j].x = min(mm[i][j-1].x+1, mm[i-1][j-1].x+1);
+				mm[i][j].y = min(mm[i-1][j].y+1, mm[i-1][j-1].y+1);
+			}
 			a = max(a, min(mm[i][j].x, mm[i][j].y));
 		}
 	}
