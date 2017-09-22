f925438f022cfd16d4138b7f2ceda1a2c8af29aa
Denis Kokarev <d_kokarev@mail.ru>
2017-09-21 22:43:57 -0400

    acmp 122 O(Nlog(N)) solution

--- c/acmp/acmp122.cpp
+++ c/acmp/acmp122.cpp
@@ -1,5 +1,6 @@
 /* ACMP 122 */
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -7,24 +8,18 @@ using namespace std;
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	int aa[n+1];
-	aa[0] = INT_MAX;
-	for (int i=1; i<=n; i++)
-		cin >> aa[i];
-	int cc[n+1];
-	fill(cc, cc+n+1, 0);
-	cc[0] = 0;
-	for (int i=1; i<=n; i++) {
-		int mxj = i;
-		for (int j=i-1; j>0; j--) {
-			if (aa[j]<aa[i] && cc[j]>cc[mxj])
-				mxj = j;
+	vector<int> ll(n+1, INT_MAX);
+	int mx = 2;
+	ll[0] = INT_MIN;
+	for (int i=0; i<n; i++) {
+		int a;
+		cin >> a;
+		auto f = upper_bound(ll.begin(), ll.begin()+mx, a);
+		if (*(f-1) < a) {
+			*f = min(*f, a);
+			mx = max(mx, int(f-ll.begin())+1);
 		}
-		cc[i] = cc[mxj]+1;
 	}
-	int mx = 0;
-	for (auto c:cc)
-		mx = max(mx, c);
-	cout << mx << endl;
+	cout << mx-1 << endl;
 	return 0;
 }
