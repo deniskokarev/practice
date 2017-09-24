f12320cb24936f4b6878a4034ee508fd2a061544
Denis Kokarev <d_kokarev@mail.ru>
2017-09-23 23:07:10 -0400

    acmp 525 optimized solution

--- c/acmp/acmp525.cpp
+++ c/acmp/acmp525.cpp
@@ -8,17 +8,10 @@ using namespace std;
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	int l2n = int(ceil(log2(n+1)));
-	vector<vector<uint64_t>> cc(l2n, vector<uint64_t>(n+1));
-	for (int i=0; i<=n; i++)
-		cc[0][i] = 1;
-	int l2, p2;
-	for (l2=1,p2=2; l2<l2n; l2++,p2*=2) {
-		for (int i=0; i<p2; i++)
-			cc[l2][i] = cc[l2-1][i];
+	vector<uint64_t> cc(n+1, 1);
+	for (int p2=2; p2<=n; p2*=2)
 		for (int i=p2; i<=n; i++)
-			cc[l2][i] = cc[l2][i-p2]+cc[l2-1][i];
-	}
-	cout << cc[l2-1][n] << endl;
+			cc[i] += cc[i-p2];
+	cout << cc[n] << endl;
 	return 0;
 }
