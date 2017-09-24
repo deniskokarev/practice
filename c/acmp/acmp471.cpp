c3f2ab762beb72bdd456979f3e73f06db0756a75
Denis Kokarev <d_kokarev@mail.ru>
2017-09-24 09:41:02 -0400

    acmp 471 still wrong answer 3

--- c/acmp/acmp471.cpp
+++ c/acmp/acmp471.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 // cell->(n, nxt1, nxt2)
-int nn[10][3] = {
+int nn[10][4] = {
 	{2, 4, 6},
-	{2, 8, 6},
+	{2, 6, 8},
 	{2, 7, 9},
 	{2, 4, 8},
-	{2, 3, 9},
+	{3, 0, 3, 9},
 	{0, -1, -1},
-	{2, 1, 7},
+	{3, 0, 1, 7},
 	{2, 2, 6},
 	{2, 1, 3},
 	{2, 2, 4}
@@ -29,7 +29,8 @@ int main(int argc, char **argv) {
 		for (int j=0; j<10; j++)
 			for (int k=1; k<=nn[j][0]; k++)
 				cc[i&1][j] += cc[(i+1)&1][nn[j][k]];
-	uint64_t s = accumulate(cc[(n+1)&1], cc[(n+1)&1]+10, 0); 
+	int r = (n+1)&1;
+	uint64_t s = accumulate(cc[r], cc[r]+10, 0);
 	cout << s << endl;
 	return 0;
 }
