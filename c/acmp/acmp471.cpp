13602811a82eefa4f040c0193cbad324897acb83
Denis Kokarev <d_kokarev@mail.ru>
2017-09-24 09:43:07 -0400

    acmp 471 wrong answer 10

--- c/acmp/acmp471.cpp
+++ c/acmp/acmp471.cpp
@@ -11,7 +11,7 @@ int nn[10][4] = {
 	{2, 7, 9},
 	{2, 4, 8},
 	{3, 0, 3, 9},
-	{0, -1, -1},
+	{0},
 	{3, 0, 1, 7},
 	{2, 2, 6},
 	{2, 1, 3},
@@ -25,10 +25,13 @@ int main(int argc, char **argv) {
 		{0, 1, 1, 1, 1, 1, 1, 1, 0, 1},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	};
-	for (int i=1; i<n; i++)
-		for (int j=0; j<10; j++)
+	for (int i=1; i<n; i++) {
+		for (int j=0; j<10; j++) {
+			cc[i&1][j] = 0;
 			for (int k=1; k<=nn[j][0]; k++)
 				cc[i&1][j] += cc[(i+1)&1][nn[j][k]];
+		}
+	}
 	int r = (n+1)&1;
 	uint64_t s = accumulate(cc[r], cc[r]+10, 0);
 	cout << s << endl;
