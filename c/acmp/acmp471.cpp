d0b258732af360bf891f1ffa0d3164deb5f783fe
Denis Kokarev <d_kokarev@mail.ru>
2017-09-24 10:10:26 -0400

    acmp 471 solution

--- c/acmp/acmp471.cpp
+++ c/acmp/acmp471.cpp
@@ -1,8 +1,53 @@
 /* ACMP 471 */
 #include <iostream>
-#include <numeric>
 
 using namespace std;
+
+/* composition of http://e-maxx.ru/algo/big_integer */
+#include <vector>
+#include <cstdio>
+#include <cassert>
+#include <string>
+
+struct lnum10 {
+	constexpr static int base = 1000*1000*1000;
+	using VEC = std::vector<int>;
+	VEC a;
+	// NB!: this is not == 0
+	lnum10():a() {
+	}
+	lnum10(short n):a() {
+		a.push_back(n);
+	}
+	lnum10(const std::string &s):a() {
+		for (int i=(int)s.length(); i>0; i-=9)
+			if (i < 9)
+				a.push_back(atoi(s.substr (0, i).c_str()));
+			else
+				a.push_back(atoi(s.substr (i-9, 9).c_str()));
+	}
+	std::string to_string() const {
+		char buf[11];
+		buf[10] = 0;
+		snprintf(buf, 10, "%d", a.empty() ? 0 : a.back());
+		std::string s(buf);
+		for (int i=(int)a.size()-2; i>=0; --i) {
+			snprintf(buf, 10, "%09d", a[i]);
+			s.append(buf);
+		}
+		return s;
+	}
+	void operator+=(const lnum10 &b) {
+		int carry = 0;
+		for (size_t i=0; i<std::max(a.size(),b.a.size()) || carry; ++i) {
+			if (i == a.size())
+				a.push_back (0);
+			a[i] += carry + (i < b.a.size() ? b.a[i] : 0);
+			carry = a[i] >= base;
+			if (carry)  a[i] -= base;
+		}
+	}
+};
 
 // cell->(n, nxt1, nxt2, nxt3)
 const static int nn[10][4] = {
@@ -21,7 +66,7 @@ const static int nn[10][4] = {
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	uint64_t cc[2][10] = {
+	lnum10 cc[2][10] = {
 		{0, 1, 1, 1, 1, 1, 1, 1, 0, 1},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	};
@@ -33,7 +78,9 @@ int main(int argc, char **argv) {
 		}
 	}
 	int r = (n+1)&1;
-	uint64_t s = accumulate(cc[r], cc[r]+10, 0ULL);
-	cout << s << endl;
+	lnum10 s(0);
+	for (int i=0; i<10; i++)
+		s += cc[r][i];
+	cout << s.to_string() << endl;
 	return 0;
 }
