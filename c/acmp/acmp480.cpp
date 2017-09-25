92777a396f002a9464a01f4287da245b7b2e30dc
Denis Kokarev <d_kokarev@mail.ru>
2017-09-24 20:11:13 -0400

    acmp 480 solution

--- c/acmp/acmp480.cpp
+++ c/acmp/acmp480.cpp
@@ -12,25 +12,26 @@ struct SC {
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	vector<uint64_t> aa(n+1);
+	vector<uint64_t> aa(n);
 	for (int i=0; i<n; i++)
 		cin >> aa[i];
 	int k;
 	cin >> k;
 	// (p,n,k) -> (sc1,sc2)
-	vector<vector<vector<SC>>> sc(2, vector<vector<SC>>(n+1, vector<SC>(k+1, {{0,0}})));
+	vector<vector<vector<SC>>> sc(2, vector<vector<SC>>(n+1, vector<SC>(k, {{0,0}})));
 	for (int i=n-1; i>=0; i--) {
 		for (int p=0; p<2; p++) {
 			uint64_t s = 0;
-			for (int k1=1; k1<=k && i+k1<n+1; k1++) {
-				s += aa[i+k1-1];
-				sc[p][i][k1] = sc[!p][i+k1][k1];
+			for (int k1=0; k1<k && i+k1<n; k1++) {
+				s += aa[i+k1];
+				sc[p][i][k1] = sc[!p][i+k1+1][k1];
 				sc[p][i][k1].s[p] += s;
+			}
+			for (int k1=1; k1<k; k1++)
 				if (sc[p][i][k1].s[p] < sc[p][i][k1-1].s[p])
 					sc[p][i][k1] = sc[p][i][k1-1];
-			}
 		}
 	}
-	cout << sc[0][0][k].s[0] << endl;
+	cout << sc[0][0][k-1].s[0] << endl;
 	return 0;
 }
