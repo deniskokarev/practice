705d90a08c5690f97063adb512f2d9338f80bb0f
Denis Kokarev <d_kokarev@mail.ru>
2017-09-25 20:30:04 -0400

    acmp 401 wrong answer 8

--- c/acmp/acmp401.cpp
+++ c/acmp/acmp401.cpp
@@ -1,20 +1,26 @@
 /* ACMP 401 */
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-uint64_t cnt(int n, int a) {
-	uint64_t s = 0;
-	if (n>0)
-		for (int i=0; i<=a; i++)
-			s += cnt(n-1, i);
-	else
-		s++;
-	return s;
+uint64_t cnt(int n, int a, vector<vector<uint64_t>> &cache) {
+	if (cache[n][a] == 0) {
+		uint64_t s = 0;
+		if (n>0)
+			for (int i=0; i<=a; i++)
+				s += cnt(n-1, i, cache);
+		else
+			s++;
+		cache[n][a] = s;
+	}
+	return cache[n][a];
 }
 
 int main(int argc, char **argv) {
 	int n, a, b;
 	cin >> n >> a >> b;
-	cout << cnt(n, a) * cnt(n, b) << endl;
+	vector<vector<uint64_t>> cache(n+1, vector<uint64_t>(max(a,b)+1, 0));
+	cout << cnt(n, a, cache) * cnt(n, b, cache) << endl;
 	return 0;
 }
