52f3938dfd791f7eb2886c3a7daf646dfd8006e1
Denis Kokarev <d_kokarev@mail.ru>
2017-09-29 18:43:59 -0400

    acmp 335 solution with plain arrays

--- c/acmp/acmp335.cpp
+++ c/acmp/acmp335.cpp
@@ -2,28 +2,31 @@
 #include <iostream>
 #include <algorithm>
 #include <numeric>
-#include <vector>
 
 using namespace std;
 
 constexpr uint64_t mod = 1e9+9;
 
+#define dim(X)	(sizeof(X)/sizeof(X[0]))
+
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	vector<bool> pp(1000, true);
+	bool pp[1000];
+	fill(pp, pp+dim(pp), true);
 	pp[0] = pp[1] = false;
 	for (int i=2; i<32; i++)
 		for (int j=i+i; j<1000; j+=i)
 			pp[j] = false;
 	for (int i=0; i<100; i++)
 		pp[i] = false;
-	vector<vector<uint64_t>> hh(2, vector<uint64_t>(100));
+	uint64_t hh[2][100];
+	fill(hh[0], hh[0]+dim(hh[0]), 0);
 	for (int i=0; i<1000; i++)
 		if (pp[i])
 			hh[0][(i/10)%100]++;
 	for (int k=1; k<=n-3; k++) {
-		fill(hh[k&1].begin(), hh[k&1].end(), 0);
+		fill(hh[k&1], hh[k&1]+dim(hh[0]), 0);
 		for (int i=1; i<10; i++)
 			for (int j=0; j<100; j++)
 				if (pp[i*100+j])
@@ -31,7 +34,7 @@ int main(int argc, char **argv) {
 		for (auto &h:hh[k&1])
 			h %= mod;
 	}
-	uint64_t ans = accumulate(hh[(n+1)&1].begin(), hh[(n+1)&1].end(), 0ULL);
+	uint64_t ans = accumulate(hh[(n+1)&1], hh[(n+1)&1]+dim(hh[0]), 0ULL);
 	cout << ans % mod << endl;
 	return 0;
 }
