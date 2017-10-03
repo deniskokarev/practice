12d00e02347fd92a5351ca5901b3efea5f9b9f13
Denis Kokarev <d_kokarev@mail.ru>
2017-10-03 18:59:05 -0400

    acmp 16 solution

--- c/acmp/acmp16.cpp
+++ c/acmp/acmp16.cpp
@@ -1,25 +1,19 @@
 /* ACMP 16 */
 #include <iostream>
-
+#include <vector>
+#include <numeric>
 using namespace std;
-
-unsigned cnt(int n, int base) {
-	unsigned c = 0;
-	if (n > 0 && base > 0 && n >= base) {
-		c++;
-		while (n>base) {
-			n--;
-			c += cnt(n, base-1);
-		}
-		for (int i=1; i<n; i++)
-			c += cnt(i, base-i-1);
-	}
-	return c;
-}
 
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	cout << cnt(n, n) << endl;
+	vector<vector<unsigned>> cc(n+1, vector<unsigned>(n+1, 0));
+	for (int i=0; i<=n; i++)
+		cc[i][i] = 1;
+	for (int i=1; i<=n; i++)
+		for (int b=1; b<=n; b++)
+			for (int k=b+1; k+i<=n; k++)
+				cc[k+i][k] += cc[i][b];
+	cout << accumulate(&cc[n][1], &cc[n][n+1], 0ULL) << endl;
 	return 0;
 }
