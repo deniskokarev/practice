db34bc84dc1d21a15d9fe7158291959681f327f8
Denis Kokarev <d_kokarev@mail.ru>
2017-10-07 20:59:58 -0400

    acmp 194 solution

--- c/acmp/acmp194.cpp
+++ c/acmp/acmp194.cpp
@@ -1,36 +1,23 @@
 /* ACMP 194 */
 #include <iostream>
-#include <numeric>
-#include <algorithm>
 
 using namespace std;
 
-#define dim(X) (sizeof(X)/sizeof(X[0]))
+constexpr int sz = 5;
 
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	uint64_t s = 0;
-	if (n < 2) {
-		s = 1;
-	} else if (n < 3) {
-		s = 1;
-	} else if (n < 4) {
-		s = 2;
-	} else {
-		uint64_t cc[2][5] = {{1, 1, 1, 1, 0}};
-		for (int i=4; i<n; i++) {
-			int prev = i&1;
-			int cur = (i+1)&1;
-			fill(cc[cur], cc[cur]+dim(cc[0]), 0);
-			cc[cur][0] = cc[prev][0];
-			cc[cur][1] = cc[prev][0]+cc[prev][4];
-			cc[cur][2] = cc[prev][2] + cc[prev][1];
-			cc[cur][3] = cc[prev][3] + cc[prev][1];
-			cc[cur][4] = cc[cur][4]+1;
-		}
-		s = accumulate(cc[n&1], cc[n&1]+dim(cc[0]), 0ULL);
+	// look for solution on forums ;-(
+	// f[n] = f[n-1]+f[n-3]+1
+	uint64_t ff[sz] = {1, 1, 2, 4, 0};
+	for (int i=4; i<n; i++) {
+		int cur = i%sz;
+		int prev = (i+sz-1)%sz;
+		int prev3 = (i+sz-3)%sz;
+		ff[cur] = ff[prev] + ff[prev3] + 1;
 	}
-	cout << s << endl;
+	int prev = (n+sz-1)%sz;
+	cout << ff[prev] << endl;
 	return 0;
 }
