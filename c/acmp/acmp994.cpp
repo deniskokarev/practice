4256ee36bd09f80a7ce625e451ed10af065b74f2
Denis Kokarev <d_kokarev@mail.ru>
2017-10-09 22:44:46 -0400

    acmp 994 solution with some comments and conventional arrays instead of vectors

--- c/acmp/acmp994.cpp
+++ c/acmp/acmp994.cpp
@@ -1,34 +1,41 @@
 /* ACMP 994 */
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <cassert>
 
 using namespace std;
+
+/* upper left -> bottom right */
+uint16_t ssl[2002][2002];
+/* bottom right -> upper left */
+uint16_t ssr[2002][2002];
+/* technically, ssl and ssr could be reused for mxl and mxr, but we're still going to be under 64MB */
+/* max common substring from the upper left */
+uint16_t mxl[2002][2002];
+/* max common substring from the bottom right */
+uint16_t mxr[2002][2002];
 
 int main(int argc, char **argv) {
 	string s1, s2;
 	cin >> s1 >> s2;
-	/* matrix a-la max substring */
-	vector<vector<uint16_t>> ssl(s1.length()+2, vector<uint16_t>(s2.length()+2, 0));
+	assert(s1.length() < 2001 && s2.length() < 2001);
+	/* filling matrixes a-la max substring in all directions */
 	for (int i=1; i<=s1.length(); i++)
 		for (int j=1; j<=s2.length(); j++)
 			if (s1[i-1] == s2[j-1])
 				ssl[i][j] = ssl[i-1][j-1]+1;
-	vector<vector<uint16_t>> ssr(s1.length()+2, vector<uint16_t>(s2.length()+2, 0));
 	for (int i=s1.length(); i>=1; i--)
 		for (int j=s2.length(); j>=1; j--)
 			if (s1[i-1] == s2[j-1])
 				ssr[i][j] = ssr[i+1][j+1]+1;
- 	vector<vector<uint16_t>> mxl(s1.length()+2, vector<uint16_t>(s2.length()+2, 0));
 	for (int i=1; i<=s1.length(); i++)
 		for (int j=1; j<=s2.length(); j++)
 			mxl[i][j] = max(ssl[i][j], max(mxl[i-1][j], mxl[i][j-1]));
- 	vector<vector<uint16_t>> mxr(s1.length()+2, vector<uint16_t>(s2.length()+2, 0));
 	for (int i=s1.length(); i>=1; i--)
 		for (int j=s2.length(); j>=1; j--)
 			mxr[i][j] = max(ssr[i][j], max(mxr[i+1][j], mxr[i][j+1]));
-	int mx = 0;
-	int mxi = 0;
+	/* find max total of common substring from the left + from the right */
+	int mx = 0;		// total len
+	int mxi = 0;	// which row mx was found
 	int a = 0, al = 0;
 	int b = 0, bl = 0;
 	for (int i=1; i<=s1.length(); i++) {
