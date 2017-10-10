e5af3e124bb23cdb76945fe0e5507ca9c80ded69
Denis Kokarev <d_kokarev@mail.ru>
2017-10-09 20:19:24 -0400

    acmp 994 wrong answer 6

--- c/acmp/acmp994.cpp
+++ c/acmp/acmp994.cpp
@@ -6,62 +6,49 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	string s1, s2;
-	cin >> s1 >> s2;
+	string sa, sb;
+	cin >> sa >> sb;
+	string &s1 = (sa.length() < sb.length()) ? sa : sb;
+	string &s2 = (sa.length() >= sb.length()) ? sa : sb;
 	/* matrix a-la max substring */
-	/* left->right */
 	vector<vector<uint16_t>> ss(s1.length()+2, vector<uint16_t>(s2.length()+2, 0));
 	for (int i=1; i<=s1.length(); i++)
 		for (int j=1; j<=s2.length(); j++)
 			if (s1[i-1] == s2[j-1])
 				ss[i][j] = ss[i-1][j-1]+1;
-	vector<uint16_t> mxl(s1.length()+2, 0);
-	/* max from left */
-	for (int i=1; i<=s1.length(); i++)
-		for (int j=1; j<=s2.length(); j++)
-			mxl[i] = max(mxl[i], ss[i][j]);
-	for (int i=1; i<=s1.length(); i++)
-		mxl[i] = max(mxl[i], mxl[i-1]);
-	/* right->left */
-	for (auto &i : ss)
-		fill(i.begin(), i.end(), 0);
-	for (int i=s1.length(); i>=1; i--)
+	for (int i=s1.length(); i>=0; i--)
 		for (int j=s2.length(); j>=1; j--)
-			if (s1[i-1] == s2[j-1])
-				ss[i][j] = ss[i+1][j+1]+1;
-	/* max from right */
-	vector<uint16_t> mxr(s1.length()+2, 0);
-	for (int i=1; i<=s1.length(); i++)
-		for (int j=1; j<=s2.length(); j++)
-			mxr[i] = max(mxr[i], ss[i][j]);
-	for (int i=s1.length(); i>=1; i--)
-		mxr[i] = max(mxr[i], mxr[i+1]);
-	/* find best max_left + max_right */
-	uint16_t mx = 0;
-	int mxi = 0;
-	for (int i=1; i<=s1.length(); i++) {
-		if (mx < mxl[i]+mxr[i+1]) {
-			mxi = i;
-			mx = mxl[i]+mxr[i+1];
+			if (ss[i][j] != 0)
+				ss[i][j] = max(ss[i][j], ss[i+1][j+1]);
+	int mx = 0;
+	int ai = 0, al = 0;
+	int bi = 0, bl = 0;
+	for (int j=1; j<=s2.length(); j++) {
+		/* max from left */
+		vector<uint16_t> mxl(s1.length()+2, 0);
+		for (int i=1; i<=s1.length(); i++)
+			mxl[i] = max(ss[i][j], mxl[i-1]);
+		/* max from right */
+		vector<uint16_t> mxr(s1.length()+2, 0);
+		for (int i=s1.length(); i>=1; i--)
+			mxr[i] = max(ss[i][j], mxr[i+1]);
+		for (int i=1; i<s1.length(); i++) {
+			if (mx < mxl[i]+mxr[i+1]) {
+				mx = mxl[i]+mxr[i+1];
+				ai = i-1;
+				al = mxl[i];
+			}
 		}
-	}
-	/* map it back to subtrings */
-	string a;
-	for (int i=1; i<=s1.length(); i++) {
-		if (mxl[mxi] == mxl[i]) {
-			a = s1.substr(i-mxl[i], mxl[i]);
-			break;
-		}
-	}
-	string b;
-	for (int i=s1.length(); i>=1; i--) {
-		if (mxr[mxi+1] == mxr[i+1]) {
-			b = s1.substr(i, mxr[i+1]);
-			break;
+		for (int i=s1.length(); i>ai+mx; i--) {
+			if (mxr[ai+1] == mxr[i]) {
+				bi = i-mxr[i];
+				bl = mxr[i];
+				break;
+			}
 		}
 	}
 	//cerr << mx << endl; // debug
-	cout << a << endl;
-	cout << b << endl;
+	cout << s1.substr(ai, al) << endl;
+	cout << s1.substr(bi, bl) << endl;
 	return 0;
 }
