24037ea7980124ebaa652655c29e0a4054c25e78
Denis Kokarev <d_kokarev@mail.ru>
2017-10-15 18:08:49 -0400

    acmp 77 solution with 32 bits and comments

--- c/acmp/acmp77.cpp
+++ c/acmp/acmp77.cpp
@@ -19,10 +19,14 @@ uint64_t choose(int n, int k) {
 	return res;
 }
 
-uint64_t f(const bitset<64> nn, int k, int b) {
+uint64_t cnt(const bitset<32> nn, int k, int b) {
 	uint64_t s = 0;
 	if (k < b) {
 		int zm = 0;
+		// remove hi 1 and consider seq of 1s of diminishing length
+		for (int i=k; i<b-1; i++)
+			s += choose(i, k);
+		// keep hi 1 and try to flip lower 1s
 		for (int i=b-2; i>=0; i--) {
 			if (nn[i]) {
 				if (zm <= k && i >= k-zm-1)
@@ -33,23 +37,22 @@ uint64_t f(const bitset<64> nn, int k, int b) {
 				zm++;
 			}
 		}
+		// special case of hi 1 followed by k 0s
 		if (zm == k)
 			s++;
-		for (int i=k; i<b-1; i++)
-			s += choose(i, k);
 	}
 	return s;
 }
 
 int main(int argc, char **argv) {
-	uint64_t n;
+	uint32_t n;
 	int k;
 	cin >> n >> k;
-	bitset<64> nn(n);
+	bitset<32> nn(n);
 	int b = 0;
-	for (int i=0; i<64; i++)
+	for (int i=0; i<32; i++)
 		if (nn[i])
 			b = i+1;
-	cout << f(nn, k, b) << endl;
+	cout << cnt(nn, k, b) << endl;
 	return 0;
 }
