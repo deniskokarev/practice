f964b3daff9a43f95ace2f07b6c709acc186e522
Denis Kokarev <dk1942@att.com>
2017-10-23 14:52:24 -0400

    acmp 671 solution

--- c/acmp/acmp671.cpp
+++ c/acmp/acmp671.cpp
@@ -4,45 +4,41 @@
 
 using namespace std;
 
-uint32_t f(const int *d, int p) {
-	uint32_t s = 0;
+uint64_t f(const string &d, int p) {
+	uint64_t s = 0;
 	if (p > 0) {
-		if (d[p] > 7)
-			s = 2UL << p;
-		if (d[p] == 7)
-			s = f(d, p-1) + (1UL << p);
-		else if (d[p] > 4)
-			s = (1UL << p);
-		else if (d[p] == 4)
+		if (d[p] > '7')
+			s = 2ULL << p;
+		else if (d[p] == '7')
+			s = f(d, p-1) + (1ULL << p);
+		else if (d[p] > '4')
+			s = (1ULL << p);
+		else if (d[p] == '4')
 			s = f(d, p-1);
 	} else if (p == 0) {
-		if (d[p] >= 7)
+		if (d[p] >= '7')
 			s = 2;
-		else if (d[p] >= 4)
+		else if (d[p] >= '4')
 			s = 1;
 	}
 	return s;
 }
 
-uint32_t F(const int *d, int p) {
-	uint32_t s = f(d, p);
+uint64_t F(const string &d, int p) {
+	uint64_t s = f(d, p);
 	while (p > 0) {
-		s += 1UL << p;
+		s += 1ULL << p;
 		p--;
 	}
 	return s;
 }
 
 int main(int argc, char **argv) {
-	uint32_t n;
-	cin >> n;
-	int d[20] = {0};
-	int l = 0;
-	while (n>0) {
-		d[l++] = n%10;
-		n /= 10;
-	}
-	uint64_t s = F(d, l-1);
-	cout << s << endl;
+	string sn;
+	cin >> sn;
+	int l = sn.length();
+	reverse(sn.begin(), sn.end());
+	uint64_t a = F(sn, l-1);
+	cout << a << endl;
 	return 0;
 }
