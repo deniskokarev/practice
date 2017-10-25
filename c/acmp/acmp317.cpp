003bb093a8b05e9a5dedcc0c5cf96c32648e6237
Denis Kokarev <dk1942@att.com>
2017-10-25 10:53:04 -0400

    acmp 317 solution without unnecessary optimization

--- c/acmp/acmp317.cpp
+++ c/acmp/acmp317.cpp
@@ -1,17 +1,11 @@
 /* ACMP 317 */
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	int w;
-	int xyz[3]; // wonna make the outer loops the smallest
-	cin >> xyz[0] >> xyz[1] >> xyz[2] >> w;
-	sort(xyz, xyz+3);
-	int z = xyz[0];
-	int y = xyz[1];
-	int x = xyz[2];
+	int x, y, z, w;
+	cin >> x >> y >> z >> w;
 	uint64_t s = 0;
 	for (int xs=0,xi=w/x; xi>=0; xi--, xs+=x)
 		for (int ys=0,yi=(w-xs)/y; yi>=0; yi--, ys+=y)
