e07fd22582ad44365452a33ce2075d211922c3d2
Denis Kokarev <dk1942@att.com>
2017-10-25 11:25:36 -0400

    acmp 24 solution

--- c/acmp/acmp24.cpp
+++ c/acmp/acmp24.cpp
@@ -7,15 +7,13 @@ int main(int argc, char **argv) {
 	int n, m;
 	cin >> n >> m;
 	uint64_t s = 0;
-	if (m>0) {
-		for (int d=0; d<n; d++) {
-			if ((m-1)*d+m > n)
-				break;
+	if (m>1)
+		for (int d=0; (m-1)*d+m <= n; d++)
 			s += (n-(m-1)*d-m)+1;
-		}
-	} else {
+	else if (m == 1)
+		s = n;
+	else
 		s = 1;
-	}
 	cout << s << endl;
 	return 0;
 }
