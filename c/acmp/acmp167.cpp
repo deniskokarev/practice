c9c30e919da4f4a38e3f01d13c91c7a29345c54a
Denis Kokarev <d_kokarev@mail.ru>
2017-10-24 20:11:21 -0400

    acmp 167 solution

--- c/acmp/acmp167.cpp
+++ c/acmp/acmp167.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	uint64_t n;
+	int64_t n;
 	cin >> n;
-	uint64_t s = 0;
+	int64_t s = 0;
 	// iterate over t+1 - triangle sizes
 	// number of up triangles = u1*u2/2
 	// number of down triangles = d1*d2/2
-	uint64_t u1 = n+1;
-	uint64_t u2 = n;
-	uint64_t d1 = n;
-	uint64_t d2 = n-1;
+	int64_t u1 = n+1;
+	int64_t u2 = n;
+	int64_t d1 = n;
+	int64_t d2 = n-1;
 	for (int t=0; t<n; t++) {
 		s += u1*u2/2;
 		u1--;
 		u2--;
-		if (d1>d2) {
+		if (d2>0) {
 			s += d1*d2/2;
 			d1 -= 2;
 			d2 -= 2;
