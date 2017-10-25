f71a6c3d762facbcd2b9c8c8c9b8bb6e48c741b1
Denis Kokarev <dk1942@att.com>
2017-10-25 17:06:52 -0400

    acmp 1098 still timeout 13

--- c/acmp/acmp1098.cpp
+++ c/acmp/acmp1098.cpp
@@ -8,71 +8,38 @@ using namespace std;
 using BRD = bitset<10>[10];
 using MSK = BRD[10][10];
 
-enum {
-	R = 0,
-	Q = 1
-};
+static MSK mq, mr;
 
-struct Mask {
-	MSK msk[2];
-	Mask(int n) {
-		for (int p=0; p<2; p++)
-			for (int i=0; i<n; i++)
-				for (int j=0; j<n; j++)
-					for (int i1=0; i1<n; i1++)
-						msk[p][i][j][i1] = bitset<10>(0);
-		// bottom part of R piece attack mask
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<n; j++) {
-				BRD &b = msk[R][i][j];
-				for (int r=0; i+r<n; r++)
-					b[i+r][j] = true;
-			}
-		}
-		// bottom part of Q piece attack mask
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<n; j++) {
-				BRD &b = msk[Q][i][j];
-				for (int r=0; i+r<n; r++) {
-					b[i+r][j] = true;
-					if (j-r>=0)
-						b[i+r][j-r] = true;
-					if (j+r<n)
-						b[i+r][j+r] = true;
-				}
-			}
-		}
-	}
-};
+int cnt;
 
 // try to place k[0] and k[1] pieces on brd starting row r
-static inline void place(const BRD brd[2], int r, int n, int k[2], const MSK msk[2], int &cnt) {
-	if (k[R] == 0 && k[Q] == 0) {
+static inline void place(const BRD &bq, const BRD &br, int r, int n, int kq, int kr) {
+	if (kq == 0 && kr == 0) {
 		cnt++;
 	} else {
-		for (int i=r; i<n-k[R]-k[Q]+1; i++) {
+		for (int i=r; i<n-kq-kr+1; i++) {
 			for (int j=0; j<n; j++) {
-				if (!brd[R][i][j]) {
-					BRD cbrd[2];
-					copy(brd[R], brd[R]+n, cbrd[R]);
-					copy(brd[Q], brd[Q]+n, cbrd[Q]);
-					if (k[R] > 0) {
-						k[R]--;
+				if (!br[i][j]) {
+					BRD cbq, cbr;
+					copy(bq, bq+n, cbq);
+					copy(br, br+n, cbr);
+					if (kr > 0) {
+						kr--;
 						for (int i1=i+1; i1<n; i1++) {
-							cbrd[R][i1] |= msk[R][i][j][i1];
-							cbrd[Q][i1] |= msk[Q][i][j][i1];
+							cbr[i1] |= mr[i][j][i1];
+							cbq[i1] |= mq[i][j][i1];
 						}
-						place(cbrd, i+1, n, k, msk, cnt);
-						k[R]++;
+						place(cbq, cbr, i+1, n, kq, kr);
+						kr++;
 					}
-					if (!brd[Q][i][j] && k[Q] > 0) {
-						k[Q]--;
+					if (!bq[i][j] && kq > 0) {
+						kq--;
 						for (int i1=i+1; i1<n; i1++) {
-							cbrd[R][i1] |= msk[Q][i][j][i1];
-							cbrd[Q][i1] |= msk[Q][i][j][i1];
+							cbr[i1] |= mq[i][j][i1];
+							cbq[i1] |= mq[i][j][i1];
 						}
-						place(cbrd, i+1, n, k, msk, cnt);
-						k[Q]++;
+						place(cbq, cbr, i+1, n, kq, kr);
+						kq++;
 					}
 				}
 			}
@@ -80,17 +47,36 @@ static inline void place(const BRD brd[2], int r, int n, int k[2], const MSK msk
 	}
 }
 
+static BRD brdq, brdr;
+
 int main(int argc, char **argv) {
-	int n, k[2];
-	cin >> n >> k[Q] >> k[R];
+	int n, kq, kr;
+	cin >> n >> kq >> kr;
 	assert(n<11);
-	int cnt = 0;
-	BRD brd[2];
-	for (int p=0; p<2; p++)
-		for (int i=0; i<n; i++)
-			brd[p][i] = bitset<10>(0);
-	Mask m(n);
-	place(brd, 0, n, k, m.msk, cnt);
+
+	// bottom part of R piece attack mask
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+			BRD &b = mr[i][j];
+			for (int r=0; i+r<n; r++)
+				b[i+r][j] = true;
+		}
+	}
+	// bottom part of Q piece attack mask
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+			BRD &b = mq[i][j];
+			for (int r=0; i+r<n; r++) {
+				b[i+r][j] = true;
+				if (j-r>=0)
+					b[i+r][j-r] = true;
+				if (j+r<n)
+					b[i+r][j+r] = true;
+			}
+		}
+	}
+
+	place(brdq, brdr, 0, n, kq, kr);
 	cout << cnt << endl;
 	return 0;
 }
