09266b67f6e490bef3a2a1a0d55ec6f01af0f19f
Denis Kokarev <dk1942@att.com>
2017-10-25 11:19:59 -0400

    acmp 24 wrong answer 9

--- c/acmp/acmp24.cpp
+++ c/acmp/acmp24.cpp
@@ -7,10 +7,14 @@ int main(int argc, char **argv) {
 	int n, m;
 	cin >> n >> m;
 	uint64_t s = 0;
-	for (int d=0; d<n; d++) {
-		if ((m-1)*d+m > n)
-			break;
-		s += (n-(m-1)*d-m)+1;
+	if (m>0) {
+		for (int d=0; d<n; d++) {
+			if ((m-1)*d+m > n)
+				break;
+			s += (n-(m-1)*d-m)+1;
+		}
+	} else {
+		s = 1;
 	}
 	cout << s << endl;
 	return 0;
