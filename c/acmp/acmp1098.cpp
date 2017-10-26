6e309c0b7905f9a8b1436aae1d6cdf7efe3b09a2
Denis Kokarev <d_kokarev@mail.ru>
2017-10-25 22:09:13 -0400

    acmp 1098 still timeout 13

--- c/acmp/acmp1098.cpp
+++ c/acmp/acmp1098.cpp
@@ -1,80 +1,64 @@
 /* ACMP 1098 */
 #include <iostream>
-#include <algorithm>
 #include <cassert>
 
 using namespace std;
 
 struct Brd {
-	int n;
-	// free column
-	int c[10];
 	// left and right diagonals where one can place Rook
-	int rdl[20];
-	int rdr[20];
+	uint64_t c_rd;
 	// left and right diagonals where one can place Queen
-	int qdl[20];
-	int qdr[20];
-	Brd(int _n):n(_n) {
-		assert(n<11);
-		fill(c, c+n, 0);
-		fill(rdl, rdl+n+n, 0);
-		fill(rdr, rdr+n+n, 0);
-		fill(qdl, qdl+n+n, 0);
-		fill(qdr, qdr+n+n, 0);
-	}
+	uint64_t qd;
 };
 
+static int cnt;
+static int n;
+static Brd brd;
+static int kq;
+static int kr;
+
 // try to place k[0] and k[1] pieces on brd starting row r
-static inline int place(Brd &brd, int r, int kq, int kr) {
-	int cnt = 0;
+static void place(int r) {
 	if (kq == 0 && kr == 0) {
 		cnt++;
 	} else {
-		int n = brd.n;
-		for (int i=r; i<n-kq-kr+1; i++) {
-			for (int j=0; j<n; j++) {
-				int dl = i+j;
-				int dr = n-i-1+j;
-				if (!brd.c[j] && !brd.rdl[dl] && !brd.rdr[dr]) {
-					brd.c[j] = 1;
+		int im = n-kq-kr+1;
+		for (int i=r; i<im; i++) {
+			const uint64_t dl = 1<<(i+n+n);
+			const uint64_t dr = 1<<(n-i-1);
+			uint64_t c = 1ULL<<40;
+			uint64_t dmsk = dl|dr|c;
+			for (int j=0; j<n; j++,dmsk<<=1,c<<=1) {
+				if (!(brd.c_rd & dmsk)) {
+					brd.c_rd ^= c;
 					if (kr > 0) {
 						kr--;
-						int oqdl = brd.qdl[dl];
-						int oqdr = brd.qdr[dr];
-						brd.qdl[dl] = 1;
-						brd.qdr[dr] = 1;
-						cnt += place(brd, i+1, kq, kr);
-						brd.qdl[dl] = oqdl;
-						brd.qdr[dr] = oqdr;
+						const auto oqd = brd.qd;
+						brd.qd |= dmsk;
+						place(i+1);
+						brd.qd = oqd;
 						kr++;
 					}
-					if (!brd.qdl[dl] && !brd.qdr[dr] && kq > 0) {
+					brd.c_rd ^= c;
+					if (!(brd.qd & dmsk) && kq > 0) {
 						kq--;
-						brd.rdl[dl] = 1;
-						brd.rdr[dr] = 1;
-						brd.qdl[dl] = 1;
-						brd.qdr[dr] = 1;
-						cnt += place(brd, i+1, kq, kr);
-						brd.rdl[dl] = 0;
-						brd.rdr[dr] = 0;
-						brd.qdl[dl] = 0;
-						brd.qdr[dr] = 0;
+						brd.c_rd ^= dmsk;
+						brd.qd ^= dmsk;
+						place(i+1);
+						brd.c_rd ^= dmsk;
+						brd.qd ^= dmsk;
 						kq++;
 					}
-					brd.c[j] = 0;
 				}
 			}
 		}
 	}
-	return cnt;
 }
 
 int main(int argc, char **argv) {
-	int n, kq, kr;
 	cin >> n >> kq >> kr;
-	Brd brd(n);
-	int cnt = place(brd, 0, kq, kr);
+	assert(n<11);
+	place(0);
 	cout << cnt << endl;
 	return 0;
 }
