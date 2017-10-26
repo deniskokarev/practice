0dfcf1f58d56665be3f053b1f2fbfbdea55f52b9
Denis Kokarev <d_kokarev@mail.ru>
2017-10-26 14:38:28 -0400

    acmp 553 simpler solution

--- c/acmp/acmp553.cpp
+++ c/acmp/acmp553.cpp
@@ -1,6 +1,5 @@
 /* ACMP 553 */
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -8,8 +7,7 @@ struct B {
 	unsigned m, k;
 	unsigned score;
 	B operator+(const B&b) const {
-		B r = {m, b.k, m * b.k + score + b.score};
-		return r;
+		return B {m, b.k, m * b.k + score + b.score};
 	}
 	bool operator<(const B &b) const {
 		return score < b.score;
@@ -19,20 +17,17 @@ struct B {
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	vector<vector<B>> bb(n+1, vector<B>(n+1));
+	B bb[n][n];	// no need to clean
 	for (int i=0; i<n; i++) {
 		unsigned m, k;
 		cin >> m >> k;
 		bb[i][i] = B {m, k, 0};
 	}
 	for (int l=1; l<n; l++) {
-		for (int i=0; i<=n-l; i++) {
+		for (int i=0; i<n-l; i++) {
 			B best = bb[i][i]+bb[i+1][i+l];
-			for (int j=i+1; j<i+l; j++) {
-				B curr = bb[i][j]+bb[j+1][i+l];
-				if (curr < best)
-					best = curr;
-			}
+			for (int j=i+1; j<i+l; j++)
+				best = min(best, bb[i][j]+bb[j+1][i+l]);
 			bb[i][i+l] = best;
 		}
 	}
