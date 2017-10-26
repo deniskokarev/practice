862e49f894227a927c3bc624fc06db22e88eb52a
Denis Kokarev <d_kokarev@mail.ru>
2017-10-26 15:18:01 -0400

    acmp 822 solution

--- c/acmp/acmp822.cpp
+++ c/acmp/acmp822.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 struct V {
-	int x, y;
+	int64_t x, y;
 	void operator -=(const V &v) {
 		x -= v.x;
 		y -= v.y;
@@ -17,7 +17,10 @@ int main(int argc, char **argv) {
 	cin >> p[0].x >> p[0].y >> p[1].x >> p[1].y >> p[2].x >> p[2].y;
 	p[1] -= p[0];
 	p[2] -= p[0];
-	double a = fabs((p[1].x*p[2].y - p[2].x*p[1].y)/2.0);
-	cout << a << endl;
+	int64_t a2 = abs(p[1].x*p[2].y - p[2].x*p[1].y);
+	if ((a2 & 1) == 0)
+		cout << a2/2 << endl;
+	else
+		cout << a2/2 << ".5" << endl;
 	return 0;
 }
