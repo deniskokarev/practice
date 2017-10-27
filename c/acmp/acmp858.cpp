2e24dcf4f8062ee9281ae4e457c1e805a575c264
Denis Kokarev <d_kokarev@mail.ru>
2017-10-27 19:20:19 -0400

    acmp 858 solution

--- c/acmp/acmp858.cpp
+++ c/acmp/acmp858.cpp
@@ -1,4 +1,4 @@
-/* ACMP 858 */
+/* ACMatP 858 */
 #include <iostream>
 #include <memory>
 #include <cassert>
@@ -31,28 +31,18 @@ template<typename I, typename F> void __heaps_perm_r(const I &beg, const I &end,
 
 using namespace std;
 
-template<typename N> struct M {
+template<typename N> struct Mat {
 	using R = N*;
 	int rows, cols, sz;
 	unique_ptr<N[]> uxp;
 	N *x;
-	M(int _r, int _c):rows(_r),cols(_c),sz(rows*cols),uxp(new N[sz]),x(uxp.get()){}
+	Mat(int _r, int _c):rows(_r),cols(_c),sz(rows*cols),uxp(new N[sz]),x(uxp.get()){}
 	R operator[](int r) const {
 		return R {x+r*cols};
 	}
-	void operator -=(const M &b) {
-		assert(rows == b.rows && cols == b.cols);
-		for (int i=0; i<sz; i++)
-			x[i] -= b.x[i];
-	}
-	void operator +=(const M &b) {
-		assert(rows == b.rows && cols == b.cols);
-		for (int i=0; i<sz; i++)
-			x[i] += b.x[i];
-	}
 	N det() const {
 		assert(rows == cols);
-		const M &m = *this;
+		const Mat &m = *this;
 		int pp[rows];
 		for (int i=0; i<rows; i++)
 			pp[i] = i;
@@ -70,23 +60,9 @@ template<typename N> struct M {
 		});
 		return sum;
 	}
-	M mul(const M &b) const {
-		const M &a = *this;
-		assert(a.cols == b.rows);
-		M res(a.rows, b.cols);
-		for (int r1=0; r1<a.rows; r1++) {
-			for (int c2=0; c2<b.cols; c2++) {
-				N s = 0;
-				for (int c1=0; c1<a.cols; c1++)
-					s += a[r1][c1]*b[c1][c2];
-				res[r1][c2] = s;
-			}
-		}
-		return res;
-	}
-	M adj(int r, int c) const {
-		const M &m = *this;
-		M res(rows-1, cols-1);
+	Mat adj(int r, int c) const {
+		const Mat &m = *this;
+		Mat res(rows-1, cols-1);
 		for (int di=0,si=0; si<rows; si++) {
 			if (si == r)
 				continue;
@@ -102,30 +78,17 @@ template<typename N> struct M {
 	}
 };
 
-template<typename N> struct V2 : public M<N> {
-	V2():M<N>(1,2){}
-    N &operator[](int _c) const {
-		return ((M<N>*)this)->operator[](0)[_c];
-	}
-	N cross(const V2 &b) const {
-		const V2 &a = *this;
-		return a[0]*b[1]-b[0]*a[1];
-	}	
-};
-
 int main(int argc, char **argv) {
 	// https://math.stackexchange.com/questions/901819/direct-formula-for-area-of-a-triangle-formed-by-three-lines-given-their-equatio
-	M<int64_t> m(3,3);
+	Mat<int64_t> m(3,3);
 	for (int i=0; i<3; i++)
 		for (int j=0; j<3; j++)
 			cin >> m[i][j];
 	int64_t det = m.det();
 	if (det != 0) {
 		double c[3];
-		for (int i=0; i<3; i++) {
-			c[i] = m.adj(i, 2).det();
-			c[i] *= (i&1)?1:-1;
-		}
+		for (int i=0; i<3; i++)
+			c[i] = abs(m.adj(i, 2).det());
 		double det2 = det*det;
 		double res = det2/2/c[0]/c[1]/c[2];
 		cout << res << endl;
