06f01986968aad2f470739a62ba72489bf9ee9da
Denis Kokarev <d_kokarev@mail.ru>
2017-10-29 12:54:56 -0400

    acmp 390 solution

--- c/acmp/acmp390.cpp
+++ c/acmp/acmp390.cpp
@@ -1,13 +1,12 @@
 /* ACMP 390 */
 #include <cstdio>
 #include <cmath>
-#include <cinttypes>
 
 using namespace std;
 
 struct P {
-	int64_t x, y;
-	int64_t cross(const P &b) const {
+	int x, y;
+	int cross(const P &b) const {
 		return x*b.y-y*b.x;
 	}
 	P operator-(const P&b) const {
@@ -16,29 +15,24 @@ struct P {
 		c.y -= b.y;
 		return c;
 	}
-	long double len() const {
-		return sqrtl(x*x+y*y);
+	double len() const {
+		return sqrt(x*x+y*y);
 	}
 };
 
 int main(int argc, char **argv) {
 	P pp[3];
 	P c;
-	for (auto &p:pp) {
-		scanf("%lld%lld", &p.x, &p.y);
-		p.x *= 1e3;
-		p.y *= 1e3;
-	}
-	scanf("%lld%lld", &c.x, &c.y);
-	c.x *= 1e3;
-	c.y *= 1e3;
-	long double mn = 1e17;
-	for (int i=0; i<2; i++) {
+	for (auto &p:pp)
+		scanf("%d%d", &p.x, &p.y);
+	scanf("%d%d", &c.x, &c.y);
+	double mn = 1e17;
+	for (int i=0; i<3; i++) {
 		int np = (i+1)%3;
-		long double s2 = fabsl((long double)((pp[i]-c).cross(pp[np]-c)));
-		long double h = s2/(pp[np]-pp[i]).len();
+		double s2 = fabs((pp[i]-c).cross(pp[np]-c));
+		double h = s2/(pp[np]-pp[i]).len();
 		mn = (mn>h)?h:mn;
 	}
-	printf("%.7f\n", double(mn/1e3));
+	printf("%.7f\n", mn);
 	return 0;
 }
