46d0a99047d5569351af6b9e3d43c86a428c985b
Denis Kokarev <d_kokarev@mail.ru>
2017-10-29 12:24:00 -0400

    acmp 390 wrong answer 6

--- c/acmp/acmp390.cpp
+++ c/acmp/acmp390.cpp
@@ -1,6 +1,5 @@
 /* ACMP 390 */
-#include <iostream>
-#include <iomanip>
+#include <cstdio>
 #include <cmath>
 
 using namespace std;
@@ -25,16 +24,15 @@ int main(int argc, char **argv) {
 	P pp[3];
 	P c;
 	for (auto &p:pp)
-		cin >> p.x >> p.y;
-	cin >> c.x >> c.y;
+		scanf("%d%d", &p.x, &p.y);
+	scanf("%d%d", &c.x, &c.y);
 	double mn = 1e17;
 	for (int i=0; i<2; i++) {
 		int np = (i+1)%3;
 		double s2 = fabs((pp[i]-c).cross(pp[np]-c));
 		double h = s2/(pp[np]-pp[i]).len();
-		mn = min(mn, h);
+		mn = (mn>h)?h:mn;
 	}
-	cout << setprecision(9);
-	cout << mn << endl;
+	printf("%.7f\n", mn);
 	return 0;
 }
