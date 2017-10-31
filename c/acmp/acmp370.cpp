46263f732b59d1398877c5c85ebe853213a2281b
Denis Kokarev <dk1942@att.com>
2017-10-31 15:10:15 -0400

    acmo 370, hmm, still timeout 7 ???

--- c/acmp/acmp370.cpp
+++ c/acmp/acmp370.cpp
@@ -10,32 +10,14 @@ struct P {
 	double cross(const P &b) const {
 		return x*b.y - y*b.x;
 	}
-	void operator += (const P &b) {
-		x += b.x;
-		y += b.y;
-	}
-	void operator -= (const P &b) {
-		x -= b.x;
-		y -= b.y;
-	}
-	void operator /= (double b) {
-		x /= b;
-		y /= b;
-	}
 };
 
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
 	P vv[n];
-	P o {0, 0};
-	for (auto &v:vv) {
-		cin >> v.x >> v.y;
-		o += v;
-	}
-	o /= n;
 	for (auto &v:vv)
-		v -= o;
+		cin >> v.x >> v.y;
 	double s = 0;
 	for (int i=0; i<n; i++) {
 		int j = (i+1)%n;
