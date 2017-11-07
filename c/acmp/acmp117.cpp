7f72fc7dd92358d409da822eb37b70e019530da4
Denis Kokarev <dk1942@att.com>
2017-11-07 11:17:37 -0500

    acmp 117 solution with proper convex hull implementation

--- c/acmp/acmp117.cpp
+++ c/acmp/acmp117.cpp
@@ -6,88 +6,71 @@
 
 using namespace std;
 
-// rational number to represent accuratre angle
-struct Q {
-	int64_t n, d;
-	Q(int _n, int _d) {
-		if (d<0) {
-			n = -_n;
-			d = -_d;
-		} else {
-			n = _n;
-			d = _d;
-		}
-	}
-	bool operator==(const Q &b) const {
-		return n*b.d == b.n*d;
-	}
-	bool operator<(const Q &b) const {
-		return n*b.d < b.n*d;
-	}
-};
-
-int sign(int n) {
-	return (n>0) - (n<0);
-}
-
 struct P {
-	int x, y;
-	bool operator==(const P &b) const {
-		return x==b.x && y == b.y;
+	int64_t x, y;
+	int64_t len2() const {
+		return x*x + y*y;
 	}
-	bool operator<(const P &b) const {
-		return x*x+y*y < b.x*b.x+b.y*b.y;
+	P operator-(const P &b) const {
+		P res(*this);
+		res.x -= b.x;
+		res.y -= b.y;
+		return res;
 	}
-	int cross(const P &b) const {
+	int64_t cross(const P &b) const {
 		return x*b.y - y*b.x;
 	}
 };
 
-// signed sin^2 between a,b and b,c
-Q sin2(const P &a, const P &b, const P &c) {
-	int x1 = b.x-a.x, y1 = b.y-a.y;
-	int x2 = c.x-b.x, y2 = c.y-b.y;
-	int lab2 = x1*x1+y1*y1;
-	int lbc2 = x2*x2+y2*y2;
-	int cr = x1*y2-y1*x2;
-	return Q(cr*cr*sign(cr), lab2*lbc2);
+int sign(int64_t n) {
+	return (n>0) - (n<0);
 }
 
 // a->b->c turns left = +1, right = -1
 int orientation(const P &a, const P &b, const P &c) {
-	int x1 = b.x-a.x, y1 = b.y-a.y;
-	int x2 = c.x-b.x, y2 = c.y-b.y;
-	int cr = x1*y2-y1*x2;
+	int64_t cr = (b-a).cross(c-b);
 	return sign(cr);
 }
 
 // Building positive convex hull using Graham method
-// All angles are signed sin^2 in rational numbers
-// for accuracy
 vector<P> conv(const vector<P> &pp) {
 	assert(pp.size() > 1);
-	int sidx = 0;
-	int i = 0;
-	for (auto &p:pp) {
-		if (pp[sidx].x > p.x || (pp[sidx].x >= p.x && pp[sidx].y > p.y))
-			sidx = i;
-		i++;
+	vector<P> op(pp); // order points left->right (down->up)
+	sort(op.begin(), op.end(), [](const P &a, const P &b){return a.x<b.x || (a.x==b.x && a.y<b.y);});
+	P &ap = op.front();
+	P &bp = op.back();
+	P vab = bp-ap;
+	vector<P> l, r; // points to left/right from ap->bp line (including ap and bp)
+	l.push_back(ap);
+	for (int i=1; i<op.size()-1; i++) {
+		if (sign(vab.cross(op[i]-ap)) > 0)
+			l.push_back(op[i]);
+		else
+			r.push_back(op[i]);
 	}
-	vector<pair<Q,P>> ordp;
-	for (int i=0; i<pp.size(); i++) {
-		if (i!=sidx)
-			ordp.push_back(make_pair(sin2(P{pp[sidx].x-1,pp[sidx].y}, pp[sidx], pp[i]), pp[i]));
+	r.push_back(bp);
+	vector<P> cvr; // right side of the convex hull including helper point and ap and bp
+	cvr.push_back(P {ap.x-1, ap.y});
+	cvr.push_back(ap);
+	cvr.push_back(r.front());
+	for (int i=1; i<r.size(); i++) {
+		while (cvr.size()>2 && orientation(*(cvr.end()-2), *(cvr.end()-1), r[i]) <= 0)
+			cvr.pop_back();
+		cvr.push_back(r[i]);
 	}
-	sort(ordp.begin(), ordp.end());
-	vector<P> conv;
-	conv.push_back(pp[sidx]);
-	conv.push_back(ordp[0].second);
-	for (int i=1; i<ordp.size(); i++) {
-		while (orientation(*(conv.end()-2), *(conv.end()-1), ordp[i].second) < 0)
-			conv.pop_back();
-		conv.push_back(ordp[i].second);
+	vector<P> cvl; // left side of the convex hull including helper point and bp and ap
+	cvl.push_back(P {bp.x+1, bp.y});
+	cvl.push_back(bp);
+	cvl.push_back(l.back());
+	for (int i=l.size()-1; i>=0; i--) {
+		while (cvl.size()>2 && orientation(*(cvl.end()-2), *(cvl.end()-1), l[i]) <= 0)
+			cvl.pop_back();
+		cvl.push_back(l[i]);
 	}
-	return conv;
+	vector<P> cv; // exclude helper points and ap/bp ends
+	cv.insert(cv.end(), cvr.begin()+1, cvr.end()-1);
+	cv.insert(cv.end(), cvl.begin()+1, cvl.end()-1);
+	return cv;
 }
 
 int main(int argc, char **argv) {
@@ -96,9 +79,6 @@ int main(int argc, char **argv) {
 	vector<P> pp(n);
 	for (auto &p:pp)
 		cin >> p.x >> p.y;
-	//!NB: must remove duplicate points, otherwise Wrong Answer on 10
-	sort(pp.begin(), pp.end());
-	pp.erase(unique(pp.begin(), pp.end()), pp.end());
 	if (pp.size() > 1) {
 		int s = 0;
 		auto ch = conv(pp);
