8056b985f4585517330442af5e34e88f8318d0ee
Denis Kokarev <d_kokarev@mail.ru>
2017-11-11 20:06:04 -0500

    acmp 533 timeout 16

---
+++ c/acmp/acmp533.cpp
@@ -0,0 +1,54 @@
+/* ACMP 533 */
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+struct P {
+	int64_t x, y;
+	int64_t len2() {
+		return x*x+y*y;
+	}
+	P operator-(const P &b) const {
+		P r(*this);
+		r.x -= b.x;
+		r.y -= b.y;
+		return r;
+	}
+	int64_t cross(const P &b) const {
+		return x*b.y - y*b.x;
+	}
+};
+
+int main(int argc, char **argv) {
+	int n;
+	cin >> n;
+	P pp[n];
+	for (auto &p:pp)
+		cin >> p.x >> p.y;
+	int64_t ans = 0;
+	for (int i=0; i<n; i++) {
+		map<int64_t, vector<int>> dist;
+		for (int j=0; j<n; j++) {
+			int64_t l2 = (pp[j]-pp[i]).len2();
+			dist[l2].push_back(j);
+		}
+		for (auto &pr:dist) {
+			const auto vn = pr.second;
+			for (int k=0; k<vn.size()-1; k++) {
+				for (int l=k+1; l<vn.size(); l++) {
+					P sd1 = pp[vn[l]] - pp[i];
+					P sd2 = pp[vn[k]] - pp[i];
+					P sd3 = pp[vn[k]] - pp[vn[l]];
+					// count equilaterals only once
+					bool eqt = (sd1.len2() == sd2.len2() == sd3.len2());
+					if (sd1.cross(sd2) != 0 && (!eqt || (i<vn[l] && i<vn[k])))
+						ans++;
+				}
+			}
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
