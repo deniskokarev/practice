255a0ae4f9832a610a8651386328bbd960f49c68
Denis Kokarev <d_kokarev@mail.ru>
2017-11-11 21:22:25 -0500

    acmp 533 solution

--- c/acmp/acmp533.cpp
+++ c/acmp/acmp533.cpp
@@ -1,12 +1,17 @@
 /* ACMP 533 */
 #include <iostream>
 #include <vector>
+#include <set>
 #include <unordered_map>
 
 using namespace std;
 
-struct P {
-	int64_t x, y;
+struct P : public pair<int64_t,int64_t> {
+	int64_t &x, &y;
+	P():x(first),y(second) {
+	}
+	P(const P &b):pair<int64_t,int64_t>(b),x(first),y(second) {
+	}
 	int64_t len2() const {
 		return x*x+y*y;
 	}
@@ -14,6 +19,12 @@ struct P {
 		P r(*this);
 		r.x -= b.x;
 		r.y -= b.y;
+		return r;
+	}
+	P operator*(int n) const {
+		P r(*this);
+		r.x *= n;
+		r.y *= n;
 		return r;
 	}
 	int64_t cross(const P &b) const {
@@ -24,29 +35,22 @@ struct P {
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	P pp[n];
+	vector<P> pp(n);
 	for (auto &p:pp)
 		cin >> p.x >> p.y;
 	int64_t ans = 0;
 	for (int i=0; i<n; i++) {
-		unordered_map<int64_t, vector<int>> dist;
+		unordered_map<int64_t, set<P>> dist;
 		for (int j=0; j<n; j++) {
 			int64_t l2 = (pp[j]-pp[i]).len2();
-			dist[l2].push_back(j);
-		}
-		for (auto &pr:dist) {
-			const auto vn = pr.second;
-			for (int k=0; k<int(vn.size())-1; k++) {
-				for (int l=k+1; l<int(vn.size()); l++) {
-					P sd1 = pp[vn[l]] - pp[i];
-					P sd2 = pp[vn[k]] - pp[i];
-					P sd3 = pp[vn[k]] - pp[vn[l]];
-					// count equilaterals only once
-					bool eqt = (sd1.len2() == sd2.len2() && sd2.len2() == sd3.len2());
-					if (sd1.cross(sd2) != 0 && (!eqt || (i<vn[l] && i<vn[k])))
-						ans++;
-				}
+			auto &ort = dist[l2];
+			int cnt = ort.size();
+			if (cnt > 0) {
+				if (ort.find(pp[j]) != ort.end())
+					cnt--;
+				ans += cnt;
 			}
+			ort.insert((pp[i]*2)-pp[j]);
 		}
 	}
 	cout << ans << endl;
