3444668f1574b1ac047f81dc65476f6c12976077
Denis Kokarev <d_kokarev@mail.ru>
2017-11-11 21:41:57 -0500

    acmp 533 solution with some comments

--- c/acmp/acmp533.cpp
+++ c/acmp/acmp533.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <unordered_map>
+#include <map>
 
 using namespace std;
 
-struct P : public pair<int64_t,int64_t> {
-	int64_t &x, &y;
+struct P : public pair<int,int> {
+	int &x, &y;
 	P():x(first),y(second) {
 	}
-	P(const P &b):pair<int64_t,int64_t>(b),x(first),y(second) {
+	P(const P &b):pair<int,int>(b),x(first),y(second) {
 	}
 	int64_t len2() const {
-		return x*x+y*y;
+		return int64_t(x)*x+int64_t(y)*y;
 	}
 	P operator-(const P &b) const {
 		P r(*this);
@@ -27,11 +27,9 @@ struct P : public pair<int64_t,int64_t> {
 		r.y *= n;
 		return r;
 	}
-	int64_t cross(const P &b) const {
-		return x*b.y - y*b.x;
-	}
 };
 
+// don't need to worry about equilateral triangles, as we're on integer grid
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
@@ -40,7 +38,8 @@ int main(int argc, char **argv) {
 		cin >> p.x >> p.y;
 	int64_t ans = 0;
 	for (int i=0; i<n; i++) {
-		unordered_map<int64_t, set<P>> dist;
+		// length^2 -> opposite points (to exclude degenerate trianges)
+		map<int64_t, set<P>> dist;
 		for (int j=0; j<n; j++) {
 			int64_t l2 = (pp[j]-pp[i]).len2();
 			auto &ort = dist[l2];
