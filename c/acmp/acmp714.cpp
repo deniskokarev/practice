50f17e2b17d90a3820148a058da6f930588f3276
Denis Kokarev <d_kokarev@mail.ru>
2017-11-18 13:18:36 -0500

    acmp 714 solution

--- c/acmp/acmp714.cpp
+++ c/acmp/acmp714.cpp
@@ -107,7 +107,7 @@ inline int64_t pow(int64_t b, int p) {
 }
 
 // p[0],p[1],p[2] on same circle
-bool circle_3(Q &x, Q &y, Q &r2, const P p[4]) {
+bool circle_3(Q &x, Q &y, const P p[4]) {
 	int64_t d = 2*(p[0].x - p[1].x)*(p[1].y - p[2].y) - 2*(p[0].y - p[1].y)*(p[1].x - p[2].x);
 	if (d == 0) {
 		return false;
@@ -116,7 +116,6 @@ bool circle_3(Q &x, Q &y, Q &r2, const P p[4]) {
 		int64_t yn = (p[0].x - p[1].x)*(pow(p[1].x, 2) + pow(p[1].y, 2) - pow(p[2].x, 2) - pow(p[2].y, 2)) - (p[1].x - p[2].x)*(pow(p[0].x, 2) + pow(p[0].y, 2) - pow(p[1].x, 2) - pow(p[1].y, 2));
 		x = Q {xn, d};
 		y = Q {yn, d};
-		r2 = Q {pow(p[0].x*d-xn, 2) + pow(p[0].y*d-yn, 2), d*d};
 		return true;
 	}
 }
@@ -141,6 +140,8 @@ bool circle_3_1(Q &x, Q &y, Q &rl2, Q &rh2, const P p[4]) {
 		lx = xn/d; ly = yn/d; lrl2 = rl2; lrh2 = rh2;
 		cerr << "3_1 x: " << x << " y: " << y << " r: " << (sqrtl(lrl2)+sqrtl(lrh2))/2 << endl;
 #endif
+		if (rh2<rl2)
+			swap(rh2,rl2);
 		return true;
 	}
 }
@@ -165,11 +166,30 @@ bool circle_2_2(Q &x, Q &y, Q &rl2, Q &rh2, const P p[4]) {
 		lx = xn/d; ly = yn/d; lrl2 = rl2; lrh2 = rh2;
 		cerr << "2_2 x: " << x << " y: " << y << " r: " << (sqrtl(lrl2)+sqrtl(lrh2))/2 << endl;
 #endif
+		if (rh2<rl2)
+			swap(rh2,rl2);
 		return true;
 	}
 }
 
-constexpr long double scale = 1e7;
+bool circle_line(Q &x, Q &y, Q &rl2, Q &rh2, const P p[4]) {
+	int64_t xn=0, yn=0;
+	for (int i=0; i<4; i++) {
+		xn += p[i].x;
+		yn += p[i].y;
+	}
+	x = Q(xn, 4);
+	y = Q(yn, 4);
+	int64_t r2[4];
+	for (int i=0; i<4; i++)
+		r2[i] = pow(xn-p[i].x*4, 2)+pow(yn-p[i].y*4, 2);
+	sort(r2, r2+4);
+	rl2 = Q(r2[0], 16);
+	rh2 = Q(r2[3], 16);
+	if (rh2<rl2)
+		swap(rh2,rl2);
+	return true;
+}
 
 int main(int argc, char **argv) {
 	P pp[4];
@@ -177,67 +197,77 @@ int main(int argc, char **argv) {
 		cin >> p.x >> p.y;
 	//for (int i=0; i<4; i++)
 	//	cerr << "pp[" << i << "]: (" << pp[i].x << ", " << pp[i].y << ")" << endl;
-	Q cx, cy, cr2;
+	Q cx, cy;
 	bool can_circle = false;
-	circle_3(cx, cy, cr2, pp); // as per requirement always true
+	circle_3(cx, cy, pp); // as per requirement always true
 	P psel[3];
 	can_circle = true;
 	for (int i=0; i<4; i++) {
 		copy(pp, pp+i, psel);
 		copy(pp+i+1, pp+4, psel+i);
-		Q x, y, r2;
-		if (!circle_3(x, y, r2, psel) || x != cx || y != cy || r2 != cr2)
+		Q x, y;
+		if (!circle_3(x, y, psel) || x != cx || y != cy)
 			can_circle = false;
 	}
 	if (can_circle) {
 		cout << "Infinity" << endl;
 		long double x = cx;
 		long double y = cy;
-		cout << fixed << setprecision(6) << x << " " << y << " 0" << endl;
+		cout << fixed << setprecision(7) << x << " " << y << " 0" << endl;
 	} else {
-		set<tuple<Q,Q,int64_t>> res; //x,y,r*scale
+		set<tuple<Q,Q,Q,Q>> res; //x,y,r*scale
 		for (int i=0; i<2*3*4; i++) {
 			Q x, y, rl2, rh2;
 			//for (int i=0; i<4; i++)
 			//	cerr << "pp[" << i << "]: (" << pp[i].x << ", " << pp[i].y << ")" << endl;
 			long double lx, ly, lrl2, lrh2;
 			if (circle_3_1(x, y, rl2, rh2, pp)) {
-				long double lrl2 = rl2;
-				long double lrh2 = rh2;
-				long double r = sqrtl(lrh2)+sqrt(lrl2);
-				res.insert(make_tuple(x, y, r*scale));
+				res.insert(make_tuple(x, y, rl2, rh2));
 				//lx = x; ly = y;	lrl2 = rl2; lrh2 = rh2;
 				//cerr << "x: " << lx << " y: " << y << " r: " << (sqrtl(lrl2)+sqrtl(lrh2))/2 << endl;
 			}
 			if (circle_2_2(x, y, rl2, rh2, pp)) {
-				long double lrl2 = rl2;
-				long double lrh2 = rh2;
-				long double r = sqrtl(lrh2)+sqrt(lrl2);
-				res.insert(make_tuple(x, y, r*scale));
+				res.insert(make_tuple(x, y, rl2, rh2));
 				//lx = x; ly = y;	lrl2 = rl2; lrh2 = rh2;
 				//cerr << "x: " << lx << " y: " << y << " r: " << (sqrtl(lrl2)+sqrtl(lrh2))/2 << endl;
 			}
 			next_permutation(pp, pp+4);
 		}
-		tuple<Q,Q,int64_t> mn = make_tuple(Q{0,0},Q{0,0},0);
-		long double rmn = INT_MAX;
-		for (auto &t:res) {
-			long double r = get<2>(t);
+		if (res.size() > 0) {
+			tuple<Q,Q,Q,Q> mn;
+			long double rmn = INT_MAX;
+			for (auto &t:res) {
+				long double rl2 = get<2>(t);
+				long double rh2 = get<3>(t);
+				long double r = (sqrtl(rh2)+sqrtl(rl2))/2;
 #if 0
-			Q x = get<0>(t);
-			Q y = get<1>(t);
-			cerr << "x: " << x.n << "/" << x.d << " y: " << y.n << "/" << y.d << " r: " << get<2>(t) << endl;
+				Q x = get<0>(t);
+				Q y = get<1>(t);
+				cerr << "x: " << x.n << "/" << x.d << " y: " << y.n << "/" << y.d << " r: " << r << endl;
 #endif
-			if (r<rmn) {
-				rmn = r;
-				mn = t;
+				if (r<rmn) {
+					rmn = r;
+					mn = t;
+				}
 			}
+			long double x = get<0>(mn);
+			long double y = get<1>(mn);
+			cout << res.size() << endl;
+			cout << fixed << setprecision(7) << x << " " << y << " " << rmn << endl;
+		} else {
+			assert(false);
+			// we know they must be symmetrical on a line (as per the problem definition)
+			Q cx, cy, crl2, crh2;
+			circle_line(cx, cy, crl2, crh2, pp);
+			long double x = cx;
+			long double y = cy;
+			long double lrl2 = crl2;
+			long double lrh2 = crh2;
+			assert(lrh2>0 && lrl2>0);
+			long double r = sqrtl(lrh2)+sqrtl(lrl2);
+			cout << 1 << endl;
+			cout << fixed << setprecision(7) << x << " " << y << " " << r/2 << endl;
 		}
-		long double x = get<0>(mn);
-		long double y = get<1>(mn);
-		cout << res.size() << endl;
-		cout << fixed << setprecision(6) << x << " " << y << " " << rmn/2/scale << endl;
-		
 	}
 	return 0;
 }
