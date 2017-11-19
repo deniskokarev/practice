280bb9c170d942ae46ea207655138ccf9b5af782
Denis Kokarev <d_kokarev@mail.ru>
2017-11-19 11:58:58 -0500

    cf894a solution

--- c/cf/cf894a.cc
+++ c/cf/cf894a.cc
@@ -6,9 +6,9 @@ int main(int argc, char **argv) {
 	string s;
 	cin >> s;
 	int64_t cnt = 0;
-	for (int i=0; i<s.size()-2; i++)
-		for (int j=i+1; j<s.size()-1; j++)
-			for (int k=j+1; k<s.size(); k++)
+	for (int i=0; i<int(s.size())-2; i++)
+		for (int j=i+1; j<int(s.size())-1; j++)
+			for (int k=j+1; k<int(s.size()); k++)
 				if (s[i] == 'Q' && s[j] == 'A' && s[k] == 'Q')
 					cnt++;
 	cout << cnt << endl;
