ffbfdb7242911956b78c21a85f2150df9d1906af
Denis Kokarev <d_kokarev@mail.ru>
2017-11-22 14:37:53 -0500

    acmp 283 solution

--- c/acmp/acmp283.cpp
+++ c/acmp/acmp283.cpp
@@ -6,22 +6,18 @@ int main(int argc, char **argv) {
 	int sz[6] = {0};
 	int ch;
 	int ln = 0;
-	while ((ch=fgetc(stdin)) > 0) {
-		if (ch == '\n')
-			break;
+	while ((ch=fgetc(stdin)) > 0 && isalpha(ch)) {
 		if (ch>='A' && ch<='Z') {
-			if (ln > 4) {
+			if (ln >= 0 && ln < 5)
+				sz[ln]++;
+			else
 				sz[5]++;
-				break;
-			}
-			sz[ln]++;
 			ln = 0;
-		} else {
-			ln++;
 		}
+		ln++;
 	}
 	sz[std::min(ln, 5)]++;
-	if (sz[0] == 1 && sz[5] == 0 && sz[1]+sz[2]+sz[3] > 0)
+	if (sz[0] == 1 && sz[1] == 0 && sz[5] == 0 && sz[2]+sz[3]+sz[4] > 0)
 		printf("Yes\n");
 	else
 		printf("No\n");
