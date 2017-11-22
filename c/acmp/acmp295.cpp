0f7893c63059b3dd8586b8756dcb3e5086e6ae77
Denis Kokarev <d_kokarev@mail.ru>
2017-11-22 15:28:27 -0500

    acmp 295 still wrong answer 7

--- c/acmp/acmp295.cpp
+++ c/acmp/acmp295.cpp
@@ -6,18 +6,16 @@ using namespace std;
 int main(int argc, char **argv) {
 	string e, s;
 	cin >> e >> s;
-	string e_sq(e);
-	string s_sq(s);
+	basic_string<uint8_t> e_sq;
+	basic_string<uint8_t> s_sq;
 	int mod = 'Z' - 'A' + 1;
 	for (int i=0; i<s.size()-1; i++)
-		s_sq[i] = ((int(s[i+1])-'A')+mod-(s[i]-'A'))%mod;
-	s_sq.resize(s_sq.size()-1);
+		s_sq.push_back(((int(s[i+1]))-int(s[i])+mod)%mod);
 	for (int i=0; i<e.size()-1; i++)
-		e_sq[i] = ((int(e[i+1])-'A')+mod-(e[i]-'A'))%mod;
-	e_sq.resize(e_sq.size()-1);
+		e_sq.push_back(((int(e[i+1]))-int(e[i])+mod)%mod);
 	int k = INT_MAX;
 	for (size_t pos=e_sq.find(s_sq); pos != string::npos; pos=e_sq.find(s_sq, pos+1)) {
-		k = min(k, (int(s[0])+mod-e[pos])%mod);
+		k = min(k, (int(s[0])-int(e[pos])+mod)%mod);
 		//cerr << "pos: " << pos << " k: " << k << endl;
 	}
 	if (k < INT_MAX) {
