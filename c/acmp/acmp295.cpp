e45f6d61386322183368d48c8cb31ba9cb3747ae
Denis Kokarev <d_kokarev@mail.ru>
2017-11-22 19:39:03 -0500

    acmp 295 solution

--- c/acmp/acmp295.cpp
+++ c/acmp/acmp295.cpp
@@ -3,27 +3,30 @@
 
 using namespace std;
 
+constexpr unsigned mod = 'Z' - 'A' + 1;
+
 int main(int argc, char **argv) {
 	string e, s;
 	cin >> e >> s;
-	basic_string<int> e_sq;
-	basic_string<int> s_sq;
+	basic_string<unsigned> e_sq;
+	basic_string<unsigned> s_sq;
 	for (int i=0; i<s.size()-1; i++)
-		s_sq.push_back((int(s[i+1])-int(s[i])));
+		s_sq.push_back((unsigned(s[i+1])+mod-unsigned(s[i]))%mod);
 	for (int i=0; i<e.size()-1; i++)
-		e_sq.push_back((int(e[i+1])-int(e[i])));
-	int k = INT_MIN;
+		e_sq.push_back((unsigned(e[i+1])+mod-unsigned(e[i]))%mod);
+	unsigned k = INT_MAX;
 	bool match = false;
 	for (size_t pos=e_sq.find(s_sq); pos != string::npos; pos=e_sq.find(s_sq, pos+1)) {
 		match = true;
-		int nk = (int(s[0])-int(e[pos]));
-		if (abs(k)<abs(nk))
+		unsigned nk = (unsigned(e[pos])+mod-unsigned(s[0]))%mod;
+		//cerr << "pos: " << pos << " k: " << k << " nk: " << nk << endl;
+		if (nk < k)
 			k = nk;
-		//cerr << "pos: " << pos << " k: " << k << endl;
 	}
+	k = (k + mod) % mod;
 	if (match) {
-		for (auto &c:e)
-			c = int(c)+k;
+		for (char &c:e)
+			c = (unsigned(c)+mod-'A'-k)%mod + 'A';
 		cout << e << endl;
 	} else {
 		cout << "IMPOSSIBLE" << endl;
