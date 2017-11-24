8e04ddc640c89f645e31ad67e0d5943238ffb921
Denis Kokarev <d_kokarev@mail.ru>
2017-11-24 13:10:56 -0500

    acmp 415 solution with comments

--- c/acmp/acmp415.cpp
+++ c/acmp/acmp415.cpp
@@ -5,10 +5,13 @@
 
 using namespace std;
 
-// longest common substring
+// NB!: more complicated solution
+
+// n*m statistics a-la longest common substring
 struct sstat {
 	const string &n1, &n2;
 	vector<vector<int>> mm;
+	// O(n*m) space and complexity
 	sstat(const string &_n1, const string &_n2):n1(_n1),n2(_n2),mm(n1.size()+1, vector<int>(n2.size()+1, 0)) {
 		for (int i=0; i<n1.size(); i++)
 			for (int j=0; j<n2.size(); j++)
@@ -17,8 +20,9 @@ struct sstat {
 				else
 					mm[i+1][j+1] = 0;
 	}
-	// 2 may be a continuation of 1 starting at this position
-	int maycat12() const {
+	// find biggest overlap where
+	// 2 is a continuation of 1 starting at this position
+	int cat12() const {
 		int pos = 0;
 		int i = n1.size();
 		for (int j=1; j<=n2.size(); j++)
@@ -26,8 +30,9 @@ struct sstat {
 				pos = j;
 		return n1.size()-pos;
 	}
-	// 1 may be a continuation of 2 starting at this position
-	int maycat21() const {
+	// find biggest overlap where
+	// 1 is a continuation of 2 starting at this position
+	int cat21() const {
 		int pos = 0;
 		int j = n2.size();
 		for (int i=1; i<=n1.size(); i++)
@@ -35,8 +40,9 @@ struct sstat {
 				pos = i;
 		return n2.size()-pos;
 	}
-	// 2 is inside 1 at the given pos
-	int inside12() const {
+	// find earliest occurence where
+	// 2 is entirerly enveloped by 1
+	int in12() const {
 		int pos = n1.size();
 		int j = n2.size();
 		for (int i=n1.size()-1; i>j; i--)
@@ -44,8 +50,9 @@ struct sstat {
 				pos = i-j;
 		return pos;
 	}
-	// 1 is inside 2 at the given pos
-	int inside21() const {
+	// find earliest occurence where
+	// 1 is entirerly enveloped by 2
+	int in21() const {
 		int pos = n2.size();
 		int i = n1.size();
 		for (int j=n2.size()-1; j>i; j--)
@@ -65,20 +72,21 @@ int main(int argc, char **argv) {
 	vector<string> oo;
 	int p;
 	string s;
-	p = ss.maycat12();
+	// need to consider 4 scenarios
+	p = ss.cat12();
 	s = n1.substr(0, p) + n2;
 	oo.push_back(s);
-	p = ss.maycat21();
+	p = ss.cat21();
 	s = n2.substr(0, p) + n1;
 	oo.push_back(s);
-	p = ss.inside12();
+	p = ss.in12();
 	s = n1.substr(0, p) + n2 + n1.substr(min(n1.size(), p+n2.size()));
 	oo.push_back(s);
-	p = ss.inside21();
+	p = ss.in21();
 	s = n2.substr(0, p) + n1 + n2.substr(min(n2.size(), p+n1.size()));
 	oo.push_back(s);
-#if 1
-	cerr << "p12: " << ss.maycat12() << " p21: " << ss.maycat21() << " in12: " << ss.inside12() << " in21: " << ss.inside21() << endl;
+#if 0
+	cerr << "p12: " << ss.cat12() << " p21: " << ss.cat21() << " in12: " << ss.in12() << " in21: " << ss.in21() << endl;
 	for (auto &o:oo)
 		cerr << "o: " << o << endl;
 #endif
