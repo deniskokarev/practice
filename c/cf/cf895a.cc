3c05ebbc30f06132323bf3d8394d7a91f15362bf
Denis Kokarev <dk1942@att.com>
2017-11-27 14:48:23 -0500

    cf895a still wrong answer 39

--- c/cf/cf895a.cc
+++ c/cf/cf895a.cc
@@ -9,11 +9,13 @@ constexpr int sm = 360;
 // try to make the number as close to vi as possible
 vector<vector<int>> ks(int n, const int *aa) {
 	vector<vector<int>> vv(sm+1, vector<int>(n+1, 0));
-	for (int vi=1; vi<sm; vi++)
+	for (int vi=0; vi<sm; vi++)
 		for (int j=1; j<=n; j++)
 			for (int k=0; k<j; k++)
-				if (vv[vi][k]+aa[j-1] <= vi)
+				if (vv[vi][k]+aa[j-1] <= vi+1)
 					vv[vi+1][j] = max(vv[vi][k], max(vv[vi+1][j], vv[vi][k]+aa[j-1]));
+				else
+					vv[vi+1][j] = max(vv[vi][k], vv[vi+1][j]);
 	return vv;
 }
 
@@ -25,7 +27,7 @@ int main(int argc, char **argv) {
 		cin >> a;
 	int mn = INT_MAX;
 	vector<vector<int>> k = ks(n, aa);
-	for (int i=0; i<=360; i++) {
+	for (int i=0; i<=180; i++) {
 		int d = i-k[i][n];
 		mn = min(mn, abs((sm-i+d) - (i-d)));
 	}
