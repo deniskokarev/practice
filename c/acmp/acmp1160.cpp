9ec01d2c3f7bbd6af95be445a89ea824d25dff18
Denis Kokarev <d_kokarev@mail.ru>
2017-11-26 21:47:37 -0500

    acmp 1160 still timeout 10

--- c/acmp/acmp1160.cpp
+++ c/acmp/acmp1160.cpp
@@ -6,21 +6,13 @@ using namespace std;
 int main(int argc, char **argv) {
 	string s;
 	cin >> s;
-	int pp[s.size()]; // prefix fn
+	int pp[s.size()]; // prefix function
 	fill(pp, pp+s.size(), 0);
 	int k=0;
 	for (int i=1; i<s.size(); i++) {
 		if (i>k) {
-			if (s[i] != s[k]) {
-				while (k>0) {
-					int j;
-					for (j=0; j<k && s[i-j] == s[k-j-1]; j++);
-					if (j == k)
-						break;
-					k--;
-				}
-				pp[i] = k;
-			} else {
+			for (; k>0 && s[i] != s[k]; k=pp[k-1]);
+			if (s[i] == s[k]) {
 				k++;
 				pp[i] = k;
 			}
