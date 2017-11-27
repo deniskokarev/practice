1ba3d41b5497356b7528d8fd726265b6623e2b60
Denis Kokarev <dk1942@att.com>
2017-11-27 14:22:30 -0500

    cf895a wrong answer 39

--- c/cf/cf895a.cc
+++ c/cf/cf895a.cc
@@ -4,27 +4,29 @@
 /* CodeForces CF895A problem */
 using namespace std;
 
-// try to make the number as close to v as possible
-int ks(int v, int n, const int *aa) {
-	vector<vector<int>> vv(v+1, vector<int>(n+1, 0));
-	for (int vi=1; vi<v; vi++)
+constexpr int sm = 360;
+
+// try to make the number as close to vi as possible
+vector<vector<int>> ks(int n, const int *aa) {
+	vector<vector<int>> vv(sm+1, vector<int>(n+1, 0));
+	for (int vi=1; vi<sm; vi++)
 		for (int j=1; j<=n; j++)
 			for (int k=0; k<j; k++)
-				if (vv[vi][k]+aa[j-1] <= v)
+				if (vv[vi][k]+aa[j-1] <= vi)
 					vv[vi+1][j] = max(vv[vi][k], max(vv[vi+1][j], vv[vi][k]+aa[j-1]));
-	return v-vv[v][n];
+	return vv;
 }
 
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
 	int aa[n];
-	int sm = 360;
 	for (auto &a:aa)
 		cin >> a;
 	int mn = INT_MAX;
+	vector<vector<int>> k = ks(n, aa);
 	for (int i=0; i<=360; i++) {
-		int d = ks(i, n, aa);
+		int d = i-k[i][n];
 		mn = min(mn, abs((sm-i+d) - (i-d)));
 	}
 	cout << mn << endl;
