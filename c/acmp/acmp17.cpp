60ba6f998067d98472da5879efeb31eb7e348aa4
Denis Kokarev <d_kokarev@mail.ru>
2017-12-02 12:23:14 -0500

    acmp 17 another solution with suffix function

--- c/acmp/acmp17.cpp
+++ c/acmp/acmp17.cpp
@@ -1,21 +1,32 @@
 /* ACMP 17 */
 #include <cstdio>
+#include <algorithm>
+#include <iterator>
 
-// prefix function
-// compute prefix for string s of size sz
-// pp random access iterator should have the same size
-template <typename RIC, typename RIN> void prefix_function(const RIC s, size_t sz, RIN pp) {
-	size_t k = 0;
-	pp[0] = 0;
-	for (size_t i=1; i<sz; i++) {
-		for (; k>0 && s[i] != s[k]; k=pp[k-1]);
-		if (s[i] == s[k]) {
-			k++;
-			pp[i] = k;
-		} else {
-			pp[i] = 0;
-		}
-	}
+// suffix function (z function)
+// compute suffix sizes for string s of size sz
+// zz random access iterator should have the same size
+// taken from wikipedia (e-maxx implementation has a performance problem)
+template <typename RIC, typename RIN> void suffix_function(const RIC s, size_t len, RIN z) {
+	using T = typename std::iterator_traits<RIN>::value_type;
+    z[0] = len;
+    for (T j,i=1,l=0,r=0; i < len; i++) {
+        if (i > r) {
+            for (j = 0; ((j + i) < len) && (s[i + j] == s[j]) ; j++);
+            z[i] = j;
+            l = i;
+            r = (i + j - 1);
+        } else {
+            if (z[i - l] < (r - i + 1)) {
+                z[i] = z[i - l];
+            } else {
+                for (j = 1; ((j + r) < len) && (s[r + j] == s[r - i + j]); j++);
+                z[i] = (r - i + j);
+                l = i;
+                r = (r + j - 1);
+            }
+        }
+    }
 }
 
 constexpr int MX_SZ = 1024*32;
@@ -27,11 +38,12 @@ int main(int argc, char **argv) {
 	scanf("%d", &n);
 	while (sz < n)
 		scanf("%d", &s[sz++]);
-	int pp[sz]; // prefix function
-	prefix_function(s, sz, pp);
-	int ans = sz-1;
-	if ((sz-1)%(sz-1-pp[sz-2]) == 0)
-		ans = sz-1-pp[sz-2];
+	unsigned zz[sz]; // suffix function
+	suffix_function(s, sz, zz);
+    int ans = 1;
+    for (int i=sz-1; i>0; i--)
+        if (i+zz[i] == sz && (sz-1)%i == 0)
+            ans = i;	
 	printf("%d\n", ans);
 	return 0;
 }
