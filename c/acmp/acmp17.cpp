5fdd57c5a15812ac923536bd1e98910f00cd48a7
Denis Kokarev <d_kokarev@mail.ru>
2017-12-02 13:34:08 -0500

    acmp 17 solution with two alternative suffix function implementations

--- c/acmp/acmp17.cpp
+++ c/acmp/acmp17.cpp
@@ -1,18 +1,23 @@
 /* ACMP 17 */
 #include <cstdio>
-#include <algorithm>
-#include <iterator>
 
+#include <iterator>
+#include <algorithm>
+
+// two implementations of suffix function
+// depending on the input set one may work better than other
+
+#if 1
 // suffix function (z function)
 // compute suffix sizes for string s of size sz
 // zz random access iterator should have the same size
-// taken from wikipedia (e-maxx implementation has a performance problem)
+// implementation from wikipedia
 template <typename RIC, typename RIN> void suffix_function(const RIC s, size_t len, RIN z) {
 	using T = typename std::iterator_traits<RIN>::value_type;
-    z[0] = len;
+    z[0] = 0;
     for (T j,i=1,l=0,r=0; i < len; i++) {
         if (i > r) {
-            for (j = 0; ((j + i) < len) && (s[i + j] == s[j]) ; j++);
+            for (j = 0; ((j + i) < len) && (s[i + j] == s[j]); j++);
             z[i] = j;
             l = i;
             r = (i + j - 1);
@@ -28,6 +33,26 @@ template <typename RIC, typename RIN> void suffix_function(const RIC s, size_t l
         }
     }
 }
+#else
+// implementation from e-maxx
+// for some reason too slow on acmp 17 test cases
+// must use implementation from wikipedia instead
+template <typename RIC, typename RIN> void suffix_function(const RIC s, size_t sz, RIN zz) {
+	using T = typename std::iterator_traits<RIN>::value_type;
+    zz[0] = 0;
+    for (T i=1,l=0,r=0; i<sz; i++) {
+        if (i <= r)
+            zz[i] = std::min(r-i+1, zz[i-l]);
+        T cnt;
+        for (cnt=0; i+cnt < sz && s[cnt] == s[i+cnt]; cnt++);
+        zz[i] = cnt;
+        if (i+cnt-1 > r) {
+            l = i;
+            r = i+cnt-1;
+        }
+    }
+}
+#endif
 
 constexpr int MX_SZ = 1024*32;
 
