4fe488c5c9e296b78c461591e88d13138049d4bd
Denis Kokarev <d_kokarev@mail.ru>
2017-12-02 11:41:19 -0500

    acmp 1160 solution with universal prefix_function()

--- c/acmp/acmp1160.cpp
+++ c/acmp/acmp1160.cpp
@@ -1,16 +1,13 @@
 /* ACMP 1160 */
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-	char s[1024*1024];
-	int sz = 0;
-	int c;
-	while ((c=fgetc(stdin)) >=0 && c != '\n')
-		s[sz++] = c;
-	int pp[sz]; // prefix function
-	int k=0;
+// prefix function
+// compute prefix for string s of size sz
+// pp random access iterator should have the same size
+template <typename RIC, typename RIN> void prefix_function(const RIC s, size_t sz, RIN pp) {
+	size_t k = 0;
 	pp[0] = 0;
-	for (int i=1; i<sz; i++) {
+	for (size_t i=1; i<sz; i++) {
 		for (; k>0 && s[i] != s[k]; k=pp[k-1]);
 		if (s[i] == s[k]) {
 			k++;
@@ -19,6 +16,16 @@ int main(int argc, char **argv) {
 			pp[i] = 0;
 		}
 	}
+}
+
+int main(int argc, char **argv) {
+	char s[1024*1024];
+	int sz = 0;
+	int c;
+	while ((c=fgetc(stdin)) >=0 && c != '\n')
+		s[sz++] = c;
+	int pp[sz]; // prefix function
+	prefix_function(s, sz, pp);
 	for (auto &p:pp)
 		printf("%d ", p);
 	printf("\n");
