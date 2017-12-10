68a0dc2dc133f7a6a7d692894e7c38123d7fd5bd
Denis Kokarev <d_kokarev@mail.ru>
2017-12-10 15:41:01 -0500

    acmp 1156 solution

--- c/acmp/acmp1156.cpp
+++ c/acmp/acmp1156.cpp
@@ -4,16 +4,21 @@
 
 using namespace std;
 
+// using polynomial hash-assisted string comparison
+// using two independent PolyHashes, as 64bit hash is too tight for our ~10^10 substrings
+// (didn;t want to use long arithmetic)
+
 // polynomial hash for all substrings of s string
-// needs O(n) space and time to precompute
+// 64 bit space gives 50% cache collision on ~5*10^9 strings
+// needs O(n) space and time to precompute, then O(1)
 // suitable to compare the substrings of s
 // may not be used to compare the substrings beteen two separate PolyHash
-// object. The s.length() is used as highest power
+// object, as s.length() is used as highest power
 struct PolyHash {
-	static constexpr unsigned base = 17; // polynomial base
-	vector<uint64_t> hh; // 64 bit space gives 50% cache collision on ~5*10^9 strings
+	unsigned base; // polynomial base
+	vector<uint64_t> hh;
 	vector<uint64_t> pp; // memorize the powers of base too
-	PolyHash(const string &s):hh(s.length()+1),pp(hh.size()) {
+	PolyHash(const string &s, unsigned b):base(b),hh(s.length()+1),pp(hh.size()) {
 		int len = s.length();
 		uint64_t p=1,h=0;
 		pp[0] = p;
@@ -34,12 +39,13 @@ struct PolyHash {
 // hash-assisted less operator working in O(log(len))
 // a-la:
 // s.subtr(a, len) < s.substr(b, len)
-bool h_less(const string &s, const PolyHash &hash, int a, int b, int len) {
+// NB: using 2 independent hashes as I'm too lazy to implement proper 128 bit hash with long arithmetic 
+bool h_less(const string &s, const PolyHash &hash1, const PolyHash &hash2, int a, int b, int len) {
 	int f = 0, t = len;
 	// find first mismatching length
 	while (f<t) {
 		int mid = f+(t-f)/2;
-		if (hash(a,mid) != hash(b,mid))
+		if (hash1(a,mid) != hash1(b,mid) || hash2(a,mid) != hash2(b,mid))
 			t = mid;
 		else
 			f = mid+1;
@@ -51,11 +57,14 @@ int main(int argc, char **argv) {
 	string s;
 	cin >> s;
 	string ss = s+s;
-	PolyHash hash(ss);
+	// using 2 independent hashes as I'm too lazy to implement proper hash with long arithmetic
+	// 64 bit hash is too tight for our 10^10 substrings
+	PolyHash hash1(ss, 3);
+	PolyHash hash2(ss, 5);
 	int l = s.length();
 	int mn = 0;
 	for (int i=0; i<l; i++)
-		if (h_less(ss, hash, i, mn, l))
+		if (h_less(ss, hash1, hash2, i, mn, l))
 			mn = i;
 	cout << ss.substr(mn, l) << endl;
 	return 0;
