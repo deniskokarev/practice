22a245188d6c019b8d60d1c39eca48530c40d839
Denis Kokarev <d_kokarev@mail.ru>
2017-12-10 22:16:26 -0500

    acmp 595 solution without hashes

--- c/acmp/acmp595.cpp
+++ c/acmp/acmp595.cpp
@@ -5,34 +5,6 @@
 
 using namespace std;
 
-// polynomial hash for all substrings of s string
-// 64 bit space gives 50% cache collision on ~5*10^9 strings
-// needs O(n) space and time to precompute, then O(1)
-// suitable to compare the substrings of s
-// may not be used to compare the substrings beteen two separate PolyHash
-// object, as s.length() is used as highest power
-struct PolyHash {
-	unsigned base; // polynomial base
-	vector<uint64_t> hh;
-	vector<uint64_t> pp; // memorize the powers of base too
-	PolyHash(const string &s, unsigned b=3):base(b),hh(s.length()+1),pp(hh.size()) {
-		int len = s.length();
-		uint64_t p=1,h=0;
-		pp[0] = p;
-		hh[0] = h;
-		for (int j=0; j<len; j++) {
-			p *= base;
-			h += s[j]*p;
-			pp[j+1] = p;
-			hh[j+1] = h;
-		}
-	}
-	uint64_t operator()(int b, int len) const {
-		uint64_t h = hh[b+len] - hh[b];
-		return h * pp[hh.size()-b-len];
-	}
-};
-
 // prefix function
 // compute prefix for string s of size sz
 // pp random access array should have the same size
@@ -50,7 +22,7 @@ template <typename RIC, typename RIN> void prefix_function(const RIC s, size_t s
 	}
 }
 
-// we simply go by all known matched prefixes and check if the suffix matches too
+// we simply match the biggest last prefix and then match the remaining inverted string
 int main(int argc, char **argv) {
 	string s1, s2;
 	cin >> s1 >> s2;
@@ -61,19 +33,16 @@ int main(int argc, char **argv) {
 		prefix_function(ps.begin(), ps.length(), pp);
 		string rs1(s1);
 		reverse(rs1.begin(), rs1.end());
-		PolyHash hash1(rs1);
-		PolyHash hash2(s2);
-		int i=1;
-		for (int j=s2.length()+1; j<ps.length(); j++,i++) {
-			//cerr << "pfx1: " << ps.substr(j-pp[j]+1, pp[j]) << " pfx2: " << ps.substr(0, pp[j]) << endl;
-			//cerr << "sfx1: " << rs1.substr(pp[j], len-pp[j]) << " sfx2: " << s2.substr(pp[j], len-pp[j]) << endl;
-			if (pp[j] > 0 && hash1(pp[j], len-pp[j]) == hash2(pp[j], len-pp[j]) && rs1.substr(pp[j], len-pp[j]) == s2.substr(pp[j], len-pp[j])) {
-				cout << "Yes" << endl;
-				cout << i-pp[j] << endl;
-				return 0;
-			}
+		int lst = ps.length()-1;
+		//cerr << "pfx1: " << ps.substr(lst-pp[lst]+1, pp[lst]) << " pfx2: " << ps.substr(0, pp[lst]) << endl;
+		//cerr << "sfx1: " << rs1.substr(pp[lst], len-pp[lst]) << " sfx2: " << s2.substr(pp[lst], len-pp[lst]) << endl;
+		if (rs1.substr(pp[lst], len-pp[lst]) == s2.substr(pp[lst], len-pp[lst])) {
+			cout << "Yes" << endl << len-pp[lst] << endl;
+		} else {
+			cout << "No" << endl;
 		}
+	} else {
+		cout << "No" << endl;
 	}
-	cout << "No" << endl;
 	return 0;
 }
