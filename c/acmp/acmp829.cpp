062a6e7112769ee28f8e801f9b9fb65523a5c931
Denis Kokarev <d_kokarev@mail.ru>
2017-12-11 22:02:41 -0500

    acmp 829 wrong answer 23

--- c/acmp/acmp829.cpp
+++ c/acmp/acmp829.cpp
@@ -44,53 +44,23 @@ template<typename IT> PolyHash<IT> create_polyhash(const IT begin, const IT end,
 	return PolyHash<IT>(begin, end, base);
 }
  
-// composition of 64-bit hashes
-template<typename IT, int W> struct MultiHash {
-	PolyHash<IT> hash[W];
-	std::array<uint64_t, W> operator()(int b, int len) const {
-		std::array<uint64_t, W> res;
-		for (int i=0; i<W; i++)
-			res[i] = hash[i](b, len);
-		return res;
-	}
-};
- 
-// hepler function to create multihash
-template<typename IT, int W> MultiHash<IT, W> create_multihash(const IT begin, const IT end, const int (&bases)[W]) {
-	MultiHash<IT,W> mh;
-	for (int i=0; i<W; i++)
-		mh.hash[i] = create_polyhash(begin, end, bases[i]);
-	return mh;
-}
-
-template<typename T, size_t W> string to_string(const array<T, W> &aa) {
-	string s;
-	for (auto a:aa)
-		s += to_string(a) + '\x0';
-	return s;
-}
-
 int main(int argc, char **argv) {
-	const int bases[] = {3}; // composition of 2 polynomial hashes is enough
 	string a, b;
 	cin >> a >> b;
 	size_t blen = b.length();
 	// must pad both strings to equal length be able to compare hashes
-	int mxsz = 1e5+1;
-	set<string> known_hashes; // need to keep all known hashes for quick lookup
-	{
-		string bb = b+b;
-		bb += string(mxsz - bb.length(), '\x0');
-		auto hb = create_multihash(bb.begin(), bb.end(), bases);
-		for (int i=0; i<blen; i++)
-			known_hashes.insert(to_string(hb(i, blen)));
-	}
+	int mxsz = 2e5+1;
+	set<uint64_t> known_hashes; // need to keep all known hashes for quick lookup
+	string bb = b+b;
+	bb += string(mxsz - bb.length(), '\x0');
+	auto hb = create_polyhash(bb.begin(), bb.end());
+	for (int i=0; i<blen; i++)
+		known_hashes.insert(hb(i, blen));
 	a += string(mxsz - a.length(), '\x0');
-	auto ha = create_multihash(a.begin(), a.end(), bases);
+	auto ha = create_polyhash(a.begin(), a.end());
 	int cnt = 0;
 	for (int i=0; i<=a.length()-blen; i++) {
-		string hs = to_string(ha(i, blen));
-		if (known_hashes.find(hs) != known_hashes.end()) {
+		if (known_hashes.find(ha(i, blen)) != known_hashes.end()) {
 			//cerr << "i: " << i << " s: " << a.substr(i, blen) << endl;
 			cnt++;
 		}
