3f3437ab8310859efaba06c1709a0e634d560945
Denis Kokarev <d_kokarev@mail.ru>
2017-12-11 22:50:18 -0500

    acmp 829 solution

--- c/acmp/acmp829.cpp
+++ c/acmp/acmp829.cpp
@@ -5,7 +5,7 @@
 #include <set>
   
 using namespace std;
-  
+
 // polynomial hash for all substrings of s string
 // 64 bit space gives 50% cache collision on ~5*10^9 strings
 // needs O(n) space and O(n) time to precompute, then O(1)
@@ -14,14 +14,15 @@ using namespace std;
 // object, as s.length() is used as highest power
 // NB!!! if you need to have better probability use MultiHash composition
 template<typename IT> struct PolyHash {
+	using value_type = uint64_t;
 	unsigned base; // polynomial base
 	int sz;
-	std::vector<uint64_t> hash;
-	std::vector<uint64_t> powers;
+	std::vector<value_type> hash;
+	std::vector<value_type> powers;
 	PolyHash():base(0),sz(0) {
 	}
 	PolyHash(const IT begin, const IT end, unsigned b=3):base(b),sz(std::distance(begin, end)+1),hash(sz),powers(sz) {
-		uint64_t p=1,h=0;
+		value_type p=1,h=0;
 		hash[0] = h;
 		powers[0] = p;
 		int i=1;
@@ -32,8 +33,8 @@ template<typename IT> struct PolyHash {
 			powers[i] = p;
 		}
 	}
-	uint64_t operator()(int b, int len) const {
-		uint64_t h;
+	value_type operator()(int b, int len) const {
+		value_type h;
 		h = hash[b+len] - hash[b];
 		return h * powers[sz-b-len];
 	}
@@ -44,18 +45,19 @@ template<typename IT> PolyHash<IT> create_polyhash(const IT begin, const IT end,
 	return PolyHash<IT>(begin, end, base);
 }
   
-// composition of 64-bit hashes
+// composition of hashes
 template<typename IT, int W> struct MultiHash {
-	using value_type = std::array<uint64_t, W>;
+	using hash_type = typename PolyHash<IT>::value_type;
+	using value_type = std::array<hash_type, W>;
 	PolyHash<IT> hash[W];
-	std::array<uint64_t, W> operator()(int b, int len) const {
-		std::array<uint64_t, W> res;
+	std::array<hash_type, W> operator()(int b, int len) const {
+		std::array<hash_type, W> res;
 		for (int i=0; i<W; i++)
 			res[i] = hash[i](b, len);
 		return res;
 	}
 };
-  
+
 // hepler function to create multihash
 template<typename IT, int W> MultiHash<IT, W> create_multihash(const IT begin, const IT end, const int (&bases)[W]) {
 	MultiHash<IT,W> mh;
@@ -65,30 +67,32 @@ template<typename IT, int W> MultiHash<IT, W> create_multihash(const IT begin, c
 }
  
 int main(int argc, char **argv) {
-	const int bases[] = {3,5}; // composition of 2 polynomial hashes is enough
+	// composite hash of 2x64-bit values is enough for this problem
+	// if you're careful it still fits the 16MB memory constraint
+	const int bases[] = {3, 5};
 	string a, b;
 	cin >> a >> b;
 	size_t blen = b.length();
 	// must pad both strings to equal length be able to compare hashes
-	int mxsz = 2e5+1;
-	a += string(mxsz - a.length(), '\x0');
-	auto ha = create_multihash(a.begin(), a.end(), bases);
-	set<decltype(ha)::value_type> known_hashes; // need to keep all known hashes for quick lookup
+	int mxsz = max(a.length(), blen*2)+1;
+	// determine the multihash element type without actually running it yet
+	using hash_value_type = typename decltype(create_multihash(a.begin(), a.end(), bases))::value_type;
+	// need to keep all known hashes for quick lookup
+	set<hash_value_type> known_hashes;
 	{
+		// release memory for hash b immediately after adding hashes into the set
 		string bb = b+b;
-		bb += string(mxsz - bb.length(), '\x0');
+		bb += string(mxsz - bb.length(), '\x0'); // pad for eq length
 		auto hb = create_multihash(bb.begin(), bb.end(), bases);
-		for (int i=0; i<blen; i++) {
+		for (int i=0; i<blen; i++)
 			known_hashes.insert(hb(i, blen));
-		}
 	}
+	a += string(mxsz - a.length(), '\x0'); // pad for eq length
+	auto ha = create_multihash(a.begin(), a.end(), bases);
 	int cnt = 0;
-	for (int i=0; i<=a.length()-blen; i++) {
-		if (known_hashes.find(ha(i, blen)) != known_hashes.end()) {
-			//cerr << "i: " << i << " s: " << a.substr(i, blen) << endl;
+	for (int i=0; i<=a.length()-blen; i++)
+		if (known_hashes.find(ha(i, blen)) != known_hashes.end())
 			cnt++;
-		}
-	}
 	cout << cnt << endl;
 	return 0;
 }
