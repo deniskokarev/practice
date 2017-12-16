8357e0dffc55ae085279f4baf4693c1b84bb5c53
Denis Kokarev <d_kokarev@mail.ru>
2017-12-15 21:22:52 -0500

    adding suffix array implementation

--- c/draft/str.cc
+++ c/draft/str.cc
@@ -130,3 +130,58 @@ template<typename IT, int W> MultiHash<IT, W> create_multihash(const IT begin, c
 		mh.hash[i] = create_polyhash(begin, end, bases[i]);
 	return mh;
 }
+
+/*
+ * simple N*logN*logN, plain C implementation
+ * of suffix array
+ */
+struct SA_SP {
+	unsigned pos;
+	uint64_t score; // score for two halfs
+};
+
+#define dim(X)	(sizeof(X)/sizeof(X[0]))
+				 
+static int cmp_score(const void *a, const void *b) {
+	SA_SP *pa = (SA_SP*)a;
+	SA_SP *pb = (SA_SP*)b;
+	return (pa->score > pb->score) - (pa->score < pb->score);
+}
+
+static void one_sort_stage(SA_SP *sp, SA_SP *tmp, unsigned sz, unsigned len) {
+	for (unsigned i=0; i<sz; i++) {
+		tmp[i].score = (sp[i].score << 32) + sp[i+len].score;
+		tmp[i].pos = i;
+	}
+	qsort(tmp, sz, sizeof(tmp[0]), cmp_score);
+	unsigned ns = 0;
+	uint64_t os = tmp[0].score;
+	for (unsigned i=0; i<sz; i++) {
+		if (os != tmp[i].score) {
+			os = tmp[i].score;
+			ns++;
+		}
+		int p = tmp[i].pos;
+		sp[p].score = ns;
+	}
+	memcpy(sp+sz, sp, sz*(sizeof(sp[0])));
+}
+
+// build suffix array for the string s
+// simple O(N*log(N)*log(N)) solution
+// sa temporary buffer must have 3*sz free space
+// dst result must have sz free space
+void suffix_array(unsigned *dst, const char *s, unsigned sz, SA_SP *sa) {
+	SA_SP *sp = sa;
+	SA_SP *tmp = sa+sz*2;
+	for (int i=0; i<sz; i++) {
+		sp[i].pos = i;
+		sp[i].score = s[i];
+	}
+	memcpy(sp+sz, sp, sz*(sizeof(sp[0])));
+	one_sort_stage(sp, tmp, sz, 0);
+	for (int p2=1; p2<sz; p2<<=1)
+		one_sort_stage(sp, tmp, sz, p2);
+	for (int i=0; i<sz; i++)
+		dst[i] = tmp[i].pos;
+}
