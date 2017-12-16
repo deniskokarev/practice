35181c815697f1c202f0bc542527f572d51dff2a
Denis Kokarev <d_kokarev@mail.ru>
2017-12-15 21:13:49 -0500

    acmp 1158 simplified O(log(N)*N*log(N)) solution - if you need polyhashes solution look at previous commits

--- c/acmp/acmp1158.cpp
+++ c/acmp/acmp1158.cpp
@@ -3,10 +3,11 @@
 #include <cstring>
 #include <cstdlib>
 #include <cassert>
+#include <cstdint>
 
 struct SA_SP {
 	unsigned pos;
-	unsigned score[2]; // score for two halfs and remainder
+	uint64_t score; // score for two halfs
 };
 
 #define dim(X)	(sizeof(X)/sizeof(X[0]))
@@ -14,64 +15,53 @@ struct SA_SP {
 static int cmp_score(const void *a, const void *b) {
 	SA_SP *pa = (SA_SP*)a;
 	SA_SP *pb = (SA_SP*)b;
-	int d;
-	int i = 0;
-	do {
-		d = (int)pa->score[i] - (int)pb->score[i];
-		i++;
-	} while(d==0 && i<dim(pa->score));
-	return d;
+	return (pa->score > pb->score) - (pa->score < pb->score);
 }
 
-static void one_sort_stage(SA_SP *sp, SA_SP *tmp, const char *s, unsigned sz, unsigned len) {
-	unsigned hlen = len>>1;
+static void one_sort_stage(SA_SP *sp, SA_SP *tmp, unsigned sz, unsigned len) {
 	for (unsigned i=0; i<sz; i++) {
-		tmp[i].score[0] = sp[i].score[0];
-		tmp[i].score[1] = sp[i+hlen].score[0]*256 + s[i+len-1];
+		tmp[i].score = (sp[i].score << 32) + sp[i+len].score;
 		tmp[i].pos = i;
 	}
 	qsort(tmp, sz, sizeof(tmp[0]), cmp_score);
 	unsigned ns = 0;
-	unsigned *os = tmp[0].score;
+	uint64_t os = tmp[0].score;
 	for (unsigned i=0; i<sz; i++) {
-		if (memcmp(os, tmp[i].score, sizeof(tmp[i].score)) != 0) {
+		if (os != tmp[i].score) {
 			os = tmp[i].score;
 			ns++;
 		}
 		int p = tmp[i].pos;
-		sp[p].score[0] = ns;
+		sp[p].score = ns;
 	}
 	memcpy(sp+sz, sp, sz*(sizeof(sp[0])));
 }
 
 // build suffix array for the string s
 // simple O(N*log(N)*log(N)) solution
-// s content must be duplicated (sz is still the size of the original str)
-// sa temporary buffer must have 4*sz free space
+// sa temporary buffer must have 3*sz free space
 // dst result must have sz free space
 void suffix_array(unsigned *dst, const char *s, unsigned sz, SA_SP *sa) {
-	unsigned len_stages[32];
-	unsigned n, p2 = 0;
-	for (p2=0,n=sz; n>0; n>>=1,p2++)
-		len_stages[p2] = n;
 	SA_SP *sp = sa;
 	SA_SP *tmp = sa+sz*2;
-	memset(sp, 0, sizeof(sp[0])*sz*2);
-	for (int i=0; i<sz; i++)
-		sp[i].pos = sp[i+sz].pos = i;
-	for (int i=p2-1; i>=0; i--)
-		one_sort_stage(sp, tmp, s, sz, len_stages[i]);
+	for (int i=0; i<sz; i++) {
+		sp[i].pos = i;
+		sp[i].score = s[i];
+	}
+	memcpy(sp+sz, sp, sz*(sizeof(sp[0])));
+	one_sort_stage(sp, tmp, sz, 0);
+	for (int p2=1; p2<sz; p2<<=1)
+		one_sort_stage(sp, tmp, sz, p2);
 	for (int i=0; i<sz; i++)
 		dst[i] = tmp[i].pos;
 }
   
 int main(int argc, char **argv) {
-	char ss[200001];
+	char ss[100001];
 	int rc = scanf("%100000s", ss);
 	unsigned len = (unsigned)strlen(ss);
 	assert(rc == 1 && len <= 100000 && "problem with input data constraints");
-	memcpy(ss+len, ss, sizeof(ss[0])*len);
-	SA_SP sp[len*4];
+	SA_SP sp[len*3];
 	unsigned pos[len];
 	suffix_array(pos, ss, len, sp);
 	int fp = -1;
@@ -82,10 +72,10 @@ int main(int argc, char **argv) {
 #if 0
 		// debug print sorted suffixes
 		for (int j=0; j<len; j++)
-			fputc(ss[pos[i]+j], stderr);
+			fputc(ss[(pos[i]+j)%len], stderr);
 		fputc('\n', stderr);
 #endif
-		out[i] = ss[pos[i]+len-1];
+		out[i] = ss[(pos[i]+len-1)%len];
 	}
 	fprintf(stdout, "%d\n", fp+1);
 	fwrite(out, 1, len, stdout);
