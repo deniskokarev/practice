a5711f0ea28df8efeadbb2d97a54d9efa4a4a0a9
Denis Kokarev <d_kokarev@mail.ru>
2017-12-16 15:58:16 -0500

    acmp 1432 simplified solution

--- c/acmp/acmp1432.cpp
+++ c/acmp/acmp1432.cpp
@@ -6,33 +6,34 @@ using namespace std;
 
 constexpr unsigned mod31 = (1ULL<<31)-1;
 
+unsigned inline next_r(unsigned r) {
+	return (r*1103515245+12345) & mod31;
+}
+
 int main(int argc, char **argv) {
 	unsigned n, q;
-	uint64_t r;
+	unsigned r;
 	cin >> n >> q >> r;
 	assert((n&(n-1))==0 && "n must be power of 2");
 	unsigned modn = n-1;
-	unsigned *aa = new unsigned[n+1];
-	unsigned *r15 = new unsigned[n+1];
+	unsigned aa[n+1];
 	aa[0] = r;
-	unsigned rdiv = r >> 16;
-	r15[0] = rdiv;
+	unsigned r15 = r >> 16;
 	for (int i=1; i<=n; i++) {
-		r = (r*1103515245+12345) & mod31;
-		rdiv = r >> 16;
-		r15[i] = rdiv;
-		aa[i] = aa[i-1] + rdiv; // don't bother with mod31
+		r = next_r(r);
+		r15 = r >> 16;
+		aa[i] = aa[i-1] + r15; // don't bother with mod31
 		//cerr << r << " ";
-		//cerr << r15[i] << " ";
+		//cerr << r15 << " ";
 	}
 	unsigned sum = 0;
 	for (int i=1; i<2*q; i++,i++) {
-		r = (r*1103515245+12345) & mod31;
-		rdiv = r >> 16;
-		unsigned b1 = (rdiv&modn)+1;
-		r = (r*1103515245+12345) & mod31;
-		rdiv = r >> 16;
-		unsigned b2 = (rdiv&modn)+1;
+		r = next_r(r);
+		r15 = r >> 16;
+		unsigned b1 = (r15&modn)+1;
+		r = next_r(r);
+		r15 = r >> 16;
+		unsigned b2 = (r15&modn)+1;
 		unsigned l = min(b1, b2);
 		unsigned r = max(b1, b2);
 		//cerr << "l: " << l << " r: " << r << endl;
@@ -40,8 +41,6 @@ int main(int argc, char **argv) {
 		//cerr << s << endl;
 		sum += s;
 	}
-	delete[] aa;
-	delete[] r15;
 	cout << (sum & mod31) << endl;
 	return 0;
 }
