f2bb1878d3a4c2f0f1130f39cf58fb7e838d7681
Denis Kokarev <d_kokarev@mail.ru>
2017-12-17 13:07:37 -0500

    acmp 1082 solution (fuc..ing stdio)

--- c/acmp/acmp1082.cpp
+++ c/acmp/acmp1082.cpp
@@ -1,5 +1,5 @@
 /* ACMP 1082 */
-#include <stdio.h>
+#include <stdio.h> // cstdio is too slow, iostreams is even slower
 
 int main(int argc, char **argv) {
 	int n, m, k;
@@ -12,10 +12,11 @@ int main(int argc, char **argv) {
 		for (int j=1; j<=m; j++) {
 			int a;
 			scanf("%d", &a);
-			aa[i][j] = aa[i-1][j]+aa[i][j-1]-aa[i-1][j-1]+(long long)a;
+			aa[i][j] = aa[i-1][j]+aa[i][j-1]-aa[i-1][j-1]+a;
 		}
 	}
 #if 0
+	// debug
 	for (int i=0; i<=n; i++) {
 		for (int j=0; j<=m; j++)
 			fprintf(stderr, "%lld ", aa[i][j]);
@@ -26,7 +27,13 @@ int main(int argc, char **argv) {
 		int x1, y1, x2, y2;
 		scanf("%d%d%d%d", &y1, &x1, &y2, &x2);
 		long long sum = aa[y2][x2] - aa[y1-1][x2] - aa[y2][x1-1] + aa[y1-1][x1-1];
-		printf("%lld\n", sum);
+		// fu..ing stdio.h sometimes fails printing long longs with %lld
+		// doing it manually
+		int hi=sum/1000000, lo=sum%1000000;
+		if (hi>0)
+			printf("%d%06d\n", hi, lo);
+		else
+			printf("%d\n", lo);
 	}
 	return 0;
 }
