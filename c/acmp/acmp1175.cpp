d0d743186ec24dc8c934a7f3f0a153afcd6fce17
Denis Kokarev <d_kokarev@mail.ru>
2017-12-17 20:10:31 -0500

    acmp 1175 solution

--- c/acmp/acmp1175.cpp
+++ c/acmp/acmp1175.cpp
@@ -1,14 +1,13 @@
 /* ACMP 1175 */
 #include <stdio.h> // cstdio is too slow, iostreams is even slower
-#include <limits.h>
 
 #define MAX(A,B)	((A>B)?A:B)
 #define MIN(A,B)	((A<B)?A:B)
 
-// precompute log2 values [1..mx] inclusive
+// precompute floor(log2) values [1..mx] inclusive
 // ll buf must have mx+1 free space
-void fill_log2(int mx, unsigned char *ll) {
-	ll[0] = 255; // denoting undefined value
+void fill_floor_log2(int mx, unsigned char *ll) {
+	ll[0] = 255; // undefined value
 	ll[1] = 0;
 	int i = 2;
 	int l2, p2;
@@ -22,18 +21,33 @@ void fill_log2(int mx, unsigned char *ll) {
 		ll[i++] = l2;
 }
 
+// precompute ceil(log2) values [1..mx] inclusive
+// ll buf must have mx+1 free space
+void fill_ceil_log2(int mx, unsigned char *ll) {
+	fill_floor_log2(mx, ll);
+	for (int i=1; i<=mx; i++)
+		ll[i]++;
+	for (int p2=1; p2<=mx; p2<<=1)
+		ll[p2]--;
+}
+
 int main(int argc, char **argv) {
 	int n;
 	scanf("%d", &n);
-	unsigned char log2[n+1];
-	fill_log2(n, log2);
-	int l2 = log2[n];
-	int aa[l2+1][n];
+	unsigned char floor_log2[n+1];
+	unsigned char ceil_log2[n+1];
+	fill_floor_log2(n, floor_log2);
+	fill_ceil_log2(n, ceil_log2);
+	int l2 = ceil_log2[n]+1;
+	int aa[l2][n];
 	for (auto &a:aa[0])
 		scanf("%d", &a);
-	for (int i=1,p2=2; i<=l2; i++,p2<<=1)
-		for (int j=0; j<=n-p2; j++)
-			aa[i][j] = MIN(aa[i-1][j], aa[i-1][j+1]);
+	for (int i=1,p2=1; i<l2; i++,p2<<=1)
+		for (int j=0; j<n; j++)
+			if (j+p2<n)
+				aa[i][j] = MIN(aa[i-1][j], aa[i-1][j+p2]);
+			else
+				aa[i][j] = aa[i-1][j];
 	int m;
 	scanf("%d", &m);
 	while (m-- > 0) {
@@ -41,10 +55,10 @@ int main(int argc, char **argv) {
 		scanf("%d%d", &l, &r);
 		l--, r--;
 		int len = r-l+1;
-		int l2 = log2[len];
-		int p2 = 1<<l2;
-		int mnl = aa[l2][l];
-		int mnr = aa[l2][r-p2+1];
+		int fl2 = floor_log2[len];
+		int p2 = 1<<fl2;
+		int mnl = aa[fl2][l];
+		int mnr = aa[fl2][r-p2+1];
 		int mn = MIN(mnl, mnr);
 		printf("%d ", mn);
 	}
