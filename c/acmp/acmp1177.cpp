3a8278614ab86af141c7194c40edfd6bc3a06bff
Denis Kokarev <d_kokarev@mail.ru>
2017-12-21 17:32:47 -0500

    acmp 1177 out of mem 19

--- c/acmp/acmp1177.cpp
+++ c/acmp/acmp1177.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -60,45 +61,35 @@ int main(int argc, char **argv) {
 	int h = b-d-2+1;
 	int l2w = floor_log2[w];
 	int l2h = floor_log2[h];
-	vector<vector<vector<vector<int>>>> aa = \
-		vector<vector<vector<vector<int>>>>(l2h+1, vector<vector<vector<int>>>(l2w+1));
-	aa[0][0] = vector<vector<int>>(n, vector<int>(m, INT_MIN));
-	for (int i=0; i<=n-d; i++) {
-		for (int j=0; j<=m-c; j++) {
-			aa[0][0][i][j] = ssum[i+d][j+c] - ssum[i+d][j] - ssum[i][j+c] + ssum[i][j];
-			//fprintf(stderr, "%d ", aa[0][0][i][j]);
-		}
-		//fprintf(stderr, "\n");
-	}
-	// fill 0-level for each row
-	for (int l2j=1,p2j=1; l2j<=l2w; l2j++,p2j<<=1) {
-		aa[0][l2j] = vector<vector<int>>(n, vector<int>(m));
-		for (int i=0; i<n; i++)
-			for (int j=0; j<=m-2*p2j; j++)
-				aa[0][l2j][i][j] = min(aa[0][l2j-1][i][j], aa[0][l2j-1][i][j+p2j]);
-	}
-	// fill 0-level for each col
-	for (int l2i=1,p2i=1; l2i<=l2h; l2i++,p2i<<=1) {
-		aa[l2i][0] = vector<vector<int>>(n, vector<int>(m));
-		for (int i=0; i<=n-2*p2i; i++)
-			for (int j=0; j<m; j++)
-				aa[l2i][0][i][j] = min(aa[l2i-1][0][i][j], aa[l2i-1][0][i+p2i][j]);
-	}
-	// fill all levels for upto l2 hight and l2 width
-	for (int l2i=1,p2i=1; l2i<=l2h; l2i++,p2i<<=1) {
+	vector<vector<int>> aa(n, vector<int>(vector<int>(m)));
+	{
+		vector<vector<vector<int>>> aar(l2w+1, vector<vector<int>>(n, vector<int>(m)));
+		for (int i=0; i<=n-d; i++)
+			for (int j=0; j<=m-c; j++)
+				aar[0][i][j] = ssum[i+d][j+c] - ssum[i+d][j] - ssum[i][j+c] + ssum[i][j];
 		for (int l2j=1,p2j=1; l2j<=l2w; l2j++,p2j<<=1) {
-			aa[l2i][l2j] = vector<vector<int>>(n, vector<int>(m));
-			for (int i=0; i<=n-2*p2i; i++) {
-				for (int j=0; j<=m-2*p2j; j++) {
-					int a = aa[l2i-1][l2j-1][i][j];
-					a = min(a, aa[l2i-1][l2j-1][i+p2i][j]);
-					a = min(a, aa[l2i-1][l2j-1][i][j+p2j]);
-					a = min(a, aa[l2i-1][l2j-1][i+p2i][j+p2j]);
-					aa[l2i][l2j][i][j] = a;
-				}
-			}
-			aa[l2i-1][l2j-1] = vector<vector<int>>(); // release prior result
+			aar[l2j] = vector<vector<int>>(n, vector<int>(m));
+			for (int i=0; i<n; i++)
+				for (int j=0; j<=m-2*p2j; j++)
+					aar[l2j][i][j] = min(aar[l2j-1][i][j], aar[l2j-1][i][j+p2j]);
+			aar[l2j-1].clear();
+			aar[l2j-1].shrink_to_fit();
 		}
+		vector<vector<vector<int>>> aac(l2h+1, vector<vector<int>>(n, vector<int>(m)));
+		for (int i=0; i<=n-d; i++)
+			for (int j=0; j<=m-c; j++)
+				aac[0][i][j] = aar[l2w][i][j];
+		for (int l2i=1,p2i=1; l2i<=l2h; l2i++,p2i<<=1) {
+			aac[l2i] = vector<vector<int>>(n, vector<int>(m));
+			for (int i=0; i<=n-2*p2i; i++)
+				for (int j=0; j<m; j++)
+					aac[l2i][i][j] = min(aac[l2i-1][i][j], aac[l2i-1][i+p2i][j]);
+			aac[l2i-1].clear();
+			aac[l2i-1].shrink_to_fit();
+		}
+		for (int i=0; i<=n-d; i++)
+			for (int j=0; j<=m-c; j++)
+				aa[i][j] = aac[l2h][i][j];
 	}
 	int p2w = 1<<l2w;
 	int p2h = 1<<l2h;
@@ -109,10 +100,10 @@ int main(int argc, char **argv) {
 			int s = ssum[i+b][j+a] - ssum[i+b][j] - ssum[i][j+a] + ssum[i][j];
 			//fprintf(stderr, "%d ", s);
 			int x1 = j+1, y1 = i+1, x2 = j+w, y2 = i+h;
-			int mn_ul = aa[l2h][l2w][y1][x1];
-			int mn_bl = aa[l2h][l2w][y2-p2h+1][x1];
-			int mn_ur = aa[l2h][l2w][y1][x2-p2w+1];
-			int mn_br = aa[l2h][l2w][y2-p2h+1][x2-p2w+1];
+			int mn_ul = aa[y1][x1];
+			int mn_bl = aa[y2-p2h+1][x1];
+			int mn_ur = aa[y1][x2-p2w+1];
+			int mn_br = aa[y2-p2h+1][x2-p2w+1];
 			//fprintf(stderr, "min ul: %d bl: %d ur: %d br: %d\n", mn_ul, mn_bl, mn_ur, mn_br);
 			int mn = mn_ul;
 			mn = min(mn, mn_bl);
