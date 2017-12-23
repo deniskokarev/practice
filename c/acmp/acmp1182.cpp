7c0dc85e79dee75bef6ac78d86413be18e3ccc94
Denis Kokarev <d_kokarev@mail.ru>
2017-12-22 19:25:25 -0500

    acmp 1182 wrong answer 2

--- c/acmp/acmp1182.cpp
+++ c/acmp/acmp1182.cpp
@@ -12,11 +12,11 @@ int main(int argc, char **argv) {
 	scanf("%d%d%d", &n, &k, &m);
 	const int len = max(sqrt(n), 1);
 	const int nb = (n+len-1)/len;
-	int pbucket[nb]; // partial segment stats
-	int wbucket[nb]; // whole segment stats
+	int mxmxbt[nb]; // partial segment stats
+	int mxmnbt[nb]; // whole segment stats
 	int aa[n];
-	memset(pbucket, 0, sizeof(pbucket));
-	memset(wbucket, 0, sizeof(wbucket));
+	memset(mxmxbt, 0, sizeof(mxmxbt));
+	memset(mxmnbt, 0, sizeof(mxmnbt));
 	memset(aa, 0, sizeof(aa));
 	while (m-- > 0) {
 		int l, r;
@@ -25,9 +25,9 @@ int main(int argc, char **argv) {
 		const int rb = r/len;
 		int mx = 0;
 		for (int bi=lb+1; bi<rb; bi++)
-			mx = max(mx, pbucket[bi]);
-		mx = max(mx, wbucket[lb]);
-		mx = max(mx, wbucket[rb]);
+			mx = max(mx, mxmxbt[bi]);
+		mx = max(mx, mxmnbt[lb]);
+		mx = max(mx, mxmnbt[rb]);
 		if (rb>lb) {
 			int l_hi = (lb+1)*len;
 			for (int i=l; i<l_hi; i++)
@@ -42,8 +42,8 @@ int main(int argc, char **argv) {
 		if (mx < k) {
 			printf("Yes\n");
 			for (int bi=lb+1; bi<rb; bi++) {
-				wbucket[bi]++;
-				pbucket[bi]++;
+				mxmxbt[bi]++;
+				mxmnbt[bi]++;
 			}
 			if (rb > lb) {
 				mx = 0;
@@ -52,21 +52,21 @@ int main(int argc, char **argv) {
 					aa[i]++;
 					mx = max(mx, aa[i]);
 				}
-				pbucket[lb] = max(pbucket[lb], mx);
+				mxmxbt[lb] = max(mxmxbt[lb], mx+mxmnbt[lb]);
 				mx = 0;
 				int r_lo = rb*len;
 				for (int i=r_lo; i<=r; i++) {
 					aa[i]++;
 					mx = max(mx, aa[i]);
 				}
-				pbucket[rb] = max(pbucket[rb], mx);
+				mxmxbt[rb] = max(mxmxbt[rb], mx+mxmnbt[rb]);
 			} else {
 				mx = 0;
 				for (int i=l; i<=r; i++) {
 					aa[i]++;
 					mx = max(mx, aa[i]);
 				}
-				pbucket[lb] = max(pbucket[lb], mx);
+				mxmxbt[lb] = max(mxmxbt[lb], mx+mxmnbt[lb]);
 			}
 		} else {
 			printf("No\n");
