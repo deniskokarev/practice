4ecb5bcfcd075b12db60120ad9ea7a563752f5d4
Denis Kokarev <d_kokarev@mail.ru>
2017-12-24 13:04:21 -0500

    acmp 1083 wrong answer 2

--- c/acmp/acmp1083.cpp
+++ c/acmp/acmp1083.cpp
@@ -1,177 +1,188 @@
 /* ACMP 1083 */
-#include <cstdio>
-#include <cstring>
-#include <climits>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 #include <cassert>
 
-#define min(A,B) ((A<B)?A:B)
-#define dim(X)	((int)(sizeof(X)/sizeof(X[0])))
+using namespace std;
 
-typedef struct {
-	long long sum;
-	int set;
-	int inc;
+struct Bucket {
+	int64_t sum;
 	int min;
-} BUCKET;
-		
-static void scatter_bucket(int *aa, int len, BUCKET *bucket, int b) {
-	if (bucket[b].set >= 0) {
-		for (int i=b*len; i<(b+1)*len; i++)
-			aa[i] = bucket[b].set;
-		bucket[b].set = -1;
-	}
-	for (int i=b*len; i<(b+1)*len; i++)
-		aa[i] += bucket[b].inc;
-	bucket[b].inc = 0;
-}
+	int inc;
+	int set;
+};
 
-BUCKET bucket[15];
-constexpr int len = 3001;
-int aa[len*dim(bucket)];
+struct Data {
+	int len;
+	vector<int> data;
+	vector<Bucket> bucket;
+	Data(int sz):len(sqrt(sz)),data(sz),bucket((sz+len-1)/len) {
+	}
+	int get(int i) const {
+		int b = i/len;
+		if (bucket[b].set >= 0)
+			return bucket[b].set+bucket[b].inc;
+		else
+			return data[i]+bucket[b].inc;
+	}
+	// set individual elements and zero out .set and .inc
+	void scatter_bucket(int b) {
+		if (bucket[b].set >= 0) {
+			for (int i=b*len; i<(b+1)*len && i<data.size(); i++)
+				data[i] = bucket[b].set;
+			bucket[b].set = -1;
+		}
+		for (int i=b*len; i<(b+1)*len && i<data.size(); i++)
+			data[i] += bucket[b].inc;
+		bucket[b].inc = 0;
+	}
+	// may be invoked only after scatter
+	void recalc_bucket(int b) {
+		bucket[b].sum = 0;
+		bucket[b].min = INT_MAX;
+		bucket[b].inc = 0;
+		bucket[b].set = -1;
+		for (int i=b*len; i<(b+1)*len && i<data.size(); i++) {
+			bucket[b].sum += data[i];
+			bucket[b].min = min(bucket[b].min, data[i]);
+		}
+	}
+	void recalc_all_buckets() {
+		for (int b=0; b<bucket.size(); b++)
+			recalc_bucket(b);
+	}
+	void set(int l, int r, int x) {
+		int lb = l/len;
+		int rb = r/len;
+		if (lb<rb) {
+			for (int b=lb+1; b<rb; b++) {
+				bucket[b].sum = x*len;
+				bucket[b].min = x;
+				bucket[b].inc = 0;
+				bucket[b].set = x;
+			}
+			scatter_bucket(lb);
+			for (int i=l; i<(lb+1)*len; i++)
+				data[i] = x;
+			recalc_bucket(lb);
+			scatter_bucket(rb);
+			for (int i=rb*len; i<=r; i++)
+				data[i] = x;
+			recalc_bucket(rb);
+		} else {
+			scatter_bucket(lb);
+			for (int i=l; i<=r; i++)
+				data[i] = x;
+			recalc_bucket(lb);
+		}
+	}
+	void inc(int l, int r, int x) {
+		int lb = l/len;
+		int rb = r/len;
+		if (lb<rb) {
+			for (int b=lb+1; b<rb; b++) {
+				bucket[b].sum += x*len;
+				bucket[b].min += x;
+				bucket[b].inc += x;
+			}
+			scatter_bucket(lb);
+			for (int i=l; i<(lb+1)*len; i++)
+				data[i] += x;
+			recalc_bucket(lb);
+			for (int i=rb*len; i<=r; i++)
+				data[i] += x;
+			recalc_bucket(rb);
+		} else {
+			scatter_bucket(lb);
+			for (int i=l; i<=r; i++)
+				data[i] += x;
+			recalc_bucket(lb);
+		}
+	}
+	int64_t getSum(int l, int r) {
+		int64_t sum = 0;
+		int lb = l/len;
+		int rb = r/len;
+		if (lb<rb) {
+			for (int b=lb+1; b<rb; b++)
+				sum += bucket[b].sum;
+			scatter_bucket(lb);
+			for (int i=l; i<(lb+1)*len; i++)
+				sum += data[i];
+			scatter_bucket(rb);
+			for (int i=rb*len; i<=r; i++)
+				sum += data[i];
+		} else {
+			scatter_bucket(lb);
+			for (int i=l; i<=r; i++)
+				data[i] += data[i];
+			recalc_bucket(lb);
+		}
+		return sum;
+	}
+	int getMin(int l, int r) {
+		int mn = INT_MAX;
+		int lb = l/len;
+		int rb = r/len;
+		if (lb<rb) {
+			for (int b=lb+1; b<rb; b++)
+				mn = min(mn, bucket[b].min);
+			scatter_bucket(lb);
+			for (int i=l; i<(lb+1)*len; i++)
+				mn = min(mn, data[i]);
+			scatter_bucket(rb);
+			for (int i=rb*len; i<=r; i++)
+				mn = min(mn, data[i]);
+		} else {
+			scatter_bucket(lb);
+			for (int i=l; i<=r; i++)
+				mn = min(mn, data[i]);
+		}
+		return mn;
+	}
+};
 
 int main(int argc, char **argv) {
-	int rc;
 	int n;
-	rc = scanf("%d", &n);
-	assert(rc == 1 && "expected input format");
-	assert(n<dim(aa) && "must not exceed buffer size");
-	for (auto &b:bucket) {
-		b.sum = 0;
-		b.set = -1;
-		b.inc = 0;
-		b.min = INT_MAX;
-	}
-	for (int i=0; i<n; i++) {
-		rc = scanf("%d", &aa[i]);
-		assert(rc == 1 && "expected input format");
-		int b = i/len;
-		bucket[b].min = min(bucket[b].min, aa[i]);
-		bucket[b].sum += aa[i];
-	}
+	cin >> n;
+	Data dd(n);
+	for (int i=0; i<n; i++)
+		cin >> dd.data[i];
+	dd.recalc_all_buckets();
 	int m;
-	rc = scanf("%d", &m);
-	assert(rc == 1 && "expected input format");
+	cin >> m;
 	while (m-- > 0) {
-		char op[32];
-		int b, i, l, r, x;
-		rc = scanf("%s", op);
-		assert(rc == 1 && "expected input format");
-		if (strcmp(op, "get") == 0) {
-			rc = scanf("%d", &i);
-			assert(rc == 1 && "expected input format");
+		string cmd;
+		cin >> cmd;
+		if (cmd == "get") {
+			int i;
+			cin >> i;
 			i--;
-			b = i/len;
-			printf("%d\n", ((bucket[b].set>=0)?bucket[b].set:aa[i])+bucket[b].inc);
-		} else if (strcmp(op, "update") == 0) {
-			rc = scanf("%d%d%d", &l, &r, &x);
-			assert(rc == 3 && "expected input format");
+			cout << dd.get(i) << "\n";
+		} else if (cmd == "update") {
+			int l, r, x;
+			cin >> l >> r >> x;
 			l--, r--;
-			for (b=l/len+1; b<r/len; b++) {
-				bucket[b].sum = x*len;
-				bucket[b].set = x;
-				bucket[b].inc = 0;
-				bucket[b].min = x;
-			}
-			if (l/len < r/len) {
-				b = l/len;
-				scatter_bucket(aa, len, bucket, b);
-				bucket[b].min = min(bucket[b].min, x);
-				for (int i=l; i<(b+1)*len; i++) {
-					bucket[b].sum -= aa[i];
-					aa[i] = x;
-					bucket[b].sum += aa[i];
-				}
-				b = r/len;
-				scatter_bucket(aa, len, bucket, b);
-				bucket[b].min = min(bucket[b].min, x);
-				for (int i=b*len; i<=r; i++) {
-					bucket[b].sum -= aa[i];
-					aa[i] = x;
-					bucket[b].sum += aa[i];
-				}				
-			} else {
-				b = l/len;
-				scatter_bucket(aa, len, bucket, b);
-				bucket[b].min = min(bucket[b].min, x);
-				for (int i=l; i<=r; i++) {
-					bucket[b].sum -= aa[i];
-					aa[i] = x;
-					bucket[b].sum += aa[i];
-				}
-			}
-		} else if (strcmp(op, "add") == 0) {
-			rc = scanf("%d%d%d", &l, &r, &x);
-			assert(rc == 3 && "expected input format");
+			dd.set(l, r, x);
+		} else if (cmd == "add") {
+			int l, r, x;
+			cin >> l >> r >> x;
 			l--, r--;
-			for (b=l/len+1; b<r/len; b++) {
-				bucket[b].sum += x*len;
-				bucket[b].inc += x;
-				bucket[b].min += x;
-			}
-			if (l/len < r/len) {
-				b = l/len;
-				scatter_bucket(aa, len, bucket, b);
-				for (int i=l; i<(b+1)*len; i++) {
-					aa[i] += x;
-					bucket[b].sum += x;
-					bucket[b].min = min(bucket[b].min, aa[i]);
-				}
-				b = r/len;
-				scatter_bucket(aa, len, bucket, b);
-				for (int i=b*len; i<=r; i++) {
-					aa[i] += x;
-					bucket[b].sum += x;
-					bucket[b].min = min(bucket[b].min, aa[i]);
-				}
-			} else {
-				b = l/len;
-				scatter_bucket(aa, len, bucket, b);
-				for (int i=l; i<=r; i++) {
-					aa[i] += x;
-					bucket[b].sum += x;
-					bucket[b].min = min(bucket[b].min, aa[i]);
-				}
-			}
-		} else if (strcmp(op, "rsq") == 0) {
-			rc = scanf("%d%d", &l, &r);
-			assert(rc == 2 && "expected input format");
+			dd.inc(l, r, x);
+		} else if (cmd == "rsq") {
+			int l, r;
+			cin >> l >> r;
 			l--, r--;
-			long long sum = 0;
-			for (b=l/len+1; b<r/len; b++)
-				sum += bucket[b].sum;
-			if (l/len < r/len) {
-				b = l/len;
-				for (int i=l; i<(b+1)*len; i++)
-					sum += ((bucket[b].set>=0)?bucket[b].set:aa[i]) + bucket[b].inc;
-				b = r/len;
-				for (int i=b*len; i<=r; i++)
-					sum += ((bucket[b].set>=0)?bucket[b].set:aa[i]) + bucket[b].inc;
-			} else {
-				b = l/len;
-				for (int i=l; i<=r; i++)
-					sum += ((bucket[b].set>=0)?bucket[b].set:aa[i]) + bucket[b].inc;
-			}
-			printf("%lld\n", sum);
-		} else if (strcmp(op, "rmq") == 0) {
-			rc = scanf("%d%d", &l, &r);
-			assert(rc == 2 && "expected input format");
+			cout << dd.getSum(l, r) << "\n";
+		} else if (cmd == "rmq") {
+			int l, r;
+			cin >> l >> r;
 			l--, r--;
-			int mn = INT_MAX;
-			for (b=l/len+1; b<r/len; b++)
-				mn = min(mn, bucket[b].min);
-			if (l/len < r/len) {
-				b = l/len;
-				for (int i=l; i<(b+1)*len; i++)
-					mn = min(mn, ((bucket[b].set>=0)?bucket[b].set:aa[i]) + bucket[b].inc);
-				b = r/len;
-				for (int i=b*len; i<=r; i++)
-					mn = min(mn, ((bucket[b].set>=0)?bucket[b].set:aa[i]) + bucket[b].inc);
-			} else {
-				b = l/len;
-				for (int i=l; i<=r; i++)
-					mn = min(mn, ((bucket[b].set>=0)?bucket[b].set:aa[i]) + bucket[b].inc);
-			}
-			printf("%d\n", mn);
+			cout << dd.getMin(l, r) << "\n";
+		} else {
+			assert(false && "unknown command");
 		}
 	}
 	return 0;
