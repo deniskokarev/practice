347966c423aeae2c2918b5ea46ebaced0a9849f2
Denis Kokarev <d_kokarev@mail.ru>
2017-12-25 16:06:38 -0500

    cf895a solution (knapsack was not needed)

--- c/cf/cf895a.cc
+++ c/cf/cf895a.cc
@@ -1,35 +1,27 @@
 #include <iostream>
-#include <vector>
-
 /* CodeForces CF895A problem */
 using namespace std;
-
-constexpr int sm = 360;
-
-// try to make the number as close to vi as possible
-vector<vector<int>> ks(int n, const int *aa) {
-	vector<vector<int>> vv(sm+1, vector<int>(n+1, 0));
-	for (int vi=0; vi<sm; vi++)
-		for (int j=1; j<=n; j++)
-			for (int k=0; k<j; k++)
-				if (vv[vi][k]+aa[j-1] <= vi+1)
-					vv[vi+1][j] = max(vv[vi][k], max(vv[vi+1][j], vv[vi][k]+aa[j-1]));
-				else
-					vv[vi+1][j] = max(vv[vi][k], vv[vi+1][j]);
-	return vv;
-}
 
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	int aa[n];
-	for (auto &a:aa)
-		cin >> a;
+	int ss[n+1];
+	ss[0] = 0;
+	int sum = 0;
+	for (int i=1; i<=n; i++) {
+		cin >> ss[i];
+		sum += ss[i];
+	}
+	for (int i=1; i<=n; i++)
+		ss[i] += ss[i-1];
 	int mn = INT_MAX;
-	vector<vector<int>> k = ks(n, aa);
-	for (int i=0; i<=180; i++) {
-		int d = i-k[i][n];
-		mn = min(mn, abs((sm-i+d) - (i-d)));
+	for (int i=0; i<n; i++) {
+		for (int j=i+1; j<=n; j++) {
+			int p1 = ss[j]-ss[i];
+			int p2 = sum - p1;
+			int diff = abs(p1-p2);
+			mn = min(mn, diff);
+		}
 	}
 	cout << mn << endl;
 	return 0;
