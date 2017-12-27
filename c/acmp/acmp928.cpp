63e3124e760e78a1e9fd97bd60cff5d35c574f07
Denis Kokarev <d_kokarev@mail.ru>
2017-12-27 18:55:29 -0500

    acmp 928 wrong answer 5

--- c/acmp/acmp928.cpp
+++ c/acmp/acmp928.cpp
@@ -29,33 +29,22 @@ int main(int argc, char **argv) {
 	int mx = 0;
 	for (int i=0; i<n; i++) { // for each x
 		for (int j=0; j<m; j++) { // for each y
-			int f=i, t=n;
-			double dl;
-			while (f<t) {
-				int m=f+(t-f)/2;
-				dl = l-sq[i][j]-(xx[m]-xx[i])-sq[m][j];
-				if (dl < 0)
-					t = m;
-				else
-					f = m+1;
-			}
-			if (dl < 0) {
-				if (f > i)
-					f--;
-				else
-					continue;
-			}
-			int i1 = f; // rightmost second point on x obtained by binary search
-			mx = max(mx, i1-i+2);
-			while (i1>=i) {
+			double dl = l-sq[i][j];
+			int i1; // rightmost second point on x
+			for (i1=i; i1 < n && dl >= (xx[i1]-xx[i]) + sq[i1][j]; i1++);
+			if (i1 > i)
 				i1--;
-				dl = l-sq[i][j]-(xx[i1]-xx[i])-sq[i1][j];
+			else
+				continue;
+			while (i1>=i) {
+				dl = l-sq[i][j]-(xx[i1]-xx[i]);
 				for (int j1=j; j1<m; j1++) { // rightmost second point on y
-					if (dl - (yy[j1]-yy[j]) - sq[i1][j1] < 0)
+					if (dl < (yy[j1]-yy[j]) + sq[i1][j1])
 						break;
 					else
-						mx = max(mx, i1-i+j1-j1+2);
+						mx = max(mx, i1-i+j1-j+2);
 				}
+				i1--;
 			}
 		}
 	}
