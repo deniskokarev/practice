eba6183ae2a52d4a78f3b0a0de884694c6cdbbe0
Denis Kokarev <d_kokarev@mail.ru>
2017-12-27 21:29:28 -0500

    acmp 928 trivial cubic solution (no sqrt decomposition used)

--- c/acmp/acmp928.cpp
+++ c/acmp/acmp928.cpp
@@ -9,7 +9,7 @@ constexpr int MAXSZ = 2000;
 
 double xx[MAXSZ];
 double yy[MAXSZ];
-double sq[MAXSZ][MAXSZ]; // precomputed diagonals
+double diag[MAXSZ][MAXSZ]; // precomputed diagonals
 
 // improved cubic solution
 int main(int argc, char **argv) {
@@ -28,32 +28,29 @@ int main(int argc, char **argv) {
 	for (int i=0; i<n; i++) {
 		for (int j=0; j<m; j++) {
 			auto d = xx[i]-yy[j];
-			sq[i][j] = sqrt(w*w + d*d);
+			diag[i][j] = sqrt(w*w + d*d);
 		}
 	}
 	int mx = 0;
 	for (int i=0; i<n; i++) { // for each x
 		for (int j=0; j<m; j++) { // for each y
-			double dl = l-sq[i][j];
-			if (dl < sq[i][j]) // even 2 points won't fit (l<2*sq[i][j])
+			double dl = l-diag[i][j];
+			if (dl < diag[i][j]) // when even 2 points won't fit (l<2*diag[i][j])
 				continue;
-			int i1; // find rightmost second point on x using upper_bound binary search
+			int i1; // find rightmost point on x alley using upper_bound binary search
 			int f=i, t=n;
 			while (f<t) {
 				int m = f+(t-f)/2;
-				if (dl >= (xx[m]-xx[i]) + sq[m][j])
+				if (dl >= (xx[m]-xx[i]) + diag[m][j])
 					f = m+1;
 				else
 					t = m;
 			}
 			i1 = f-1;
-			int j1 = j; // rightmost second point on y alley
-			// sligind two pointers i1 and j1 in opposite directions
-			while (i1>=i && j1<m) {
-				while (j1<m) {
-					dl = l-sq[i][j]-sq[i1][j1]-(xx[i1]-xx[i])-(yy[j1]-yy[j]);
-					if (dl < 0)
-						break;
+			int j1 = j; // rightmost point on y alley
+			// sligind pointers i1 and j1 in opposite directions
+			while (i1 >= i && j1 < m) {
+				while (j1 < m && l >= diag[i][j]+diag[i1][j1]+(xx[i1]-xx[i])+(yy[j1]-yy[j])) {
 					mx = max(mx, i1-i+j1-j+2);
 					j1++;
 				}
