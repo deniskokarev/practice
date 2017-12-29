5322e7245a3519e697bc23b1ee5a6066d032dc9e
Denis Kokarev <d_kokarev@mail.ru>
2017-12-29 00:15:56 -0500

    acmp 1178 solution with comments

--- c/acmp/acmp1178.cpp
+++ c/acmp/acmp1178.cpp
@@ -3,18 +3,34 @@
 #include <memory.h>
 #include <assert.h>
 
-constexpr int MXHT = 1e5;
+#define dim(X)	(sizeof(X)/sizeof(X[0]))
+
+#define MXHT  100000
 
 enum {
 	CMD_INS = 1,
 	CMD_REM = 2
 };
 
-#define dim(X)	(sizeof(X)/sizeof(X[0]))
+static void fw_init(int *fw, int sz) {
+	memset(fw, 0, sizeof(fw[0])*sz);
+}
+
+static void fw_upd(int *fw, int p, int inc, int sz) {
+	for (int i=p; i<sz; i|=i+1)
+		fw[i] += inc;
+}
+
+static int fw_sum(int *fw, int p, int sz) {
+	int sum = 0;
+	for (int i=p; i>=0; i=(i&(i+1))-1)
+		sum += fw[i];
+	return sum;
+}
 
 int main(int argc, char **argv) {
-	int fw[MXHT+1];
-	memset(fw, 0, sizeof(fw)); // rev order (solder height 100000 stands at 0)
+	int fw[MXHT+1];		 // rev order (solder height 100000 stands at 0)
+	fw_init(fw, dim(fw));
 	int n;
 	scanf("%d", &n);
 	while (n-- > 0) {
@@ -22,32 +38,24 @@ int main(int argc, char **argv) {
 		scanf("%d%d", &cmd, &arg);
 		assert(arg >= 0 && arg <= MXHT);
 		int rp = MXHT-arg;	// rev pos
-		int npeople;
+		int f, t;
 		switch (cmd) {
 		case CMD_INS:
-			npeople = 0;
-			for (int i=rp-1; i>=0; i=(i&(i+1))-1)
-				npeople += fw[i];
-			printf("%d\n", npeople);
-			for (int i=rp; i<dim(fw); i|=i+1)
-				fw[i]++;
+			printf("%d\n", fw_sum(fw, rp-1, dim(fw)));
+			fw_upd(fw, rp, 1, dim(fw));
 			break;
 		case CMD_REM:
 			// lower_bound binary search
-			int f=0, t=dim(fw);
+			f = 0, t = dim(fw);
 			arg++;
 			while (f<t) {
 				int m = f+(t-f)/2;
-				npeople = 0;
-				for (int i=m; i>=0; i=(i&(i+1))-1)
-					npeople += fw[i];
-				if (npeople < arg)
+				if (fw_sum(fw, m, dim(fw)) < arg)
 					f = m+1;
 				else
 					t = m;
   			}
-			for (int i=f; i<dim(fw); i|=i+1)
-				fw[i]--;
+			fw_upd(fw, f, -1, dim(fw));
 			break;
 		}
 	}
