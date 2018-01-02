d46e2518986bb0a2b17ae7ec14edd6be815155ca
Denis Kokarev <dk1942@att.com>
2018-01-02 16:15:05 -0500

    cf900c solution

--- c/cf/cf900c.cc
+++ c/cf/cf900c.cc
@@ -4,41 +4,42 @@
 /* CodeForces CF900C problem */
 using namespace std;
 
+int fw_sum(int *fw, int p) {
+	int s = 0;
+	for (; p>=0; p=(p&(p+1))-1)
+		s += fw[p];
+	return s;
+}
+
+void fw_inc(int *fw, int p, int sz) {
+	for (; p<sz; p|=(p+1))
+		fw[p]++;
+}
+
+#define dim(X)	(int(sizeof(X)/sizeof(X[0])))
+
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
 	int aa[n];
 	for (auto &a:aa)
 		cin >> a;
-	int mx[n][2]; // 2 max elements to the left
-	mx[0][0] = aa[0];
-	mx[0][1] = -1;
-	for (int i=1; i<n; i++) {
-		if (aa[i] < mx[i-1][1]) {
-			mx[i][1] = mx[i-1][1];
-			mx[i][0] = mx[i-1][0];
-		} else if (aa[i] < mx[i-1][0]) {
-			mx[i][1] = aa[i];
-			mx[i][0] = mx[i-1][0];
-		} else {
-			mx[i][1] = mx[i-1][0];
-			mx[i][0] = aa[i];
-		}
+	int stat[n+1]; // contibutions/losses to the record
+	fill(stat, stat+dim(stat), 0);
+	int fw[n+1]; // number of less than current
+	fill(fw, fw+dim(fw), 0);
+	int mx = INT_MIN;
+	for (int i=0; i<n; i++) {
+		int cnt = fw_sum(fw, aa[i]-1);
+		int gr = i-cnt;
+		if (gr == 1)
+			stat[mx]--;
+		else if (gr == 0)
+			stat[aa[i]]++;
+		fw_inc(fw, aa[i], dim(fw));
+		mx = max(mx, aa[i]);
 	}
-	int recmx[n+1];
-	fill(recmx, recmx+n+1, 0);
-	for (int i=1; i<n; i++)
-		if (mx[i-1][0] > aa[i] && mx[i-1][1] < aa[i] && mx[i-1][1] > 0)
-			recmx[mx[i-1][0]]++;
-		else if (aa[i] > mx[i-1][0])
-			recmx[mx[i-1][0]]--;
-#if 0
-	for (int i=0; i<n; i++)
-		cerr << "mx0: " << mx[i][0] << " mx1: " << mx[i][1] << endl;
-	for (int i=0; i<=n; i++)
-		cerr << "recmx[" << i << "]: " << recmx[i] << endl;
-#endif
-	auto me = max_element(recmx+1, recmx+n+1);
-	cout << (me - recmx) << endl;
+	auto e = min_element(stat+1, stat+dim(stat));
+	cout << e-stat << endl;
 	return 0;
 }
