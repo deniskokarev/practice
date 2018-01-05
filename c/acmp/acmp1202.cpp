82ba9db0270070e02f012d36865d1cd4fcdd2c95
Denis Kokarev <dk1942@att.com>
2018-01-05 15:34:27 -0500

    acmp 1202 wrong answer 13

---
+++ c/acmp/acmp1202.cpp
@@ -0,0 +1,26 @@
+/* ACMP 1202 */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	int n, m;
+	cin >> n >> m;
+	int mm[n][n+1];
+	for (auto &mr:mm)
+		mr[0] = 0;
+	while (m-- > 0) {
+		int u, v;
+		cin >> u >> v;
+		u--;
+		mm[u][1+mm[u][0]++] = v;
+	}
+	cout << n << endl;
+	for (int i=0; i<n; i++) {
+		cout << mm[i][0] << ' ';
+		for (int j=0; j<mm[i][0]; j++)
+		    cout << mm[i][1+j] << ' ';
+		cout << endl;
+	}
+	return 0;
+}
