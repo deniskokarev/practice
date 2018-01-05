2daab3b9f753f3bc24aa7e6aa39183c9526ba62a
Denis Kokarev <dk1942@att.com>
2018-01-05 17:10:10 -0500

    acmp 1359 wrong answer 22

---
+++ c/acmp/acmp1359.cpp
@@ -0,0 +1,26 @@
+/* ACMP 1359 */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	int n, m;
+	cin >> n >> m;
+	int mm[n][n];
+	for (auto &mr:mm)
+		fill(mr, mr+n, 0);
+	int pos = 0;
+	int neg = 0;
+	while (m-- > 0) {
+		int i, j;
+		cin >> i >> j;
+		i--, j--;
+		if (mm[i][j] == 0 && mm[j][i] == 0)
+			pos++;
+		else
+			neg++;
+		mm[i][j] = mm[j][i] = 1;
+	}
+	cout << (neg==0 && pos==n*(n-1)/2 ? "YES":"NO") << endl;
+	return 0;
+}
