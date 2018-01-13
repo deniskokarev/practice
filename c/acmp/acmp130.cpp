dba36d77567c240002d5fadde0ccff061083837a
Denis Kokarev <d_kokarev@mail.ru>
2018-01-13 16:59:20 -0500

    acmp 130 runtime error 1

--- c/acmp/acmp130.cpp
+++ c/acmp/acmp130.cpp
@@ -5,46 +5,53 @@
 
 using namespace std;
 
-struct P {
-	short x, y;
-};
+constexpr int BSZ = 8;
 
-inline P operator+(const P a, const P b) {
-	return P {(short)(a.x+b.x), (short)(a.y+b.y)};
-}
+struct P {
+	int x, y;
+	P operator+(const P b) const {
+		return P {x+b.x, y+b.y};
+	}
+	operator bool() const {
+		return x>=0 && x<BSZ && y>=0 && y<BSZ;
+	}
+};
 
 static const P kmoves[] = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
 
-constexpr int BSZ = 8;
-
 int main(int argc, char **argv) {
-	vector<vector<vector<int>>> dist(2, vector<vector<int>>(BSZ, vector<int>(BSZ, 0)));
+	// epoch,color,row,col -> dist
+	vector<vector<vector<vector<int>>>> dist(2, vector<vector<vector<int>>>(2, vector<vector<int>>(BSZ, vector<int>(BSZ, 0))));
 	string sp1, sp2;
 	cin >> sp1 >> sp2;
-	P pp[2] = {P{(short)(sp1[0]-'a'), (short)(sp1[1]-'1')}+P{2,2}, P{(short)(sp2[0]-'a'), (short)(sp2[1]-'1')}+P{2,2}};
-	queue<P> qq[2];
-	for (int i=0; i<2; i++) {
-		qq[i].push(pp[i]);
-		dist[i][pp[i].y][pp[i].x] = 0;
-	}
+	P pp[2] = {P {sp1[0]-'a', sp1[1]-'1'}, P {sp2[0]-'a', sp2[1]-'1'}};
 	int ans = -1;
 	int epoch = 0;
-	while (qq[0].size() < BSZ*BSZ*BSZ) {
+	for (int color=0; color<2; color++)
+		dist[epoch][color][pp[color].y][pp[color].x] = 1;
+	// we know there will be a solution
+	while (true) {
 		epoch++;
-		for (int i=0; i<2; i++) {
-			for (int qsz = qq[i].size(); qsz>0; qsz--) {
-				const P &np = qq[i].front();
-				for (const P &km:kmoves) {
-					const P nnp = np+km;
-					if (nnp.y >= 0 && nnp.y < BSZ && nnp.x >= 0 && nnp.x < BSZ) {
-						dist[i][nnp.y][nnp.x] = epoch;
-						if (i == 1 && dist[1][nnp.y][nnp.x] == dist[0][nnp.y][nnp.x]) {
-							ans = epoch;
-							goto end;
+		for (int color=0; color<2; color++) {
+			auto &td = dist[epoch][color];
+			auto &od = dist[(epoch+1)&1][color];
+			auto &alttd = dist[epoch][(color+1)&1];
+			for (int i=0; i<BSZ; i++) {
+				for (int j=0; j<BSZ; j++) {
+					if (od[i][j] == epoch) {
+						P p = P {j, i};
+						for (const P &km:kmoves) {
+							const P np = p+km;
+							if (np) {
+								td[np.y][np.x] = epoch+1;
+								if (td[np.y][np.x] == alttd[np.y][np.x]) {
+									ans = epoch;
+									goto end;
+								}
+							}
 						}
 					}
 				}
-				qq[i].pop();
 			}
 		}
 	}
