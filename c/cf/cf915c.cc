0eb5af07de79f7ed24d862a0ef7b66f6168e21b7
Denis Kokarev <d_kokarev@mail.ru>
2018-01-16 19:29:21 -0500

    cf 915c timeout 6

---
+++ c/cf/cf915c.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+/* CodeForces CF915C problem */
+
+int main(int argc, char **argv) {
+	std::string a, b;
+	std::cin >> a >> b;
+	std::sort(a.begin(), a.end(), std::greater<char>());
+	std::string o(a);
+	if (b.length() == a.length()) {
+		int i=0;
+		while (i<b.size()) {
+			for (int j=0; j<a.length(); j++) {
+				if (a[j] == b[i]) {
+					o[i++] = a[j];
+					a[j] = '\127';
+					break;
+				} else if (a[j] < b[i]) {
+					o[i++] = a[j];
+					a[j] = '\127';
+					goto end;
+				}
+			}
+		}
+	end:
+	    for(int j=0; j<a.size(); j++)
+			if (a[j] != '\127')
+				o[i++] = a[j];
+	}
+	std::cout << o << std::endl;
+	return 0;
+}
