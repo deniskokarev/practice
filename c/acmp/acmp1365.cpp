5843e8be8235988a6cf994646e40a74eddcb95fe
Denis Kokarev <d_kokarev@mail.ru>
2018-01-20 14:11:09 -0500

    acmp 1365 hmm, still timeout 22

--- c/acmp/acmp1365.cpp
+++ c/acmp/acmp1365.cpp
@@ -9,7 +9,10 @@
 #define str(a) __str(a)
 
 struct P {
-	int x, y;
+	short int x, y;
+	inline P operator+(const P &b) const {
+		return P {(short int)(x+b.x), (short int)(y+b.y)};
+	}
 };
 
 const P moves[] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
@@ -19,36 +22,36 @@ int main(int argc, char **argv) {
 	unsigned dist[MSZ+2][MSZ+2];
 	memset(map, '#', sizeof(map));
 	memset(dist, 0xff, sizeof(dist));
-	int n, m;
+	short int n, m;
 	P start, end;
-	scanf("%d%d%d%d%d%d", &n, &m, &start.y, &start.x, &end.y, &end.x);
+	scanf("%hd%hd%hd%hd%hd%hd", &n, &m, &start.y, &start.x, &end.y, &end.x);
 	assert(n > 0 && m > 0 && start.x > 0 && start.y > 0 && end.x > 0 && end.y > 0);
 	assert(n <= MSZ && m <= MSZ && start.x <= MSZ && start.y <= MSZ && end.x <= MSZ && end.y <= MSZ);
 	for (int i=1; i<=n; i++)
 		scanf("%" str(MSZ) "s", map[i]+1);
 	dist[start.y][start.x] = 0;
-	const int qsz = 1<<(int)ceil(log2((n+2)*(m+2)));
-	const int qmask = qsz-1;
-	P qq[qsz];
-	int h = 0, t = 0;
-	qq[t++] = start;
+    const int qsz = 1<<(int)ceil(log2((n+2)*(m+2)));
+    const int qmask = qsz-1;
+    P qq[qsz];
+    int h = 0, t = 0;
+    qq[t++] = start;
 	while (h != t) {
-		const P &p = qq[h];
-		if (map[p.y][p.x] != '#') {
-			for (auto &m:moves) {
-				const P np = {p.x+m.x, p.y+m.y};
-				if (map[np.y][np.x] == '.' && dist[np.y][np.x] > dist[p.y][p.x]+1) {
-					dist[np.y][np.x] = dist[p.y][p.x]+1;
-					qq[t] = np;
-					t++; t &= qmask;
-				} else if (map[np.y][np.x] == 'W' && dist[np.y][np.x] > dist[p.y][p.x]+2) {
-					dist[np.y][np.x] = dist[p.y][p.x]+2;
-					qq[t] = np;
-					t++; t &= qmask;
-				}
+		const P p = qq[h++];
+		h &= qmask;
+		const unsigned d = dist[p.y][p.x];
+		for (auto m:moves) {
+			const P np = p + m;
+			unsigned &nd = dist[np.y][np.x];
+			if (map[np.y][np.x] == '.' && nd > d+1) {
+				nd = d+1;
+				qq[t++] = np;
+				t &= qmask;
+			} else if (map[np.y][np.x] == 'W' && nd > d+2) {
+				nd = d+2;
+				qq[t++] = np;
+				t &= qmask;
 			}
 		}
-		h++; h &= qmask;
 	}
 	int ans = (int)dist[end.y][end.x];
 	printf("%d\n", ans);
