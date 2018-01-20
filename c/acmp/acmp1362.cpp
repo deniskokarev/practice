bf95278fe42cf543f6b607f58d056022b1f3dcd5
Denis Kokarev <d_kokarev@mail.ru>
2018-01-19 20:34:26 -0500

    acmp 1362 solution

--- c/acmp/acmp1362.cpp
+++ c/acmp/acmp1362.cpp
@@ -11,21 +11,22 @@ enum {
 	P_FREE = 0,	P_WALL,	P_HALL
 };
 
-enum {
+enum { // directions
 	D_R = 0, D_D, D_L, D_U, D_SZ
 };
 
-struct B { // ball
+struct P { // point
+	int x, y;
+};
+
+struct B { // rolling ball
 	int x, y;
 	int dir;
 };
 
-const array<B, D_SZ> dirs {{ B {1, 0, D_R}, B {0, 1, D_D}, B {-1, 0, D_L}, B {0, -1, D_U} }};
+const array<P, D_SZ> dirs {{ P {1, 0}, P {0, 1}, P {-1, 0}, P {0, -1} }};
 
-struct D { // distance
-	int mnd;
-	array<int, D_SZ> d; // disance is direction-dependent
-};
+using D = array<int, D_SZ>; // disance is direction-dependent
 	
 int main(int argc, char **argv) {
 	int n, m;
@@ -34,38 +35,36 @@ int main(int argc, char **argv) {
 	for (int i=1; i<=n; i++)
 		for (int j=1; j<=m; j++)
 			cin >> map[i][j];
-	vector<vector<D>> dist(n+2, vector<D>(m+2, D {INT_MAX, {{INT_MAX, INT_MAX, INT_MAX, INT_MAX}}}));
+	vector<vector<D>> dist(n+2, vector<D>(m+2, D {{INT_MAX, INT_MAX, INT_MAX, INT_MAX}}));
 	queue<B> qq;
 	// start with 2 balls
 	qq.push(B {1, 1, D_R});
 	qq.push(B {1, 1, D_D});
-	dist[1][1] = {1, {{1, 1, 1, 1}}};
+	dist[1][1] = {{1, 1, 1, 1}};
 	while (qq.size() > 0) {
 		const B &p = qq.front();
 		if (map[p.y][p.x] != P_WALL) {
 			int dir = p.dir;
 			const B np = B {p.x+dirs[dir].x, p.y+dirs[dir].y, dir};
 			if (map[np.y][np.x] == P_WALL) {
-				dist[p.y][p.x].mnd = min(dist[p.y][p.x].mnd, dist[p.y][p.x].d[dir]);
 				// try to bounce into orthogonal direction 1
 				int o1 = (dir+1)%D_SZ; 
 				const B onp1 = B {p.x+dirs[o1].x, p.y+dirs[o1].y, o1};
-				if (dist[onp1.y][onp1.x].d[o1] > dist[p.y][p.x].mnd+1) {
-					dist[onp1.y][onp1.x].d[o1] = dist[p.y][p.x].mnd+1;
+				if (dist[onp1.y][onp1.x][o1] > dist[p.y][p.x][dir]+1) {
+					dist[onp1.y][onp1.x][o1] = dist[p.y][p.x][dir]+1;
 					qq.push(onp1);
 				}
 				// try to bounce into orthogonal direction 2
 				int o2 = (dir+3)%D_SZ;
 				const B onp2 = B {p.x+dirs[o2].x, p.y+dirs[o2].y, o2};
-				if (dist[onp2.y][onp2.x].d[o2] > dist[p.y][p.x].mnd+1) {
-					dist[onp2.y][onp2.x].d[o2] = dist[p.y][p.x].mnd+1;
+				if (dist[onp2.y][onp2.x][o2] > dist[p.y][p.x][dir]+1) {
+					dist[onp2.y][onp2.x][o2] = dist[p.y][p.x][dir]+1;
 					qq.push(onp2);
 				}
 			} else {
 				// keep rolling
-				if (dist[np.y][np.x].d[dir] > dist[p.y][p.x].d[dir]) {
-					dist[np.y][np.x].mnd = min(dist[np.y][np.x].mnd, dist[p.y][p.x].mnd);
-					dist[np.y][np.x].d[dir] = dist[p.y][p.x].d[dir];
+				if (dist[np.y][np.x][dir] > dist[p.y][p.x][dir]) {
+					dist[np.y][np.x][dir] = dist[p.y][p.x][dir];
 					qq.push(np);
 				}
 			}
@@ -76,7 +75,7 @@ int main(int argc, char **argv) {
 	for (int i=1; i<=n; i++)
 		for (int j=1; j<=m; j++)
 			if (map[i][j] == P_HALL)
-				ans = min(ans, dist[i][j].mnd);
+				ans = *min_element(dist[i][j].begin(), dist[i][j].end());
 	cout << ans << endl;
 	return 0;
 }
