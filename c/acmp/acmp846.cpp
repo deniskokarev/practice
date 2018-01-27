fec48aceda8ea9d04381a68a4390f9afc8808f0f
Denis Kokarev <d_kokarev@mail.ru>
2018-01-27 10:55:08 -0500

    acmp 846 runtime error 7

--- c/acmp/acmp846.cpp
+++ c/acmp/acmp846.cpp
@@ -14,23 +14,23 @@ const P moves[] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 int main(int argc, char **argv) {
 	int n, m;
 	cin >> n >> m;
-	vector<string> map(n+2, string(m+2, '1'));
-	for (int i=1; i<=n; i++) {
-		string s;
-		cin >> s;
-		copy(s.begin(), s.begin()+min(m, int(s.length())), map[i].begin()+1);
-	}
+	vector<string> map(n);
+	for (auto &r:map)
+		cin >> r;
 	P q;
 	cin >> q.x >> q.y;
+	q.x--, q.y--;
 	int l;
 	cin >> l;
 	struct M {
 		P p;
 		int c;
 	} mm[4];
-	for (int i=0; i<4; i++)
+	for (int i=0; i<4; i++) {
 		cin >> mm[i].p.x >> mm[i].p.y >> mm[i].c;
-	vector<vector<int>> dist(n+2, vector<int>(m+2, INT_MAX-1));
+		mm[i].p.x--; mm[i].p.y--;
+	}
+	vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
 	queue<P> qq;
 	if (map[q.y][q.x] == '0') {
 		qq.push(q);
