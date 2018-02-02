8ea9dd67ac3a6f59adc41e6948850e9b03aa7bc3
Denis Kokarev <dk1942@att.com>
2018-02-02 16:26:39 -0500

    cf 920a wrong answer 2

---
+++ c/cf/cf920a.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+/* CodeForces CF920A problem */
+using namespace std;
+
+int main(int argc, char **argv) {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		int x;
+		cin >> x;
+		int mx = x;
+		for (int i=1; i<k; i++) {
+			int x1;
+			cin >> x1;
+			mx = max(mx, (x1-x+1)/2);
+			x = x1;
+		}
+		mx = max(mx, n-x+1);
+		cout << mx << endl;
+	}
+	return 0;
+}
