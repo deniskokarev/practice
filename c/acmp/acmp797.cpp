c8d96de62be2c369783ba4eedb83f18b019d2d16
Denis Kokarev <d_kokarev@mail.ru>
2018-02-10 10:50:14 -0500

    acmp out of mem 2

--- c/acmp/acmp797.cpp
+++ c/acmp/acmp797.cpp
@@ -41,8 +41,59 @@ struct Q {
 	}
 };
 
+// precompute segment subtractions
+struct SubtractSeg {
+	IR cached_res[SZ][SZ][SZ][SZ];
+	SubtractSeg() {
+		for (int8_t rl=0; rl<SZ; rl++) {
+			for (int8_t rr=0; rr<SZ; rr++) {
+				for (int8_t l=0; l<SZ; l++) {
+					for (int8_t r=0; r<SZ; r++) {
+						if (rr<rl || r<l) {
+							cached_res[l][r][rl][rr] = {INT8_MAX, INT8_MIN};
+						} else {
+							struct E {
+								int8_t e;
+								int8_t v;
+								bool operator<(const E &b) const {
+									return e<b.e;
+								}
+							} ends[4] = {{rl, +1}, {int8_t(rr+1), -1}, {l, -1}, {int8_t(r+1), +1}};
+							sort(ends, ends+4);
+							struct {
+								int8_t l, r;
+								int8_t v;
+							} ir[3] = {{ends[0].e, ends[1].e, ends[0].v}, {ends[1].e, ends[2].e, ends[1].v}, {ends[2].e, ends[3].e, ends[2].v}};
+							ir[1].v += ir[0].v;
+							ir[2].v += ir[1].v;
+							int8_t mne = INT8_MAX, mxe = INT8_MIN;
+							for (int i=0; i<3; i++) {
+								if (ir[i].l < ir[i].r && ir[i].v == 1) {
+									mne = min(mne, ir[i].l);
+									mxe = max(mxe, ir[i].r);
+								}
+							}
+							if (mne < mxe)
+								cached_res[l][r][rl][rr] = IR {mne, int8_t(mxe-1)};
+							else
+								cached_res[l][r][rl][rr] = IR {INT8_MAX, INT8_MIN};
+						}
+					}
+				}
+			}
+		}
+	}
+	IR sub(IR a, IR b) const {
+		if (a)
+			return cached_res[b.l][b.r][a.l][a.r];
+		else
+			return a;
+	}
+};
+
 // "subtract" the segment [l,r] from remaining rows row, row+1,..,row+k-1
 static void upd_rows_rem(REM &rem, const vector<vector<int>> &map, int row, int8_t l, int8_t r, int k) {
+	static const SubtractSeg sseg;
 	if (r<l)
 		swap(l, r);
 	for (int i=0; i<k; i++) {
@@ -51,33 +102,9 @@ static void upd_rows_rem(REM &rem, const vector<vector<int>> &map, int row, int8
 			break;
 		auto &rows_y = rem[y];
 		if (rows_y) {
-			struct E {
-				int8_t e;
-				int8_t v;
-				bool operator<(const E &b) const {
-					return e<b.e;
-				}
-			} ends[4] = {{rows_y.l, +1}, {int8_t(rows_y.r+1), -1}, {l, -1}, {int8_t(r+1), +1}};
-			sort(ends, ends+4);
-			struct {
-				int8_t l, r;
-				int8_t v;
-			} ir[3] = {{ends[0].e, ends[1].e, ends[0].v}, {ends[1].e, ends[2].e, ends[1].v}, {ends[2].e, ends[3].e, ends[2].v}};
-			ir[1].v += ir[0].v;
-			ir[2].v += ir[1].v;
-			int8_t mne = INT8_MAX, mxe = INT8_MIN;
-			for (int i=0; i<3; i++) {
-				if (ir[i].l < ir[i].r && ir[i].v == 1) {
-					mne = min(mne, ir[i].l);
-					mxe = max(mxe, ir[i].r);
-				}
-			}
-			if (mne < mxe) {
-				rows_y = IR {mne, int8_t(mxe-1)};
-			} else {
-				rows_y = IR {INT8_MAX, INT8_MIN};
+			rows_y = sseg.sub(rows_y, IR {l, r});
+			if (!rows_y)
 				rem.cnt--;
-			}
 		}
 	}
 }
