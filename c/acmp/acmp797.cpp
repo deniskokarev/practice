8cda13b74dd8316cbb7bfea93ea1156d62a1f794
Denis Kokarev <d_kokarev@mail.ru>
2018-02-10 11:48:04 -0500

    acmp 797 timeout 2

--- c/acmp/acmp797.cpp
+++ c/acmp/acmp797.cpp
@@ -43,41 +43,41 @@ struct Q {
 
 // precompute segment subtractions
 struct SubtractSeg {
-	IR cached_res[SZ][SZ][SZ][SZ];
+	vector<vector<vector<vector<IR>>>> cached_res;
 	SubtractSeg() {
+		cached_res.resize(SZ);
 		for (int8_t rl=0; rl<SZ; rl++) {
-			for (int8_t rr=0; rr<SZ; rr++) {
+			cached_res[rl].resize(SZ-rl);
+			for (int8_t r_len=0; r_len<SZ-rl; r_len++) {
+				cached_res[rl][r_len].resize(SZ);
 				for (int8_t l=0; l<SZ; l++) {
-					for (int8_t r=0; r<SZ; r++) {
-						if (rr<rl || r<l) {
-							cached_res[l][r][rl][rr] = {INT8_MAX, INT8_MIN};
-						} else {
-							struct E {
-								int8_t e;
-								int8_t v;
-								bool operator<(const E &b) const {
-									return e<b.e;
-								}
-							} ends[4] = {{rl, +1}, {int8_t(rr+1), -1}, {l, -1}, {int8_t(r+1), +1}};
-							sort(ends, ends+4);
-							struct {
-								int8_t l, r;
-								int8_t v;
-							} ir[3] = {{ends[0].e, ends[1].e, ends[0].v}, {ends[1].e, ends[2].e, ends[1].v}, {ends[2].e, ends[3].e, ends[2].v}};
-							ir[1].v += ir[0].v;
-							ir[2].v += ir[1].v;
-							int8_t mne = INT8_MAX, mxe = INT8_MIN;
-							for (int i=0; i<3; i++) {
-								if (ir[i].l < ir[i].r && ir[i].v == 1) {
-									mne = min(mne, ir[i].l);
-									mxe = max(mxe, ir[i].r);
-								}
+					cached_res[rl][r_len][l].resize(SZ-l);
+					for (int8_t len=0; len<SZ-l; len++) {
+						struct E {
+							int8_t e;
+							int8_t v;
+							bool operator<(const E &b) const {
+								return e<b.e;
 							}
-							if (mne < mxe)
-								cached_res[l][r][rl][rr] = IR {mne, int8_t(mxe-1)};
-							else
-								cached_res[l][r][rl][rr] = IR {INT8_MAX, INT8_MIN};
+						} ends[4] = {{rl, +1}, {int8_t(rl+r_len+1), -1}, {l, -1}, {int8_t(l+len+1), +1}};
+						sort(ends, ends+4);
+						struct {
+							int8_t l, r;
+							int8_t v;
+						} ir[3] = {{ends[0].e, ends[1].e, ends[0].v}, {ends[1].e, ends[2].e, ends[1].v}, {ends[2].e, ends[3].e, ends[2].v}};
+						ir[1].v += ir[0].v;
+						ir[2].v += ir[1].v;
+						int8_t mne = INT8_MAX, mxe = INT8_MIN;
+						for (int i=0; i<3; i++) {
+							if (ir[i].l < ir[i].r && ir[i].v == 1) {
+								mne = min(mne, ir[i].l);
+								mxe = max(mxe, ir[i].r);
+							}
 						}
+						if (mne < mxe)
+							cached_res[rl][r_len][l][len] = IR {mne, int8_t(mxe-1)};
+						else
+							cached_res[rl][r_len][l][len] = IR {INT8_MAX, INT8_MIN};
 					}
 				}
 			}
@@ -85,7 +85,7 @@ struct SubtractSeg {
 	}
 	IR sub(IR a, IR b) const {
 		if (a)
-			return cached_res[b.l][b.r][a.l][a.r];
+			return cached_res[a.l][a.r-a.l][b.l][b.r-b.l];
 		else
 			return a;
 	}
@@ -94,8 +94,6 @@ struct SubtractSeg {
 // "subtract" the segment [l,r] from remaining rows row, row+1,..,row+k-1
 static void upd_rows_rem(REM &rem, const vector<vector<int>> &map, int row, int8_t l, int8_t r, int k) {
 	static const SubtractSeg sseg;
-	if (r<l)
-		swap(l, r);
 	for (int i=0; i<k; i++) {
 		int y = row+i;
 		if (y >= SZ)
@@ -136,24 +134,27 @@ int main(int argc, char **argv) {
 	for (int8_t y=0; y<SZ; y++)
 		if (allrows[y])
 			rem.cnt++;
+	priority_queue<Q> qq;
 	for (int y=0; y<5; y++)
 		rem.rows[y] = allrows[y];
 	upd_rows_rem(rem, map, 0, 0, k-1, k);
-	Q q {0, P{0, 0}, rem};
-	while (true) {
+	qq.push(Q {0, P{0, 0}, rem});
+	while (!qq.empty()) {
+		const Q q = qq.top();
 		if (q.rem.cnt == 0)
 			break;
+		qq.pop();
 		const auto &row_y = q.rem[q.p.y];
 		if (row_y) {
 			if (q.p.x > row_y.l) {
 				REM nr(q.rem);
 				upd_rows_rem(nr, map, q.p.y, row_y.l, q.p.x+k-1, k);
-				q = Q { q.dist+(q.p.x-row_y.l), P { row_y.l, q.p.y}, nr};
+				qq.push(Q { q.dist+(q.p.x-row_y.l), P { row_y.l, q.p.y}, nr});
 			}
 			if (q.p.x+k-1 < row_y.r) {
 				REM nr(q.rem);
 				upd_rows_rem(nr, map, q.p.y, q.p.x, row_y.r, k);
-				q = Q { q.dist+(row_y.r-q.p.x-k+1), P { row_y.r-k+1, q.p.y}, nr};
+				qq.push(Q { q.dist+(row_y.r-q.p.x-k+1), P { row_y.r-k+1, q.p.y}, nr});
 			}
 		} else {
 			// done with row, just up
@@ -161,10 +162,11 @@ int main(int argc, char **argv) {
 			if (q.p.y+5 < SZ)
 				nr[q.p.y] = allrows[q.p.y+5];
 			upd_rows_rem(nr, map, q.p.y+1, q.p.x, q.p.x+k-1, k);
-			q = Q { q.dist+1, P { q.p.x, q.p.y+1}, nr};
+			qq.push(Q { q.dist+1, P { q.p.x, q.p.y+1}, nr});
 		}
 	}
-	const Q &top = q;
+	assert(!qq.empty() && "there must be a solution");
+	const Q &top = qq.top();
 	int over = top.p.y+k-1 - mxy;
 	if (over < 0 || top.p.y == 0)
 		over = 0;
