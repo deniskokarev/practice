2f7e4f9d7cfe75aa64adfa3763f67f6aeeb5c101
Denis Kokarev <d_kokarev@mail.ru>
2018-02-17 18:02:35 -0500

    acmp1370 solution

--- c/acmp/acmp1370.cpp
+++ c/acmp/acmp1370.cpp
@@ -1,21 +1,39 @@
 /* ACMP 1370 */
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	int ans = INT_MAX;
+	int64_t mm[n][n];
+	for (int i=0; i<n; i++)
+		for (int j=0; j<n; j++)
+			cin >> mm[i][j];
+	for (int k=0; k<n; k++)
+		for (int i=0; i<n; i++)
+			for (int j=0; j<n; j++)
+				mm[i][j] = min(mm[i][j], mm[i][k]+mm[k][j]);
+	int64_t mm2[n][n];
+	memcpy(mm2, mm, sizeof(mm));
+	for (int k=0; k<n; k++)
+		for (int i=0; i<n; i++)
+			for (int j=0; j<n; j++)
+				mm2[i][j] = min(mm2[i][j], mm2[i][k]+mm[k][j]);
+	int64_t ans = INT_MAX;
 	for (int i=0; i<n; i++) {
 		for (int j=0; j<n; j++) {
-			int v;
-			cin >> v;
 			if (i != j) {
-				ans = min(ans, v);
+				if (mm2[i][j] >= mm[i][j]) {
+					ans = min(ans, mm2[i][j]);
+				} else {
+					cout << -1 << endl;
+					return 0;
+				}
 			}
 		}
 	}
-	cout << ((ans<0)?-1:ans) << endl;
+	cout << ans << endl;
 	return 0;
 }
