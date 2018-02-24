e0ac607eab9c95f9511916d113238e0c2186407d
Denis Kokarev <d_kokarev@mail.ru>
2018-02-24 13:35:00 -0500

    acmp 137 solution

--- c/acmp/acmp137.cpp
+++ c/acmp/acmp137.cpp
@@ -10,6 +10,8 @@ struct N {
 	int dist;
 };
 
+constexpr int DMIN = -100 * 100 * 100 * 2; // cannot get lower than that without neg loops
+
 int main(int argc, char **argv) {
 	int n;
 	scanf("%d", &n);
@@ -24,21 +26,21 @@ int main(int argc, char **argv) {
 		for (int i=0; i<n; i++)
 			for (int j=0; j<n; j++)
 				if (mm[i][j].conn && mm[i][k].conn && mm[k][j].conn)
-					mm[i][j] = {1, min(mm[i][j].dist, mm[i][k].dist+mm[k][j].dist)};
+					mm[i][j] = {1, min(mm[i][j].dist, max(DMIN, mm[i][k].dist+mm[k][j].dist))};
 				else if (mm[i][k].conn && mm[k][j].conn)
-					mm[i][j] = {1, mm[i][k].dist+mm[k][j].dist};
+					mm[i][j] = {1, max(DMIN, mm[i][k].dist+mm[k][j].dist)};
 	N savemm[n][n];
 	memcpy(savemm, mm, sizeof(mm));
 	for (int k=0; k<n; k++)
 		for (int i=0; i<n; i++)
 			for (int j=0; j<n; j++)
 				if (mm[i][j].conn && mm[i][k].conn && mm[k][j].conn)
-					mm[i][j] = {1, min(mm[i][j].dist, mm[i][k].dist+mm[k][j].dist)};
+					mm[i][j] = {1, min(mm[i][j].dist, max(DMIN, mm[i][k].dist+mm[k][j].dist))};
 				else if (mm[i][k].conn && mm[k][j].conn)
-					mm[i][j] = {1, mm[i][k].dist+mm[k][j].dist};
+					mm[i][j] = {1, max(DMIN, mm[i][k].dist+mm[k][j].dist)};
 	for (int i=0; i<n; i++)
 		for (int j=0; j<n; j++)
-			if (mm[i][j].dist < savemm[i][j].dist)
+			if (mm[i][j].dist == DMIN || mm[i][j].dist < savemm[i][j].dist)
 				mm[i][j].conn = 2;
 			else if (mm[i][j].dist)
 				mm[i][j].conn = 1;
