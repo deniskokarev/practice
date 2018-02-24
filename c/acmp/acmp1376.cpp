9c6fab28284766a9e0eebb1762184bac830bd119
Denis Kokarev <d_kokarev@mail.ru>
2018-02-24 18:25:00 -0500

    acmp 1376 out of mem 13

---
+++ c/acmp/acmp1376.cpp
@@ -0,0 +1,92 @@
+/* ACMP 1376 */
+#include <cstdio>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+struct N {
+	int conn;
+	int dist;
+	int to;
+	int to_id;
+};
+
+constexpr int DMAX = INT_MAX/2; // cannot get higher than that without loops
+
+void append_path(vector<int> &path, const vector<vector<N>> &mm, int f, int t) {
+	while (mm[f][t].to != t) {
+		path.push_back(mm[f][t].to_id);
+		f = mm[f][t].to;
+	}
+	path.push_back(mm[f][t].to_id);
+}
+
+int main(int argc, char **argv) {
+	int n, m, k;
+	scanf("%d %d %d", &n, &m, &k);
+	vector<vector<N>> mm(n+1, vector<N>(n+1, N {0, 0, 0, 0}));
+	for (int id=1; id<=m; id++) {
+		int i, j;
+		int d;
+		scanf("%d %d %d", &i, &j, &d);
+		mm[i][j] = N {1, d, j, id};
+	}
+#if 0
+	for (int i=1; i<=n; i++) {
+		for (int j=1; j<=n; j++)
+			fprintf(stderr, "(c:%d d:%d to:%d) ", mm[i][j].conn, mm[i][j].dist, mm[i][j].to);
+		fprintf(stderr, "\n");
+	}
+	fprintf(stderr, "\n");
+#endif
+	for (int k=1; k<=n; k++) {
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n; j++) {
+				int ndist = mm[i][k].dist+mm[k][j].dist;
+				if (ndist > DMAX)
+					ndist = DMAX;
+				if (mm[i][j].conn && mm[i][k].conn && mm[k][j].conn) {
+					if (mm[i][j].dist < ndist)
+						mm[i][j] = N {1, ndist, mm[i][k].to, mm[i][k].to_id};
+				} else if (mm[i][k].conn && mm[k][j].conn) {
+					mm[i][j] = N {1, ndist, mm[i][k].to, mm[i][k].to_id};
+				}
+			}
+		}
+	}
+	for (int i=1; i<=n; i++)
+		if (mm[i][i].dist > 0)
+				mm[i][i].conn = 2; // pos loop detected
+#if 0
+	for (int i=1; i<=n; i++) {
+		for (int j=1; j<=n; j++)
+			fprintf(stderr, "(c:%d d:%d to:%d) ", mm[i][j].conn, mm[i][j].dist, mm[i][j].to);
+		fprintf(stderr, "\n");
+	}
+	fprintf(stderr, "\n");
+#endif
+	bool inf = false;
+	vector<int> path;
+	int f, t;
+	scanf("%d", &t);
+	k--;
+	while (k--) {
+		f = t;
+		scanf("%d", &t);
+		if (mm[f][f].conn == 2 || mm[t][t].conn == 2) {
+			inf = true;
+			break;
+		}
+		append_path(path, mm, f, t);
+	}
+	if (!inf) {
+		printf("%d\n", (int)path.size());
+		for (auto p:path)
+			printf("%d ", p);
+		printf("\n");
+	} else {
+		printf("infinitely kind\n");
+	}
+	return 0;
+}
