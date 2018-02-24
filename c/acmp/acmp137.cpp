9fd89cee5c6cbb2599ad3503046e647ca5692b6f
Denis Kokarev <d_kokarev@mail.ru>
2018-02-24 13:17:12 -0500

    acmp 137 wrong answer 6

--- c/acmp/acmp137.cpp
+++ c/acmp/acmp137.cpp
@@ -5,40 +5,46 @@
 
 using namespace std;
 
+struct N {
+	int conn;
+	int dist;
+};
+
 int main(int argc, char **argv) {
 	int n;
 	scanf("%d", &n);
-	int mm[n][n];
-	for (int i=0; i<n; i++)
-		for (int j=0; j<n; j++)
-			scanf("%d", &mm[i][j]);
-	for (int i=0; i<n; i++)
-		mm[i][i] = 1;
+	N mm[n][n];
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+			scanf("%d", &mm[i][j].dist);
+			mm[i][j].conn = (i==j) || (mm[i][j].dist != 0);
+		}
+	}
 	for (int k=0; k<n; k++)
 		for (int i=0; i<n; i++)
 			for (int j=0; j<n; j++)
-				if (mm[i][j]*mm[i][k]*mm[k][j])
-					mm[i][j] = min(mm[i][j], mm[i][k]+mm[k][j]);
-				else if (mm[i][k]*mm[k][j])
-					mm[i][j] = mm[i][k]+mm[k][j];
-	int savemm[n][n];
+				if (mm[i][j].conn && mm[i][k].conn && mm[k][j].conn)
+					mm[i][j] = {1, min(mm[i][j].dist, mm[i][k].dist+mm[k][j].dist)};
+				else if (mm[i][k].conn && mm[k][j].conn)
+					mm[i][j] = {1, mm[i][k].dist+mm[k][j].dist};
+	N savemm[n][n];
 	memcpy(savemm, mm, sizeof(mm));
 	for (int k=0; k<n; k++)
 		for (int i=0; i<n; i++)
 			for (int j=0; j<n; j++)
-				if (mm[i][j]*mm[i][k]*mm[k][j])
-					mm[i][j] = min(mm[i][j], mm[i][k]+mm[k][j]);
-				else if (mm[i][k]*mm[k][j])
-					mm[i][j] = mm[i][k]+mm[k][j];
+				if (mm[i][j].conn && mm[i][k].conn && mm[k][j].conn)
+					mm[i][j] = {1, min(mm[i][j].dist, mm[i][k].dist+mm[k][j].dist)};
+				else if (mm[i][k].conn && mm[k][j].conn)
+					mm[i][j] = {1, mm[i][k].dist+mm[k][j].dist};
 	for (int i=0; i<n; i++)
 		for (int j=0; j<n; j++)
-			if (mm[i][j] < savemm[i][j])
-				mm[i][j] = 2;
-			else if (mm[i][j])
-				mm[i][j] = 1;
+			if (mm[i][j].dist < savemm[i][j].dist)
+				mm[i][j].conn = 2;
+			else if (mm[i][j].dist)
+				mm[i][j].conn = 1;
 	for (int i=0; i<n; i++) {
 		for (int j=0; j<n; j++)
-			printf("%d ", mm[i][j]);
+			printf("%d ", mm[i][j].conn);
 		printf("\n");
 	}
 	return 0;
