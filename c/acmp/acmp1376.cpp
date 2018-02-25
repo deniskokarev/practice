2995c56c624ad57cb1d226c93d7d169810b10851
Denis Kokarev <d_kokarev@mail.ru>
2018-02-24 22:35:22 -0500

    acmp 1376 out of mem 8 (going backwards)

--- c/acmp/acmp1376.cpp
+++ c/acmp/acmp1376.cpp
@@ -9,15 +9,17 @@ using namespace std;
 
 constexpr int DINF = INT_MAX/2;
 
+struct N {
+	int conn;
+	int w;
+	int to;
+	int id;
+};
+
 int main(int argc, char **argv) {
 	int n, m, k;
 	scanf("%d %d %d", &n, &m, &k);
-	struct {
-		int conn;
-		int w;
-		int to;
-		int id;
-	} mm[n][n];
+	N mm[n][n];
 	memset(mm, 0, sizeof(mm));
 	for (int id=0; id<m; id++) {
 		int i, j, w;
@@ -39,6 +41,26 @@ int main(int argc, char **argv) {
 			}
 		}
 	}
+	N savemm[n][n];
+	memcpy(savemm, mm, sizeof(mm));
+	// floyd again to reliably detect pos loops 
+	for (int k=0; k<n; k++) {
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				int ndist = min(DINF, mm[i][k].w+mm[k][j].w);
+				if (mm[i][j].conn && mm[i][k].conn && mm[k][j].conn) {
+					if (mm[i][j].w < ndist)
+						mm[i][j] = {1, ndist, mm[i][k].to, mm[i][k].id}; 
+				} else if (mm[i][k].conn && mm[k][j].conn) {
+					mm[i][j] = {1, ndist, mm[i][k].to, mm[i][k].id};
+				}
+			}
+		}
+	}
+	for (int i=0; i<n; i++)
+		if (mm[i][i].w > savemm[i][i].w)
+			savemm[i][i].conn = 2; // mark loops
+	memcpy(mm, savemm, sizeof(mm));
 	vector<int> path;
 	int t;
 	scanf("%d", &t);
@@ -49,17 +71,17 @@ int main(int argc, char **argv) {
 		t--;
 		int n = f;
 		while (n != t) {
-			if (mm[n][n].w > 0) {
+			if (mm[n][n].conn == 2) {
 				printf("infinitely kind\n");
 				goto end;
-			}
+			}	
 			path.push_back(mm[n][t].id);
 			n = mm[n][t].to;
 		}
-		if (mm[n][n].w > 0) {
+		if (mm[n][n].conn == 2) {
 			printf("infinitely kind\n");
 			goto end;
-		}
+		}	
 	}
 	printf("%d\n", (int)path.size());
 	for (auto p:path)
