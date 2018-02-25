cfe8daf126ad915c6b54021eb3cb0ad739b5ff54
Denis Kokarev <d_kokarev@mail.ru>
2018-02-24 20:55:34 -0500

    acmp 1376, out of mem 13 again

--- c/acmp/acmp1376.cpp
+++ c/acmp/acmp1376.cpp
@@ -1,84 +1,70 @@
 /* ACMP 1376 */
 #include <cstdio>
-#include <vector>
 #include <climits>
-#include <cassert>
+#include <cstring>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-struct N {
-	int conn;
-	int dist;
-	int to;
-	int to_id;
-};
-
-constexpr int DMAX = INT_MAX/2; // cannot get higher than that without loops
-constexpr int DMIN = INT_MIN/2; // cannot get lower than that without loops
-
-void append_path(vector<int> &path, const vector<vector<N>> &mm, int f, int t) {
-	int cnt = 0;
-	while (mm[f][t].to != t) {
-		path.push_back(mm[f][t].to_id);
-		f = mm[f][t].to;
-		cnt++;
-		assert(cnt < 1000 && "should be no loops");
-	}
-	path.push_back(mm[f][t].to_id);
-}
+constexpr int DINF = INT_MAX/2;
 
 int main(int argc, char **argv) {
 	int n, m, k;
 	scanf("%d %d %d", &n, &m, &k);
-	vector<vector<N>> mm(n+1, vector<N>(n+1, N {0, 0, 0, 0}));
-	for (int id=1; id<=m; id++) {
-		int i, j;
-		int d;
-		scanf("%d %d %d", &i, &j, &d);
-		mm[i][j] = N {1, d, j, id};
+	struct {
+		int conn;
+		int w;
+		int to;
+		int id;
+	} mm[n][n];
+	memset(mm, 0, sizeof(mm));
+	for (int id=0; id<m; id++) {
+		int i, j, w;
+		scanf("%d %d %d", &i, &j, &w);
+		i--, j--;
+		mm[i][j] = {1, w, j, id};
 	}
-	for (int twice:{0,1})
-	for (int k=1; k<=n; k++) {
-		for (int i=1; i<=n; i++) {
-			for (int j=1; j<=n; j++) {
-				int ndist = mm[i][k].dist+mm[k][j].dist;
-				if (ndist > DMAX)
-					ndist = DMAX;
-				if (ndist < DMIN)
-					ndist = DMIN;
+	// floyd
+	for (int k=0; k<n; k++) {
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				int ndist = min(DINF, mm[i][k].w+mm[k][j].w);
 				if (mm[i][j].conn && mm[i][k].conn && mm[k][j].conn) {
-					if (mm[i][j].dist < ndist)
-						mm[i][j] = N {1, ndist, mm[i][k].to, mm[i][k].to_id};
+					if (mm[i][j].w < ndist)
+						mm[i][j] = {1, ndist, mm[i][k].to, mm[i][k].id}; 
 				} else if (mm[i][k].conn && mm[k][j].conn) {
-					mm[i][j] = N {1, ndist, mm[i][k].to, mm[i][k].to_id};
+					mm[i][j] = {1, ndist, mm[i][k].to, mm[i][k].id};
 				}
 			}
 		}
 	}
-	for (int i=1; i<=n; i++)
-		if (mm[i][i].dist > 0)
-				mm[i][i].conn = 2; // pos loop detected
-	bool inf = false;
 	vector<int> path;
-	int f, t;
+	int t;
 	scanf("%d", &t);
-	k--;
+	t--, k--;
 	while (k--) {
-		f = t;
+		int f = t;
 		scanf("%d", &t);
-		if (mm[f][f].conn == 2 || mm[t][t].conn == 2) {
-			inf = true;
-			break;
+		t--;
+		int n = f;
+		while (n != t) {
+			if (mm[n][n].w > 0) {
+				printf("infinitely kind\n");
+				goto end;
+			}
+			path.push_back(mm[n][t].id);
+			n = mm[n][t].to;
 		}
-		append_path(path, mm, f, t);
+		if (mm[n][n].w > 0) {
+			printf("infinitely kind\n");
+			goto end;
+		}
 	}
-	if (!inf) {
-		printf("%d\n", (int)path.size());
-		for (auto p:path)
-			printf("%d ", p);
-		printf("\n");
-	} else {
-		printf("infinitely kind\n");
-	}
+	printf("%d\n", (int)path.size());
+	for (auto p:path)
+		printf("%d ", p+1);
+	printf("\n");
+ end:
 	return 0;
 }
