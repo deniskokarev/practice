46f6aa7a97b563f9d653bedbc4726d1997bd184e
Denis Kokarev <d_kokarev@mail.ru>
2018-02-25 12:16:43 -0500

    acmp 140 solution

--- c/acmp/acmp140.cpp
+++ c/acmp/acmp140.cpp
@@ -5,18 +5,23 @@
 
 using namespace std;
 
-constexpr int DINF = 100000;
+constexpr int DINF = INT_MAX/2;
 
 int main(int argc, char **argv) {
 	int n;
 	scanf("%d", &n);
 	int mm[n][n];
-	for (int i=0; i<n; i++)
-		for (int j=0; j<n; j++)
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
 			scanf("%d", &mm[i][j]);
+			if (mm[i][j] > 10000)
+				mm[i][j] = DINF;
+		}
+	}
 	// ford
 	int dd[n];
 	fill(dd, dd+n, DINF);
+	dd[0] = 0;
 	for (int v=0; v<n; v++) {
 		for (int i=0; i<n; i++)
 			for (int j=0; j<n; j++)
