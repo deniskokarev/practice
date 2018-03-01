4978be9b223edbf7aaae1362bb4e34e0a0e5e1a5
Denis Kokarev <d_kokarev@mail.ru>
2018-02-28 21:56:49 -0500

    acmp 139, wtf, still wrong answer 13

--- c/acmp/acmp139.cpp
+++ c/acmp/acmp139.cpp
@@ -25,32 +25,50 @@ int main(int argc, char **argv) {
 	} dd[n+1];
 	fill(dd, dd+n+1, D {0, false, 0});
 	dd[1] = D {0, true};
-	for (int v=0; v<n; v++) {
+	bool path[n+1];
+	fill(path, path+n+1, 0);
+	for (int v=0; v<n-1; v++) {
 		for (const auto &e:ee) {
 			int64_t nd = min(dd[e.i].w+e.w, DINF);
 			if (dd[e.j].conn && dd[e.i].conn) {
-				if (dd[e.j].w < nd)
+				if (dd[e.j].w < nd) {
 					dd[e.j] = D {nd, true, dd[e.i].l+1};
+					path[e.j] = e.i;
+				}
 			} else if (dd[e.i].conn) {
 				dd[e.j] = D {nd, true, dd[e.i].l};
+				path[e.j] = e.i;
 			}
 		}
 	}
-	D ans = dd[n];
-	if (ans.conn) {
+	D savedd[n+1];
+	copy(dd, dd+n+1, savedd);
+	if (dd[n].conn) {
 		for (const auto &e:ee) {
 			int64_t nd = min(dd[e.i].w+e.w, DINF);
 			if (dd[e.j].conn && dd[e.i].conn) {
-				if (dd[e.j].w < nd)
+				if (dd[e.j].w < nd) {
 					dd[e.j] = D {nd, true, dd[e.i].l+1};
+					path[e.j] = e.i;
+				}
 			} else if (dd[e.i].conn) {
 				dd[e.j] = D {nd, true, dd[e.i].l};
+				path[e.j] = e.i;
 			}
 		}
-		if (ans.w == DINF || ans.l < dd[n].l)
+		bool spath[n+1];
+		fill(spath, spath+n+1, false);
+		spath[1] = spath[n] = true;
+		for (int ni=path[n]; ni!=1; ni=path[ni])
+			spath[ni] = true;
+		bool loop = false;
+		for (int i=1; i<n+1; i++)
+			if (spath[i] && (dd[i].w == DINF || dd[i].w > savedd[i].w))
+				loop = true;
+		if (loop)
 			printf(":)\n");
 		else
-			printf("%lld\n", ans.w);
+			printf("%lld\n", dd[n].w);
 	} else {
 		printf(":(\n");
 	}
