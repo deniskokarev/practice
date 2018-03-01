c945f9df182787685437892f31aa05c06987ba72
Denis Kokarev <d_kokarev@mail.ru>
2018-02-28 20:08:04 -0500

    acmp 139, still wrong answer 13

--- c/acmp/acmp139.cpp
+++ c/acmp/acmp139.cpp
@@ -18,20 +18,40 @@ int main(int argc, char **argv) {
 	for (int ei=0; ei<m; ei++)
 		scanf("%d%d%d", &ee[ei].i, &ee[ei].j, &ee[ei].w);
 	// ford
-	int64_t dd[n+1];
-	fill(dd, dd+n+1, -DINF);
-	dd[1] = 0;
-	for (int v=0; v<n-1; v++)
-		for (const auto &e:ee)
-			dd[e.j] = max(dd[e.j], min(dd[e.i]+e.w, DINF));
-	int64_t ans = dd[n];
-	for (const auto &e:ee)
-		dd[e.j] = max(dd[e.j], min(dd[e.i]+e.w, DINF));
-	if (ans == DINF || ans < dd[n])
-		printf(":)\n");
-	else if (ans <= -DINF/2)
+	struct D {
+		int64_t w;
+		bool conn;
+	} dd[n+1];
+	fill(dd, dd+n+1, D {0, false});
+	dd[1] = D {0, true};
+	for (int v=0; v<n-1; v++) {
+		for (const auto &e:ee) {
+			int64_t nd = min(dd[e.i].w+e.w, DINF);
+			if (dd[e.j].conn && dd[e.i].conn) {
+				if (dd[e.j].w < nd)
+					dd[e.j] = D {nd, true};
+			} else if (dd[e.i].conn) {
+				dd[e.j] = D {nd, true};
+			}
+		}
+	}
+	D ans = dd[n];
+	for (const auto &e:ee) {
+		int64_t nd = min(dd[e.i].w+e.w, DINF);
+		if (dd[e.j].conn && dd[e.i].conn) {
+			if (dd[e.j].w < nd)
+				dd[e.j] = D {nd, true};
+		} else if (dd[e.i].conn) {
+			dd[e.j] = D {nd, true};
+		}
+	}
+	if (ans.conn) {
+		if (ans.w == DINF || ans.w < dd[n].w)
+			printf(":)\n");
+		else
+			printf("%lld\n", ans.w);
+	} else {
 		printf(":(\n");
-	else
-		printf("%lld\n", ans);
+	}
 	return 0;
 }
