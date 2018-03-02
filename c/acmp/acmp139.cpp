1215353eac1811360df996996677cbe0b40fb732
Denis Kokarev <d_kokarev@mail.ru>
2018-03-01 19:54:05 -0500

    acmp 139 wrong answer 13

--- c/acmp/acmp139.cpp
+++ c/acmp/acmp139.cpp
@@ -1,76 +1,58 @@
 /* ACMP 139 */
 #include <cstdio>
-#include <climits>
 #include <algorithm>
 
 using namespace std;
 
-constexpr int64_t DINF = INT64_MAX/2;
-
 struct E {
-	int i, j, w;
+	int i, j;
+	int w;
+	int cnt;
+};
+
+struct D {
+	int64_t w;
+	int from_i;
 };
 
 int main(int argc, char **argv) {
 	int n, m;
 	scanf("%d%d", &n, &m);
 	E ee[m];
-	for (int ei=0; ei<m; ei++)
-		scanf("%d%d%d", &ee[ei].i, &ee[ei].j, &ee[ei].w);
-	// ford
-	struct D {
-		int64_t w;
-		bool conn;
-		int l;
-	} dd[n+1];
-	fill(dd, dd+n+1, D {0, false, 0});
-	dd[1] = D {0, true};
-	bool path[n+1];
-	fill(path, path+n+1, 0);
-	for (int v=0; v<n-1; v++) {
-		for (const auto &e:ee) {
-			int64_t nd = min(dd[e.i].w+e.w, DINF);
-			if (dd[e.j].conn && dd[e.i].conn) {
-				if (dd[e.j].w < nd) {
-					dd[e.j] = D {nd, true, dd[e.i].l+1};
-					path[e.j] = e.i;
-				}
-			} else if (dd[e.i].conn) {
-				dd[e.j] = D {nd, true, dd[e.i].l};
-				path[e.j] = e.i;
+	fill(ee, ee+m, E {0, 0, 0, 0});
+	for (int i=0; i<m; i++)
+		scanf("%d%d%d", &ee[i].i, &ee[i].j, &ee[i].w);
+	// keep only biggest edges from same i->j
+	sort(ee, ee+m, [](const E &a, const E &b) {return (a.i<b.i) || (a.j==b.j && a.j < b.j) || (a.i==b.i && a.j==b.j && a.w > b.w);});
+	auto t = unique(ee, ee+m, [](const E &a, const E &b){return a.i==b.i && a.j==b.j;});
+	m = t-ee;
+	D dd[n+1];
+	fill(dd, dd+n+1, D {INT64_MIN/2, 0});
+	dd[1] = D {0, -1};
+	bool loop[n+1];
+	fill(loop, loop+n+1, false);
+	for (int v=0; v<2*n; v++) {
+		for (int i=0; i<m; i++) {
+			auto &e = ee[i];
+			if (dd[e.i].from_i && dd[e.j].w < dd[e.i].w+e.w) {
+				dd[e.j].w = min(dd[e.i].w+e.w, INT64_MAX/2);
+				dd[e.j].from_i = e.i;
+				if (e.cnt++)
+					loop[e.j] = true;
 			}
 		}
 	}
-	D savedd[n+1];
-	copy(dd, dd+n+1, savedd);
-	if (dd[n].conn) {
-		for (const auto &e:ee) {
-			int64_t nd = min(dd[e.i].w+e.w, DINF);
-			if (dd[e.j].conn && dd[e.i].conn) {
-				if (dd[e.j].w < nd) {
-					dd[e.j] = D {nd, true, dd[e.i].l+1};
-					path[e.j] = e.i;
-				}
-			} else if (dd[e.i].conn) {
-				dd[e.j] = D {nd, true, dd[e.i].l};
-				path[e.j] = e.i;
-			}
-		}
-		bool spath[n+1];
-		fill(spath, spath+n+1, false);
-		spath[1] = spath[n] = true;
-		for (int ni=path[n]; ni!=1; ni=path[ni])
-			spath[ni] = true;
-		bool loop = false;
-		for (int i=1; i<n+1; i++)
-			if (spath[i] && (dd[i].w == DINF || dd[i].w > savedd[i].w))
-				loop = true;
-		if (loop)
-			printf(":)\n");
-		else
-			printf("%lld\n", dd[n].w);
-	} else {
+	int i;
+	bool has_loop = false;
+	for (i=n; i>0 && !(has_loop=loop[i]); i=dd[i].from_i) {
+		fprintf(stderr, "%d <-\n", i);
+	}
+	if (has_loop) {
+		printf(":)\n");
+	} else if (dd[n].from_i == 0) {
 		printf(":(\n");
+	} else {
+		printf("%lld\n", dd[n].w);
 	}
 	return 0;
 }
