9c715a61e8a206b73cf3a6b9f0c4ea76aa7a4bdc
Denis Kokarev <d_kokarev@mail.ru>
2018-03-03 19:57:12 -0500

    multiple blocks

--- cuda/acuda.cu
+++ cuda/acuda.cu
@@ -5,7 +5,9 @@
 // Kernel function to add the elements of two arrays
 __global__
 void add(int n, float *x, float *y) {
-	for (int i = 0; i < n; i++)
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+	for (int i = index; i < n; i+=stride)
 		y[i] = x[i] + y[i];
 }
 
@@ -23,8 +25,10 @@ int main(void) {
 		y[i] = 2.0f;
 	}
 
+	int blockSize = 256;
+	int numBlocks = (N + blockSize - 1) / blockSize;
 	// Run kernel on 1M elements on the GPU
-	add<<<1, 256>>>(N, x, y);
+	add<<<numBlocks, blockSize>>>(N, x, y);
 
 	// Wait for GPU to finish before accessing on host
 	cudaDeviceSynchronize();
