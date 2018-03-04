a1807d1eaa0952ba9f43555fd0fab56e1bdf33be
Denis Kokarev <d_kokarev@mail.ru>
2018-03-04 10:16:25 -0500

    mem transfer or regular malloced memory

---
+++ cuda/memtrans.cu
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <cuda_runtime.h>
+
+int main() {
+    const unsigned int N = 1048576*16;
+    const unsigned int bytes = N * sizeof(int);
+    int *h_a = (int*)malloc(bytes);
+    int *d_a;
+    cudaMalloc((int**)&d_a, bytes);
+
+    memset(h_a, 0, bytes);
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(h_a, d_a, bytes, cudaMemcpyDeviceToHost);
+
+    return 0;
+}
