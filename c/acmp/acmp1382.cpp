13ca705fc4fe1b652cb5e830d00a1480c5378124
Denis Kokarev <d_kokarev@mail.ru>
2018-03-14 23:32:35 -0400

    acmp 1382, hmm still timeout 23

--- c/acmp/acmp1382.cpp
+++ c/acmp/acmp1382.cpp
@@ -7,33 +7,34 @@
 using namespace std;
 
 struct Q {
-	int len;
-	int node;
+	int16_t len;
+	int16_t node;
 	bool operator<(const Q &b) const {
 		return len > b.len; // for min heap
 	}
 };
 
 struct E {
-	int to;
+	int16_t to;
 	int weight;
-	int len;
+	int16_t len;
 };
 
 constexpr int DINF = INT_MAX;
 
-inline int dijkstra(const vector<vector<E>> &ee, int n, int mxw) {
+inline int dijkstra(priority_queue<Q> &qq, const vector<vector<E>> &ee, int n, int mxw, int16_t lenlim) {
 	vector<int> dist(n, DINF);
-	priority_queue<Q> qq;
 	qq.push(Q{0,0});
 	while (!qq.empty()) {
 		Q top = qq.top();
 		qq.pop();
+		if (top.len > lenlim)
+			break; // don't bother continuing
 		if (dist[top.node] == DINF) {
 			dist[top.node] = top.len;
 			for (auto &e:ee[top.node])
 				if (e.weight >= mxw)
-					qq.push(Q {top.len+e.len, e.to});
+					qq.push(Q {int16_t(top.len+e.len), e.to});
 		}
 	}
 	return dist[n-1];
@@ -44,7 +45,7 @@ int main(int argc, char **argv) {
 	cin >> n >> m;
 	vector<vector<E>> ee(n);
 	while (m--) {
-		int from;
+		int16_t from;
 		E e;
 		cin >> from >> e.to >> e.len >> e.weight;
 		from--, e.to--;
@@ -52,19 +53,20 @@ int main(int argc, char **argv) {
 		swap(from, e.to);
 		ee[from].push_back(e);
 	}
+	priority_queue<Q> qq;
 	// upper bound search
 	int f = 0, t = INT_MAX;
 	while (f<t) {
 		int m = f+(t-f)/2;
-		if (dijkstra(ee, n, m) > 1440)
+		if (dijkstra(qq, ee, n, m, 1440) > 1440)
 			t = m;
 		else
 			f = m+1;
 	}
 	f--;
-	cerr << "dijkstra(" << f << ")=" << dijkstra(ee, n, f) << endl;
+	//cerr << "dijkstra(" << f << ")=" << dijkstra(ee, n, f, 1440) << endl;
 	int ans = f-3*1000*1000;
 	ans = (ans<0)?0:ans/100;
-	cout << min(ans,10000000) << endl;
+	cout << min(ans,10000000) << endl; // 10M tricky condition
 	return 0;
 }
