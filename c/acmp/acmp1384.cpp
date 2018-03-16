003eddceab28d1ce0afaecc8df6715efe093239e
Denis Kokarev <d_kokarev@mail.ru>
2018-03-15 20:04:30 -0400

    acmp 1384 timeout 44

--- c/acmp/acmp1384.cpp
+++ c/acmp/acmp1384.cpp
@@ -10,6 +10,7 @@ using namespace std;
 struct Q {
 	int node;
 	unsigned c;
+	int from;
 	bool operator<(const Q &b) const {
 		return c > b.c; // for min heap
 	}
@@ -20,25 +21,46 @@ struct E {
 	unsigned c;
 };
  
-unsigned minpath(const vector<vector<E>> &ee, int start, const vector<int> &tt, int &end) {
+struct D {
+	int from;
+	int to;
+	unsigned c;
+	unsigned cto;
+};
+ 
+void minpath(const vector<vector<E>> &ee, vector<D> &dist, int start, const vector<int> &tt) {
 	priority_queue<Q> qq;
-	qq.push(Q{start, 0});
-	vector<bool> seen(tt.size(), false);
+	for (auto &d:dist) {
+		d.c = UINT_MAX;
+		d.from = -1;
+	}
+	qq.push(Q{start, 0, -1});
 	int fnd_type = tt[start]%2+1; // 1<->2
 	while (!qq.empty()) {
 		Q top = qq.top();
 		qq.pop();
-		if (!seen[top.node]) {
-			seen[top.node] = true;
-			if (tt[top.node] == fnd_type) {
-				end = top.node;
-				return top.c;
+		if (tt[top.node] == fnd_type) {
+			// short-circuit everything on the way
+			for (int f=top.from; f>=0; f=dist[f].from) {
+				if (dist[f].cto > top.c - dist[f].c) {
+					dist[f].cto = top.c - dist[f].c;
+					dist[f].to = top.node;
+				}
 			}
-			for (auto &e:ee[top.node])
-				qq.push(Q{e.to, top.c+e.c});
+			return;
+		} else {
+			if (dist[top.node].c == UINT_MAX) {
+				dist[top.node].c = top.c;
+				dist[top.node].from = top.from;
+				if (dist[top.node].cto == UINT_MAX) {
+					for (auto &e:ee[top.node])
+						qq.push(Q{e.to, top.c+e.c, top.node});
+				} else {
+					qq.push(Q{dist[top.node].to, top.c+dist[top.node].cto, top.node});
+				}
+			}
 		}
 	}
-	return UINT_MAX;
 }
  
 int main(int argc, char **argv) {
@@ -57,22 +79,24 @@ int main(int argc, char **argv) {
 		swap(f, e.to);
 		ee[f].push_back(e);
 	}
+	vector<D> dist(tt.size(), D{-1,-1,UINT_MAX,UINT_MAX});
+	for (int i=0; i<n; i++) {
+		if (tt[i] == 1) {
+			minpath(ee, dist, i, tt);
+#if 0
+			fprintf(stderr, "=== working on %d ===\n", i);
+			for (int i=0; i<n; i++)
+				fprintf(stderr, "i=%d, cto=%d, to=%d\n", i, dist[i].cto, dist[i].to);
+#endif
+		}
+	}
 	unsigned mn = UINT_MAX;
 	int mni, mnj;
 	for (int i=0; i<n; i++) {
-		if (tt[i] == 1) {
-			int j;
-			unsigned m = minpath(ee, i, tt, j);
-			if (m != UINT_MAX) {
-				// short-circuit
-				ee[i].clear();
-				ee[i].push_back(E{j, m});
-			}
-			if (m < mn) {
-				mni = i;
-				mnj = j;
-				mn = m;
-			}
+		if (tt[i] == 1 && dist[i].cto < mn) {
+			mn = dist[i].cto;
+			mni = i;
+			mnj = dist[i].to;
 		}
 	}
 	if (mn == UINT_MAX)
