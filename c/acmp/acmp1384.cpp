4fb37c0d3bf977359649535093bf53e2355439bc
Denis Kokarev <d_kokarev@mail.ru>
2018-03-15 20:24:49 -0400

    acmp 1384 solution

--- c/acmp/acmp1384.cpp
+++ c/acmp/acmp1384.cpp
@@ -1,6 +1,5 @@
 /* ACMP 1384 */
 #include <cstdio>
-#include <algorithm>
 #include <vector>
 #include <queue>
 #include <climits>
@@ -8,8 +7,8 @@
 using namespace std;
   
 struct Q {
-	int16_t node;
-	int16_t from;
+	int node;
+	int from;
 	unsigned c;
 	bool operator<(const Q &b) const {
 		return c > b.c; // for min heap
@@ -22,13 +21,13 @@ struct E {
 };
   
 struct D {
-	int16_t from;
-	int16_t to;
+	int from;
+	int to;
 	unsigned c;
 	unsigned cto;
 };
   
-void minpath(const vector<vector<E>> &ee, vector<D> &dist, int16_t start, const vector<int> &tt) {
+static void minpath(const vector<vector<E>> &ee, vector<D> &dist, int start, const vector<int> &tt) {
 	priority_queue<Q> qq;
 	qq.push(Q{start, -1, 0});
 	int fnd_type = tt[start]%2+1; // 1<->2
@@ -38,19 +37,22 @@ void minpath(const vector<vector<E>> &ee, vector<D> &dist, int16_t start, const 
 		if (tt[top.node] == fnd_type) {
 			// short-circuit everything on the way
 			for (int f=top.from; f>=0; f=dist[f].from) {
-				if (dist[f].cto == UINT_MAX) {
-					dist[f].cto = top.c - dist[f].c;
-					dist[f].to = top.node;
-				}
+				dist[f].cto = top.c - dist[f].c;
+				dist[f].to = top.node;
 			}
 			return;
 		} else {
 			if (dist[top.node].c > top.c) {
+				// I trully don't understand how this heuristics works ;-)
+				// The idea is you don't want to come back upstream to
+				// already explored src nodes
+				// Having conventional visited[] array doesn't work per
+				// time constraints - too expensive to clear it every time 
 				dist[top.node].c = top.c;
 				dist[top.node].from = top.from;
 				if (dist[top.node].cto == UINT_MAX) {
 					for (auto &e:ee[top.node])
-						qq.push(Q{int16_t(e.to), top.node, top.c+e.c});
+						qq.push(Q{e.to, top.node, top.c+e.c});
 				} else {
 					qq.push(Q{dist[top.node].to, top.node, top.c+dist[top.node].cto});
 				}
@@ -76,11 +78,11 @@ int main(int argc, char **argv) {
 		ee[f].push_back(e);
 	}
 	vector<D> dist(tt.size(), D{-1,-1,UINT_MAX,UINT_MAX});
-	for (int i=0; i<n; i++) {
-		if (tt[i] == 1 && dist[i].cto == UINT_MAX) {
+	// find min path from each type 1 node and shortcircuit everything on the way
+	for (int i=0; i<n; i++)
+		if (tt[i] == 1)
 			minpath(ee, dist, i, tt);
-		}
-	}
+	// find best shortcut
 	unsigned mn = UINT_MAX;
 	int mni, mnj;
 	for (int i=0; i<n; i++) {
