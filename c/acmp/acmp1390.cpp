71addc08ec305f37bfe92b7bc1d006b07fd70704
Denis Kokarev <d_kokarev@mail.ru>
2018-03-18 18:09:52 -0400

    acmp 1390 solution

--- c/acmp/acmp1390.cpp
+++ c/acmp/acmp1390.cpp
@@ -36,31 +36,34 @@ int main(int argc, char **argv) {
 	bool e_seen[n*2];
 	fill(e_seen, e_seen+2*n, false);
 	S stack[n*4];
-	int sz = 0;
+	S ans[n*4];
+	int stack_sz = 0, ans_sz = 0;;
 	int cnt = 0;
 	if (ee[0]) {
-		stack[sz++] = S {0, ee[0]->vt, ee[0]};
+		stack[stack_sz++] = S {0, ee[0]->vt, ee[0]};
 		e_seen[ee[0]->id] = true;
 		cnt++;
 	}
-	while (true) {
-		S top = stack[sz-1];
+	while (stack_sz > 0) {
+		S top = stack[stack_sz-1];
 		E *e;
 		for (e=ee[top.vt]; e; e=e->next) {
 			if (!e_seen[e->id]) {
 				e_seen[e->id] = true;
-				stack[sz++] = S {top.vt, e->vt, e};
+				stack[stack_sz++] = S {top.vt, e->vt, e};
 				cnt++;
 				break;
 			}
 		}
-		if (e == nullptr)
-			break;
+		if (e == nullptr) {
+			ans[ans_sz++] = top;
+			stack_sz--;
+		}
 	}
-	if (cnt == n*2) {
+	if (cnt == n*2 && ans[0].vt == 0) {
 		printf("Yes\n");
-		for (int i=0; i<sz; i++)
-			printf("%d %d ", stack[i].e->gf, stack[i].e->gt);
+		for (int i=ans_sz-1; i>=0; i--)
+			printf("%d %d ", ans[i].e->gf, ans[i].e->gt);
 		printf("\n");
 	} else {
 		printf("No\n");
