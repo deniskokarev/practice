cb0399abb2919e2ac4c35eff456c0c451cfd4414
Denis Kokarev <d_kokarev@mail.ru>
2018-04-01 17:53:35 -0400

    acmp 1274 solution

--- c/acmp/acmp1274.cpp
+++ c/acmp/acmp1274.cpp
@@ -10,14 +10,20 @@ int main(int argc, char **argv) {
 	for (auto &c:cc)
 		scanf("%d", &c);
 	int64_t mxend = s;
-	int64_t mnend = s;
-	int64_t mn, mx;
-	mx = mn = cc[0];
-	for (int i=0; i<n; i++) {
-		mx = std::max(mx, int64_t(cc[i]));
+	int64_t mn;
+	mn = cc[0];
+	for (int i=1; i<n; i++) {
 		mn = std::min(mn, int64_t(cc[i]));
 		mxend = std::max(mxend, int64_t(cc[i])*(s/mn)+s%mn);
-		mnend = std::min(mnend, int64_t(cc[i])*(s/mx)+s%mx);
+	}
+	// min scenario requires right->left scan
+	// as the biggest total drop will not necessarily come from
+	// max peak. We can only be sure that it will end at min val
+	int64_t mnend = s;
+	mn = cc[n-1];
+	for (int i=n-2; i>=0; i--) {
+		mn = std::min(mn, int64_t(cc[i]));
+		mnend = std::min(mnend, int64_t(mn)*(s/cc[i])+s%cc[i]);
 	}
 	printf("%lld %lld\n", mnend, mxend);
 	return 0;
