fa9201e97d26092033a458bf3209a59782da3e62
Denis Kokarev <d_kokarev@mail.ru>
2018-04-08 14:02:01 -0400

    enveloping the detection into a class

--- patmat/uuidgsp.cc
+++ patmat/uuidgsp.cc
@@ -6,14 +6,13 @@
  */
 #include <cstdio>
 #include <cstdlib>
-#include <errno.h>
-#include "die.h"
-#include <memory>
 #include <cstring>
+#include <memory>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 #include "uuidmatch.h"
+#include "die.h"
 
 constexpr int STREAMS = 8;
 constexpr int STRSZ = 1<<20;
@@ -24,84 +23,99 @@ struct MATCH {
 	unsigned sz;
 };
 
-unsigned match_one_stream(const char *s, unsigned s_sz, MATCH *out) {
-	MATCH *res(out);
-	UMSTATE umstate;
-	um_init(&umstate);
-	for (unsigned i=0; i<s_sz; i++,s++)
-		if (um_match(&umstate, *s))
-			*res++ = MATCH {i-UMPATLEN+1, UMPATLEN};
-	return res-out;
-}
-
-struct THARG {
+class Detect {
 	const char *ibuf;
 	MATCH *obuf;
-	unsigned *o_sz;
+	unsigned *nmatch;
+	int batch;
+	int cnt;
 	// producer notifies, consumer waits
 	std::mutex mtx_producer;
 	std::condition_variable cv_producer;
 	// consumer notifies, producer waits
 	std::mutex mtx_consumer;
 	std::condition_variable cv_consumer;
-	volatile int batch;
-	volatile int cnt;
-};
-
-void match_thread(THARG *t, int n) {
-	const char *in = t->ibuf+STRSZ*n;
-	MATCH *out = t->obuf+STRSZ*n;
-	int batch = 0;
-	while (true) {
-		// wait for new data from producer
-		{
-			std::unique_lock<std::mutex> lck(t->mtx_producer);
-			while (t->batch == batch)
-				t->cv_producer.wait(lck);
-			batch = t->batch;
-		}
-		if (t->cnt > 0) {
-			// process this batch and notify producer
-			t->o_sz[n] = match_one_stream(in, STRSZ, out);
+	// pre-spawn this many threads
+	std::thread threads[STREAMS];
+protected:
+	void threads_go() {
+		std::lock_guard<std::mutex> lck(mtx_producer);
+		batch++;
+		cnt += STREAMS;
+		cv_producer.notify_all();
+	}
+	void threads_wait() {
+		std::unique_lock<std::mutex> lck(mtx_consumer);
+		while (cnt > 0)
+			cv_consumer.wait(lck);
+	}
+	void threads_done() {
+		std::lock_guard<std::mutex> lck(mtx_producer);
+		batch++;
+		cnt--; // go below 0
+		cv_producer.notify_all();
+	}
+	unsigned match_one_stream(const char *s, unsigned s_sz, MATCH *out) {
+		MATCH *res(out);
+		UMSTATE umstate;
+		um_init(&umstate);
+		for (unsigned i=0; i<s_sz; i++,s++)
+			if (um_match(&umstate, *s))
+				*res++ = MATCH {i-UMPATLEN+1, UMPATLEN};
+		return res-out;
+	}
+	static void match_thread(Detect *d, int n) {
+		int batch = 0;
+		while (true) {
+			// wait for new data from producer
 			{
-				std::lock_guard<std::mutex> lck(t->mtx_consumer);
-				t->cnt--;
-				t->cv_consumer.notify_one();
+				std::unique_lock<std::mutex> lck(d->mtx_producer);
+				while (d->batch == batch)
+					d->cv_producer.wait(lck);
+				batch = d->batch;
 			}
-		} else {
-			break;
+			const char *in = d->ibuf+STRSZ*n;
+			MATCH *out = d->obuf+STRSZ*n;
+			if (d->cnt > 0) {
+				// process this batch and notify producer
+				d->nmatch[n] = d->match_one_stream(in, STRSZ, out);
+				{
+					std::lock_guard<std::mutex> lck(d->mtx_consumer);
+					d->cnt--;
+					d->cv_consumer.notify_one();
+				}
+			} else {
+				break;
+			}
 		}
 	}
-}
+public:
+	Detect():ibuf{nullptr},obuf{nullptr},nmatch{nullptr},batch{0},cnt{0} {
+		for (int i=0; i<STREAMS; ++i)
+			threads[i] = std::thread(match_thread, this, i);
+	}
+	~Detect() {
+		threads_done();
+		for (auto &t: threads)
+			t.join();
+	}
+	void operator()(const char *_ibuf, MATCH *_obuf, unsigned *_nmatch, unsigned &rowsz) {
+		ibuf = _ibuf;
+		obuf = _obuf;
+		nmatch = _nmatch;
+		threads_go();
+		threads_wait();
+		rowsz = STRSZ;
+	}
+};
 
-void threads_go(THARG *t) {
-	std::lock_guard<std::mutex> lck(t->mtx_producer);
-	t->batch++;
-	t->cnt += STREAMS;
-	t->cv_producer.notify_all();
-}
-
-void threads_wait(THARG *t) {
-	std::unique_lock<std::mutex> lck(t->mtx_consumer);
-	while (t->cnt > 0)
-		t->cv_consumer.wait(lck);
-}
-
-void threads_done(THARG *t) {
-	std::lock_guard<std::mutex> lck(t->mtx_producer);
-	t->batch++;
-	t->cnt--; // go below 0
-	t->cv_producer.notify_all();
-}
-
-void prn(const char *ibuf, const MATCH *obuf, const unsigned *o_sz) {
-	int rc;
+void prn(const char *ibuf, const MATCH *obuf, const unsigned *nmatch, const unsigned rowsz) {
 	for (int stream=0; stream<STREAMS; stream++) {
-		unsigned sz = o_sz[stream];
-		const MATCH *mm = obuf+STRSZ*stream;
+		unsigned sz = nmatch[stream];
+		const MATCH *mm = obuf+rowsz*stream;
 		const char *s = ibuf+STRSZ*stream;
 		for (unsigned i=0; i<sz; i++) {
-			if ((rc=fwrite(s+mm[i].pos, 1, mm[i].sz, stdout)) != (int)mm[i].sz)
+			if ((fwrite(s+mm[i].pos, 1, mm[i].sz, stdout)) != (int)mm[i].sz)
 				die("Write error");
 			if (fputc('\n', stdout) != '\n')
 				die("Write error");
@@ -122,11 +136,8 @@ int main(int argc, char **argv) {
 	std::unique_ptr<MATCH[]> up_obuf(new MATCH[STRSZ*(STREAMS)]);
 	char *ibuf = up_ibuf.get();
 	MATCH *obuf = up_obuf.get();
-	unsigned o_sz[STREAMS];
-	std::thread threads[STREAMS];
-	THARG tharg {ibuf, obuf, o_sz};
-	for (int i=0; i<STREAMS; ++i)
-		threads[i] = std::thread(match_thread, &tharg, i);
+	unsigned nmatch[STREAMS];
+	Detect detect;
 	// read input and scatter it into STREAMS channels
 	while (!feof(stdin)) {
 		int ns = 0;
@@ -147,18 +158,16 @@ int main(int argc, char **argv) {
 			} else if (rc < 0) {
 				die("read error");
 			} else {
-				// eof - we didn't get enough data
+				// we didn't get full block - must be eof
 				memset(buf+over+rc, SFILL, STRSZ-over-rc);
 				ns++;
 				memset(ibuf+STRSZ*ns, SFILL, (STREAMS-ns)*STRSZ); // fill up all unused channels
 				break;
 			}
 		}
-		threads_go(&tharg);
-		threads_wait(&tharg);
-		prn(ibuf, obuf, o_sz);
+		//////// run the batch
+		unsigned rowsz;
+		detect(ibuf, obuf, nmatch, rowsz);
+		prn(ibuf, obuf, nmatch, rowsz);
 	}
-	threads_done(&tharg);
-	for (auto &t: threads)
-		t.join();
 }
