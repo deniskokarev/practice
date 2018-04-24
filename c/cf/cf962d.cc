6deac96a7ade423678624937ace567a3328f0f03
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-04-24 13:46:54 -0400

    cf962d wrong answer 6

---
+++ c/cf/cf962d.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <set>
+/* CodeForces CF962D problem */
+using namespace std;
+
+int main(int argc, char **argv) {
+	map<int64_t,set<int>> mm;
+	int n;
+	cin >> n;
+	int aa[n];
+	for (int i=0; i<n; i++) {
+		int &a = aa[i];
+		cin >> a;
+		mm[a].insert(i);
+	}
+	int del = 0;
+	for (auto &m:mm) {
+		while (m.second.size()>1) {
+			auto p = m.second.begin();
+			aa[*p] = -1;
+			m.second.erase(p);
+			p = m.second.begin();
+			m.second.erase(p);
+			aa[*p] = m.first*2;
+			mm[m.first*2].insert(*p);
+			del++;
+		}
+	}
+	cout << n-del << endl;
+	for (auto &a:aa)
+		if (a>=0)
+			cout << a << " ";
+	cout << endl;
+	return 0;
+}
