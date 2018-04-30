1968d296cc4c7ff5f1e0884e6d28e37dcd242db4
Denis Kokarev <d_kokarev@mail.ru>
2018-04-29 22:17:18 -0400

    adding simple parallel execution primitives and unit tests for them

--- c/alg/par.hh
+++ c/alg/par.hh
@@ -1,14 +1,80 @@
+#ifndef __PAR_HH__
+#define __PAR_HH__
+
+/**
+ * Parallel execution primitives
+ * @author Denis Kokarev
+ */
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <vector>
 
+/**
+ * Simple parallel execution. Start n threads and have them all run their exec_slice()
+ * in the loop. All exec_slice()es executed once for every exec() command
+ * Derive your class, add data fields and overload exec_slice(n).
+ * Threads created at constructor and joined at destructor.
+ */
+class ParallelExec {
+protected:
+	int nthreads;
+private:
+	// marching tick for threads
+	int tick;
+	// pre-spawn this many threads
+	std::vector<std::thread> threads;
+	// number of active threads on this batch
+	int active_cnt;
+	// threads wait on this cv to start the batch
+	std::mutex mtx_begin;
+	std::condition_variable cv_begin;
+	// threads notify this cv at the end of batch
+	std::mutex mtx_end;
+	std::condition_variable cv_end;
+	void threads_go();
+	void threads_wait();
+	void threads_done();
+	static void run_thread(ParallelExec *th, int n);
+	void run(int n);
+protected:
+	virtual void exec_slice(int n) = 0;
+	ParallelExec(int nthreads);
+	~ParallelExec();
+	void exec();
+};
+
+/**
+ * Parallel conveyor primitives
+ * PipeHeadExec > PipeStageExec > PipeOutput -+
+ *                                            +-> PipeOutputIterator
+ *
+ * Derive your own Head and Stages. Arguments are passed by reference. Therefore
+ * each converor element is responsible for storing at least 2 instances of results
+ * so that the next stage can always access the previously obtained result. 1 instance
+ * of the result for prior execution and 1 for current execution thus
+ * allowing parallel operation. If the result needs to be passed to the stage after the
+ * following then 3 instances of the result have to be stored.
+ *
+ * PipeHeadExec requires overloading void *next() method which returns a pointer
+ * to next portion of the data or nullptr
+ * 
+ * PipeStageExec requires overloading void *next(void *arg) method
+ *
+ * PipeOutput is used to obtain PipeOutputIterator. Every increment
+ * on PipeOutputIterator advances all members of the converor. Iterator holds
+ * the result of last void *next(void *arg) of the conveyor pipeline
+ *
+ * Each conveyor element may use 'batch' counter to determine which copy
+ * of the result to work on this iteration
+ */
 class PipeStageExec;
-class PipeTailExec;
+class PipeOutputIterator;
 
 class PipeHeadExec {
 	friend PipeStageExec;
-	friend PipeTailExec;
-public:
+	friend PipeOutputIterator;
+protected:
 	int batch;
 private:
 	bool go;
@@ -24,31 +90,52 @@ private:
 	void thread_wait_done();
 	void thread_notify_done();
 	virtual void run();
-	virtual void *get() = 0;
+	virtual void *next() = 0;
 public:
 	PipeHeadExec();
 };
 
 class PipeStageExec: public PipeHeadExec {
-	friend PipeTailExec;
+	friend PipeOutputIterator;
 private:
 	PipeHeadExec &parent;
 	std::thread thread;
+	static void run_thread(PipeHeadExec *th);
 	virtual void run() override;
-	virtual void *get() override final;
+	virtual void *next() override final;
 protected:
-	virtual void *map(void *arg) = 0;
+	virtual void *next(void *arg) = 0;
 public:
 	PipeStageExec(PipeHeadExec &parent);
 	~PipeStageExec();
 };
 
-class PipeTailExec: public PipeStageExec {
-private:
-	virtual void *map(void *arg) override final;
+class PipeOutput;
+
+class PipeOutputIterator {
+	friend PipeOutput;
 protected:
-	virtual void proc(void *arg) = 0;
+	PipeStageExec &tail;
+	void *value;
+	PipeOutputIterator &seek_begin();
+	PipeOutputIterator &seek_end();
 public:
-	using PipeStageExec::PipeStageExec;
-	virtual void run() override;
+	PipeOutputIterator(PipeStageExec &tail);
+	bool operator==(const PipeOutputIterator &b) const;
+	bool operator!=(const PipeOutputIterator &b) const;
+	void operator++();
+	void *operator*() const;
 };
+
+class PipeOutput: public PipeStageExec {
+protected:
+	virtual void *next(void *arg) override final;
+	PipeOutputIterator begin_it;
+	PipeOutputIterator end_it;
+public:
+	PipeOutput(PipeHeadExec &parent);
+	PipeOutputIterator &begin();
+	PipeOutputIterator &end();
+};
+
+#endif // __PAR_HH__
--- c/alg/par_test.cc
+++ c/alg/par_test.cc
@@ -1,176 +1,6 @@
-#include <cstdio>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-
-class PipeStageExec;
-class PipeOutputIterator;
-
-class PipeHeadExec {
-	friend PipeStageExec;
-	friend PipeOutputIterator;
-public:
-	int batch;
-private:
-	bool go;
-	bool done;
-	bool abort;
-	int depth;
-	void *last_arg;
-	std::mutex mtx;
-	std::condition_variable cv;
-	void thread_wait_go() {
-		std::unique_lock<std::mutex> lck(mtx);
-		while (!go)
-			cv.wait(lck);
-	}
-	void thread_notify_go() {
-		std::lock_guard<std::mutex> lck(mtx);
-		go = true;
-		cv.notify_one();
-	}
-	void thread_notify_abort() {
-		std::lock_guard<std::mutex> lck(mtx);
-		go = true;
-		abort = true;
-		cv.notify_one();
-	}
-	void thread_wait_done() {
-		std::unique_lock<std::mutex> lck(mtx);
-		while (go)
-			cv.wait(lck);
-	}
-	void thread_notify_done() {
-		std::lock_guard<std::mutex> lck(mtx);
-		go = false;
-		cv.notify_one();
-	}
-	virtual void run() {
-		while (!done && !abort) {
-			thread_wait_go();
-			if (abort)
-				break;
-			last_arg = next();
-			done = (last_arg == nullptr);
-			batch++;
-			thread_notify_done();
-		}
-	}
-	virtual void *next() = 0;
-public:
-	PipeHeadExec() {
-		batch = 0;
-		go = false;
-		done = false;
-		abort = false;
-		depth = 0;
-		last_arg = nullptr;
-	}
-};
-
-class PipeStageExec: public PipeHeadExec {
-	friend PipeOutputIterator;
-private:
-	PipeHeadExec &parent;
-	std::thread thread;
-	static void run_thread(PipeHeadExec *th) {
-		th->run();
-	}
-	virtual void run() override {
-		void *parent_last_arg = nullptr;
-		while (!done && !abort) {
-			thread_wait_go();
-			if (abort)
-				break;
-			if (!parent.done) {
-				parent.thread_notify_go();
-				// idle until conveyor is filled
-				if (batch >= depth)
-					last_arg = next(parent_last_arg);
-				parent.thread_wait_done();
-				parent_last_arg = parent.last_arg;
-			} else {
-				last_arg = nullptr;
-				done = true;
-			}
-			batch++;
-			thread_notify_done();
-		}
-	}
-	virtual void *next() override final {
-		return nullptr;
-	}
-protected:
-	virtual void *next(void *arg) = 0;
-public:
-	PipeStageExec(PipeHeadExec &parent):PipeHeadExec(),parent(parent) {
-		depth = parent.depth+1;
-		thread = std::thread(run_thread, &parent);
-	};
-	~PipeStageExec() {
-		parent.thread_notify_abort();
-		thread.join();
-	}
-};
-
-class PipeOutputIterator {
-	PipeStageExec &tail;
-	void *value;
-public:
-	PipeOutputIterator(PipeStageExec &tail):tail(tail),value(nullptr) {
-	}
-	bool operator==(const PipeOutputIterator &b) const {
-		return value == b.value;
-	}
-	bool operator!=(const PipeOutputIterator &b) const {
-		return value != b.value;
-	}
-	PipeOutputIterator &seek_begin() {
-		while (!tail.parent.done && tail.parent.batch<=tail.parent.depth) {
-			tail.parent.thread_notify_go();
-			tail.parent.thread_wait_done();
-		}
-		value = tail.parent.last_arg;
-		tail.parent.thread_notify_go();
-		return *this;
-	}
-	PipeOutputIterator &seek_end() {
-		value = nullptr;
-		return *this;
-	}
-	void operator++() {
-		tail.parent.thread_wait_done();
-		value = tail.parent.last_arg;
-		if (!tail.parent.done) {
-			tail.parent.thread_notify_go();
-		} else {
-			value = nullptr;
-		}
-	}
-	void *operator*() const {
-		return value;
-	}
-};
-
-class PipeOutput: public PipeStageExec {
-protected:
-	virtual void *next(void *arg) override final {
-		return nullptr;
-	};
-	PipeOutputIterator begin_it;
-	PipeOutputIterator end_it;
-public:
-	PipeOutput(PipeHeadExec &parent):PipeStageExec(parent),
-									 begin_it(PipeOutputIterator(*this)),
-									 end_it(PipeOutputIterator(*this)) {
-	}
-	PipeOutputIterator &begin() {
-		return begin_it.seek_begin();
-	}
-	PipeOutputIterator &end() {
-		return end_it.seek_end();
-	}
-};
+#include <memory>
+#include "gtest/gtest.h"
+#include "par.hh"
 
 /**
  * Usage example
@@ -220,23 +50,170 @@ private:
 	static constexpr int stages = 2;
 	const char *label[2] {"even", "odd"};
 	PIPE_ELEMENT el[stages];
+	char slabel[stages][5];	// we'll copy the string values for labels
 	virtual void *next(void *arg) override {
 		int stage = batch%stages;
 		PIPE_ELEMENT *parent_el = (PIPE_ELEMENT*)arg;
 		el[stage] = *parent_el;
-		el[stage].label = label[parent_el->category];
+		strncpy(slabel[stage], label[parent_el->category], 5); // use copy to excercise memory
+		el[stage].label = slabel[stage];
 		return &el[stage];
 	}
 public:
 	using PipeStageExec::PipeStageExec;
 };
 
-int main(int argc, char **argv) {
-	GenStage generate(1);
+TEST(ParTest, Simple) {
+	constexpr int n = 10;
+	GenStage generate(n);
 	CategorizeStage categorize(generate);
 	LabelStage label(categorize);
+	int cnt = 0;
 	for (auto pel:PipeOutput(label)) {
 		PIPE_ELEMENT *el = (PIPE_ELEMENT*)pel;
-		fprintf(stdout, "num=%d, category=%d, label=%s\n", el->num, el->category, el->label);
+		fprintf(stdout, "line num=%d, category=%d, label=%s\n", el->num, el->category, el->label);
+		cnt++;
 	}
+	EXPECT_TRUE(cnt == n);	// must get exactly n lines
+}
+
+TEST(ParTest, Volume) {
+	constexpr int64_t n = 10000;
+	GenStage generate(n);
+	CategorizeStage categorize(generate);
+	LabelStage label(categorize);
+	int64_t num_sum = 0;
+	int64_t cat_sum = 0;
+	for (auto pel:PipeOutput(label)) {
+		PIPE_ELEMENT *el = (PIPE_ELEMENT*)pel;
+		num_sum += el->num;
+		cat_sum += el->category;
+		if (el->num&1)
+			EXPECT_TRUE(el->category == 1 && !strcmp(el->label, "odd"));
+		else
+			EXPECT_TRUE(el->category == 0 && !strcmp(el->label, "even"));
+	}
+	EXPECT_TRUE(num_sum == n*(n-1)/2 && cat_sum == n/2);
+}
+
+TEST(ParTest, Idle) {
+	{
+		GenStage generate(1000);
+		CategorizeStage categorize(generate);
+		LabelStage label(categorize);
+	}
+	EXPECT_TRUE("creation and teardown without execution");
+}
+
+// produce 100 batches of 1M elements each
+class GenValues: public ParallelExec, public PipeHeadExec {
+protected:
+	int limit;
+	int sz;
+public:
+	struct TRESULT {
+		std::unique_ptr<int[]> n;
+		int sz;
+	};
+protected:
+	static constexpr int stages = 2;
+	TRESULT res[stages];
+	virtual void exec_slice(int n) override {
+		TRESULT &r = res[batch%stages];
+		int sz = r.sz;
+		int blocksz = (sz+nthreads-1)/nthreads;
+		int upto = std::min(blocksz*(n+1), sz);
+		for (int i=blocksz*n; i<upto; i++)
+			r.n[i] = batch*sz+i;
+	}
+	void *next() override {
+		if (batch < limit) {
+			TRESULT &r = res[batch%stages];
+			r = TRESULT { std::unique_ptr<int[]>(new int[sz]), sz};
+			exec();
+			return &r;
+		} else {
+			return nullptr;
+		}
+	}
+public:
+	GenValues(int limit, int sz, int nthreads):ParallelExec(nthreads),PipeHeadExec(),limit(limit),sz(sz) {
+	}
+};
+
+// add a fixed delta to every number
+class AddValues: public ParallelExec, public PipeStageExec {
+public:
+	using TRESULT = GenValues::TRESULT;
+protected:
+	int inc;
+	static constexpr int stages = 2;
+	TRESULT res[stages];
+	virtual void exec_slice(int n) override {
+		TRESULT &r = res[batch%stages];
+		int blocksz = (r.sz+nthreads-1)/nthreads;
+		int upto = std::min(blocksz*(n+1),r.sz);
+		for (int i=blocksz*n; i<upto; i++)
+			r.n[i] += inc;
+	}
+	void *next(void *arg) override {
+		TRESULT *ra = (TRESULT*)arg;
+		TRESULT &r = res[batch%stages];
+		r.n = std::move(ra->n);
+		r.sz = ra->sz;
+		exec();
+		return &r;
+	}
+public:
+	AddValues(int inc, int nthreads, PipeHeadExec &parent):ParallelExec(nthreads),PipeStageExec(parent),inc(inc) {
+	}
+};
+
+// accumulate the buffer
+class Accumulate: public ParallelExec, public PipeStageExec {
+public:
+	static constexpr int nth = 4;
+	struct TSUM {
+		int64_t sum[nth];
+	};
+protected:
+	static constexpr int stages = 2;
+	TSUM res[stages];
+	AddValues::TRESULT *arg;
+	virtual void exec_slice(int n) override {
+		TSUM &r = res[batch%stages];
+		int blocksz = (arg->sz+nthreads-1)/nthreads;
+		int upto = std::min(blocksz*(n+1),arg->sz);
+		for (int i=blocksz*n; i<upto; i++)
+			r.sum[n] += arg->n[i];
+	}
+	void *next(void *a) override {
+		arg = (AddValues::TRESULT*)a;
+		TSUM &r = res[batch%stages];
+		for (int j=0; j<nthreads; j++)
+			r.sum[j] = 0;
+		exec();
+		return &r;
+	}
+public:
+	#if 1
+	Accumulate(PipeHeadExec &parent):ParallelExec(nth),PipeStageExec(parent) {
+	}
+	#endif
+};
+
+TEST(ParTest, ParallelConveyor) {
+	int64_t limit = 100;
+	int64_t sz = 1000013;
+	GenValues gen(limit, sz, 7);
+	AddValues add_one(1, 3, gen);
+	AddValues add_minus_one(-1, 5, add_one);
+	Accumulate acc(add_minus_one);
+	int64_t sum = 0;
+	for (auto pel:PipeOutput(acc)) {
+		Accumulate::TSUM *el = (Accumulate::TSUM*)pel;
+		for (int i=0; i<acc.nth; i++)
+			sum += el->sum[i];
+	}
+	EXPECT_TRUE(limit*sz*(limit*sz-1)/2 == sum);
 }
