7742a397d3a19816cd3e0995ade52714ee41f660
Denis Kokarev <d_kokarev@mail.ru>
2018-05-15 22:25:16 -0400

    cf754d wrong answer 10

--- c/cf/cf754d.cc
+++ c/cf/cf754d.cc
@@ -5,6 +5,7 @@ using namespace std;
 
 struct C {
 	int64_t l, r;
+	int id;
 };
 
 struct CS {
@@ -18,8 +19,8 @@ int fill_ccs(CS *ccs, int n, int64_t shrink, C *cc) {
 	for (int i=0; i<n; i++) {
 		auto &c = cc[i];
 		if (c.r > c.l+shrink) {
-			ccs[len++] = CS {c.l, +1, i};
-			ccs[len++] = CS {c.r-shrink, -1, i};
+			ccs[len++] = CS {c.l, +1, c.id};
+			ccs[len++] = CS {c.r-shrink, -1, c.id};
 		}
 	}
 	sort(ccs, ccs+len, [](const CS &a, const CS &b){return a.pos<b.pos || (a.pos==b.pos && a.sign<b.sign);});
@@ -45,6 +46,7 @@ void select_coupons(int n, int64_t shrink, C *cc, int *sel, int mx) {
 	for (int i=0; i<len; i++) {
 		cnt += ccs[i].sign;
 		sel[ccs[i].id] += ccs[i].sign;
+		//cerr << "id=" << ccs[i].id << " sign=" << ccs[i].sign << " cnt=" << cnt << endl;
 		if (cnt == mx)
 			break;
 	}
@@ -54,9 +56,11 @@ int main(int argc, char **argv) {
 	int n, k;
 	cin >> n >> k;
 	C cc[n];
-	for (auto &c:cc) {
+	for (int i=0; i<n; i++) {
+		auto &c = cc[i];
 		cin >> c.l >> c.r;
 		c.r++;
+		c.id = i;
 	}
 	sort(cc, cc+n, [](const C &a, const C &b){return a.l<b.l;});
 	int64_t f=0, t=LONG_MAX;
@@ -64,10 +68,10 @@ int main(int argc, char **argv) {
 	while (f<t) {
 		int64_t m = f+(t-f)/2;
 		cnt = count_overlaps(n, m, cc);
-		if (cnt < k)
-			t = m;
-		else
+		if (cnt >= k)
 			f = m+1;
+		else
+			t = m;
 	}
 	int shrink = f;
 	cout << shrink << endl;
