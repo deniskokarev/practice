fb2836c9ad3a6d37f833fd170aee224cc5663da8
Denis Kokarev <d_kokarev@mail.ru>
2018-05-17 22:37:05 -0400

    acmp 1393 solution

--- c/acmp/acmp1393.cpp
+++ c/acmp/acmp1393.cpp
@@ -10,12 +10,6 @@ struct Q {
     int min_flow;
 };
 
-// edge and the graph matrix elements at the same time
-struct E {
-	int cost;
-    int next;
-};
- 
 /**
  * BFS for shortest path from src to drn and identify the "thinnest" edge
  * on the way. Populate the path
@@ -27,7 +21,7 @@ struct E {
  * @param[out] *min_flow - smallest edge on the path
  * @return - true when path was found - otherwise false
  */
-static int bfs_shortest_path(const E *ff, int sz, int src, int drn, int *path, int *min_flow) {
+static int bfs_shortest_path(const int *ff, int sz, int src, int drn, int *path, int *min_flow) {
     memset(path, 0xff, sz*sizeof(*path)); // fill(path, path+sz, -1)
     int sz_l2 = int(ceil(log2(sz+1)));
     int qsz = 1<<sz_l2;     // sz rounded up to nearest power of 2
@@ -38,11 +32,12 @@ static int bfs_shortest_path(const E *ff, int sz, int src, int drn, int *path, i
     queue[qt++] = q;
     path[src] = src;
     while (qh != qt && queue[qh].to != drn) {
-		int idx_0 = queue[qh].to*sz;
-        for (int v=ff[idx_0].next; v; v=ff[idx_0+v].next) {
-            if (ff[idx_0+v].cost>0 && path[v]==-1) {
+		int f = queue[qh].to;
+		int idx_0 = f*sz;
+        for (int v=0; v<sz; v++) {
+            if (ff[idx_0+v]>0 && path[v]==-1) {
                 q.to = v;
-                q.min_flow = min(queue[qh].min_flow, ff[idx_0+v].cost);
+                q.min_flow = min(queue[qh].min_flow, ff[idx_0+v]);
                 path[q.to] = queue[qh].to;
                 queue[qt++] = q;
                 qt &= qsz_mask;
@@ -64,25 +59,18 @@ static int bfs_shortest_path(const E *ff, int sz, int src, int drn, int *path, i
  * @return maximum possible flow
  * ff[sz][sz] will reflect the said maximum flow subtracted
  */
-int maxflow(E *ff, int sz, int src, int drn) {
+int maxflow(int *ff, int sz, int src, int drn) {
     int path[sz];
     int df;
     int flow = 0;
     while (bfs_shortest_path(ff, sz, src, drn, path, &df)) {
         flow += df;
         for (int v=drn,vp=path[v]; v!=src; v=vp,vp=path[v]) {
-            ff[vp*sz+v].cost -= df; // ff[vp][v] -= df;
-            ff[v*sz+vp].cost += df; // ff[v][vp] += df;
+            ff[vp*sz+v] -= df; // ff[vp][v] -= df;
+            ff[v*sz+vp] += df; // ff[v][vp] += df;
         }
     }
     return flow;
-}
-
-void add_edge(E *mm, int dim, int f, int t) {
-	mm[f*dim+t] = E {1, mm[f*dim].next};
-	mm[f*dim].next = t;
-	mm[t*dim+f] = E {0, mm[t*dim].next};
-	mm[t*dim].next = f;
 }
 
 int main(int argc, char **argv) {
@@ -92,7 +80,7 @@ int main(int argc, char **argv) {
 	scanf("%100s", nm);
 	int nml = (int)strlen(nm);
 	int dim = n+n+2+1; // for all 1-base nodes, including src and drain
-	E mm[dim][dim];
+	int mm[dim][dim];
 	memset(mm, 0, sizeof(mm));
 	for (int qi=n; qi<n+n; qi++) {
 		char qb[7];
@@ -100,27 +88,25 @@ int main(int argc, char **argv) {
 		int qbl = (int)strlen(qb);
 		for (int i=0; i<nml; i++)
 			for (int j=0; j<qbl; j++)
-				if (nm[i] == qb[j] && mm[i+1][qi+1].cost == 0)
-					add_edge((E*)mm, dim, i+1, qi+1);
+				if (nm[i] == qb[j])
+					mm[i+1][qi+1] = 1;
 	}
 	int src = n+n+1, drn = n+n+2;
 	for (int i=0; i<nml; i++)
-		add_edge((E*)mm, dim, src, i+1);
+		mm[src][i+1] = 1;
 	for (int qi=n; qi<n+n; qi++)
-		add_edge((E*)mm, dim, qi+1, drn);
-	int flow = maxflow((E*)mm, dim, src, drn);
-	if (flow < n) {
+		mm[qi+1][drn] = 1;
+	int mm_save[dim][dim];
+	memcpy(mm_save, mm, sizeof(mm));
+	int flow = maxflow((int*)mm, dim, src, drn);
+	if (flow != nml) {
 		printf("NO\n");
 	} else {
 		printf("YES\n");
-		for (int i=0; i<nml; i++) {
-			for (int v=mm[i+1][0].next; v; v=mm[i+1][v].next) {
-				if (mm[i+1][v].cost == 0) {
+		for (int i=1; i<=nml; i++)
+			for (int v=n+1; v<=n+n; v++)
+				if (mm_save[i][v] == 1 && mm[i][v] == 0)
 					printf("%d ", v-n);
-					break;
-				}
-			}
-		}
 		printf("\n");
 	}
 }
