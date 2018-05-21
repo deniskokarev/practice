97ebbba5f0262982ed509fe86eb5ef296fc3cf19
Denis Kokarev <d_kokarev@mail.ru>
2018-05-21 17:00:19 -0400

    acmp 26 solution

--- c/acmp/acmp26.cpp
+++ c/acmp/acmp26.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	int64_t x1, y1, r1;
-	int64_t x2, y2, r2;
+	int x1, y1, r1;
+	int x2, y2, r2;
 	cin >> x1 >> y1 >> r1;
 	cin >> x2 >> y2 >> r2;
-	int64_t dsq = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
-	int64_t rsq = (r1+r2)*(r1+r2);
-	cout << ((rsq>=dsq)?"YES":"NO") << endl;
+	int dsq = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+	int rsq_hi = (r1+r2)*(r1+r2);
+	int rsq_lo = (r1-r2)*(r1-r2);
+	if (rsq_hi >= dsq && dsq >= rsq_lo)
+		cout << "YES\n";
+	else
+		cout << "NO\n";
 	return 0;
 }
