77b74c63da897c931faf82783142eb399fee3d41
Denis Kokarev <d_kokarev@mail.ru>
2018-05-21 18:05:43 -0400

    acmp 82 wrong answer 4

---
+++ c/acmp/acmp82.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cassert>
+/* ACMP 82 */
+using namespace std;
+
+constexpr int MX = 1e5;
+
+int main(int argc, char **argv) {
+	int n, m;
+	scanf("%d%d", &n, &m);
+	int cnt[MX+1];
+	memset(cnt, 0, sizeof(cnt));
+	int a;
+	while (n--) {
+		scanf("%d", &a);
+		assert(0 <= a && a <= MX);
+		cnt[a] = 1;
+	}
+	while (m--) {
+		scanf("%d", &a);
+		assert(0 <= a && a <= MX);
+		cnt[a]++;
+	}
+	for (int i=0; i<=MX; i++)
+		if (cnt[i]>1)
+			printf("%d ", i);
+	printf("\n");
+	return 0;
+}
