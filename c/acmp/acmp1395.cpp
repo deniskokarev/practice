4e36a4d891f9b57132bac65c0cfa7295b7746297
Denis Kokarev <d_kokarev@mail.ru>
2018-05-20 21:11:11 -0400

    acmp 1395 solution with Ford

--- c/acmp/acmp1395.cpp
+++ c/acmp/acmp1395.cpp
@@ -4,7 +4,6 @@
 #include <cassert>
 #include <queue>
 /* ACMP 1395 */
-/* solution from Fedor Menshikov */
 
 // graph edge
 struct E {
@@ -14,49 +13,38 @@ struct E {
 };
 
 /**
- * SPFA min path over cost from src to drn
- * Populate the path
+ * Ford min path over cost from src to drn and identify the "thinnest" edge
+ * on the way. Populate the path
  * @param[in] ff[sz][sz] - graph matrix with flow and cost capacities
  * @param[in] sz - number of nodes
  * @param[in] src - source node
  * @param[in] dst - drain node
  * @param[out] path[sz] - trace path in parent-of fashion
- * @param[out] min_flow - min possible flow from src to dst
+ * @param[out] *min_flow - smallest edge on the path
  * @return - true when path was found - otherwise false
  */
-static int spfa_path(const std::vector<std::vector<E>> &ff, int sz, int src, int drn, std::vector<int> &path, int &min_flow) {
-    constexpr int INF = INT_MAX;
-    std::vector<int> dd(sz, INF);
-    std::vector<int> mf(sz, INF);
-    std::vector<bool> inQ(sz, false);
-    std::queue<int> qq;
-	dd[src] = 0;
-	inQ[src] = true;
-	path[src] = src;
-	path[drn] = -1;
-	qq.push(src);
-	while (!qq.empty()) {
-		int cur = qq.front();
-		qq.pop();
-		inQ[cur] = false;
-		for (int nei=0; nei<sz; nei++) {
-			if (ff[cur][nei].flow < ff[cur][nei].cap) {
-				if (dd[nei] > dd[cur]+ff[cur][nei].cost) {
-					dd[nei] = dd[cur]+ff[cur][nei].cost;
-					path[nei] = cur;
-					mf[nei] = std::min(mf[cur], ff[cur][nei].cap-ff[cur][nei].flow);
-					if (!inQ[nei]) {
-						qq.push(nei);
-						inQ[nei] = true;
-					}
-				}
-			}
-		}
-	}
-	min_flow = mf[drn];
-	return (dd[drn] < INF);
+static int ford_path(const std::vector<std::vector<E>> &ff, int sz, int src, int drn, std::vector<int> &path, int &min_flow) {
+	// to find path
+	struct D {
+		int cost, min_flow;
+	};
+	std::vector<D> dd(sz, D {INT_MAX/2, INT_MAX});
+    path[src] = src;
+    dd[src] = D {0, INT_MAX};
+    for (int step=0; step<sz; step++) {
+        for (int v=0; v<sz; v++) {
+            for (int to=0; to<sz; to++) {
+                if (ff[v][to].cap > ff[v][to].flow && dd[to].cost > dd[v].cost + ff[v][to].cost) {
+                    dd[to].cost = dd[v].cost + ff[v][to].cost;
+                    dd[to].min_flow = std::min(dd[v].min_flow, ff[v][to].cap-ff[v][to].flow);
+                    path[to] = v;
+                }
+            }
+        }
+    }
+    min_flow = dd[drn].min_flow;
+    return (min_flow != INT_MAX);
 }
-
 
 /**
  * find max flow from src into drn in the graph in O(V*E*FLOW)
@@ -72,7 +60,7 @@ int maxflow_mincost(std::vector<std::vector<E>> &ff, int sz, int src, int drn, i
     int sum = 0;
 	int flow = 0;
 	int df;
-    while (spfa_path(ff, sz, src, drn, path, df)) {
+    while (ford_path(ff, sz, src, drn, path, df)) {
 		flow += df;
         for (int v=drn,vp=path[v]; v!=src; v=vp,vp=path[v]) {
 			sum += ff[vp][v].cost*df;
@@ -86,37 +74,37 @@ int maxflow_mincost(std::vector<std::vector<E>> &ff, int sz, int src, int drn, i
 
 int main(int argc, char **argv) {
     const char i2let[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";	
-    int len, nL;
-    scanf("%d %d", &len, &nL);
+    int n, k;
+    scanf("%d %d", &n, &k);
     int let2i[256]{-1};
-    for (int i=0; i<nL; i++)
+    for (int i=0; i<k; i++)
         let2i[(unsigned char)(i2let[i])] = i;	
     char s1[2000001];
     char s2[2000001];
     scanf("%2000000s %2000000s", s1, s2);
-    int dim = nL*2+2;
+    int dim = k*2+2;
     int src = dim-2;
     int drn = dim-1;
     std::vector<std::vector<E>> ff(dim, std::vector<E>(dim, E{0,0,0}));
-    for (int i=0; i<len; i++) {
+    for (int i=0; i<n; i++) {
 		int from = let2i[(unsigned char)(s1[i])];
 		int to = let2i[(unsigned char)(s2[i])];
-		ff[from][to+nL].cost--;
-		ff[to+nL][from].cost++;
+		ff[from][to+k].cost--;
+		ff[to+k][from].cost++;
 	}
-    for (int from=0; from<nL; from++)
-        for (int to=0; to<nL; to++)
-            ff[from][to+nL].cap = 1;
-    for (int i=0; i<nL; i++)
+    for (int from=0; from<k; from++)
+        for (int to=0; to<k; to++)
+            ff[from][to+k].cap = 1;
+    for (int i=0; i<k; i++)
         ff[src][i].cap = 1;
-    for (int i=0; i<nL; i++)
-        ff[i+nL][drn].cap = 1;
+    for (int i=0; i<k; i++)
+        ff[i+k][drn].cap = 1;
 	int cost;
 	maxflow_mincost(ff, dim, src, drn, &cost);
     printf("%d\n", -cost);
-    for (int from = 0; from < nL; from++)
-        for (int to = 0; to < nL; to++)
-            if (ff[from][to+nL].flow > 0)
+    for (int from = 0; from < k; from++)
+        for (int to = 0; to < k; to++)
+            if (ff[from][to+k].flow > 0)
                 printf("%c", i2let[to]);
     printf("\n");
     return 0;
