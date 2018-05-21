5619fe04cd6d5eaaaf14e456d9f55bd0849b5e9e
Denis Kokarev <d_kokarev@mail.ru>
2018-05-20 20:57:22 -0400

    acmp 1395 continue morphing Feder's solution

--- c/acmp/acmp1395.cpp
+++ c/acmp/acmp1395.cpp
@@ -21,11 +21,13 @@ struct E {
  * @param[in] src - source node
  * @param[in] dst - drain node
  * @param[out] path[sz] - trace path in parent-of fashion
+ * @param[out] min_flow - min possible flow from src to dst
  * @return - true when path was found - otherwise false
  */
-static int spfa_path(const std::vector<std::vector<E>> &ff, int sz, int src, int drn, std::vector<int> &path) {
+static int spfa_path(const std::vector<std::vector<E>> &ff, int sz, int src, int drn, std::vector<int> &path, int &min_flow) {
     constexpr int INF = INT_MAX;
     std::vector<int> dd(sz, INF);
+    std::vector<int> mf(sz, INF);
     std::vector<bool> inQ(sz, false);
     std::queue<int> qq;
 	dd[src] = 0;
@@ -42,6 +44,7 @@ static int spfa_path(const std::vector<std::vector<E>> &ff, int sz, int src, int
 				if (dd[nei] > dd[cur]+ff[cur][nei].cost) {
 					dd[nei] = dd[cur]+ff[cur][nei].cost;
 					path[nei] = cur;
+					mf[nei] = std::min(mf[cur], ff[cur][nei].cap-ff[cur][nei].flow);
 					if (!inQ[nei]) {
 						qq.push(nei);
 						inQ[nei] = true;
@@ -50,6 +53,7 @@ static int spfa_path(const std::vector<std::vector<E>> &ff, int sz, int src, int
 			}
 		}
 	}
+	min_flow = mf[drn];
 	return (dd[drn] < INF);
 }
 
@@ -63,16 +67,17 @@ static int spfa_path(const std::vector<std::vector<E>> &ff, int sz, int src, int
  * @return maximum possible flow
  * ff[sz][sz] will reflect the said maximum flow subtracted
  */
-int maxflow_mincost(std::vector<std::vector<E>> &ff, int sz, int src, int drn, int *cost, int nL) {
+int maxflow_mincost(std::vector<std::vector<E>> &ff, int sz, int src, int drn, int *cost) {
     std::vector<int> path(sz);
     int sum = 0;
-	int flow;
-    for (flow = 0; flow < nL; flow++) {
-		assert(spfa_path(ff, sz, src, drn, path));
+	int flow = 0;
+	int df;
+    while (spfa_path(ff, sz, src, drn, path, df)) {
+		flow += df;
         for (int v=drn,vp=path[v]; v!=src; v=vp,vp=path[v]) {
-			sum += ff[vp][v].cost;
-            ff[vp][v].flow++;
-            ff[v][vp].flow--;
+			sum += ff[vp][v].cost*df;
+            ff[vp][v].flow += df;
+            ff[v][vp].flow -= df;
         }
     }
 	*cost = sum;
@@ -85,7 +90,7 @@ int main(int argc, char **argv) {
     scanf("%d %d", &len, &nL);
     int let2i[256]{-1};
     for (int i=0; i<nL; i++)
-        let2i[unsigned(i2let[i])] = i;	
+        let2i[(unsigned char)(i2let[i])] = i;	
     char s1[2000001];
     char s2[2000001];
     scanf("%2000000s %2000000s", s1, s2);
@@ -94,8 +99,8 @@ int main(int argc, char **argv) {
     int drn = dim-1;
     std::vector<std::vector<E>> ff(dim, std::vector<E>(dim, E{0,0,0}));
     for (int i=0; i<len; i++) {
-		int from = let2i[int(s1[i])];
-		int to = let2i[int(s2[i])];
+		int from = let2i[(unsigned char)(s1[i])];
+		int to = let2i[(unsigned char)(s2[i])];
 		ff[from][to+nL].cost--;
 		ff[to+nL][from].cost++;
 	}
@@ -107,7 +112,7 @@ int main(int argc, char **argv) {
     for (int i=0; i<nL; i++)
         ff[i+nL][drn].cap = 1;
 	int cost;
-	maxflow_mincost(ff, dim, src, drn, &cost, nL);
+	maxflow_mincost(ff, dim, src, drn, &cost);
     printf("%d\n", -cost);
     for (int from = 0; from < nL; from++)
         for (int to = 0; to < nL; to++)
