53c2749337d9cadf15d303525c039815c3a1b280
Denis Kokarev <d_kokarev@mail.ru>
2018-05-21 17:22:29 -0400

    acmp 2 wrong answer 8

--- c/acmp/acmp2.cpp
+++ c/acmp/acmp2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main(int argc, char **argv) {
 	int n;
 	cin >> n;
-	cout << (n+1)*n/2 << endl;
+	if (n >= 0)
+		cout << (n+1)*n/2 << endl;
+	else
+		cout << 1-(n-1)*n/2 << endl;
 	return 0;
 }
