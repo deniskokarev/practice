96f095f771f05c7832d48051ded860ea7766266e
Denis Kokarev <d_kokarev@mail.ru>
2018-05-23 22:01:25 -0400

    acmp 557 timeout 7

---
+++ c/acmp/acmp557.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <vector>
+/* ACMP 557 */
+using namespace std;
+
+inline int get_el(const vector<vector<vector<int>>> &mmm, int n, int p, int mf, int mt, int i, int j) {
+	if (mf+1 == mt)
+		return mmm[mf][i][j];
+	int cc[n];
+	for (int ci=0; ci<n; ci++)
+		cc[ci] = get_el(mmm, n, p, mf, (mf+mt)/2, i, ci);
+	int rr[n];
+	for (int ri=0; ri<n; ri++)
+		rr[ri] = get_el(mmm, n, p, (mf+mt)/2, mt, ri, j);
+	int rc = 0;
+	for (int i=0; i<n; i++)
+		rc += cc[i]*rr[i] % p;
+	return rc % p;
+}
+
+int main(int argc, char **argv) {
+	int m, n;
+	scanf("%d%d", &m, &n);
+	int a, b;
+	scanf("%d%d", &a, &b);
+	int p;
+	scanf("%d", &p);
+	vector<vector<vector<int>>> mmm(m, vector<vector<int>>(n, vector<int>(n)));
+	for (int l=0; l<m; l++)
+		for (int i=0; i<n; i++)
+			for (int j=0; j<n; j++)
+				scanf("%d", &mmm[l][i][j]);
+	int ans = get_el(mmm, n, p, 0, m, a-1, b-1);
+	printf("%d\n", ans);
+	return 0;
+}
