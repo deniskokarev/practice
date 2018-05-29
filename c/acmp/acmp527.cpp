b4e216ce84de43174e09b092400b19a9d569792e
Denis Kokarev <d_kokarev@mail.ru>
2018-05-28 20:30:56 -0400

    acmp 527 wrong answer 2

--- c/acmp/acmp527.cpp
+++ c/acmp/acmp527.cpp
@@ -8,21 +8,15 @@ int main(int argc, char **argv) {
 	while (k--) {
 		uint64_t a, b, c, d;
 		cin >> a >> b >> c >> d;
-		// maintain a>b && c>d
-		if (a < b)
-			swap(a, b);
-		if (c < d)
-			swap(c, d);
+		uint64_t h = max(a,b), l = min(a,b);
 		bool ans = false;
-		if (a >= c && b >= d) {
-			while (b > 0) {
-				uint64_t temp = b;
-				b = a % b;
-				a = temp;
-				if (a == d && (c-b)%a == 0) {
-					ans = true;
-					break;
-				}
+		while (l > 0) {
+			uint64_t temp = l;
+			l = h % l;
+			h = temp;
+			if (h == d && (c == l || (c <= a && (c+l)%h == 0))) {
+				ans = true;
+				break;
 			}
 		}
 		cout << (ans?"YES":"NO") << endl;
