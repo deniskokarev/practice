9c2415000eee16aa9730ec66628c3599999698b8
Denis Kokarev <d_kokarev@mail.ru>
2018-06-02 14:56:12 -0400

    cf985c wrong answer 7

--- c/cf/cf985c.cc
+++ c/cf/cf985c.cc
@@ -1,34 +1,22 @@
 #include <iostream>
 #include <algorithm>
-#include <climits>
 /* CodeForces CF985C problem */
 using namespace std;
 
 int main(int argc, char **argv) {
 	int n, k;
-	int64_t l;
+	int l;
 	cin >> n >> k >> l;
 	int m = n*k;
-	int64_t aa[m+1];
-	aa[m] = LLONG_MAX;
+	int aa[m+1];
+	aa[m] = INT_MAX;
 	for (int i=0; i<m; i++)
 		cin >> aa[i];
 	sort(aa, aa+m);
-	// upper bound search of aa[0]+l
-	int64_t key = aa[0]+l;
-	int f = 0, t = m+1;
-	while (f<t) {
-		int d = f+(t-f)/2;
-		if (aa[d] <= key)
-			f = d+1;
-		else
-			t = d;
-	}
-	int p = f-1;
-	if (p+1>=n) {
-		int64_t ans = aa[0];
-		n--;
-		for (int i=p; i>0 && n--; i--)
+	int p = upper_bound(aa, aa+m+1, aa[0]+l)-aa;
+	if (p>=n) {
+		int64_t ans = 0;
+		for (int i=0; i<p && n--; i++)
 			ans += aa[i];
 		cout << ans << endl;
 	} else {
