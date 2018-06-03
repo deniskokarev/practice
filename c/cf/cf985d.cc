4924b41fe88c580b15f08d70ab71fac76439abd6
Denis Kokarev <d_kokarev@mail.ru>
2018-06-03 19:17:20 -0400

    cf985d solution with comments

--- c/cf/cf985d.cc
+++ c/cf/cf985d.cc
@@ -2,23 +2,22 @@
 /* CodeForces CF985D problem */
 using namespace std;
 
+// find base width of the pyramid centered at c
+// with the side hieght <= H
+// and total volume no less than n
 int64_t width(int64_t c, int64_t n, int64_t H, int64_t &s) {
 	if (c>=0) {
 		int64_t h = H+c;
 		s = h*h - H*(H-1)/2;
-		if (s >= n) {
-			cerr << "c=" << c << " s=" << s << " w=" << 2*h-H << endl;
+		if (s >= n)
 			return 2*h-H;
-		}
 	} else {
 		if (c >= -H) {
 			int64_t h = H+c;
 			if (h>0) {
 				s = h*(h+1)/2;
-				if (s >= n) {
-					cerr << "c=" << c << " s=" << s << " w=" << h << endl;
+				if (s >= n)
 					return h;
-				}
 			}
 		}
 	}
@@ -28,10 +27,11 @@ int64_t width(int64_t c, int64_t n, int64_t H, int64_t &s) {
 int main(int argc, char **argv) {
 	int64_t n, H;
 	cin >> n >> H;
+	// cap unreasonable H values to prevent overflow
 	if (H > INT_MAX)
 		H = INT_MAX;
-	int64_t s;
-	// upper bound search of pile center
+	int64_t s; // volume
+	// upper bound search of pyramid center
 	int64_t cf=INT_MIN, ct=INT_MAX;
 	while (cf < ct) {
 		int64_t cm = cf+(ct-cf)/2;
@@ -42,6 +42,7 @@ int main(int argc, char **argv) {
 			ct = cm;
 	}
 	int64_t w = width(cf, n, H, s);
+	// check if it is possible to shave off right slope without moving center of the pyramid
 	if (cf > 0 && n <= s-(w-cf))
 		w--;
 	cout <<  w << endl;
