f5839d25b79ebd1607aa653842ed38de8d679adb
Denis Kokarev <d_kokarev@mail.ru>
2018-06-03 18:45:22 -0400

    cf985d wrong answer 3

---
+++ c/cf/cf985d.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+/* CodeForces CF985D problem */
+using namespace std;
+
+int64_t width(int64_t c, int64_t n, int64_t H, int64_t &s) {
+	if (c>=0) {
+		int64_t h = H+c;
+		s = h*h - H*(H-1)/2;
+		//cerr << "c=" << c << " s=" << s << " w=" << 2*h-H << endl;
+		if (s >= n)
+			return 2*h-H;
+	} else {
+		if (c >= -H) {
+			int64_t h = H+c;
+			if (h>0) {
+				s = h*(h+1)/2;
+				//cerr << "c=" << c << " s=" << s << " w=" << h << endl;
+				if (s >= n)
+					return h;
+			}
+		}
+	}
+	return -1;
+}
+
+int main(int argc, char **argv) {
+	int64_t n, H;
+	cin >> n >> H;
+	int64_t s;
+	// upper bound search of pile center
+	int64_t cf=INT_MIN, ct=INT_MAX;
+	while (cf < ct) {
+		int64_t cm = cf+(ct-cf)/2;
+		int64_t w = width(cm, n, H, s);
+		if (w == -1)
+			cf = cm+1;
+		else
+			ct = cm;
+	}
+	int64_t w = width(cf, n, H, s);
+	if (cf > 0 && n >= s-(w-cf))
+		w--;
+	cout <<  w << endl;
+	return 0;
+}
