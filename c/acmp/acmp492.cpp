aa7488734aa78cd31e67f49b254f8b222015c152
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-07-12 20:54:10 -0400

    acmp 492 wrong asnwer 6

--- c/acmp/acmp492.cpp
+++ c/acmp/acmp492.cpp
@@ -2,13 +2,6 @@
 /* ACMP 492 problem */
 using namespace std;
 
-using numtype = __float128; 
-
-numtype dist2(numtype x, numtype y, numtype scale) {
-	numtype d2 = (x-x*scale)*(x-x*scale) + (y-y*scale)*(y-y*scale);
-	return d2;
-}
- 
 int main(int argc, char **argv) {
 	int x0, y0;
 	cin >> x0 >> y0;
@@ -16,22 +9,16 @@ int main(int argc, char **argv) {
 	cin >> vx >> vy;
 	int v, t, d;
 	cin >> v >> t >> d;
-	numtype x1 = x0 + vx*t;
-	numtype y1 = y0 + vy*t;
-	numtype eps = 1e-17;
-	numtype sf = -1e10, st = 1+eps;
-	while (sf+eps < st) {
-		numtype sm = (sf+st)/2;
-		if (dist2(x1, y1, sm) > d*d)
-			sf = sm+eps;
-		else
-			st = sm;
-	}
-	//cerr << sf << endl;
-	numtype d2 = (x1*x1+y1*y1)*sf*sf;
-	if (v*v*t*t >= d2)
-		cout << "YES" << endl;
-	else
+	int x1 = x0+vx*t;
+	int y1 = y0+vy*t;
+	int64_t dsp2 = d+v*t;
+	dsp2 *= dsp2;
+	int64_t dsm2 = d-v*t;
+	dsm2 *= dsm2;
+	int64_t dt2 = x1*x1+y1*y1;
+	if (dsp2 < dt2 || dsm2 > dt2)
 		cout << "NO" << endl;
+	else
+		cout << "YES" << endl;
 	return 0;
 }
