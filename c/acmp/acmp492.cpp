a4c7b6a37788d2b26898ef261b668be4c27fb0cb
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-07-12 22:17:15 -0400

    acmp 492 solution

--- c/acmp/acmp492.cpp
+++ c/acmp/acmp492.cpp
@@ -11,16 +11,12 @@ int main(int argc, char **argv) {
 	cin >> v >> t >> d;
 	int64_t x1 = x0+vx*t;
 	int64_t y1 = y0+vy*t;
-	int64_t dsp2 = d+v*t;
-	dsp2 *= dsp2;
-	int64_t dsm2 = d-v*t;
-	dsm2 *= dsm2;
 	int64_t dt2 = x1*x1+y1*y1;
 	bool ans;
 	if (dt2 >= d*d)
-		ans = (dsp2 >= dt2);
+		ans = ((d+v*t)*(d+v*t) >= dt2);
 	else
-		ans = (dsm2 <= dt2);
+		ans = ((d-v*t)*(d-v*t) <= dt2) || (v*t>d);
 	cout << (ans?"YES":"NO") << endl;
 	return 0;
 }
