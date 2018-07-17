175a5f5ed8a0661522cf1fd741c859e541f3635c
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-07-17 16:24:00 -0400

    acmp 300, WA 33

--- c/acmp/acmp300.cpp
+++ c/acmp/acmp300.cpp
@@ -14,39 +14,28 @@ int main(int argc, char **argv) {
 	for (int i=0; i<4; i++) {
 		auto &r = rr[i];
 		cin >> r.t >> r.v;
-		if (r.v > 0)
-			vvvv *= r.v;
+		vvvv *= r.v;
 		r.angle = i;
 	}
 	uint64_t t, d;
 	cin >> t >> d;
 	uint64_t dv = d * vvvv;
 	for (auto &r:rr)
-		if (r.v > 0)
-			r.atv = r.t * vvvv + dv/r.v;
-		else
-			r.atv = ULLONG_MAX;
+		r.atv = r.t * vvvv + dv/r.v;
 	sort(rr, rr+4, [](const R &a, const R &b){return a.atv < b.atv;});
 	int cnt = 1;
-	int killed = (rr[0].atv != ULLONG_MAX);
 	uint64_t tv = t * vvvv;
 	for (int i=1; i<4; i++) {
 		int a = min((rr[i].angle+4-rr[i-1].angle)%4, (rr[i-1].angle+4-rr[i].angle)%4);
-		if (rr[i-1].atv == ULLONG_MAX || rr[i].atv == ULLONG_MAX) {
+		if (rr[i-1].atv + a*tv <= rr[i].atv)
 			cnt++;
-		} else if (tv == 0 && rr[i-1].atv == rr[i].atv) {
+		else
 			break;
-		} else if (rr[i-1].atv + a*tv <= rr[i].atv) {
-			cnt++;
-			killed++;
-		} else {
-			break;
-		}
 	}
 	//cerr << cnt << endl;
 	if (cnt == 4)
 		cout << "ALIVE" << endl;
 	else
-		cout << killed << endl;
+		cout << cnt++ << endl;
 	return 0;
 }
