dfdd1f3ff0902124632229fb3f865ab46dc7a647
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-07-17 17:02:14 -0400

    acmp 300 solution

--- c/acmp/acmp300.cpp
+++ c/acmp/acmp300.cpp
@@ -8,6 +8,10 @@ struct R {
 	uint64_t t, v, atv;
 };
 
+int angle(R *rr, int i, int j) {
+	return min((rr[i].angle+4-rr[j].angle)%4, (rr[j].angle+4-rr[i].angle)%4);
+}
+
 int main(int argc, char **argv) {
 	R rr[4];
 	uint64_t vvvv = 1;
@@ -23,15 +27,18 @@ int main(int argc, char **argv) {
 	for (auto &r:rr)
 		r.atv = r.t * vvvv + dv/r.v;
 	sort(rr, rr+4, [](const R &a, const R &b){return a.atv < b.atv;});
+	for (int f=0,i=2; i<4; i++)
+		if (rr[i-1].atv == rr[i].atv && angle(rr, f, i-1) > angle(rr, f, i))
+			swap(rr[i-1], rr[i]);
+		else
+			f = i-1;
 	int cnt = 1;
 	uint64_t tv = t * vvvv;
-	for (int i=1; i<4; i++) {
-		int a = min((rr[i].angle+4-rr[i-1].angle)%4, (rr[i-1].angle+4-rr[i].angle)%4);
-		if (rr[i-1].atv + a*tv <= rr[i].atv)
+	for (int i=1; i<4; i++)
+		if (rr[i-1].atv + angle(rr, i-1, i)*tv <= rr[i].atv)
 			cnt++;
 		else
 			break;
-	}
 	//cerr << cnt << endl;
 	if (cnt == 4)
 		cout << "ALIVE" << endl;
