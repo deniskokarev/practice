65be9f53b49b613f25962fa188a4805ceef5227f
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-07-17 11:56:50 -0400

    acmp 300, hmm, still wrong answer 33

--- c/acmp/acmp300.cpp
+++ c/acmp/acmp300.cpp
@@ -14,20 +14,24 @@ int main(int argc, char **argv) {
 	for (int i=0; i<4; i++) {
 		auto &r = rr[i];
 		cin >> r.t >> r.v;
-		vvvv *= r.v;
+		if (r.v > 0)
+			vvvv *= r.v;
 		r.angle = i;
 	}
 	uint64_t t, d;
 	cin >> t >> d;
 	uint64_t dv = d * vvvv;
 	for (auto &r:rr)
-		r.atv = r.t * vvvv + dv/r.v;
+		if (r.v > 0)
+			r.atv = r.t * vvvv + dv/r.v;
+		else
+			r.atv = UINT_MAX;
 	sort(rr, rr+4, [](const R &a, const R &b){return a.atv < b.atv;});
 	int cnt = 1;
 	uint64_t tv = t * vvvv;
 	for (int i=1; i<4; i++) {
 		int a = min((rr[i].angle+4-rr[i-1].angle)%4, (rr[i-1].angle+4-rr[i].angle)%4);
-		if (rr[i-1].atv + a*tv <= rr[i].atv)
+		if (rr[i].atv == UINT_MAX || rr[i-1].atv + a*tv <= rr[i].atv)
 			cnt++;
 		else
 			break;
