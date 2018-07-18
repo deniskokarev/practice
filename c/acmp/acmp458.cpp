b7a90e3dfd20890cbf20a7dfa9cd2c5a16332f94
Denis Kokarev <d_kokarev@mail.ru>
2018-07-17 20:31:54 -0400

    acmp 458 solution

--- c/acmp/acmp458.cpp
+++ c/acmp/acmp458.cpp
@@ -10,17 +10,25 @@ int main(int argc, char **argv) {
 	int oo[h];
 	for (auto &o:oo)
 		scanf("%d", &o);
-	int rr[h];
-	for (int i=0; i<h; i++)
-		rr[oo[i]-1] = i;
-	char s[1024];
-	memset(s, ' ', sizeof(s));
-	scanf(" %1023s", s);
-	int sln = strlen(s);
+	char si[1024];
+	scanf(" %1023s", si);
+	int sln = strlen(si);
 	int w = (sln+h-1)/h;
+	int rem = h*w-sln;
+	char so[h][w];
+	memset(so, ' ', sizeof(so));
+	int p = 0, rln;
+	for (int r=0; r<h; r++) {
+		if (oo[r] > h-rem)
+			rln = w-1;
+		else
+			rln = w;
+		memcpy(so[oo[r]-1], si+p, rln);
+		p += rln;
+	}
 	for (int c=0; c<w; c++)
 		for (int r=0; r<h; r++)
-			fputc(s[w*rr[r]+c], stdout);
+			fputc(so[r][c], stdout);
 	fputc('\n', stdout);
 	return 0;
 }
