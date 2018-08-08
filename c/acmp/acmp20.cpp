619fbc263dd201b0fec9167467816d6629cd0d1e
Denis Kokarev <d_kokarev@mail.ru>
2018-08-07 20:03:05 -0400

    acmp 20 still wrong answer 11

--- c/acmp/acmp20.cpp
+++ c/acmp/acmp20.cpp
@@ -9,22 +9,21 @@ int main(int argc, char **argv) {
 	int aa[n];
 	for (auto &a:aa)
 		scanf("%d", &a);
-	int sign[2] = {-1, 1};
 	int mx = 0;
-	for (auto s:sign) {
-		int l = 0;
-		int o = s;
-		for (int i=1; i<n; i++) {
-			if (aa[i-1]*o < aa[i]*o) {
-				if (l == 0)
-					l = 2;
-				else
-					l++;
-				mx = max(mx, l);
-			} else {
-				l = 0;
+	for (auto sign:{-1, 1}) {
+		for (auto ofs:{1, 2}) {
+			int l = 0;
+			for (int i=ofs,op=sign; i<n; i++,op*=-1) {
+				if (aa[i-1]*op < aa[i]*op) {
+					if (l == 0)
+						l = 2;
+					else
+						l++;
+					mx = max(mx, l);
+				} else {
+					l = 0;
+				}
 			}
-			o *= -1;
 		}
 	}
 	printf("%d\n", mx);
