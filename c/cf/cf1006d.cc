143993810b10fa898fffb7ac61b2d839ba966267
Denis Kokarev <d_kokarev@mail.ru>
2018-08-12 20:49:46 -0400

    cf 1006 d solution

--- c/cf/cf1006d.cc
+++ c/cf/cf1006d.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 /* CodeForces CF1006D problem */
 using namespace std;
@@ -10,33 +10,34 @@ bool isok(char *s) {
 
 int main(int argc, char **argv) {
 	int n;
-	string a, b;
-	cin >> n;
-	cin >> a >> b;
+	scanf("%d", &n);
+	char a[n+1], b[n+1];
+	scanf("%s %s", a, b);
 	int i, j;
 	int ans = 0;
 	for (i=0,j=n-1; i<j; i++,j--) {
 		struct {
 			char s[4];
-			int ans;
+			int repl;
 		} aa[] = {
 			{{a[i], a[j], b[i], b[j]}, 0},
 			{{b[i], a[j], b[i], b[j]}, 1},
 			{{b[j], a[j], b[i], b[j]}, 1},
 			{{a[i], b[i], b[i], b[j]}, 1},
-			{{a[i], b[j], b[i], b[j]}, 1}
+			{{a[i], b[j], b[i], b[j]}, 1},
+			{{a[i], a[i], b[i], b[j]}, 1},
+			{{a[j], a[j], b[i], b[j]}, 1},
+			{{b[i], b[j], b[i], b[j]}, 2}
 		};
 		for (auto &a:aa) {
 			if (isok(a.s)) {
-				ans += a.ans;
-				goto next;
+				ans += a.repl;
+				break;
 			}
 		}
-		ans += 2;
-	next:;
 	}
 	if (i==j)
 		ans += (a[i]!=b[i]);
-	cout << ans << endl;
+	printf("%d\n", ans);
 	return 0;
 }
