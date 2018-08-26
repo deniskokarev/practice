d69e2a63f383e5cefbc3f74fd4ce6a99b38b7368
Denis Kokarev <d_kokarev@mail.ru>
2018-08-26 11:52:13 -0400

    cf 1005e solution

--- c/cf/cf1005e.cc
+++ c/cf/cf1005e.cc
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#define __STDC_FORMAT_MACROS // for PRId64 in mingw
+#include <inttypes.h>
 /* CodeForces CF1005E problem */
 
 /**
  * Plain C-style Fenwick implementation
  */
-static void fw_init(int *fw, int sz) {
+static void fw_init(int64_t *fw, int sz) {
 	memset(fw, 0, sizeof(fw[0])*sz);
 }
 
-static void fw_upd(int *fw, int p, int inc, int sz) {
+static void fw_upd(int64_t *fw, int p, int inc, int sz) {
 	for (int i=p; i<sz; i|=i+1)
 		fw[i] += inc;
 }
 
-static int fw_sum(int *fw, int p, int sz) {
-	int sum = 0;
+static int64_t fw_sum(int64_t *fw, int p, int sz) {
+	int64_t sum = 0;
 	for (int i=p; i>=0; i=(i&(i+1))-1)
 		sum += fw[i];
 	return sum;
 }
 
-int cnt(int *aa, int n, int m) {
-	int c = 0;
+int64_t cnt(int *aa, int n, int m) {
+	int64_t c = 0;
 	int s_ofs = n+1;	// 2 make diff pos
 	int fw_sz = 2*n+2;
-	int fw[fw_sz];
+	int64_t fw[fw_sz];
 	fw_init(fw, fw_sz);
 	int ge[n+1];
 	int lt[n+1];
@@ -49,6 +51,6 @@ int main(int argc, char **argv) {
 	int aa[n+1];
 	for (int i=1; i<=n; i++)
 		scanf("%d", aa+i);
-	printf("%d\n", cnt(aa, n, m)-cnt(aa, n, m+1));
+	printf("%" PRId64 "\n", cnt(aa, n, m)-cnt(aa, n, m+1));
 	return 0;
 }
