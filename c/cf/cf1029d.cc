6b13840880cfc92bd49b7dee3602950824b17e2b
Denis Kokarev <d_kokarev@mail.ru>
2018-09-01 15:08:56 -0400

    cf1029d solution

--- c/cf/cf1029d.cc
+++ c/cf/cf1029d.cc
@@ -1,10 +1,16 @@
 #include <cstdio>
-#include <map>
+#include <unordered_map>
 #include <cmath>
 #define __STDC_FORMAT_MACROS // for PRId64 in mingw
 #include <cinttypes>
 /* CodeForces CF1029D problem */
 using namespace std;
+
+struct H {
+	size_t operator()(const pair<int,int> &p) const {
+		return (((size_t)p.first)<<4)+(size_t)p.second;
+	}
+};
 
 int main(int argc, char **argv) {
 	int n, k;
@@ -12,7 +18,7 @@ int main(int argc, char **argv) {
 	int aa[n];
 	for (auto &a:aa)
 		scanf("%d", &a);
-	map<pair<int,int>, int64_t> mm;
+	unordered_map<pair<int,int>, int64_t, H> mm(10000019);
 	int64_t ans = 0;
 	for (int i=0; i<n; i++) {
 		int needsfx = ceil(log10(aa[i]+1));
