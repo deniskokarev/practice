1fde451051082c45415b27cc10e96425b1c1db25
Denis Kokarev <d_kokarev@mail.ru>
2018-09-08 21:29:37 -0400

    cf1039b solution

--- c/cf/cf1039b.cc
+++ c/cf/cf1039b.cc
@@ -1,41 +1,56 @@
 #include <iostream>
 #include <random>
+#include <cassert>
+#include <algorithm>
 /* CodeForces CF139B problem */
 using namespace std;
 
 int main(int argc, char **argv) {
-	int n, k;
+	int64_t n, k;
 	cin >> n >> k;
 	bool ans = false;
 	mt19937 rnd(0);
 	string s;
 	int cnt = 0;
-	int f = 1, t = n+1;
-	while (!ans && cnt < 4500) {
-		while (f+4*k<t) {
-			int m=f+(t-f)/2;
+	int64_t f = 1, t = n+1;
+	if (k == 0) {
+		while (f<t) {
+			int64_t m=f+(t-f)/2;
 			cnt++;
 			cout << f << " " << m << endl;
 			cin >> s;
-			if (s == "Yes") {
-				f = max(1, f-k);
-				t = min(n+1, m+k);
-			} else {
-				f = max(1, m-k+1);
-				t = min(n+1, t+k);
-			}
+			if (s == "Yes")
+				t = m;
+			else
+				f = m+1;
 		}
 		cnt++;
-		if (t>f) {
-			unsigned pos = f+(rnd()%(t-f));
-			cout << pos << " " << pos << endl;
-		} else {
-			cout << f << " " << f << endl;
-		}
+		cout << f << " " << f << endl;
 		cin >> s;
-		ans = (s == "Yes");
-		f = max(1, f-k);
-		t = min(n+1, t+k);
+		assert(s == "Yes");
+	} else {
+		while (!ans) {
+			while (f+5*k<t) {
+				int64_t m=f+(t-f)/2;
+				cnt++;
+				cout << f << " " << m << endl;
+				cin >> s;
+				if (s == "Yes") {
+					f = max(1LL, f-k);
+					t = min(n+1, m+k+1);
+				} else {
+					f = max(1LL, m-k+1);
+					t = min(n+1, t+k);
+				}
+			}
+			cnt++;
+			int64_t pos = f+(rnd()%(t-f));
+			cout << pos << " " << pos << endl;
+			cin >> s;
+			ans = (s == "Yes");
+			f = max(1LL, f-k-1);
+			t = min(n+1, t+k+1);
+		}
 	}
 	return 0;
 }
