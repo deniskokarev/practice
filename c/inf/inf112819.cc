abbad723c3d1bc5afe28075e6eb95aeb033bb4a1
Denis Kokarev <d_kokarev@mail.ru>
2018-09-23 12:58:11 -0400

    ROI 2015A wrong answer 2

--- c/inf/inf112819.cc
+++ c/inf/inf112819.cc
@@ -10,27 +10,22 @@ constexpr int MXP2 = 62;
 struct R {
 	int64_t rem;  // total remainder
 	int64_t r[3]; // individual portion
-	bool operator==(const R &b) const {
-		return rem == b.rem;
-	}
-	bool operator<(const R &b) const {
-		return rem < b.rem;
-	}
+	int64_t msk;  // bitmask of r[0] | r[1] | r[2]
 };
 
 // build all allowed remainders and get their bitmasks
 static vector<R> build_all_rem() {
 	vector<R> rem;
-	rem.push_back(R{0, {0, 0, 0}});
+	rem.push_back(R{0, {0, 0, 0}, 0});
 	for (int i=0; i<MXP2; i++) {
 		int64_t vi = 1LL<<i;
-		rem.push_back(R{vi, {vi, 0, 0}});
+		rem.push_back(R{vi, {vi, 0, 0}, vi});
 	}
 	for (int i=0; i<MXP2; i++) {
 		for (int j=0; j<MXP2; j++) {
 			int64_t vi = 1LL<<i;
 			int64_t vj = 1LL<<j;
-			rem.push_back(R{vi+vj, {vi, vj, 0}});
+			rem.push_back(R{vi+vj, {vi, vj, 0}, vi|vj});
 		}
 	}
 	for (int i=0; i<MXP2; i++) {
@@ -39,13 +34,10 @@ static vector<R> build_all_rem() {
 				int64_t vi = 1LL<<i;
 				int64_t vj = 1LL<<j;
 				int64_t vk = 1LL<<k;
-				rem.push_back(R{vi+vj+vk, {vi, vj, vk}});
+				rem.push_back(R{vi+vj+vk, {vi, vj, vk}, vi|vj|vk});
 			}
 		}
 	}
-	sort(rem.begin(), rem.end());
-	auto sz = unique(rem.begin(), rem.end()) - rem.begin();
-	rem.resize(sz);
 	return rem;
 }
 
@@ -60,29 +52,29 @@ int main(int argc, char **argv) {
 	while (t--) {
 		int64_t n;
 		cin >> n;
-		bool fnd = false;
-		for (auto r:rem) {
-			if (n < r.rem) {
-				break;
-			} else if (n == r.rem) {
-				cout << r.r[0] << ' ' << r.r[1] << ' ' << r.r[2] << '\n';
-				fnd = true;
-				break;
+		int fnd = -1;
+		int mxq = 0;
+		for (int i=0; i<rem.size(); i++) {
+			auto &r = rem[i];
+			if (n == r.rem && mxq == 0) {
+				fnd = i;
 			} else {
 				if ((n-r.rem) % 3 != 0)
 					continue;
 				int64_t q = (n-r.rem)/3;
 				if (!iseven(q))
 					continue;
-				if ((q & r.rem) != 0)
+				if ((q & r.msk))
 					continue;
-				//cerr << q << '\n';
-				cout << q + r.r[0] << ' ' << q+r.r[1] << ' ' << q+r.r[2] << '\n';
-				fnd = true;
-				break;
+				if (q > mxq) {
+					fnd = i;
+					mxq = q;
+				}
 			}
 		}
-		assert(fnd && "must find the answer");
+		assert((fnd>=0) && "must find the answer");
+		//cerr << mxq << '\n';
+		cout << mxq + rem[fnd].r[0] << ' ' << mxq + rem[fnd].r[1] << ' ' << mxq + rem[fnd].r[2] << '\n';
 	}
 	return 0;
 }
