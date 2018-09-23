4e87a8dbcc5788b15aab7b6a0dc45054fdcabef4
Denis Kokarev <d_kokarev@mail.ru>
2018-09-23 14:22:29 -0400

    ROI 2015E solution

--- c/inf/inf112819.cc
+++ c/inf/inf112819.cc
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <cassert>
 /* ROI 2015E */
-using namespace std;
-
-constexpr int MXP2 = 62;
 
 struct R {
 	int64_t rem;  // total remainder
@@ -13,28 +9,30 @@ struct R {
 	int64_t msk;  // bitmask of r[0] | r[1] | r[2]
 };
 
-// build all allowed remainders and get their bitmasks
-static vector<R> build_all_rem() {
-	vector<R> rem;
+constexpr int MXP2 = 62; // upto 2^MXP2
+
+// build all allowed remainders and get their bitmasks for upto 2^mxp2 each
+static const std::vector<R> build_allrem(int mxp2) {
+	std::vector<R> rem;
 	rem.push_back(R{0, {0, 0, 0}, 0});
-	for (int i=0; i<MXP2; i++) {
+	for (int i=0; i<mxp2; i++) {
 		int64_t vi = 1LL<<i;
-		rem.push_back(R{vi, {0, 0, vi}, vi});
+		rem.push_back(R {vi, {0, 0, vi}, vi});
 	}
-	for (int i=0; i<MXP2; i++) {
-		for (int j=i; j<MXP2; j++) {
+	for (int i=0; i<mxp2; i++) {
+		for (int j=i; j<mxp2; j++) {
 			int64_t vi = 1LL<<i;
 			int64_t vj = 1LL<<j;
-			rem.push_back(R{vi+vj, {0, vi, vj}, vi|vj});
+			rem.push_back(R {vi+vj, {0, vi, vj}, vi|vj});
 		}
 	}
-	for (int i=0; i<MXP2; i++) {
-		for (int j=i; j<MXP2; j++) {
-			for (int k=j; k<MXP2; k++) {
+	for (int i=0; i<mxp2; i++) {
+		for (int j=i; j<mxp2; j++) {
+			for (int k=j; k<mxp2; k++) {
 				int64_t vi = 1LL<<i;
 				int64_t vj = 1LL<<j;
 				int64_t vk = 1LL<<k;
-				rem.push_back(R{vi+vj+vk, {vi, vj, vk}, vi|vj|vk});
+				rem.push_back(R {vi+vj+vk, {vi, vj, vk}, vi|vj|vk});
 			}
 		}
 	}
@@ -42,29 +40,31 @@ static vector<R> build_all_rem() {
 }
 
 int main(int argc, char **argv) {
-	const vector<R> rem = build_all_rem();
+	auto allrem = build_allrem(MXP2);
 	int t;
-	cin >> t;
+	std::cin >> t;
 	while (t--) {
 		int64_t n;
-		cin >> n;
-		int fnd = -1;
-		int mxq = 0;
-		for (int i=0; i<rem.size(); i++) {
-			auto &r = rem[i];
-			if (n == r.rem && mxq == 0) {
-				fnd = i;
+		std::cin >> n;
+		// find best quotient and best remainder corresponding to it
+		R *best_rem = nullptr;
+		int64_t best_q = 0;
+		for (auto &r:allrem) {
+			if (n == r.rem && best_q == 0) {
+				best_rem = &r;
 			} else if (n > r.rem && (n-r.rem) % 3 == 0) {
 				int64_t q = (n-r.rem)/3;
-				if ((q&r.msk) == 0 && q > mxq) {
-					fnd = i;
-					mxq = q;
+				if ((q & r.msk) == 0 && q > best_q) {
+					best_rem = &r;
+					best_q = q;
 				}
 			}
 		}
-		assert((fnd>=0) && "must find the answer");
-		//cerr << mxq << '\n';
-		cout << mxq + rem[fnd].r[0] << ' ' << mxq + rem[fnd].r[1] << ' ' << mxq + rem[fnd].r[2] << '\n';
+		assert((best_rem != nullptr) && "must find the answer");
+		std::cerr << best_q << '\n';
+		std::cout << best_q + best_rem->r[0] << ' '
+				  << best_q + best_rem->r[1] << ' '
+				  << best_q + best_rem->r[2] << '\n';
 	}
 	return 0;
 }
