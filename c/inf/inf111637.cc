eed86944d01c337a91f36559eef4ce51175c0c97
Denis Kokarev <d_kokarev@mail.ru>
2018-09-26 21:30:50 -0400

    ROI 2013E hmm, still wrong answer 19

--- c/inf/inf111637.cc
+++ c/inf/inf111637.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <climits>
 /* ROI 2013E */
 using namespace std;
 
@@ -25,15 +26,32 @@ int main(int argc, char **argv) {
 			pp[i] = P {h, f2p[ff[i]]};
 			f2p[ff[i]] = i;
 			for (int j=i+1; j<up; j++) {
-				pp[j] = P {h, -1};
+				pp[j] = P {h, INT_MAX};
 				f2p[ff[j]] = i;
 			}
 			up = i;
 		}
 	}
-	cout << pp[0].h << endl;
-	for (int i=0,pi=0; i<pp[0].h; i++,pi=pp[pi].nxt)
-		cout << pi+1 << ' ';
-	cout << endl;
+	if (pp[0].h > 0) {
+		vector<int> ans;
+		ans.push_back(1);
+		int pn = pp[0].nxt;
+		int mni = 0;
+		int i = 0;
+		while (pn != n-1) {
+			for (; i<=pn; i++) {
+				if (pp[i].h < pp[mni].h)
+					mni = i;
+			}
+			ans.push_back(mni+1);
+			pn = pp[mni].nxt;
+		}
+		cout << ans.size() << endl;
+		for (auto a:ans)
+			cout << a << ' ';
+		cout << endl;
+	} else {
+		cout << 0 << endl;
+	}
 	return 0;
 }
