6686e56695e2e65ed57469306b73c84709570825
Denis Kokarev <d_kokarev@mail.ru>
2018-10-14 15:40:36 -0400

    ROI 2011F solution

--- c/inf/inf3398.cc
+++ c/inf/inf3398.cc
@@ -1,25 +1,21 @@
 #include <cstdio>
 #include <vector>
+#include <cmath>
 /* ROI 2011F */
 using namespace std;
 
-double ddist(const vector<pair<int,int>> &xx, double t0, double t1, double *l) {
-	double l0_mx = 0;
-	double l1_mx = 0;
-	double l0_mn = 1e60;
-	double l1_mn = 1e60;
+constexpr double INF = 1e14;
+constexpr double EPS = 1e-14;
+
+double dist(const vector<pair<int,int>> &xx, double t) {
+	double l_mx = 0;
+	double l_mn = INF;
 	for (auto &x:xx) {
-		double d0 = x.first + x.second*t0;
-		l0_mn = min(l0_mn, d0);
-		l0_mx = max(l0_mx, d0);
-		double d1 = x.first + x.second*t1;
-		l1_mn = min(l1_mn, d1);
-		l1_mx = max(l1_mx, d1);
+		double d = x.first + x.second*t;
+		l_mn = min(l_mn, d);
+		l_mx = max(l_mx, d);
 	}
-	double l0 = l0_mx-l0_mn;
-	double l1 = l1_mx-l1_mn;
-	*l = l0;
-	return l1-l0;
+	return l_mx - l_mn;
 }
 
 int main(int argc, char **argv) {
@@ -28,17 +24,29 @@ int main(int argc, char **argv) {
 	vector<pair<int,int>> xx(n);
 	for (auto &x:xx)
 		scanf("%d%d", &x.first, &x.second);
-	// lower bound search
-	double f = 0, t = 1e60;
-	double l;
-	while (f<t) {
-		double m = f+(t-f)/2;
-		double eps = m*1e-14;
-		if (ddist(xx, m, m+eps, &l) < 0)
-			f = m+eps;
-		else
-			t = m;
+	// golden section search
+	const double phi = (1.0 + sqrt(5.0)) / 2.0;
+	const double resphi = 2 - phi;
+	double f = 0, t = INF;
+	double a = f + resphi * (t - f);
+	double b = t - resphi * (t - f);
+	double la = dist(xx, a);
+	double lb = dist(xx, b);
+	while (f*(1+EPS)<t) {
+		if (la < lb) {
+			t = b;
+			b = a;
+			lb = la;
+			a = f + resphi * (t - f);
+			la = dist(xx, a);
+		} else {
+			f = a;
+			a = b;
+			la = lb;
+			b = t - resphi * (t - f);
+			lb = dist(xx, b);
+		}
 	}
-	printf("%f %f\n", f, l);
+	printf("%f %f\n", f, la);
 	return 0;
 }
