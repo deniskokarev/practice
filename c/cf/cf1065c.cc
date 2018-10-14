15280eff1bb39b8f3c3806bf507bbba34a56f53f
Denis Kokarev <d_kokarev@mail.ru>
2018-10-14 13:26:09 -0400

    cf1065c solution

--- c/cf/cf1065c.cc
+++ c/cf/cf1065c.cc
@@ -1,32 +1,34 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <algorithm>
 /* CodeForces CF1065C problem */
 using namespace std;
 
 int main(int argc, char **argv) {
-	int64_t n, k;
-	cin >> n >> k;
-	vector<int64_t> ac(3e5+1);
+	int n, k;
+	scanf("%d%d", &n, &k);
+	vector<int> ac(2e5+1);
 	int mnh = INT_MAX;
 	for (int i=0; i<n; i++) {
 		int h;
-		cin >> h;
+		scanf("%d", &h);
 		ac[h]++;
 		mnh = min(mnh, h);
 	}
 	for (int i=int(ac.size())-2; i>=0; i--)
 		ac[i] += ac[i+1];
 	int ans = 0;
-	int h = int(ac.size())-1;
-	while (h > mnh) {
-		int64_t shave = 0;
-		while (shave + ac[h] <= k && h > mnh) {
-			shave += ac[h];
-			h--;
+	if (ac[mnh+1] > 0) {
+		int h = int(ac.size())-1;
+		while (h > mnh) {
+			int64_t shave = 0;
+			while (shave + ac[h] <= k && h > mnh) {
+				shave += ac[h];
+				h--;
+			}
+			ans++;
 		}
-		ans++;
 	}
-	cout << ans << endl;
+	printf("%d\n", ans);
 	return 0;
 }
