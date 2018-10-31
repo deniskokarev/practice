4aff4da2cbabd0f1fc68eda26cddef814c2ac901
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-10-31 16:07:04 -0400

    simpler algorithms-on-strings-2.4 solution

--- c/cr/algorithms-on-strings-2.4.cc
+++ c/cr/algorithms-on-strings-2.4.cc
@@ -1,37 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
+// building suffix array in n*log(n)*log(n)
 
-constexpr int base = 13;
+constexpr int base = 13; // polynomial base (3 doesn't pass tests)
 
 struct SSTR {
-	int b, e; // [b, e)
+	int ofs, sz;
 };
 
 // hash-assisted comparison by looking at common prefix
 struct SSTR_CMP {
-	const vector<uint64_t> &ss_hpow;
-	const vector<uint64_t> &ss_hash;
-	const string s;
+	const uint64_t *ss_hpow;
+	const uint64_t *ss_hash;
+	const string &s;
 	const int sz;
 	bool operator()(const SSTR &a, const SSTR &b) {
-		int mnl = min(a.e-a.b, b.e-b.b);
+		int mnl = min(a.sz, b.sz);
 		// upper-bound search
 		int f=1, t=mnl+1;
 		while (f<t) {
 			int m = f+(t-f)/2;
-			uint64_t ha = (ss_hash[a.b+m]-ss_hash[a.b])*ss_hpow[sz-a.b-m];
-			uint64_t hb = (ss_hash[b.b+m]-ss_hash[b.b])*ss_hpow[sz-b.b-m];
+			uint64_t ha = (ss_hash[a.ofs+m]-ss_hash[a.ofs])*ss_hpow[sz-a.ofs-m];
+			uint64_t hb = (ss_hash[b.ofs+m]-ss_hash[b.ofs])*ss_hpow[sz-b.ofs-m];
 			if (ha == hb)
 				f = m+1;
 			else
 				t = m;
 		}
 		if (f<=mnl)
-			return s[a.b+f-1] < s[b.b+f-1];
+			return s[a.ofs+f-1] < s[b.ofs+f-1];
 		else
-			return (a.e-a.b) < (b.e-b.b);
+			return (a.sz) < (b.sz);
 	}
 };
 
@@ -39,20 +39,20 @@ int main(int argc, char **argv) {
 	string s;
 	cin >> s;
 	int sz = s.size();
-	vector<SSTR> ss(sz, SSTR {0, 0});
-	vector<uint64_t> ss_hpow(sz+1);
+	SSTR ss[sz];
+	uint64_t ss_hpow[sz+1];
 	ss_hpow[0] = 1;
-	vector<uint64_t> ss_hash(sz+1);
+	uint64_t ss_hash[sz+1];
 	ss_hash[0] = 0;
 	for (int i=0; i<sz; i++) {
-		ss[i].b = i;
-		ss[i].e = sz;
+		ss[i].ofs = i;
+		ss[i].sz = sz-i;
 		ss_hpow[i+1] = ss_hpow[i]*base;
 		ss_hash[i+1] = ss_hash[i]+ss_hpow[i+1]*s[i];
 	}
-	sort(ss.begin(), ss.end(), SSTR_CMP {ss_hpow, ss_hash, s, sz});
+	sort(ss, ss+sz, SSTR_CMP {ss_hpow, ss_hash, s, sz});
 	for (int i=0; i<sz; i++)
-		cout << ss[i].b << ' ';
+		cout << ss[i].ofs << ' ';
 	cout << endl;
 	return 0;
 }
