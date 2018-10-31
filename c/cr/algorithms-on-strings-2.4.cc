cdf39a1ecdd98d26f4ce6295243407d2c09f6edc
Kokarev, Denis (dk1942) <dk1942@att.com>
2018-10-31 15:16:45 -0400

    algorithms-on-strings-2.4 n*log(n)*log(n) solution using hashes

--- c/cr/algorithms-on-strings-2.4.cc
+++ c/cr/algorithms-on-strings-2.4.cc
@@ -3,20 +3,35 @@
 #include <algorithm>
 using namespace std;
 
+constexpr int base = 13;
+
 struct SSTR {
-	const string *s;
-	int pos, sz;
-	bool operator<(const SSTR &b) const {
-		int cmp = 0;
-		int i = 0;
-		while (cmp == 0 && i < sz && i < b.sz) {
-			cmp = int((*s)[pos+i]) - int((*b.s)[b.pos+i]);
-			i++;
+	int b, e; // [b, e)
+};
+
+// hash-assisted comparison by looking at common prefix
+struct SSTR_CMP {
+	const vector<uint64_t> &ss_hpow;
+	const vector<uint64_t> &ss_hash;
+	const string s;
+	const int sz;
+	bool operator()(const SSTR &a, const SSTR &b) {
+		int mnl = min(a.e-a.b, b.e-b.b);
+		// upper-bound search
+		int f=1, t=mnl+1;
+		while (f<t) {
+			int m = f+(t-f)/2;
+			uint64_t ha = (ss_hash[a.b+m]-ss_hash[a.b])*ss_hpow[sz-a.b-m];
+			uint64_t hb = (ss_hash[b.b+m]-ss_hash[b.b])*ss_hpow[sz-b.b-m];
+			if (ha == hb)
+				f = m+1;
+			else
+				t = m;
 		}
-		if (cmp == 0)
-			return (sz<b.sz);
+		if (f<=mnl)
+			return s[a.b+f-1] < s[b.b+f-1];
 		else
-			return (cmp<0);
+			return (a.e-a.b) < (b.e-b.b);
 	}
 };
 
@@ -24,14 +39,20 @@ int main(int argc, char **argv) {
 	string s;
 	cin >> s;
 	int sz = s.size();
-	vector<SSTR> ss(sz, SSTR {&s, 0, 0});
+	vector<SSTR> ss(sz, SSTR {0, 0});
+	vector<uint64_t> ss_hpow(sz+1);
+	ss_hpow[0] = 1;
+	vector<uint64_t> ss_hash(sz+1);
+	ss_hash[0] = 0;
 	for (int i=0; i<sz; i++) {
-		ss[i].pos = i;
-		ss[i].sz = sz-i;
+		ss[i].b = i;
+		ss[i].e = sz;
+		ss_hpow[i+1] = ss_hpow[i]*base;
+		ss_hash[i+1] = ss_hash[i]+ss_hpow[i+1]*s[i];
 	}
-	sort(ss.begin(), ss.end());
+	sort(ss.begin(), ss.end(), SSTR_CMP {ss_hpow, ss_hash, s, sz});
 	for (int i=0; i<sz; i++)
-		cout << ss[i].pos << ' ';
+		cout << ss[i].b << ' ';
 	cout << endl;
 	return 0;
 }
