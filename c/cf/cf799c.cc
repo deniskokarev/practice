986771ecbe40912de31ea016debf80ff1e512d25
Denis Kokarev <d_kokarev@mail.ru>
2018-11-17 12:44:14 -0500

    cf799c solution

--- c/cf/cf799c.cc
+++ c/cf/cf799c.cc
@@ -1,93 +1,76 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
 #include <algorithm>
-/* CodeForces http://codeforces.com/problemset/problem/799/C */
+/* CodeForces CF799C problem */
 using namespace std;
 
-#define dim(X)	(sizeof(X)/sizeof(X[0]))
-
 struct F {
-	int b, p;
+	int b;
+	int p;
+	int mx;
+	bool operator<(const F &o) const {
+		return p<o.p || (p==o.p && b<o.b);
+	}
 };
 
-int sdiff(const F ffc[], int nc, const F ffd[], int nd, int c, int d) {
-	int bc = 0;
-	for (int i=0; i<nc; i++) {
-		auto &f = ffc[i];
-		if (c >= f.p) {
-			bc = f.b;
-			break;
-		}
+int get_mxb(const F *a, int n, int r) {
+	int f=0, t=n;
+	while (f<t) {
+		int m = f+(t-f)/2;
+		if (a[m].p <= r)
+			f = m+1;
+		else
+			t = m;
 	}
-	int bd = 0;
-	for (int i=0; i<nd; i++) {
-		auto &f = ffd[i];
-		if (d >= f.p) {
-			bd = f.b;
-			break;
-		}
-	}
-	if (bc && bd)
-		return bc+bd;
+	if (f>0)
+		return a[f-1].mx;
 	else
-		return 0;
-}
-
-
-int ssame(const F ff[], int n, int p) {
-	int bbmx2[100001][2] = {};
-	int bb[100001];
-	int pi = 0;
-	int bmx = 0;
-	for (int i=0; i<n; i++) {
-		while (pi<ff[i].p)
-			bb[pi++] = bmx;
-		bmx = max(bmx, ff[i].b);
-		if (ff[i].b > bbmx2[ff[i].p][0]) {
-			bbmx2[ff[i].p][1] = bbmx2[ff[i].p][0];
-			bbmx2[ff[i].p][0] = ff[i].b;
-		} else if (ff[i].b > bbmx2[ff[i].p][1]) {
-			bbmx2[ff[i].p][1] = ff[i].b;
-		}
-	}
-	while(pi < 100001)
-		bb[pi++] = bmx;
-	bmx = 0;
-	int i;
-	for (i=n-1; i >= 0 && ff[i].p > p/2; i--)
-		if (ff[i].p > p)
-			continue;
-		else if (bb[p-ff[i].p] > 0)
-			bmx = max(bmx, ff[i].b+bb[p-ff[i].p]);
-	for (; i >= 0; i--) {
-		if (bbmx2[ff[i].p][1]>0)
-			bmx = max(bmx, bbmx2[ff[i].p][0]+bbmx2[ff[i].p][1]);
-		if (bb[ff[i].p-1] > 0)
-			bmx = max(bmx, ff[i].b+bb[ff[i].p-1]);
-	}
-	return bmx;
+		return -1;
 }
 
 int main(int argc, char **argv) {
 	int n, c, d;
-	cin >> n >> c >> d;
-	F ffc[n];
-	int nc = 0;
-	F ffd[n];
-	int nd = 0;
-	for (int i=0; i<n; i++) {
+	scanf("%d%d%d", &n, &c, &d);
+	F fc[n], fd[n];
+	int nc = 0, nd = 0;
+	while (n--) {
 		int b, p;
-		string pc;
-		cin >> b >> p >> pc;
-		if (pc[0] == 'C')
-			ffc[nc++] = {b, p};
+		char t[2];
+		scanf("%d%d%s", &b, &p, t);
+		if (t[0] == 'C')
+			fc[nc++] = F {b, p, b};
 		else
-			ffd[nd++] = {b, p};
+			fd[nd++] = F {b, p, b};
 	}
-	sort(ffc, ffc+nc, [](const F &a, const F &b){return a.p<b.p;});
-	sort(ffd, ffd+nd, [](const F &a, const F &b){return a.p<b.p;});
-	int mxb = sdiff(ffc, nc, ffd, nd, c, d);
-	int mxc = ssame(ffc, nc, c);
-	int mxd = ssame(ffd, nd, d);
-	cout << max(mxb, max(mxc, mxd)) << endl;
+	sort(fc, fc+nc);
+	sort(fd, fd+nd);
+	for (int i=1; i<nc; i++)
+		fc[i].mx = max(fc[i].mx, fc[i-1].mx);
+	for (int i=1; i<nd; i++)
+		fd[i].mx = max(fd[i].mx, fd[i-1].mx);
+	int mxc = -1, mxc2 = -1;
+	int mxd = -1, mxd2 = -1;
+	for (int i=0; i<nc && fc[i].p<=c; i++)
+		mxc = fc[i].mx;
+	for (int i=0; i<nd && fd[i].p<=d; i++)
+		mxd = fd[i].mx;
+	for (int i=nc-1; i>0; i--) {
+		int r = c - fc[i].p;
+		int b = get_mxb(fc, i, r);
+		if (b >= 0)
+			mxc2 = max(mxc2, fc[i].b+b);
+	}
+	for (int i=nd-1; i>0; i--) {
+		int r = d - fd[i].p;
+		int b = get_mxb(fd, i, r);
+		if (b >= 0)
+			mxd2 = max(mxd2, fd[i].b+b);
+	}
+	//fprintf(stderr, "mxc=%d mxc2=%d mxd=%d mxd2=%d\n", mxc, mxc2, mxd, mxd2);
+	int mx = 0;
+	if (mxc >= 0 && mxd >= 0)
+		mx = max(mx, mxc+mxd);
+	mx = max(mx, max(mxc2, mxd2));
+	printf("%d\n", mx);
 	return 0;
 }
