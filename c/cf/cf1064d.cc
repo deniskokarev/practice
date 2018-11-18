4abbc61f28ff0161a8ee02ef4abe654f0daf987b
Denis Kokarev <d_kokarev@mail.ru>
2018-11-18 17:14:25 -0500

    cf1064d solution

--- c/cf/cf1064d.cc
+++ c/cf/cf1064d.cc
@@ -14,6 +14,8 @@ struct Q {
 	int x, y;
 };
 
+constexpr int INF = INT_MAX/2;
+
 int main(int argc, char **argv) {
 	int n, m;
 	scanf("%d%d", &n, &m);
@@ -21,78 +23,69 @@ int main(int argc, char **argv) {
 	scanf("%d%d", &s.r, &s.c);
 	int x, y;
 	scanf("%d%d", &x, &y);
-	char bbx[n+2][m+2];
-	memset(bbx, '*', sizeof(bbx));
+	char bb[n+2][m+2];
+	memset(bb, '*', sizeof(bb));
 	for (int i=1; i<=n; i++) {
-		scanf("%s", bbx[i]+1);
-		bbx[i][m+1] = '*';
+		scanf("%s", bb[i]+1);
+		bb[i][m+1] = '*';
 	}
-	char bby[n+2][m+2];
-	memcpy(bby, bbx, sizeof(bbx));
-	Q bb[n+2][m+2];
-	memset(bb, 0, sizeof(bb));
-	queue<P> qq;
-	qq.push(s);
-	bbx[s.r][s.c] = '+';
+	int xx[n+2][m+2];
+	int yy[n+2][m+2];
+	for (int i=0; i<n+2; i++)
+		for (int j=0; j<m+2; j++)
+			if (bb[i][j] == '.')
+				xx[i][j] = yy[i][j] = INF;
+			else
+				xx[i][j] = yy[i][j] = -INF;
+	deque<Q> qq;
+	qq.push_back(Q{s, 0, 0});
+	xx[s.r][s.c] = 0;
 	while (!qq.empty()) {
-		P p = qq.front();
-		qq.pop();
-		if (bbx[p.r+1][p.c] == '.') {
-			bbx[p.r+1][p.c] = '+';
-			bb[p.r+1][p.c] = bb[p.r][p.c];
-			qq.push(P {p.r+1, p.c});
+		Q t = qq.front();
+		qq.pop_front();
+		if (t.x < xx[t.p.r+1][t.p.c]) {
+			xx[t.p.r+1][t.p.c] = t.x;
+			qq.push_front(Q {P {t.p.r+1, t.p.c}, t.x, t.y});
 		}
-		if (bbx[p.r-1][p.c] == '.') {
-			bbx[p.r-1][p.c] = '+';
-			bb[p.r-1][p.c] = bb[p.r][p.c];
-			qq.push(P {p.r-1, p.c});
+		if (t.x < xx[t.p.r-1][t.p.c]) {
+			xx[t.p.r-1][t.p.c] = t.x;
+			qq.push_front(Q {P {t.p.r-1, t.p.c}, t.x, t.y});
 		}
-		if (bb[p.r][p.c].x < x && bbx[p.r][p.c-1] == '.') {
-			bbx[p.r][p.c-1] = '+';
-			bb[p.r][p.c-1] = bb[p.r][p.c];
-			bb[p.r][p.c-1].x++;
-			qq.push(P {p.r, p.c-1});
+		if (t.x < xx[t.p.r][t.p.c+1]) {
+			xx[t.p.r][t.p.c+1] = t.x;
+			qq.push_front(Q {P {t.p.r, t.p.c+1}, t.x, t.y});
 		}
-		if (bbx[p.r][p.c+1] == '.') {
-			bbx[p.r][p.c+1] = '+';
-			bb[p.r][p.c+1] = bb[p.r][p.c];
-			bb[p.r][p.c+1].y++;
-			qq.push(P {p.r, p.c+1});
+		if (t.x < x && t.x+1 < xx[t.p.r][t.p.c-1]) {
+			xx[t.p.r][t.p.c-1] = t.x+1;
+			qq.push_back(Q {P {t.p.r, t.p.c-1}, t.x+1, t.y});
 		}
 	}
-	memset(bb, 0, sizeof(bb));
-	qq.push(s);
-	bby[s.r][s.c] = '+';
+	qq.push_back(Q{s, 0, 0});
+	yy[s.r][s.c] = 0;
 	while (!qq.empty()) {
-		P p = qq.front();
-		qq.pop();
-		if (bby[p.r+1][p.c] == '.') {
-			bby[p.r+1][p.c] = '+';
-			bb[p.r+1][p.c] = bb[p.r][p.c];
-			qq.push(P {p.r+1, p.c});
+		Q t = qq.front();
+		qq.pop_front();
+		if (t.y < yy[t.p.r+1][t.p.c]) {
+			yy[t.p.r+1][t.p.c] = t.y;
+			qq.push_front(Q {P {t.p.r+1, t.p.c}, t.x, t.y});
 		}
-		if (bby[p.r-1][p.c] == '.') {
-			bby[p.r-1][p.c] = '+';
-			bb[p.r-1][p.c] = bb[p.r][p.c];
-			qq.push(P {p.r-1, p.c});
+		if (t.y < yy[t.p.r-1][t.p.c]) {
+			yy[t.p.r-1][t.p.c] = t.y;
+			qq.push_front(Q {P {t.p.r-1, t.p.c}, t.x, t.y});
 		}
-		if (bby[p.r][p.c-1] == '.') {
-			bby[p.r][p.c-1] = '+';
-			bb[p.r][p.c-1] = bb[p.r][p.c];
-			bb[p.r][p.c-1].x++;
-			qq.push(P {p.r, p.c-1});
+		if (t.y < y && t.y+1 < yy[t.p.r][t.p.c+1]) {
+			yy[t.p.r][t.p.c+1] = t.y+1;
+			qq.push_back(Q {P {t.p.r, t.p.c+1}, t.x, t.y+1});
 		}
-		if (bb[p.r][p.c].y < y && bby[p.r][p.c+1] == '.') {
-			bby[p.r][p.c+1] = '+';
-			bb[p.r][p.c+1] = bb[p.r][p.c];
-			bb[p.r][p.c+1].y++;
-			qq.push(P {p.r, p.c+1});
+		if (t.y < yy[t.p.r][t.p.c-1]) {
+			yy[t.p.r][t.p.c-1] = t.y;
+			qq.push_front(Q {P {t.p.r, t.p.c-1}, t.x, t.y});
 		}
 	}
 	int ans = 0;
 	for (int i=1; i<=n; i++)
 		for (int j=1; j<=m; j++)
-			ans += (bbx[i][j] == '+' && bby[i][j] == '+');
+			ans += (xx[i][j] > -INF && xx[i][j] < INF && yy[i][j] > -INF && yy[i][j] < INF);
 	printf("%d\n", ans);
 	return 0;
 }
