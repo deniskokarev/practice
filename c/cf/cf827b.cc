399874dac50fe675c62d69ce05253e1349f10db3
Denis Kokarev <d_kokarev@mail.ru>
2018-11-25 13:26:23 -0500

    cf827b wa 8

---
+++ c/cf/cf827b.cc
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+/* CodeForces CF827B problem */
+using namespace std;
+
+int main(int argc, char **argv) {
+	int n, k;
+	scanf("%d%d", &n, &k);
+	int r = n-k-1;
+	int hi = (r+k-1)/k+1;
+	int lo = r/k+1;
+	printf("%d\n", hi+lo);
+	int l;
+	for (l=0; l<lo; l++)
+		for (int i=1; i<=k; i++)
+			printf("%d %d\n", l*k+i, min((l+1)*k+i, n));
+	for (int i=1; i<=k && l*k+i < n; i++)
+		printf("%d %d\n", l*k+i, n);
+	return 0;
+}
