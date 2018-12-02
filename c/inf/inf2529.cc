c2dc86c0715bb96cb50660582582bbffa2721818
Denis Kokarev <d_kokarev@mail.ru>
2018-12-01 21:21:42 -0500

    ROI 2010A still WA 4

--- c/inf/inf2529.cc
+++ c/inf/inf2529.cc
@@ -1,12 +1,24 @@
 #include <iostream>
-#include <cmath>
+#include <limits.h>
 /* ROI 2010 A problem */
 using namespace std;
+
+int64_t isqrt(int64_t s) {
+	int64_t f=1, t=INT_MAX;
+	while (f<t) {
+		int64_t m = f+(t-f)/2;
+		if (m*m <= s)
+			f = m+1;
+		else
+			t = m;
+	}
+	return f-1;
+}
 
 int main(int argc, char **argv) {
 	uint64_t n;
 	cin >> n;
-	int64_t a = int64_t(sqrt(n/6));
+	int64_t a = isqrt(n/6);
 	// try +1
 	int64_t s1 = a*a*2+a*(a+1)*4;
 	// try +2
