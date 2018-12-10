98f9cb49ec23c313430989a5050cae05bb4f997f
Denis Kokarev <d_kokarev@mail.ru>
2018-12-10 18:45:52 -0500

    adding TopCoder solutions placeholder

---
+++ c/tc.cc
@@ -0,0 +1,16 @@
+/* TopCoder https://arena.topcoder.com/... */
+#include "gtest/gtest.h"
+
+using namespace std;
+
+class MyClass {
+public:
+	static string myMethod() {
+		return "real answer";
+	}
+};
+
+TEST(TC, TC0) {
+	string expected = "real answer";
+	EXPECT_EQ(expected, MyClass::myMethod());
+}
