e183a0b7185bd75cfc30b8b43526c48a9b9a99ca
Denis Kokarev <d_kokarev@mail.ru>
2019-01-05 17:54:40 -0500

    updating segment tree implementation and adding one more segtree

--- c/alg/segtree.hh
+++ c/alg/segtree.hh
@@ -1,29 +1,32 @@
 #ifndef __SEGTREE_HH__
 #define __SEGTREE_HH__
-/**
- * Segment tree on a vector with custom "fold" operation
- * Supports setting a value at position and "folding" values
- * in a given range. Both in O(logN) time
- * Takes O(n) space (about 2*n)
- * @author Denis Kokarev
- */
+
 #include <functional>
 #include <vector>
 #include <cmath>
 
-template<class T=int> class SegmentTree {
-	using value_type = T;
-	using OP = std::function<value_type(value_type,value_type)>;
+/**
+ * Simple Segment tree on a vector with custom "fold" operation.
+ * Supports setting a value at a position and "folding" values on a range
+ * Typically, folding would be operator+() or max()
+ * O(2*n) space
+ * set() - O(n) time
+ * fold() - O(n) time
+ * @author Denis Kokarev
+ */
+template<class ValueType=int> class SegTree {
+	using value_type = ValueType;
+	using FOLD_OP = std::function<value_type(const value_type &, const value_type &)>;
 	int sz;
-	int pad;
-	std::vector<value_type> vv; // where we keep all values, first pad elements are aggreagates
+	std::vector<value_type> tree; // where we keep all values, first sz elements are aggreagates
+	FOLD_OP fold; // "fold" operator to be executed on our interval
 	/**
-	 * size of padding necessary to keep array of size pad+sz in a perfect binary tree
+	 * If you touched values directly, you must perform rebuild()
 	 */
-	static int getPadSize(int sz) {
-		return (1<<int(ceil(log2(sz))))-1;
+	void rebuild() {
+		for (int i=sz+sz-1; i>1; i-=2)
+			tree[i>>1] = fold(tree[i-1], tree[i]);
 	}
-	OP fold; // "fold" operator to be executed on our interval
 public:
 	/**
 	 * Create segment tree with given size and custom fold operator
@@ -31,55 +34,165 @@ public:
 	 * @param _sz - maximum size
 	 * @param _fold  - custom fold operator - default std::plus
 	 */
-	SegmentTree(int _sz, OP _fold=std::plus<value_type>()):sz(_sz),pad(getPadSize(sz)),vv(pad+sz),fold(_fold) {
+	SegTree(int _sz, FOLD_OP _fold=std::plus<value_type>()):sz(_sz),tree(sz+sz),fold(_fold) {
 	}
 
+	/**
+	 * Alternative constructor to initialize the values right away
+	 * Works in O(n)
+	 * @param list - list of given values
+	 * @param _fold  - custom fold operator - default std::plus
+	 */
+	SegTree(const std::initializer_list<value_type> &list, FOLD_OP _fold=std::plus<value_type>()):SegTree(list.size(), _fold) {
+		std::copy(list.begin(), list.end(), tree.begin()+sz);
+		rebuild();
+	}
+	
 	/**
 	 * Perform online update in O(logN)
 	 * @param pos
 	 * @param v
 	 */
-	void set(int pos, const value_type v) {
-		int node = pad+pos;
-		vv[node] = v;
-		int parent = (node-1)/2;
-		if (parent*2+2 < pad+sz)
-			vv[parent] = fold(vv[parent*2+1], vv[parent*2+2]);
+	void set(int pos, const value_type &v) {
+		int node = sz+pos;
+		tree[node] = v;
+		node >>= 1;
+		if (((node<<1)|1) < sz+sz)
+			tree[node] = fold(tree[node<<1], tree[(node<<1)|1]);
 		else
-			vv[parent] = vv[node];
-		node = parent;
-		for (parent=(node-1)/2; node>0; node=parent,parent=(node-1)/2)
-			vv[parent] = fold(vv[parent*2+1], vv[parent*2+2]);
+			tree[node] = tree[node<<1];
+		for (node=node>>1; node>0; node >>= 1)
+			tree[node] = fold(tree[node<<1], tree[(node<<1)|1]);
 	}
 
 	/**
 	 * Perform interval folding on open-ended [b, e) segment. Runs in O(logN)
 	 * @param b - begin - first element inclusive
 	 * @param e - end - element after last
-	 * @return fold(vv[b], fold(vv[b+1], fold(vv[b+2], ... fold(vv[e-2], fold(vv[e-1])...)))
+	 * @return fold(tree[b], fold(tree[b+1], fold(tree[b+2], ... fold(tree[e-2], fold(tree[e-1])...)))
 	 */
 	value_type operator()(int b, int e) const {
-		b += pad;
-		e += pad;
+		b += sz;
+		e += sz;
 		if (e-b > 1) {
-			value_type vb = vv[b++];
-			value_type ve = vv[--e];
+			value_type vb = tree[b++];
+			value_type ve = tree[--e];
 			while (b < e) {
-				int pb = (b+1)/2; // parent b
-				int pe = (e-1)/2; // parent e
-				if (!(b&1))
-					vb = fold(vb, vv[b]);
-				else if (pb <= pe)
-					vb = fold(vb, vv[pb-1]);
-				if (!(e&1))
-					ve = fold(vv[e-1], ve);
-				b = pb;
-				e = pe;
+				if (b&1)
+					vb = fold(vb, tree[b++]);
+				if (e&1)
+					ve = fold(tree[--e], ve);
+				b >>= 1;
+				e >>= 1;
 			}
 			return fold(vb, ve);
 		} else {
-			return vv[b];
+			return tree[b];
 		}
+	}
+	
+	/**
+	 * Perform interval folding. Runs in O(logN)
+	 * @return fold(tree[b], fold(tree[b+1], fold(tree[b+2], ... fold(tree[e-2], fold(tree[e-1])...)))
+	 */
+	value_type operator()() const {
+		return operator()(0, sz);
+	}
+};
+
+/**
+ * Segment tree to perform "lazy" increments on all values in the open interval [b, e)
+ */
+template<class ValueType=int> class AddSegTree {
+	using value_type = ValueType;
+	using INC_OP = std::function<void(value_type &, const value_type &)>;
+	int sz;
+	int l2;
+	std::vector<value_type> tree; // where we keep all values (first sz elements are aggreagates)
+	INC_OP inc; // "increment" operator to be executed during increments
+	/**
+	 * Propagate increments down to a specific element in O(logN)
+	 */
+	void propagate_inc(int pos) {
+		for (int l=l2; l>0; l--) {
+			int p = pos >> l;
+			inc(tree[p<<1], tree[p]);
+			inc(tree[(p<<1)|1], tree[p]);
+			tree[p] = value_type();
+		}
+	}
+	/**
+	 * Propagate all increments to values in O(n)
+	 */
+	void propagate_inc() {
+		for (int i=1; i < sz; i++) {
+			inc(tree[i<<1], tree[i]);
+			inc(tree[(i<<1)|1], tree[i]);
+			tree[i] = value_type();
+		}
+	}
+	static void default_inc(value_type &a, const value_type &b) {
+		a += b;
+	};
+public:
+	/**
+	 * Create segment tree with given size and custom inc operator
+	 * Upon creation all values will be zeros
+	 * @param _sz - maximum size
+	 * @param _add  - custom add operator - default std::plus
+	 */
+	AddSegTree(int _sz, INC_OP _inc=default_inc):sz(_sz),l2(floor(log2(sz))),tree(sz+sz),inc(_inc) {
+	}
+
+	AddSegTree(const std::initializer_list<value_type> &list, INC_OP _inc=default_inc):AddSegTree(list.size(), _inc) {
+		std::copy(list.begin(), list.end(), tree.begin()+sz);
+	}
+	
+	/**
+	 * Perform "lazy" increment by v on all values in the open interval [b, e)
+	 * Runs in O(logN)
+	 */ 
+	void add_each(int b, int e, const value_type &v) {
+		b += sz;
+		e += sz;
+		while (b < e) {
+			if (b&1)
+				inc(tree[b++], v);
+			if (e&1)
+				inc(tree[--e], v);
+			b >>= 1;
+			e >>= 1;
+		}
+	}
+
+	/**
+	 * Perform "lazy" increment by v to all values
+	 * Runs in O(logN)
+	 */ 
+	void add_each(const value_type &v) {
+		add_each(0, sz, v);
+	}
+	
+	/**
+	 * Get the value at position pos in O(logN)
+	 */
+	const value_type &get(int pos) {
+		int node = sz+pos;
+		propagate_inc(node);
+		return tree[node];
+	}
+	
+	/**
+	 * Or if want to iterate all recomputed values
+	 * Needs O(n) to precompute
+	 */
+	typename std::vector<value_type>::const_iterator begin() {
+		propagate_inc();
+		return tree.cbegin() + sz;
+	}
+	
+	typename std::vector<value_type>::const_iterator end() {
+		return tree.cend();
 	}
 };
 
--- c/alg/segtree_test.cc
+++ c/alg/segtree_test.cc
@@ -1,9 +1,19 @@
 #include "segtree.hh"
 #include "gtest/gtest.h"
+#include <numeric>
 
-TEST(SegmentTree, SegmentTreeSum) {
+TEST(SegTree, Sum0) {
+	SegTree<int> sum({1,2,3});
+	EXPECT_EQ(sum(), 6);
+	sum.set(0, 2);
+	sum.set(1, 3);
+	sum.set(2, 4);
+	EXPECT_EQ(sum(), 9);
+}
+
+TEST(SegTree, Sum) {
 	for (int sz=1; sz<128; sz++) {
-		SegmentTree<int> sum(sz);
+		SegTree<int> sum(sz);
 		for (int i=0; i<sz; i++)
 			sum.set(i, i);
 		for (int w=1; w<sz; w++) {
@@ -15,9 +25,9 @@ TEST(SegmentTree, SegmentTreeSum) {
 	}
 }
 
-TEST(SegmentTree, SegmentTreeMax) {
+TEST(SegTree, Max) {
 	for (int sz=1; sz<128; sz++) {
-		SegmentTree<int> max(sz, [](int a, int b){return (a>b)?a:b;});
+		SegTree<int> max(sz, [](int a, int b){return (a>b)?a:b;});
 		for (int i=0; i<sz; i++)
 			max.set(i, i);
 		for (int w=1; w<sz; w++) {
@@ -28,3 +38,94 @@ TEST(SegmentTree, SegmentTreeMax) {
 		}
 	}
 }
+
+TEST(AddSegTree, Add1) {
+	AddSegTree<int> sum({1,2,3});
+	sum.add_each(0, 3, 1);
+	EXPECT_EQ(std::accumulate(sum.begin(), sum.end(), 0), 9);
+}
+
+TEST(AddSegTree, UpIncStat) {
+	for (int sz=1; sz<128; sz++) {
+		AddSegTree<int> sum(sz);
+		for (int i=0; i<sz; i++)
+			sum.add_each(i, sz, 1);
+		std::vector<int> res(sz);
+		for (int i=0; i<sz; i++)
+			res[i] = i+1;
+		EXPECT_EQ(std::equal(res.begin(), res.end(), sum.begin()), true);
+	}
+}
+
+TEST(AddSegTree, UpIncDyn) {
+	for (int sz=1; sz<128; sz++) {
+		AddSegTree<int> sum(sz);
+		for (int i=0; i<sz; i++)
+			sum.add_each(i, sz, 1);
+		std::vector<int> ans(sz);
+		for (int i=0; i<sz; i++)
+			ans[i] = sum.get(i);
+		std::vector<int> res(sz);
+		for (int i=0; i<sz; i++)
+			res[i] = i+1;
+		EXPECT_EQ(std::equal(res.begin(), res.end(), ans.begin()), true);
+	}
+}
+
+TEST(AddSegTree, DownIncStat) {
+	for (int sz=1; sz<128; sz++) {
+		AddSegTree<int> sum(sz);
+		for (int i=1; i<sz; i++)
+			sum.add_each(0, i, 1);
+		std::vector<int> res(sz);
+		for (int i=0; i<sz; i++)
+			res[i] = sz-i-1;
+		EXPECT_EQ(std::equal(res.begin(), res.end(), sum.begin()), true);
+	}
+}
+
+TEST(AddSegTree, DownIncDyn) {
+	for (int sz=1; sz<128; sz++) {
+		AddSegTree<int> sum(sz);
+		for (int i=1; i<sz; i++)
+			sum.add_each(0, i, 1);
+		std::vector<int> ans(sz);
+		for (int i=0; i<sz; i++)
+			ans[i] = sum.get(i);
+		std::vector<int> res(sz);
+		for (int i=0; i<sz; i++)
+			res[i] = sz-i-1;
+		EXPECT_EQ(std::equal(res.begin(), res.end(), ans.begin()), true);
+	}
+}
+
+TEST(AddSegTree, UpIncHalfSum) {
+	for (int sz=1; sz<128; sz++) {
+		AddSegTree<int> sum(sz);
+		for (int i=0; i<=sz/2; i++)
+			sum.add_each(i, sz-i, 1);
+		int ans = 0;
+		for (int i=0; i<sz; i++)
+			ans += sum.get(i);
+		int s1 = sz/2;
+		int s2 = sz-s1;
+		int arsum = s1*(s1+1)/2 + s2*(s2+1)/2;
+		EXPECT_EQ(ans, arsum);
+	}
+}
+
+TEST(AddSegTree, DownIncHalfSum) {
+	for (int sz=1; sz<128; sz++) {
+		AddSegTree<int> sum(sz);
+		for (int i=sz/2; i>=0; i--)
+			sum.add_each(i, sz-i, 1);
+		int ans = 0;
+		for (int i=sz-1; i>=0; i--)
+			ans += sum.get(i);
+		int s1 = sz/2;
+		int s2 = sz-s1;
+		int arsum = s1*(s1+1)/2 + s2*(s2+1)/2;
+		EXPECT_EQ(ans, arsum);
+	}
+}
+
