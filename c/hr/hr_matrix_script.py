30be3f9645c2bd980311c12557bf0312e3b276a4
Denis Kokarev <d_kokarev@mail.ru>
2019-01-20 17:20:21 -0500

    hr matrix-script solution

--- c/hr/hr_matrix_script.py
+++ c/hr/hr_matrix_script.py
@@ -7,14 +7,14 @@ import re
 
 def mysub(s):
     h = re.sub(r'[^A-Za-z0-9]+$', '', s)
-    t = re.sub(r'^.*[A-Za-z0-9]([^A-Za-z0-9]+)$', r'\1', s)
+    t = re.sub(r'^.*[A-Za-z0-9]([^A-Za-z0-9]*)$', r'\1', s)
     hr = re.sub(r'([A-Za-z0-9]+)[^A-Za-z0-9]*', r'\1 ', h)
     return hr[0:-1] + t
 
-n, m = map(int, input().split())
+n, m = map(int, raw_input().split())
 s = ""
 for _ in range(n):
-    s += input()
+    s += raw_input()
 ts = ""
 for p in range(m):
     ts += s[p::m]
