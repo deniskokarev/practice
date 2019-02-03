b609a5d1e1d993eb0a79f92f39113efdc61c18e4
Denis Kokarev <d_kokarev@mail.ru>
2019-02-03 09:36:34 -0500

    hr maximum-subarray-sum solution

--- c/hr/hr_maximum_subarray_sum.cc
+++ c/hr/hr_maximum_subarray_sum.cc
@@ -10,18 +10,18 @@ int main(int argc, char **argv) {
 		int n;
 		int64_t m;
 		scanf("%d%" PRId64, &n, &m);
-		std::map<int64_t, int, std::greater<int64_t>> mm;
+		std::map<int64_t, int> mm;
 		mm[0] = 0;
+		mm[m] = -1;
 		int64_t sum = 0;
 		int64_t mx = 0;
 		for (int i=1; i<=n; i++) {
 			int64_t a;
 			scanf("%" PRId64, &a);
-			a %= m;
-			auto fnd = mm.upper_bound(m - a);
-			mx = std::max(mx, fnd->first+a);
 			sum += a;
 			sum %= m;
+			auto fnd = mm.upper_bound(sum);
+			mx = std::max(mx, (m+sum-fnd->first) % m);
 			mm[sum] = i;
 		}
 		printf("%" PRId64 "\n", mx);
