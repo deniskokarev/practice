a782d7d4069042167887745d5b498f35fbf5b506
Denis Kokarev <d_kokarev@mail.ru>
2019-03-17 13:13:41 -0400

    cf1121a soluition

--- c/cf/cf1121a.cc
+++ c/cf/cf1121a.cc
@@ -4,36 +4,35 @@
 /* CodeForces CF1121A problem */
 using namespace std;
 
-using P = tuple<int,int,int>; // strength, sch, sel
+struct P {
+	int strength;
+	int school;
+	int sel;
+};
 
 int main(int argc, char **argv) {
 	int n, m, k;
 	cin >> n >> m >> k;
 	vector<P> pp(n);
 	for (auto &p:pp)
-		cin >> get<0>(p);
-	for (auto &p:pp) {
-		cin >> get<1>(p);
-		get<1>(p)--;
-	}
-	for (int i=0; i<k; i++) {
-		int j;
-		cin >> j;
-		get<2>(pp[j-1]) = 1;
-	}
+		cin >> p.strength;
 	vector<int> ms(m, -1);
 	for (int i=0; i<n; i++) {
-		auto &p = pp[i];
-		if (ms[get<1>(p)] == -1)
-			ms[get<1>(p)] = i;
-		else if (ms[get<1>(p)] < get<0>(p))
-			ms[get<1>(p)] = i;
+		int school;
+		cin >> school;
+		school--;
+		if (ms[school] < 0 || pp[ms[school]].strength < pp[i].strength)
+			ms[school] = i;
+		pp[i].school = school;
 	}
-	for (auto m:ms)
-		get<2>(pp[m]) = 0;
 	int ans = 0;
-	for (auto &p:pp)
-		ans += get<2>(p);
+	for (int i=0; i<k; i++) {
+		int sel;
+		cin >> sel;
+		sel--;
+		int school = pp[sel].school;
+		ans += (ms[school] != sel);
+	}
 	cout << ans << endl;
 	return 0;
 }
