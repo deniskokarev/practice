4364c8a7c95c11941d4c2d1ab39c778d7fa480b9
Denis Kokarev <d_kokarev@mail.ru>
2019-07-03 20:50:51 -0400

    https://leetcode.com/contest/weekly-contest-143/problems/filling-bookcase-shelves/ solution

--- c/cf/lc_filling_bookcase_shelves.cc
+++ c/cf/lc_filling_bookcase_shelves.cc
@@ -6,18 +6,21 @@ using namespace std;
 class Solution {
 public:
 	static int minHeightShelves(const vector<vector<int>>& books, int shelf_width) {
-		vector<int> ans(books.size(), INT_MAX);
-		ans[0] = books[0][1];
-		for (int i=1; i<books.size(); i++) {
-			int w = books[i][0];
-			int mxh = books[i][1];
+		int sz = books.size();
+		vector<vector<int>> bb(sz+1, vector<int>(2));
+		copy(books.begin(), books.end(), bb.begin()+1);
+		vector<int> ans(bb.size(), INT_MAX/2);
+		ans[0] = 0;
+		for (int i=1; i<bb.size(); i++) {
+			int w = bb[i][0];
+			int mxh = bb[i][1];
 			for (int j=i-1; j >= 0 && w <= shelf_width; j--) {
 				ans[i] = min(ans[i], ans[j]+mxh);
-				w += books[j][0];
-				mxh = max(mxh, books[j][1]);
+				w += bb[j][0];
+				mxh = max(mxh, bb[j][1]);
 			}
 		}
-		return ans[books.size()-1];
+		return ans[sz];
 	}
 };
 
@@ -27,5 +30,6 @@ int main(int argc, char **argv) {
 	cout << Solution::minHeightShelves({{1,1},{2,3},{2,3},{1,1},{1,1},{1,1},{1,2}}, 4) << '\n';
 	cout << Solution::minHeightShelves({{7,3},{8,7},{2,7},{2,5}}, 10) << '\n';
 	cout << Solution::minHeightShelves({{1,7},{6,4},{10,7},{6,10},{8,10},{1,10},{10,7}}, 10) << '\n';
+	cout << Solution::minHeightShelves({{1,7},{6,4},{10,7}}, 10) << '\n';
 	return 0;
 }
