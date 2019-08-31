421c4da84e4a4983723704c0125988c30077d463
Denis Kokarev <d_kokarev@mail.ru>
2019-08-31 19:19:39 -0400

    lc_peak-index-in-a-mountain-array solution

---
+++ c/cf/lc_peak-index-in-a-mountain-array.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+/* https://leetcode.com/problems/peak-index-in-a-mountain-array */
+using namespace std;
+
+class Solution {
+public:
+	static int peakIndexInMountainArray(const vector<int>& A) {
+		int f=1, t=A.size()-2;
+		while (f < t) {
+			int m = f+(t-f)/2;
+			if (A[m] < A[m+1])
+				f = m+1;
+			else
+				t = m;
+		}
+		return f;
+	}
+};
+
+int main(int argc, char **argv) {
+	cout << Solution::peakIndexInMountainArray({0,1,0}) << endl;
+	cout << Solution::peakIndexInMountainArray({0,2,1,0}) << endl;
+	cout << Solution::peakIndexInMountainArray({0,1,2,1,0}) << endl;
+	cout << Solution::peakIndexInMountainArray({0,1,2,0}) << endl;
+}
