4ba339a03b9e0d5a1d2775eaa2f7359aee51385f
Denis Kokarev <d_kokarev@mail.ru>
2019-09-14 18:38:28 -0400

    better yet https://leetcode.com/problems/minimum-window-substring solution

--- c/cf/lc_minimum-window-substring.cc
+++ c/cf/lc_minimum-window-substring.cc
@@ -9,34 +9,30 @@ using namespace std;
 
 class Solution {
 	using uchar = unsigned char;
-	struct S {
-		int mn;
-		int cnt;
-		S operator+(const S &o) const {
-			return S {min(mn, o.mn), cnt+o.cnt};
-		}
-	};
 public:
 	static string minWindow(const string &s, const string &t) {
-		int th[256] {0};
+		vector<int> th(256, 0);
 		for (uchar c:t)
 			th[c]++;
+		int cnt = 0;
 		vector<deque<int>> qq(256);
 		int mn = INT_MAX;
 		int l=-1, r=INT_MAX;
-		vector<S> hh(2*256, {INT_MAX, 0});
+		vector<int> hh(2*256, INT_MAX);
 		for (int i=0; i<s.size(); i++) {
 			uchar c = s[i];
 			if (th[c]) {
 				qq[c].push_back(i);
 				if (qq[c].size() > th[c])
 					qq[c].pop_front();
+				else if (qq[c].size() == th[c])
+					cnt += th[c];
 				int j = 256+c;
-				hh[j] = S {qq[c].front(), int(qq[c].size())};
+				hh[j] = qq[c].front();
 				for ( ; j>1; j>>=1) // 8 times
-					hh[j >> 1] = hh[j] + hh[j^1];
-				int tl = hh[1].mn;
-				if (hh[1].cnt == t.size() && mn > i-tl) {
+					hh[j >> 1] = min(hh[j], hh[j^1]);
+				int tl = hh[1];
+				if (cnt == t.size() && mn > i-tl) {
 					mn = i-tl;
 					l = tl;
 					r = i;
