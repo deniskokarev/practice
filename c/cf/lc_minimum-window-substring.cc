369eb26512e0f2ed29bdb75b435335e09ddbc9ad
Denis Kokarev <d_kokarev@mail.ru>
2019-09-14 18:21:39 -0400

    better https://leetcode.com/problems/minimum-window-substring solution

--- c/cf/lc_minimum-window-substring.cc
+++ c/cf/lc_minimum-window-substring.cc
@@ -9,46 +9,42 @@ using namespace std;
 
 class Solution {
 	using uchar = unsigned char;
+	struct S {
+		int mn;
+		int cnt;
+		S operator+(const S &o) const {
+			return S {min(mn, o.mn), cnt+o.cnt};
+		}
+	};
 public:
 	static string minWindow(const string &s, const string &t) {
 		int th[256] {0};
 		for (uchar c:t)
 			th[c]++;
 		vector<deque<int>> qq(256);
-		vector<vector<int>> lb(256, vector<int>(s.size()+1, -1));
-		for (int i=1; i<=s.size(); i++) {
-			for (int c=0; c<256; c++)
-				lb[c][i] = lb[c][i-1];
-			uchar c = s[i-1];
-			qq[c].push_back(i);
-			if (qq[c].size() > th[c])
-				qq[c].pop_front();
-			if (qq[c].size() == th[c])
-				lb[c][i] = qq[c].front();
-		}
-		for (uchar c:{'a', 'A', 'B', 'C'}) {
-			cerr << c << ": ";
-			for (int i=0; i<=s.size(); i++)
-				cerr << lb[c][i] << ' ';
-			cerr << endl;
-		}
-		cerr << endl;
 		int mn = INT_MAX;
 		int l=-1, r=INT_MAX;
-		for (int i=1; i<=s.size(); i++) {
-			int mnl = INT_MAX;
-			for (int c=0; c<256; c++)
-				if (th[c])
-					mnl = min(mnl, lb[c][i]);
-			if (mnl >= 0 && mnl < INT_MAX && i-mnl < mn) {
-				mn = i-mnl;
-				l = mnl;
-				r = i;
+		vector<S> hh(2*256, {INT_MAX, 0});
+		for (int i=0; i<s.size(); i++) {
+			uchar c = s[i];
+			if (th[c]) {
+				qq[c].push_back(i);
+				if (qq[c].size() > th[c])
+					qq[c].pop_front();
+				int j = 256+c;
+				hh[j] = S {qq[c].front(), int(qq[c].size())};
+				for ( ; j>1; j>>=1) // 8 times
+					hh[j >> 1] = hh[j] + hh[j^1];
+				int tl = hh[1].mn;
+				if (hh[1].cnt == t.size() && mn > i-tl) {
+					mn = i-tl;
+					l = tl;
+					r = i;
+				}
 			}
 		}
-		cerr << "[" << l << ", " << r << "]" << endl;
 		if (mn < INT_MAX)
-			return s.substr(l-1, r-l+1);
+			return s.substr(l, r-l+1);
 		else
 			return "";
 	}
@@ -58,4 +54,5 @@ int main(int argc, char **argv) {
 	cout << Solution::minWindow("ADOBECODEBANC", "ABC") << endl;
 	cout << Solution::minWindow("a", "aa") << endl;
 	cout << Solution::minWindow("aa", "aa") << endl;
+	cout << Solution::minWindow("ABAB", "ABB") << endl;
 }
