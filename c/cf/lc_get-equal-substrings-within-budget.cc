f471355238201170f892ff46c29bf58e4a82c9bc
Denis Kokarev <d_kokarev@mail.ru>
2019-09-29 14:14:37 -0400

    lc contest 156 solutions b,c

---
+++ c/cf/lc_get-equal-substrings-within-budget.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+/* https://leetcode.com/contest/weekly-contest-156/problems/get-equal-substrings-within-budget/ */
+using namespace std;
+
+class Solution {
+public:
+	static int equalSubstring(const string &s, const string &t, int maxCost) {
+		vector<int> cost(s.size()+1);
+		for (int i=1; i<cost.size(); i++)
+			cost[i] = cost[i-1] + std::abs(s[i-1]-t[i-1]);
+		int mx = 0;
+		for (int j=0,i=1; i<cost.size(); i++) {
+			while (j<i && cost[i]-cost[j] > maxCost)
+				j++;
+			mx = max(mx, i-j);
+		}
+		return mx;
+	}
+};
+
+int main(int argc, char **argv) {
+	cout << Solution::equalSubstring("abcd", "bcdf", 3) << endl;
+	cout << Solution::equalSubstring("abcd", "cdef", 3) << endl;
+	cout << Solution::equalSubstring("abcd", "acde", 0) << endl;
+	cout << Solution::equalSubstring("a", "b", 0) << endl;
+	return 0;
+}
