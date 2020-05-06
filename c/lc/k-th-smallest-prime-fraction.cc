9deef213573a8f1e025a8a5e092d99845c7522be
Denis Kokarev <d_kokarev@mail.ru>
2020-05-05 20:13:16 -0400

    https://leetcode.com/problems/k-th-smallest-prime-fraction

--- c/lc/k-th-smallest-prime-fraction.cc
+++ c/lc/k-th-smallest-prime-fraction.cc
@@ -1,48 +1,62 @@
+/**
+ * https://leetcode.com/problems/k-th-smallest-prime-fraction
+ */
 #include <iostream>
 #include <vector>
-#include <queue>
-/* https://leetcode.com/problems/k-th-smallest-prime-fraction/ */
+#include <algorithm>
+#include <numeric>
+
 using namespace std;
 
-struct Q {
-    int p, q;
-};
-
-struct Qcmp {
-	const vector<int> &A;
-    bool operator()(const Q &a, const Q &b) const {
-        return A[a.p]*A[b.q] > A[b.p]*A[a.q];
-    }
-};
-
 class Solution {
+	struct Q {
+		int p, q;
+		bool operator<(const Q &o) const {
+			return int64_t(p)*o.q < int64_t(o.p)*q;
+		}
+	};
+	static int cntLE(double key, Q &mx, const vector<int> &a) {
+		int sz = a.size();
+		int cnt = 0;
+		for (int i=0; i<sz-1; i++) {
+			int num = a[i];
+			int f = i+1, t=sz;
+			while (f<t) {
+				int m = (f+t)/2;
+				if (key < double(num)/a[m])
+					f = m+1;
+				else
+					t = m;
+			}
+			if (f<sz) {
+				cnt += sz-f;
+				mx = max(mx, Q{num, a[f]});
+			}
+		}
+		return cnt;
+	}
 public:
-    static vector<int> kthSmallestPrimeFraction(const vector<int>& A, int K) {
-		Qcmp qcmp = Qcmp{A};
-        priority_queue<Q,vector<Q>,Qcmp> qq(qcmp);
-        int sz = A.size();
-        vector<vector<int>> ord(sz, vector<int>(sz));
-        int seq = 1;
-        qq.push(Q{0, sz-1});
-        while(!qq.empty()) {
-            Q t = qq.top();
-            qq.pop();
-            if (!ord[t.p][t.q]) {
-				if (seq == K)
-					return vector<int>{A[t.p], A[t.q]};
-                ord[t.p][t.q] = seq++;
-                if (t.p+1 < t.q)
-                    qq.push(Q{t.p+1, t.q});
-                if (t.q-1 > t.p)
-                    qq.push(Q{t.p, t.q-1});
-            }
-        }
-        return vector<int>{-1,-1};
-    }
+	static vector<int> kthSmallestPrimeFraction(const vector<int> &a, int k) {
+		double f=0, t=1;
+		while (true) {
+			double m = (f+t)/2;
+			Q mx {0, 1};
+			int cnt = cntLE(m, mx, a);
+			if (cnt > k)
+				t = m;
+			else if (cnt < k)
+				f = m;
+			else
+				return {mx.p, mx.q};
+		}
+	}
 };
 
 int main(int argc, char **argv) {
-	auto res = Solution::kthSmallestPrimeFraction({1,2,3,5}, 3);
-	cout << res[0] << " / " << res[1] << endl;
+	vector<int> a1 = Solution::kthSmallestPrimeFraction({1, 2, 3, 5}, 3);
+	cout << a1[0] << " / " << a1[1] << endl;
+	vector<int> a2 = Solution::kthSmallestPrimeFraction({1, 7}, 1);
+	cout << a2[0] << " / " << a2[1] << endl;
 	return 0;
 }
+
