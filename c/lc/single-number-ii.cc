2affd8f136ecc8cd8ce6c0c92dd434150fe5c344
Denis Kokarev <d_kokarev@mail.ru>
2020-06-23 22:20:15 -0400

    https://leetcode.com/problems/single-number-ii/ better solution

--- c/lc/single-number-ii.cc
+++ c/lc/single-number-ii.cc
@@ -8,23 +8,9 @@ public:
 			for (int i=0; i<32; i++)
 				bit_hist[i] += bb[i];
 		}
-		int sel_bit = -1;
-		for (sel_bit=0; sel_bit<32 && bit_hist[sel_bit] % 3 == 0; sel_bit++);
-		if (sel_bit == 32)
-			return 0;
-		unsigned msk = 1U<<sel_bit;
-		int bit_hist2[32]{0};
-		for (auto &n:nums) {
-			if (n & msk) {
-				bitset<32> bb(n);
-				for (int i=0; i<32; i++)
-					bit_hist2[i] += bb[i];
-			}
-		}
 		bitset<32> res(0);
 		for (int i=0; i<32; i++)
-			if (bit_hist2[i] % 3)
-				res[i] = 1;
+			res[i] = (bit_hist[i] % 3);
 		return res.to_ulong();
 	}
 };
