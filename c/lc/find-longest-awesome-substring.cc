324b4db07210b6d3cc409965be035be69a0429eb
Denis Kokarev <d_kokarev@mail.ru>
2020-08-08 19:43:53 -0400

    better find-longest-awesome-substring

--- c/lc/find-longest-awesome-substring.cc
+++ c/lc/find-longest-awesome-substring.cc
@@ -1,8 +1,8 @@
-ass Solution {
+class Solution {
 public:
 	static int longestAwesome(const string &s) {
 		constexpr int ALP = 10;
-		constexpr int NONE = -2;
+		constexpr int NONE = INT_MAX/2;
 		vector<int> hist(1<<ALP, NONE);
 		int k = 0;
 		hist[k] = -1;
@@ -10,12 +10,10 @@ public:
 		for (int i=0; i<s.size(); i++) {
 			int c = s[i] % ALP;
 			k = k ^ (1<<c);
-			if (hist[k] != NONE)
-				ans = max(ans, i-hist[k]);
+			ans = max(ans, i-hist[k]);
 			for (int p=0; p<ALP; p++) {
 				int k_odd = k ^ (1<<p);
-				if (hist[k_odd] != NONE)
-					ans = max(ans, i-hist[k_odd]);
+				ans = max(ans, i-hist[k_odd]);
 			}
 			if (hist[k] == NONE)
 				hist[k] = i;
@@ -23,4 +21,3 @@ public:
 		return ans;
 	}
 };
-
