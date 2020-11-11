46571778e735a82e32cc519cc06563a244cfb599
Denis Kokarev <d_kokarev@mail.ru>
2020-11-10 20:42:12 -0500

    optimized solution for lc contest 212b (lexicographically-smallest-string-after-applying-operations)

--- c/lc/lexicographically-smallest-string-after-applying-operations.cc
+++ c/lc/lexicographically-smallest-string-after-applying-operations.cc
@@ -1,32 +1,25 @@
 class Solution {
 public:
 	static string findLexSmallestString(const string &s, int a, int b) {
+		string s2 = s+s;
 		int sz = s.size();
 		int ofs = gcd(sz, b);
-		int inc = gcd(a, 10);
+		int d1_inc = gcd(a, 10);
+		int d0_inc;
+		if (ofs&1)
+			d0_inc = d1_inc;
+		else
+			d0_inc = 10; // only 1 iteration
 		string mn(s);
-		if (ofs&1) {
-			for (int d0=0; d0<10; d0+=inc) {
-				for (int d1=0; d1<10; d1+=inc) {
-					string str(s);
+		string str(sz, ' ');
+		for (int pos=0; pos<sz; pos+=ofs) {
+			for (int d0=0; d0<10; d0+=d0_inc) {
+				for (int d1=0; d1<10; d1+=d1_inc) {
 					for (int i=0; i<sz; i+=2)
-						str[i] = (((str[i]-'0') + d0) % 10)+'0';
+						str[i] = (((s2[i+pos]-'0') + d0) % 10)+'0';
 					for (int i=1; i<sz; i+=2)
-						str[i] = (((str[i]-'0') + d1) % 10)+'0';
-					for (int pos=0; pos<sz; pos+=ofs) {
-						string cmp((str+str).substr(pos, sz));
-						mn = min(mn, cmp);
-					}
-				}
-			}
-		} else {
-			for (int d=0; d<10; d+=inc) {
-				string str(s);
-				for (int i=1; i<sz; i+=2)
-					str[i] = (((str[i]-'0') + d) % 10)+'0';
-				for (int pos=0; pos<sz; pos+=ofs) {
-					string cmp((str+str).substr(pos, sz));
-					mn = min(mn, cmp);
+						str[i] = (((s2[i+pos]-'0') + d1) % 10)+'0';
+					mn = min(mn, str);
 				}
 			}
 		}
