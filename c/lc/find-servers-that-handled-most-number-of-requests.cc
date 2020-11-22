dedb82fa78967b026bf13d9017ee06e06333f790
Denis Kokarev <d_kokarev@mail.ru>
2020-11-22 13:16:06 -0500

    simpler solution for lc find-servers-that-handled-most-number-of-requests

--- c/lc/find-servers-that-handled-most-number-of-requests.cc
+++ c/lc/find-servers-that-handled-most-number-of-requests.cc
@@ -1,75 +1,41 @@
-void fw_inc(vector<int> &fw, int p, int inc) {
-	int sz = fw.size();
-	while (p<sz) {
-		fw[p] += inc;
-		p |= p+1;
-	}
-}
-
-int fw_sum(vector<int> &fw, int p) {
-	int sm = 0;
-	while (p>=0) {
-		sm += fw[p];
-		p &= p+1;
-		p--;
-	}
-	return sm;
-}
-
 class Solution {
-	enum {END, START};
+	enum {EV_END, EV_START};
 	struct EV {
 		int id;
-		int type;
+		int ev_type;
 		int t;
 		bool operator<(const EV &o) const {
-			return t<o.t || (t==o.t && type<o.type);
+			return t<o.t || (t==o.t && ev_type<o.ev_type);
 		}
 	};
-	static int find_free_server(vector<int> &fw, int first, int last) {
-		int f=first, t=last;
-		while (f<=t) {
-			int mid = f+(t-f)/2;
-			int sm = fw_sum(fw, mid) - fw_sum(fw, first-1);
-			if (!sm)
-				f = mid+1;
-			else
-				t = mid-1;
-		}
-		int fnd = t+1;
-		if (fnd <= last && fw_sum(fw, fnd) - fw_sum(fw, fnd-1))
-			return fnd;
-		else
-			return -1;
-	}
 public:
 	static vector<int> busiestServers(int k, const vector<int>& arrival, const vector<int>& load) {
 		int sz = arrival.size();
 		vector<EV> ev(2*sz);
 		for (int i=0; i<sz; i++)
-			ev[i] = EV {i, START, arrival[i]};
+			ev[i] = EV {i, EV_START, arrival[i]};
 		for (int i=0; i<sz; i++)
-			ev[sz+i] = EV {i, END, arrival[i]+load[i]};
+			ev[sz+i] = EV {i, EV_END, arrival[i]+load[i]};
 		sort(ev.begin(), ev.end());
-		vector<int> fw(k);
-		for (int s=0; s<k; s++)
-			fw_inc(fw, s, +1);  // mark all servers free
 		vector<int> evid2s(sz, -1);
 		vector<int> stat(k);
+		set<int> free;
+		for (int s=0; s<k; s++)
+			free.insert(s), free.insert(s+k);
 		for (auto &e:ev) {
-			if (e.type == END) {
+			if (e.ev_type == EV_END) {
 				int s = evid2s[e.id];
 				if (s >= 0)
-					fw_inc(fw, s, +1); // +1 means make it free
+					free.insert(s), free.insert(s+k);
 			} else {
 				int s_want = e.id % k;
-				int fnd = find_free_server(fw, s_want, k-1);
-				if (fnd < 0)
-					fnd = find_free_server(fw, 0, s_want-1);
-				if (fnd >= 0) {
-					stat[fnd]++;
-					evid2s[e.id] = fnd;
-					fw_inc(fw, fnd, -1); // -1 make it busy
+				auto fnd = free.lower_bound(s_want);
+				if (fnd != free.end()) {
+					int s = *fnd % k;
+					stat[s]++;
+					evid2s[e.id] = s;
+					free.erase(s);
+					free.erase(s+k);
 				}
 			}
 		}
