fe574df451540d37548922e1fd79607058bb9724
Denis Kokarev <d_kokarev@mail.ru>
2020-12-13 13:54:36 -0500

    better solution for lc smallest-subtree-with-all-the-deepest-nodes

--- c/lc/smallest-subtree-with-all-the-deepest-nodes.cc
+++ c/lc/smallest-subtree-with-all-the-deepest-nodes.cc
@@ -1,28 +1,18 @@
 class Solution {
-    int dfs_mx_dep(const TreeNode* root, int dep) {
-        if (root)
-            return max(dfs_mx_dep(root->left, dep+1), dfs_mx_dep(root->right, dep+1));
-        else
-            return dep;
-    }
     struct R {
         TreeNode *res;
         int dep;
     };
-    R dfs_parent_mx_dep(TreeNode* root, int dep, int mx_dep) {
+    R dfs_parent_mx_dep(TreeNode* root, int dep) {
         if (root) {
-            R l = dfs_parent_mx_dep(root->left, dep+1, mx_dep);
-            R r = dfs_parent_mx_dep(root->right, dep+1, mx_dep);
-            if (l.dep == mx_dep) {
-                if (r.dep == mx_dep) {
-                    return R {root, l.dep};
-                } else {
-                    return l;
-                }
-            } else if (r.dep == mx_dep) {
+            R l = dfs_parent_mx_dep(root->left, dep+1);
+            R r = dfs_parent_mx_dep(root->right, dep+1);
+            if (l.dep > r.dep) {
+                return l;
+            } else if (l.dep < r.dep) {
                 return r;
             } else {
-                return R {nullptr, max(l.dep, r.dep)};
+                return R {root, max(l.dep, r.dep)};
             }
         } else {
             return R {nullptr, dep};
@@ -30,6 +20,6 @@ class Solution {
     }
 public:
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        return dfs_parent_mx_dep(root, 0, dfs_mx_dep(root, 0)).res;
+        return dfs_parent_mx_dep(root, 0).res;
     }
 };
