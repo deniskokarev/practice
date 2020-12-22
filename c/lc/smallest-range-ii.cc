6d6cd8736123e3dd0856ed48e242f416eac249e2
Denis Kokarev <d_kokarev@mail.ru>
2020-12-22 12:28:16 -0500

    faster lc smallest-range-ii.cc

--- c/lc/smallest-range-ii.cc
+++ c/lc/smallest-range-ii.cc
@@ -2,24 +2,26 @@ class Solution {
 public:
 	static int smallestRangeII(const vector<int>& aa, int k) {
 		if (aa.size() > 1) {
-			set<pair<int,bool>> ss;
-			for (int a:aa)
-				ss.insert({a,false});
-			// from everything combed to left we're tring to comb
+			vector<int> ss(aa);
+			sort(ss.begin(), ss.end());
+			int sz = unique(ss.begin(), ss.end()) - ss.begin();
+			ss.resize(sz);
+			// from everything combed to left we're trying to comb
 			// left border to right
 			// >>  <<<<<<
-			int mn_diff = prev(ss.end())->first - ss.begin()->first;
-			auto it = ss.begin();
-			while (it != ss.end()) {
-				if (!it->second) {
-					ss.insert({it->first+2*k, true});
-					auto nit = next(it);
-					ss.erase(it);
-					it = nit;
+			int mx = ss[sz-1];
+			int mn = ss[0];
+			int mn_diff = mx - mn;
+			int mn_mn = ss[0] + 2*k; // absolute minimum we can reach
+			for (int i=0; i < sz-1 && mn < mn_mn; i++) {
+				if (ss[i] + 2*k > mx) {
+					mx = ss[i] + 2*k;
+					mn = ss[i+1];
 				} else {
-					break;
+					mn = min(ss[i]+2*k, ss[i+1]);
 				}
-				mn_diff = min(mn_diff, prev(ss.end())->first - ss.begin()->first);
+				mn = min(mn_mn, mn);
+				mn_diff = min(mn_diff, mx - mn);
 			}
 			return mn_diff;
 		} else {
