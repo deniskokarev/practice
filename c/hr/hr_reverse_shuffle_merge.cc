516b7adf4f0c1689c5fad5fa04a824f005a0aea5
Denis Kokarev <d_kokarev@mail.ru>
2020-12-22 16:48:04 -0500

    hr reverse-shuffle-merge

--- c/hr/hr_reverse_shuffle_merge.cc
+++ c/hr/hr_reverse_shuffle_merge.cc
@@ -1,88 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <array>
-#include <algorithm>
-/* Hackerrank https://www.hackerrank.com/challenges/reverse-shuffle-merge */
-using namespace std;
 
-struct H {
-	static constexpr int SZ = 26;
-	array<int, SZ> hh;
-	H() {
-		fill(hh.begin(), hh.end(), 0);
-	}
-	H &operator+=(unsigned char c) {
-		hh[c%SZ]++;
-		return *this;
-	}
-	H &operator-=(unsigned char c) {
-		hh[c%SZ]--;
-		return *this;
-	}
-	H operator+(const H &o) const {
-		H res(*this);
-		for (int i=0; i<SZ; i++)
-			res.hh[i] += o.hh[i];
-		return res;
-	}
-	bool operator<=(const H &o) const {
-		for (int i=0; i<SZ; i++)
-			if (hh[i] > o.hh[i])
-				return false;
-		return true;
-	}
-	bool operator==(const H &o) const {
-		for (int i=0; i<SZ; i++)
-			if (hh[i] != o.hh[i])
-				return false;
-		return true;
-	}
-};
+using namespace std;
 
 int main(int argc, char **argv) {
 	string s;
 	cin >> s;
-	if (s == "aeiouuoiea") {
-		///
-		// !!!! fucked-up test case !!!!
-		///
-		cout << "eaid" << endl;
-		return 0;
+	unsigned sz = s.size();
+	vector<array<int,26>> lcnt(sz+1, {{0}});
+	vector<array<int,26>> lpos(sz+1, {{0}});
+	for (unsigned i=1; i<=sz; i++) {
+		lcnt[i] = lcnt[i-1];
+		lcnt[i][s[i-1]-'a']++;
+		lpos[i] = lpos[i-1];
+		lpos[i][s[i-1]-'a'] = i;
 	}
-	s += ' ';
-	reverse(s.begin(), s.end());
-	int len = int(s.length());
-	vector<H> hh(len);	// cumulative letters histohram
-	for (int i=1; i<len; i++) {
-		hh[i] = hh[i-1];
-		hh[i] += s[i];
-	}
-	vector<array<int, H::SZ>> nxt_right(len+1); // pointer to next letter
-	fill(nxt_right[len].begin(), nxt_right[len].end(), -1);
-	for (int i=len-1; i>0; i--) {
-		nxt_right[i] = nxt_right[i+1];
-		nxt_right[i][s[i]%H::SZ] = i;
-	}
-	nxt_right[0] = nxt_right[1];
+	array<int,26> need {{0}};
+	for (unsigned char c:s)
+		need[c-'a']++;
+	for (int &n:need)
+		n /= 2;
 	string ans;
-	H taken;
-	int lp = 0;
-	for (int i=0; i<len && 2*ans.size() < len-1; i++) {
-		for (unsigned char c='a'; c<='z'; c++) {
-			int pos = nxt_right[i+1][c%H::SZ];
-			if (pos > 0) {
-				if (hh[pos-1] + hh[pos-1] <= hh[len-1] + taken) {
-					lp = pos+1;
-					i = pos-1;
-					taken += c;
-					ans += c;
+	int end = sz;
+	while (ans.size() < sz/2) {
+		for (int c=0; c<26; c++) {
+			if (need[c]) {
+				int pos = lpos[end][c];
+				bool enough = true;
+				for (int nc=0; nc<26; nc++)
+					enough &= (need[nc] <= lcnt[pos][nc]);
+				if (enough) {
+					ans += c+'a';
+					need[c]--;
+					end = pos-1;
 					break;
 				}
 			}
 		}
 	}
-	while (2*ans.size() < len-1)
-		ans += s[lp++];
 	cout << ans << endl;
 	return 0;
 }
+
