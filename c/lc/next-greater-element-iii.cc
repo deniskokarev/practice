b999be254aad64a3b87b19de29c6ce1c528a8eb8
Denis Kokarev <d_kokarev@mail.ru>
2020-12-23 12:07:28 -0500

    lc/next-greater-element-iii.cc more elegant canonical solution

--- c/lc/next-greater-element-iii.cc
+++ c/lc/next-greater-element-iii.cc
@@ -3,21 +3,19 @@ public:
 	static int nextGreaterElement(int n) {
 		string dd = to_string(n);
 		int sz = dd.size();
-		// solution(dd) = prefix(dd) + solution(suffix(dd)) for len(suffix) in range(2,len)
 		if (sz < 2) {
 			return -1;
 		} else {
-			multiset<char> sfx;
-			sfx.insert(dd[sz-1]);
+			// going backwards until suffix is in desc order
+			// when first asc pair is met we find replacement candidate in the suffix, replacing,
+			// inverting suffix and printing result
 			for (int pos=sz-2; pos>=0; pos--) {
-				sfx.insert(dd[pos]);
-				auto fnd = sfx.upper_bound(dd[pos]);
-				if (fnd != sfx.end()) {
-					string res = dd.substr(0, pos);
-					res += *fnd;
-					sfx.erase(fnd);
-					res += string(sfx.begin(), sfx.end());
-					int64_t ans = stoll(res);
+				if (dd[pos] < dd[pos+1]) {
+					auto fnd = lower_bound(dd.begin()+pos+1, dd.end(), dd[pos], std::greater<char>());
+					--fnd;
+					swap(dd[pos], *fnd);
+					reverse(dd.begin()+pos+1, dd.end());
+					int64_t ans = stoll(dd);
 					if (ans < INT_MAX)
 						return ans;
 					else
@@ -28,4 +26,3 @@ public:
 		}
 	}
 };
-
