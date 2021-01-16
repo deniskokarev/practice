214b3ad8b0aed21e2acefe2b8515ec100a63a21b
Denis Kokarev <d_kokarev@mail.ru>
2021-01-15 19:24:17 -0500

    optimized minimum-operations-to-reduce-x-to-zero.cc

--- c/lc/minimum-operations-to-reduce-x-to-zero.cc
+++ c/lc/minimum-operations-to-reduce-x-to-zero.cc
@@ -1,28 +1,31 @@
 class Solution {
 public:
-	static int minOperations(const vector<int>& nums, int x) {
-		int sz = nums.size();
-		unordered_map<int,int> rsm;
-		rsm[0] = sz;
-		int sm = 0;
-		for (int i=sz-1; i>=0; i--) {
-			sm += nums[i];
-			rsm[sm] = i;
-		}
-		int mn = INT_MAX;
-		sm = 0;
-		auto it = rsm.find(x-sm);
-		if (it != rsm.end())
-			mn = sz - it->second;
-		for (int i=0; i<sz; i++) {
-			sm += nums[i];
-			it = rsm.find(x-sm);
-			if (it != rsm.end() && i < it->second)
-				mn = min(mn, i + 1 + sz - it->second);
-		}
-		if (mn != INT_MAX)
-			return mn;
-		else
-			return -1;
-	}
+    int minOperations(vector<int>& nums, int x) {
+        int sz = nums.size();
+        int lsm = 0;
+        int l = 0;
+        int mn = INT_MAX;
+        while (l<sz) {
+            lsm += nums[l++];
+            if (lsm == x)
+                mn = l;
+        }
+        if (lsm < x)
+            return -1;
+        // lsm is [0..sz-1]
+        int r = sz;
+        int rsm = 0;
+        while (r > 0) {
+            r--;
+            rsm += nums[r];
+            while (lsm + rsm > x && l > 0)
+                lsm -= nums[--l];
+            if (lsm + rsm == x)
+                mn = min(mn, l + sz - r);
+        }
+        if (mn == INT_MAX)
+            return -1;
+        else
+            return mn;
+    }
 };
