c85b2c4bf091e4e7e99ffe7b5acaaf9b2d0cc7a3
Denis Kokarev <d_kokarev@mail.ru>
2021-02-14 20:51:11 -0500

    lc matchsticks-to-square

--- c/lc/matchsticks-to-square.cc
+++ c/lc/matchsticks-to-square.cc
@@ -1,29 +1,30 @@
-// TL
-// O(3^|nums|)
 class Solution {
+	static bool dfs(const vector<unsigned> &fits, unsigned taken, int dep) {
+		if (!dep) {
+			return true;
+		} else {
+			for (auto m:fits)
+				if (!(m & taken) && dfs(fits, taken|m, dep-1))
+					return true;
+			return false;
+		}
+	}
 public:
 	static bool makesquare(const vector<int>& nums) {
 		int sz = nums.size();
-		if (!sz)
-			return false; // pidarasen
 		int p = accumulate(nums.begin(), nums.end(), 0);
 		int l = p/4;
-		const int ONES = (1<<sz)-1;
-		vector<bool> one_side(ONES+1);
-		for (int i=1; i<=ONES; i++) {
+		if (!sz || 4*l != p)
+			return false;
+		const unsigned ONES = (1<<sz)-1;
+		vector<unsigned> fits; // collection of bitsets that add up to l
+		for (unsigned i=1; i<=ONES; i++) {
 			int sm = 0;
 			for (int b=0; b<sz; b++)
 				sm += nums[b] * ((i>>b) & 1);
-			one_side[i] = (sm == l);
+			if (sm == l)
+				fits.push_back(i);
 		}
-		vector<bool> dp(one_side);
-		for (int side=1; side<4; side++) {
-			for (int msk=ONES; msk>0; msk--) {
-				int nmsk = ONES & ~msk;
-				for (int s=nmsk; s; s=(s-1) & nmsk)
-					dp[msk | s] = (dp[msk | s] || (dp[msk] && one_side[s]));
-			}
-		}
-		return dp[ONES];
+		return dfs(fits, 0, 4);
 	}
 };
