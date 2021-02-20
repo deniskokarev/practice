8efe4c934ce733ebdaee2b7a31a035e69759cd40
Denis Kokarev <d_kokarev@mail.ru>
2021-02-20 16:09:44 -0500

    lc feb challenge container-with-most-water

---
+++ c/lc/container-with-most-water.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	int maxArea(vector<int>& height) {
+		int mx = 0;
+		int i=0, j=height.size()-1;
+		while (i<j) {
+			mx = max(mx, min(height[i], height[j]) * (j-i));
+			if (height[i] > height[j])
+				j--;
+			else
+				i++;
+		}
+		return mx;
+	}
+};
