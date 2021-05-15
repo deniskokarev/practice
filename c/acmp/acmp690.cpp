d10241e7d109cf9629452c9227020c55f8ea6cc6
Denis Kokarev <d_kokarev@mail.ru>
2021-05-15 19:32:55 -0400

    fixing acmp690 knight tour problem

--- c/acmp/acmp690.cpp
+++ c/acmp/acmp690.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -16,7 +17,9 @@ struct P {
 
 const static P moves[8] = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
 
-inline int varsendorf(int map[SZ+4][SZ+4], const P &p) {
+inline int warnsdorff_score(int map[SZ+4][SZ+4], const P &p) {
+	if (map[p.y][p.x])
+		return INT_MAX;
 	int cnt = 0;
 	if (map[p.y][p.x] == 0) {
 		for (const auto &m:moves) {
@@ -28,34 +31,51 @@ inline int varsendorf(int map[SZ+4][SZ+4], const P &p) {
 	return cnt;
 }
 
-bool dfs(int map[SZ+4][SZ+4], const P &p, int cnt, int idx) {
-	if (!cnt)
-		return true;
+// straight walk using W-heuristic without backtracking
+bool warnsdorff_straight_walk(int map[SZ+4][SZ+4], const P &p, int wdep, int idx) {
 	if (map[p.y][p.x] != 0)
 		return false;
 	map[p.y][p.x] = idx;
-	struct VSC {
-		int vsc;
-		P m;
-		bool operator<(const VSC &b) const {
-			return vsc < b.vsc;
+	if (wdep <= 1) {
+		return true;
+	} else {
+		struct WSC {
+			int wsc;
+			P m;
+			bool operator<(const WSC &b) const {
+				return wsc < b.wsc;
+			}
+		} min_mv {INT_MAX};
+		for (const P &mv:moves) {
+			const P nxt_mv = P {p.x+mv.x, p.y+mv.y};
+			min_mv = min(min_mv, {warnsdorff_score(map, nxt_mv), nxt_mv});
 		}
-	} mv[dim(moves)];
-	for (int i=0; i<dim(moves); i++) {
-		// each step we start from next move to go in circles
-		// NB:!!! this +2 random coefficient passes all stupid tests
-		int mi = (cnt+i+2)&7;
-		const P &m = moves[mi];
-		const P nm = P {p.x+m.x, p.y+m.y};
-		mv[i] = {varsendorf(map, nm), nm};
-	}
-	sort(mv, mv+dim(mv));
-	for (auto &m:mv) {
-		if (dfs(map, m.m, cnt-1, idx+1))
+		if (warnsdorff_straight_walk(map, min_mv.m, wdep-1, idx+1)) {
 			return true;
+		} else {
+			map[p.y][p.x] = 0;
+			return false;
+		}
 	}
-	map[p.y][p.x] = 0;
-	return false;
+}
+
+// doing dumb dfs to the depth of dfs_depth
+// from where trying straight warnsdorff walk
+bool dfs_then_warnsdorff_walk(int map[SZ+4][SZ+4], const P &p, int dfs_dep, int wdep, int idx) {
+	if (map[p.y][p.x] != 0)
+		return false;
+	if (!dfs_dep) {
+		return warnsdorff_straight_walk(map, p, wdep, idx);
+	} else {
+		map[p.y][p.x] = idx;
+		for (const P &mv:moves) {
+			const P nxt_mv = P {p.x+mv.x, p.y+mv.y};
+			if (dfs_then_warnsdorff_walk(map, nxt_mv, dfs_dep-1, wdep-1, idx+1))
+				return true;
+		}
+		map[p.y][p.x] = 0;
+		return false;
+	}
 }
 
 int main(int argc, char **argv) {
@@ -82,7 +102,7 @@ int main(int argc, char **argv) {
 		}
 		fgetc(stdin); // \n
 	}
-	if (dfs(map, start, cnt, 1)) {
+	if (dfs_then_warnsdorff_walk(map, start, 2, cnt, 1)) {
 		for (int y=2; y<n+2; y++) {
 			for (int x=2; x<m+2; x++)
 				printf("%2d ", (map[y][x] > 0)?map[y][x]:0);
