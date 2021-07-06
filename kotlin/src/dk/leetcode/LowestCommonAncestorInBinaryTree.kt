8450be76876e7c975d6cea6e98c15f616d50ac27
Denis Kokarev <dkv@fb.com>
2021-07-06 14:08:49 -0400

    lc Lowest Common Ancestor of a Binary Tree - better solution with no extra mem

--- kotlin/src/dk/leetcode/LowestCommonAncestorInBinaryTree.kt
+++ kotlin/src/dk/leetcode/LowestCommonAncestorInBinaryTree.kt
@@ -1,5 +1,31 @@
 package dk.leetcode
 
+class Solution_9 {
+    private class R(val ans: TreeNode?, val cnt: Int) {}
+    private fun dfs(root: TreeNode?, either: List<TreeNode?>): R {
+        if (root != null) {
+            val l = dfs(root.left, either)
+            val r = dfs(root.right, either)
+            return when {
+                (l.cnt == 2) -> l
+                (r.cnt == 2) -> r
+                (l.cnt == 1 && r.cnt == 1) -> R(root, 2)
+                (l.cnt == 1 && root in either) -> R(root, 2)
+                (r.cnt == 1 && root in either) -> R(root, 2)
+                (root in either) -> R(null, 1)
+                (l.cnt == 1) -> l
+                (r.cnt == 1) -> r
+                else -> R(null, 0)
+            }
+        } else {
+            return R(null, 0);
+        }
+    }
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        return dfs(root, listOf(p, q)).ans
+    }
+}
+
 class Solution_8 {
     private class N(val node: TreeNode, val parent: Int, val lvl: Int) {}
     private fun dfsFlattenInOrder(dst: ArrayList<N>, parent: Int, lvl: Int, root: TreeNode?) {
