58679d1cff863e200b7892f5bc890905347423db
Denis Kokarev <dkv@fb.com>
2021-08-05 20:24:23 -0400

    lc SubsetsII coroutine solution

--- kotlin/src/dk/leetcode/SubsetsII.kt
+++ kotlin/src/dk/leetcode/SubsetsII.kt
@@ -24,3 +24,27 @@ class Solution_29 {
         return result
     }
 }
+
+// usage-friendly generator approach
+// async, but slower than above - each iteration the control and the value must go via layers of yieldAll()
+class Solution_29co {
+    private fun coDFS(tgt: MutableList<Int>, nums: IntArray, le: Int, pos: Int): Sequence<List<Int>> = sequence {
+        if (pos < 0) {
+            yield(tgt.toList())
+        } else {
+            if (nums[pos] > le) {
+                yieldAll(coDFS(tgt, nums, le, pos-1))
+            } else {
+                yieldAll(coDFS(tgt, nums, nums[pos]-1, pos-1))
+                tgt.add(nums[pos])
+                yieldAll(coDFS(tgt, nums, nums[pos], pos-1))
+                tgt.removeAt(tgt.size-1) //tgt.removeLast()
+            }
+        }
+    }
+    fun subsetsWithDup(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val tgt = mutableListOf<Int>()
+        return coDFS(tgt, nums, Int.MAX_VALUE, nums.size - 1).toList()
+    }
+}
