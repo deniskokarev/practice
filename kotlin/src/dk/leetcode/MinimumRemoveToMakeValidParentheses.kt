fa6c75ac6432eba79f6ca1a12d80d8e9c0becdbf
Denis Kokarev <dkv@fb.com>
2022-04-11 17:41:34 -0400

    better code https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/

--- kotlin/src/dk/leetcode/MinimumRemoveToMakeValidParentheses.kt
+++ kotlin/src/dk/leetcode/MinimumRemoveToMakeValidParentheses.kt
@@ -7,35 +7,20 @@ private fun <T> MutableList<T>.pop() {
 
 class Solution_srcMinimumRemoveToMakeValidParentheses {
     fun minRemoveToMakeValid(s: String): String {
-        var cnt = 0
-        var sm = 0
-        val sz = s.length
-        val rm = mutableListOf<Int>()
+        val rm = mutableSetOf<Int>()
         val stack = mutableListOf<Int>()
         for (i in s.indices) {
-            val d =
-                    when (s[i]) {
-                        '(' -> +1
-                        ')' -> -1
-                        else -> 0
-                    }
-            cnt += d
-            if (d > 0) {
+            if (s[i] == '(') {
                 stack.add(i)
-            } else if (d < 0) {
+            } else if (s[i] == ')') {
                 if (stack.isNotEmpty()) {
                     stack.pop()
-                }
-                if (cnt < 0) {
+                } else {
                     rm.add(i)
-                    cnt = 0
                 }
             }
         }
         rm.addAll(stack)
-        val ch = s.toCharArray()
-        rm.forEach { i -> ch[i] = ' ' }
-        val ans = ch.filter { c -> c != ' ' }.joinToString("")
-        return ans
+        return s.filterIndexed { i, _ -> !rm.contains(i) }
     }
 }
