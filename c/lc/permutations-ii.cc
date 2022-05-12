f439f1b16f06632e37d8fb73864f0d391761fa65
Denis Kokarev <dkv@fb.com>
2022-05-12 09:06:31 -0400

    another https://leetcode.com/problems/permutations-ii/ c++

--- c/lc/permutations-ii.cc
+++ c/lc/permutations-ii.cc
@@ -1,27 +1,30 @@
+// https://leetcode.com/problems/permutations-ii/
+
+constexpr int EMPTY = -11;
+
 class Solution {
-    void perm(vector<int> &sel, int dep, bitset<8> taken, const vector<int>& nums, vector<vector<int>> &out) {
-        if (dep < 0) {
-            out.push_back(sel);
-        } else {
-            int lst = -11;
-            for (int i=0; i<nums.size(); i++) {
-                if (!taken[i] && nums[i] != lst) {
-                    sel[dep] = nums[i];
-                    lst = nums[i];
-                    taken[i] = 1;
-                    perm(sel, dep-1, taken, nums, out);
-                    taken[i] = 0;
-                }
+    void perm(vector<vector<int>> &res, vector<int> &cur, const vector<int> &nums, int pos) {
+        int sz = cur.size();
+        if (pos == sz) {
+            res.push_back(cur);
+            return;
+        }
+        for (int i=0; i<sz; i++) {
+            if (cur[i] == nums[pos]) {
+                return;
+            }
+            if (cur[i] == EMPTY) {
+                cur[i] = nums[pos];
+                perm(res, cur, nums, pos+1);
+                cur[i] = EMPTY;
             }
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int sz = nums.size();
-        vector<int> sel(sz);
-        vector<vector<int>> out;
-        sort(nums.begin(), nums.end());
-        perm(sel, sz-1, 0, nums, out);
-        return out;
+        vector<vector<int>> res;
+        vector<int> cur(nums.size(), EMPTY);
+        perm(res, cur, nums, 0);
+        return res;
     }
 };
