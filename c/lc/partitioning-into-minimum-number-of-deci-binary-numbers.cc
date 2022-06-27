2726ec055c8cd1c6d5c69aed94627b12196bec4b
Denis Kokarev <dkv@fb.com>
2022-06-27 09:57:55 -0400

    adding comment to https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/

--- c/lc/partitioning-into-minimum-number-of-deci-binary-numbers.cc
+++ c/lc/partitioning-into-minimum-number-of-deci-binary-numbers.cc
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // there is no need in more than 9 numbers, thus
+    // carryover doesn't apply, therefore the solution is trivial
     int minPartitions(string n) {
         return *max_element(n.begin(), n.end()) - '0';
     }
