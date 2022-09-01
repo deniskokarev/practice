669b06efc4256fd50d21a2019b30105183680a4c
Denis Kokarev <dkv@fb.com>
2022-09-01 10:01:21 -0400

    https://leetcode.com/problems/count-good-nodes-in-binary-tree

--- c/lc/count-good-nodes-in-binary-tree.cc
+++ c/lc/count-good-nodes-in-binary-tree.cc
@@ -1,12 +1,16 @@
 // https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 class Solution {
-    static int goodNodes(const TreeNode *root, multiset<int> &vv) {
+    static int goodNodes(const TreeNode *root, vector<int> &vv) {
         if (root) {
-            auto fnd = vv.upper_bound(root->val);
-            int is_good = (fnd == vv.end());
-            vv.insert(root->val);
-            int cnt = goodNodes(root->left, vv) + goodNodes(root->right, vv) + is_good;
-            vv.erase(vv.find(root->val));
+            int v = root->val;
+            int cnt;
+            if (vv.empty() || vv.back() <= v) {
+                vv.push_back(v);
+                cnt = goodNodes(root->left, vv) + goodNodes(root->right, vv) + 1;
+                vv.pop_back();
+            } else {
+                cnt = goodNodes(root->left, vv) + goodNodes(root->right, vv);
+            }
             return cnt;
         } else {
             return 0;
@@ -14,7 +18,7 @@ class Solution {
     }
 public:
     static int goodNodes(const TreeNode *root) {
-        multiset<int> vv;
+        vector<int> vv;
         return goodNodes(root, vv);
     }
 };
