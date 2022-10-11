6b98e281ada26cdfe8753cc58dce1cef200fd4bc
Denis Kokarev <dkv@fb.com>
2022-10-11 08:55:56 -0400

    another solution to https://leetcode.com/problems/increasing-triplet-subsequence/

--- c/lc/increasing-triplet-subsequence.cc
+++ c/lc/increasing-triplet-subsequence.cc
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
@@ -12,6 +14,23 @@ public:
             if (seq_sz > 2)
                 return true;
         }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        const int sz = nums.size();
+        vector<int> mnl(nums); // min to the left
+        vector<int> mxr(nums); // max to the right
+        for (int i=1; i<sz; i++)
+            mnl[i] = min(mnl[i-1], mnl[i]);
+        for (int i=sz-2; i>=0; i--)
+            mxr[i] = max(mxr[i], mxr[i+1]);
+        for (int i=1; i<sz-1; i++)
+            if (mnl[i-1] < nums[i] && nums[i] < mxr[i+1])
+                return true;
         return false;
     }
 };
