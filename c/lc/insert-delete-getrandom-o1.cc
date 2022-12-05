bfb0fae72ca06f9805df846cd764524b36a9d1b9
Denis Kokarev <dkv@fb.com>
2022-12-05 09:43:47 -0500

    one more https://leetcode.com/problems/insert-delete-getrandom-o1/

--- c/lc/insert-delete-getrandom-o1.cc
+++ c/lc/insert-delete-getrandom-o1.cc
@@ -1,4 +1,69 @@
 // https://leetcode.com/problems/insert-delete-getrandom-o1/
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <random>
+
+using namespace std;
+
+class RandomizedSet {
+    vector<int> ord;
+    unordered_map<int,int> v2o;
+    mt19937 rnd;
+public:
+    RandomizedSet():ord(),v2o(),rnd(0) {
+    }
+
+    bool insert(int val) {
+        bool is_there = (v2o.find(val) != v2o.end());
+        if (!is_there) {
+            v2o[val] = ord.size();
+            ord.push_back(val);
+        }
+        return !is_there;
+    }
+
+    bool remove(int val) {
+        auto fnd = v2o.find(val);
+        bool is_there = (fnd != v2o.end());
+        if (is_there) {
+            int p = fnd->second;
+            v2o.erase(fnd);
+            if (p < ord.size() - 1) {
+                // if we're deleting not the last element
+                v2o[ord.back()] = p;
+                ord[p] = ord.back();
+            }
+            ord.pop_back();
+        }
+        return is_there;
+    }
+
+    int getRandom() {
+        int rp = rnd() % ord.size();
+        return ord[rp];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+int main(int argc, char **argv) {
+    RandomizedSet r;
+    cout << r.remove(0) << endl;
+    cout << r.remove(0) << endl;
+    cout << r.insert(0) << endl;
+    cout << r.getRandom() << endl;
+    cout << r.remove(0) << endl;
+    cout << r.insert(0) << endl;
+    return 0;
+}
+
+// https://leetcode.com/problems/insert-delete-getrandom-o1/
 class RandomizedSet {
     unordered_map<int,int> v2p;
     vector<int> vv;
