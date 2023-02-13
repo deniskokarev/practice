eae8152cc0e7ea099f62666c9ca278d459d56a90
Denis Kokarev <dkv@fb.com>
2023-02-13 09:14:52 -0500

    https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/

---
+++ c/lc/count-odd-numbers-in-an-interval-range.cc
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+class Solution {
+    static inline bool is_odd(int n) {
+        return n&1;
+    }
+public:
+    int countOdds(int low, int high) {
+        return (high+(is_odd(high) || is_odd(low)))/2 - low/2;
+        // ^^^^ from below analysis
+        if (is_odd(high)) {
+            if (is_odd(low)) {
+                // odd-odd 1,1
+                return (high+1)/2 - low/2;
+            } else {
+                // even-odd 0,1
+                return (high+1)/2 - low/2;
+            }
+        } else {
+            if (is_odd(low)) {
+                // odd-even 1,2
+                return (high+1)/2 - low/2;
+            } else {
+                // even-even 2,2 2,3,4
+                return high/2 - low/2;
+            }
+        }
+    }
+};
