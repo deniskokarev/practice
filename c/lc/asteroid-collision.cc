551c6ba4045dcecb970400cd810838ad792d0bc1
Denis Kokarev <dkv@fb.com>
2023-07-20 09:06:08 -0400

    https://leetcode.com/problems/asteroid-collision/

--- c/lc/asteroid-collision.cc
+++ c/lc/asteroid-collision.cc
@@ -4,27 +4,23 @@ public:
     static vector<int> asteroidCollision(const vector<int>& aa) {
         vector<int> res;
         for (auto a:aa) {
-            if (res.empty()) {
+            if (a > 0) {
                 res.push_back(a);
             } else {
-                if (a > 0) {
+                while (!res.empty() && res.back() > 0) {
+                    if (res.back() < -a) {
+                        res.pop_back();
+                    } else if (res.back() == -a) {
+                        res.pop_back();
+                        a = 0;
+                        break;
+                    } else {
+                        a = 0;
+                        break;
+                    }
+                }
+                if (a != 0) {
                     res.push_back(a);
-                } else {
-                    while (!res.empty() && res.back() > 0) {
-                        if (res.back() < -a) {
-                            res.pop_back();
-                        } else if (res.back() == -a) {
-                            res.pop_back();
-                            a = 0;
-                            break;
-                        } else {
-                            a = 0;
-                            break;
-                        }
-                    }
-                    if (a != 0) {
-                        res.push_back(a);
-                    }
                 }
             }
         }
