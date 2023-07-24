8cd77c31f52b099348cbb829a693929423c8659b
Denis Kokarev <dkv@fb.com>
2023-07-24 09:03:06 -0400

    https://leetcode.com/problems/powx-n/

--- c/lc/powx-n.cc
+++ c/lc/powx-n.cc
@@ -5,13 +5,10 @@ public:
         double ans = 1;
         double p = x;
         int sg = (n > 0) - (n < 0);
-        n = abs(n);
-        while (n) {
-            if (n&1) {
+        for (n = abs(n); n; n /= 2) {
+            if (n&1)
                 ans *= p;
-            }
             p *= p;
-            n /= 2;
         }
         return (sg >= 0) ? ans : 1/ans;
     }
