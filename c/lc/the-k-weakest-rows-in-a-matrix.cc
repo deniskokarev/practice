5a5b117cef13759a58a2fb992589e725313bb3fa
Denis Kokarev <dkv@fb.com>
2023-09-18 09:46:58 -0400

    https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix another solution

--- c/lc/the-k-weakest-rows-in-a-matrix.cc
+++ c/lc/the-k-weakest-rows-in-a-matrix.cc
@@ -1,5 +1,28 @@
+// https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix
 // cpu: O(h * log(w) + k * log(k))
-// mem: O(h)
+// mem: O(k)
+
+class Solution {
+public:
+    static vector<int> kWeakestRows(const vector<vector<int>>& mat, int k) {
+        int h = mat.size();
+        priority_queue<pair<int,int>> qq;
+        for (int i=0; i<h; i++) {
+            int p = upper_bound(begin(mat[i]), end(mat[i]), 1, greater<int>()) - begin(mat[i]);
+            qq.push({p, i});
+            if (qq.size() > k)
+                qq.pop();
+        }
+        vector<int> res(k);
+        for (int i = 0; i<k; i++) {
+            res[i] = qq.top().second;
+            qq.pop();
+        }
+        reverse(begin(res), end(res));
+        return res;
+    }
+};
+
 class Solution {
     int solders(const vector<int> &row) {
         int w = row.size();
