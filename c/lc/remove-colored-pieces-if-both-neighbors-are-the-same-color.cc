58c6e1b9d524463d0582eae95565c4d9b0def159
Denis Kokarev <dkv@fb.com>
2023-10-02 09:04:30 -0400

    https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/

--- c/lc/remove-colored-pieces-if-both-neighbors-are-the-same-color.cc
+++ c/lc/remove-colored-pieces-if-both-neighbors-are-the-same-color.cc
@@ -2,30 +2,21 @@
 class Solution {
 public:
     bool winnerOfGame(const string &s) {
-        int sz = s.size();
         int la = 0, lb = 0;
         int a = 0, b = 0;
-        int mode = 0;
-        for (int i=0; i<sz; i++) {
-            if (s[i] == 'B') {
-                if (la > 2) {
-                    a += la - 2;
-                }
+        for (char c:s) {
+            if (c == 'B') {
+                a += (la > 2) * (la - 2);
                 la = 0;
                 lb++;
             } else {
-                if (lb > 2) {
-                    b += lb - 2;
-                }
+                b += (lb > 2) * (lb - 2);
                 lb = 0;
                 la++;
             }
         }
-        if (la > 2) {
-            a += la - 2;
-        } else if (lb > 2) {
-            b += lb - 2;
-        }
+        a += (la > 2) * (la - 2);
+        b += (lb > 2) * (lb - 2);
         return a > b;
     }
 };
