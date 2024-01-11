c5c0895057056d0f116148e968fc792d82526096
Denis Kokarev <dkv@fb.com>
2024-01-11 09:11:47 -0500

    https://leetcode.com/problems/maximum-difference-between-node-and-ancestor

--- c/lc/maximum-difference-between-node-and-ancestor.cc
+++ c/lc/maximum-difference-between-node-and-ancestor.cc
@@ -4,20 +4,18 @@ class Solution {
         int mn, mx;
         Rng(int v): mn{v}, mx{v} {
         }
-        Rng(const Rng &o, int v): mn{o.mn}, mx{o.mx} {
-            mn = min(mn, v);
-            mx = max(mx, v);
+        Rng(const Rng &o, int v): mn{min(o.mn, v)}, mx{max(o.mx, v)} {
         }
-        int dist(int v) const {
+        int mx_dist(int v) const {
             return max(abs(mn - v), abs(mx - v));
         }
     };
-    int diff(TreeNode* root, const Rng &rng) {
+    int mx_diff_dfs(TreeNode* root, const Rng &rng) {
         if (root) {
-            int self = rng.dist(root->val);
+            int self = rng.mx_dist(root->val);
             const Rng nrng(rng, root->val);
-            int left = diff(root->left, nrng);
-            int right = diff(root->right, nrng);
+            int left = mx_diff_dfs(root->left, nrng);
+            int right = mx_diff_dfs(root->right, nrng);
             return max({self, left, right});
         } else {
             return 0;
@@ -26,6 +24,6 @@ class Solution {
 public:
     int maxAncestorDiff(TreeNode* root) {
         const Rng rng(root->val);
-        return diff(root, rng);
+        return mx_diff_dfs(root, rng);
     }
 };
