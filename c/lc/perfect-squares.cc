ff591fd5c84b1e63e65cbcb84e333b9fd9af5067
Denis Kokarev <dkv@fb.com>
2024-02-08 09:33:07 -0500

    https://leetcode.com/problems/perfect-squares/

--- c/lc/perfect-squares.cc
+++ c/lc/perfect-squares.cc
@@ -2,6 +2,21 @@
 class Solution {
 public:
     static int numSquares(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s * s <= i; s++) {
+                dp[i] = min(dp[i], dp[i - s * s] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
+
+
+class Solution {
+public:
+    static int numSquares(int n) {
         vector<int> hdp(n+1, INT_MAX);
         hdp[0] = 0;
         for (int i = 1; i <= n; i++) {
