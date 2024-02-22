ef6626e5f2e55026fdd1dd279fb653fca408940f
Denis Kokarev <dkv@fb.com>
2024-02-22 08:53:49 -0500

    https://leetcode.com/problems/find-the-town-judge

--- c/lc/find-the-town-judge.cc
+++ c/lc/find-the-town-judge.cc
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/find-the-town-judge
 class Solution {
+    struct P {
+        int f, t;
+    };
+public:
+    int findJudge(int n, const vector<vector<int>>& trust) {
+        vector<P> pp(n, {0,0});
+        for (auto &t:trust) {
+            pp[t[0]-1].f++;
+            pp[t[1]-1].t++;
+        }
+        for (int i=0; i<n; i++) {
+            if (pp[i].t == n-1 && pp[i].f == 0) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+class Solution {
     using N_CNT = struct {
         int f, t; 
     };
