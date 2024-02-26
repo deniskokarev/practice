8215d99e2425d1f82d0bea26e519a87a42888202
Denis Kokarev <dkv@fb.com>
2024-02-26 08:48:41 -0500

    https://leetcode.com/problems/same-tree/

--- c/lc/same-tree.cc
+++ c/lc/same-tree.cc
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/same-tree/
 class Solution {
 public:
+    static bool isSameTree(const TreeNode* p, const TreeNode* q) {
+        return (
+            !p && !q ||
+            p && q &&
+            p->val == q->val &&
+            isSameTree(p->left, q->left) &&
+            isSameTree(p->right, q->right)
+        );
+    }
+};
+
+class Solution {
+public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p && !q)
             return true;
