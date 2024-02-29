2d73e837e35319c0195c5bb9c9028582601dd88f
Denis Kokarev <dkv@fb.com>
2024-02-29 09:48:58 -0500

    https://leetcode.com/problems/even-odd-tree

--- c/lc/even-odd-tree.cc
+++ c/lc/even-odd-tree.cc
@@ -1,3 +1,65 @@
+// https://leetcode.com/problems/even-odd-tree
+
+class Solution {
+    // BFS
+    struct E {
+        const TreeNode *node;
+        int lvl;
+    };
+
+    static bool correct_level(const E &b) {
+        return ((b.lvl ^ b.node->val) & 1);
+    }
+
+    static bool in_order(const E &a, const E &b) {
+        if (!correct_level(b)) {
+            return false;
+        }
+        if (a.lvl == b.lvl) {
+            if (a.lvl & 1) {
+                return a.node->val > b.node->val;
+            } else {
+                return a.node->val < b.node->val;
+            }
+        } else {
+            return false;
+        }
+    }
+
+public:
+    static bool isEvenOddTree(const TreeNode *root) {
+        E rt{root, 0};
+        if (!correct_level(rt)) {
+            return false;
+        }
+        queue<E> qq;
+        qq.push(rt);
+        E prev{nullptr, -1};
+        while (!qq.empty()) {
+            auto cur = qq.front();
+            if (prev.lvl == cur.lvl) {
+                if (!in_order(prev, cur)) {
+                    return false;
+                }
+            } else {
+                if (!correct_level(cur)) {
+                    return false;
+                }
+            }
+            if (cur.node->left) {
+                qq.push(E{cur.node->left, cur.lvl + 1});
+            }
+            if (cur.node->right) {
+                qq.push(E{cur.node->right, cur.lvl + 1});
+            }
+            qq.pop();
+            prev = cur;
+        }
+        return true;
+    }
+};
+
+
 class Solution {
     bool check_dfs(const TreeNode* root, vector<int> &last, int lvl) {
         if (root) {
