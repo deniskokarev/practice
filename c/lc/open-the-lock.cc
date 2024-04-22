62767154fa0c7d9adf0cdb2d227810563a159a35
Denis Kokarev <dkv@fb.com>
2024-04-22 10:05:26 -0400

    https://leetcode.com/problems/open-the-lock

--- c/lc/open-the-lock.cc
+++ c/lc/open-the-lock.cc
@@ -2,55 +2,55 @@
 class Solution {
     struct E {
         array<uint8_t, 4> s;
-        int n;
 
-        void s2n() {
-            n = 0;
+        int n() {
+            int v = 0;
             for (auto &d: s) {
                 d += 10;
                 d %= 10;
-                n *= 10;
-                n += d;
+                v *= 10;
+                v += d;
             }
+            return v;
         }
     };
 
 public:
     static int openLock(const vector<string> &deadends, const string &target) {
+        auto tgt = strtol(target.c_str(), nullptr, 10);
         vector<int> seen(10000, 0);
         for (auto &d: deadends) {
             auto n = strtol(d.c_str(), nullptr, 10);
             seen[n] = 1;
         }
-        auto tgt = strtol(target.c_str(), nullptr, 10);
         queue<E> qq;
         if (!seen[0]) {
-            qq.push({{0, 0, 0, 0}, 0});
+            qq.push({0, 0, 0, 0});
             seen[0] = 1;
         }
         while (!qq.empty()) {
             auto top = qq.front();
             qq.pop();
-            if (top.n == tgt) {
-                return seen[top.n] - 1;
+            int tn = top.n();
+            if (tn == tgt) {
+                return seen[tn] - 1;
             }
-            for (int p = 0; p < top.s.size(); p++) {
-                E e(top);
-                e.s[p]++;
-                e.s2n();
-                if (!seen[e.n]) {
-                    seen[e.n] = seen[top.n] + 1;
-                    qq.push(e);
+            for (auto &d:top.s) {
+                d++;
+                int n = top.n();
+                if (!seen[n]) {
+                    seen[n] = seen[tn] + 1;
+                    qq.push(top);
                 }
-                e.s[p] -= 2;
-                e.s2n();
-                if (!seen[e.n]) {
-                    seen[e.n] = seen[top.n] + 1;
-                    qq.push(e);
+                d -= 2;
+                n = top.n();
+                if (!seen[n]) {
+                    seen[n] = seen[tn] + 1;
+                    qq.push(top);
                 }
+                d++;
             }
         }
         return -1;
     }
 };
-
