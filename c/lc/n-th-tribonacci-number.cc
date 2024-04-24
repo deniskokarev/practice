cb3bbeaf246245f28e58db722603b781125ffe16
Denis Kokarev <dkv@fb.com>
2024-04-24 09:00:42 -0400

    https://leetcode.com/problems/n-th-tribonacci-number

--- c/lc/n-th-tribonacci-number.cc
+++ c/lc/n-th-tribonacci-number.cc
@@ -1,4 +1,21 @@
 // https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        array<unsigned,3> tt {0, 1, 1};
+        unsigned sm = 2;
+        for (int i=tt.size(); i<=n; i++) {
+            int p = i % tt.size();
+            auto s = sm;
+            sm -= tt[p];
+            tt[p] = s;
+            sm += s;
+        }
+        return tt[n%tt.size()];
+    }
+};
+
 constexpr int SZ = 3;
 
 class Solution {
