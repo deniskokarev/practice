327bca42c241595c69cfd8840543ede2bec588b8
Denis Kokarev <dkv@fb.com>
2024-07-19 09:05:32 -0400

    https://leetcode.com/problems/lucky-numbers-in-a-matrix

--- c/lc/lucky-numbers-in-a-matrix.cc
+++ c/lc/lucky-numbers-in-a-matrix.cc
@@ -1,3 +1,45 @@
+// https://leetcode.com/problems/lucky-numbers-in-a-matrix
+
+// O(n+m) memory solution
+class Solution {
+    struct E {
+        int v, i, j;
+        bool operator<(const E &o) const {
+            return i < o.i || i == o.i && j < o.j;
+        }
+    };
+public:
+    vector<int> luckyNumbers (const vector<vector<int>>& mm) {
+        int h = mm.size();
+        int w = mm[0].size();
+        vector<E> r(h, {INT_MAX, -1, -1});
+        vector<E> c(w, {INT_MIN, -1, -1});
+        for (int i=0; i<h; i++) {
+            for (int j=0; j<w; j++) {
+                if (r[i].v > mm[i][j])
+                    r[i] = {mm[i][j], i, j};
+                if (c[j].v < mm[i][j])
+                    c[j] = {mm[i][j], i, j};
+            }
+        }
+        sort(begin(r), end(r));
+        sort(begin(c), end(c));
+        vector<int> res;
+        int i = 0, j = 0;
+        while (i < h && j < w) {
+            if (r[i] < c[j]) {
+                i++;
+            } else if (c[j] < r[i]) {
+                j++;
+            } else {
+                res.push_back(r[i].v);
+                i++, j++;
+            }
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& mm) {
