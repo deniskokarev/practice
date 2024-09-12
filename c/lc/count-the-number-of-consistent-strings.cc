231384626dcaf069507dc813bba58f3c0177fff1
Denis Kokarev <dkv@fb.com>
2024-09-12 08:59:13 -0400

    https://leetcode.com/problems/count-the-number-of-consistent-strings

--- c/lc/count-the-number-of-consistent-strings.cc
+++ c/lc/count-the-number-of-consistent-strings.cc
@@ -1,3 +1,25 @@
+// https://leetcode.com/problems/count-the-number-of-consistent-strings
+class Solution {
+    static constexpr int SZ = 'z' - 'a' + 1;
+    static unsigned long s2b(const string &s) {
+        bitset<SZ> b{};
+        for (unsigned char c:s) {
+            b[(c-'a') % SZ] = 1;
+        }
+        return b.to_ulong();
+    }
+public:
+    static int countConsistentStrings(const string &allowed, const vector<string>& words) {
+        unsigned na = ~s2b(allowed);
+        int ans = 0;
+        for (auto &s:words) {
+            unsigned w = s2b(s);            
+            ans += ((w & na) == 0);
+        }
+        return ans;        
+    }
+};
+
 class Solution {
 public:
     int countConsistentStrings(const string &allowed, const vector<string>& words) {
