dbf10d0992d72e6da692c405618e1526cc26283d
Denis Kokarev <dkv@meta.com>
2025-06-30 09:13:47 -0400

    https://leetcode.com/problems/longest-harmonious-subsequence

--- c/lc/longest-harmonious-subsequence.cc
+++ c/lc/longest-harmonious-subsequence.cc
@@ -1,3 +1,41 @@
+// https://leetcode.com/problems/longest-harmonious-subsequence
+class Solution {
+public:
+	static int findLHS(const vector<int> &nums) {
+		unordered_map<int, int> cnt;
+		int mx = 0;
+		for (auto num: nums) {
+			cnt[num]++;
+			if (cnt.find(num - 1) != cnt.end()) mx = max(mx, cnt[num] + cnt[num - 1]);
+			if (cnt.find(num + 1) != cnt.end()) mx = max(mx, cnt[num] + cnt[num + 1]);
+		}
+		return mx;
+	}
+};
+
+
+class Solution {
+public:
+	static int findLHS(const vector<int>& nums) {
+		map<int,int> cnt;
+		for (auto num : nums) {
+			cnt[num]++;
+		}
+		auto it1 = cnt.begin();
+		auto it2 = next(it1);
+		int mx = 0;
+		while (it2 != cnt.end()) {
+			if (it1->first + 1 == it2->first) {
+				mx = max(mx, it1->second + it2->second);
+			}
+			it1 = next(it1);
+			it2 = next(it2);
+		}
+		return mx;
+	}
+};
+
+
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
