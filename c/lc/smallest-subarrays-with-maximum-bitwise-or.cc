c16738c5568b91ce66599318bfe80314723aa8a7
Denis Kokarev <dkv@meta.com>
2025-07-29 13:33:35 -0400

    https://leetcode.com/problems/smallest-subarrays-with-maximum-bitwise-or

--- c/lc/smallest-subarrays-with-maximum-bitwise-or.cc
+++ c/lc/smallest-subarrays-with-maximum-bitwise-or.cc
@@ -1,7 +1,41 @@
-// 
+// https://leetcode.com/problems/smallest-subarrays-with-maximum-bitwise-or
 
+// correct approach, but still filing some tests
+class Solution {
+	static constexpr int B = 32;
 
+	struct MxTree {
+		int mx[2 * B]{};
 
+		int after_set(int p, int v) {
+			mx[p + B] = v;
+			for (int nd = p + B; nd > 0; nd /= 2) {
+				int sib = (nd & 1) ? nd - 1 : nd + 1;
+				mx[nd / 2] = max(mx[nd], mx[sib]);
+			}
+			return mx[1];
+		}
+	};
+
+public:
+	static vector<int> smallestSubarrays(const vector<int> &nn) {
+		int sz = nn.size();
+		MxTree mxt;
+		vector<int> res(sz);
+		int mx = sz - 1;
+		for (int i = sz - 1; i >= 0; i--) {
+			for (int b = 0; b < B; b++) {
+				if ((1 << b) & nn[i]) {
+					mx = mxt.after_set(b, i);
+				}
+			}
+			res[i] = mx - i + 1;
+		}
+		return res;
+	}
+};
+
+// working, but slower and memory hungry
 class Solution {
 public:
 	static vector<int> smallestSubarrays(const vector<int> &nn) {
