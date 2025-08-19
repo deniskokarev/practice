64512862e1d90d41d90ad7b1e1c838aa65e62f4a
Denis Kokarev <dkv@meta.com>
2025-08-19 08:53:43 -0400

    https://leetcode.com/problems/number-of-zero-filled-subarrays

--- c/lc/number-of-zero-filled-subarrays.cc
+++ c/lc/number-of-zero-filled-subarrays.cc
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/number-of-zero-filled-subarrays
 class Solution {
 public:
+	long long zeroFilledSubarray(const vector<int>& nums) {
+		int64_t sm = 0;
+		int z_cnt = 0;
+		for (int n:nums) {
+			z_cnt++;
+			if (n != 0) {
+				z_cnt = 0;
+			}
+			sm += z_cnt;
+		}
+		return sm;
+	}
+};
+
+
+class Solution {
+public:
     static long long zeroFilledSubarray(const vector<int>& nums) {
         long long sm = 0;
         int z = 0;
